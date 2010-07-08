02629c71fb7db0097f62d29136467f12ca46ff7b
Thomas Holzmann <t.holzmann@student.tugraz.at>
2010-07-08 12:14:34 +0200

    first version of parser, not much implemented yet

---
+++ Scratch2Android/src/com/tugraz/android/app/parser/Parser.java
@@ -0,0 +1,40 @@
+package com.tugraz.android.app.parser;
+
+
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import android.util.Xml;
+
+public class Parser {
+	private DocumentBuilder builder;
+	private Document doc;
+
+	public Parser() {}
+	
+	public void parse(InputStream stream){
+		try {
+			doc = builder.parse(stream);
+			NodeList animals = doc.getElementsByTagName("animals");
+		}
+		catch (Exception e) {
+			//TODO implement
+		}
+	}
+	
+}
+
+
+//InputStream in = getResources().openRawResource(R.raw.myXmlFile);
+//DocumentBuilder builder = DocumentBuilderFactory
+//  .newInstance().newDocumentBuilder();
+//Document doc = builder.parse(in, null);
+//NodeList animals = doc.getElementsByTagName("animal");
+//for (int i=0;i<animals.getLength();i++) {
+//  items.add(((Element)animals.item(i)).getAttribute("species"));
+//}
+//in.close();
