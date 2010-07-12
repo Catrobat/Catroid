1895baace2c84c7ee18a292b4004771a967fc939
Alexander Kalchauer <kalchauer@gmail.com>
2010-07-12 15:51:49 +0200

    BrickDefine

--- Scratch2Android/src/com/tugraz/android/app/BrickDefine.java
+++ Scratch2Android/src/com/tugraz/android/app/BrickDefine.java
@@ -1,11 +1,23 @@
 package com.tugraz.android.app;
 
 public class BrickDefine {
+	
+	//Definition of Brick
+	public static final String BRICK_ID = "brick_id";
+	public static final String BRICK_NAME = "brick_name";
+	public static final String BRICK_VALUE = "brick_value";
+	public static final String BRICK_TYPE = "brick_type";
 	// definition xyyy x = group y = brick id
 	
 	//group number 1 Control
-	public final static Integer SET_BACKGROUND = 1001; 
-	public final static Integer WAIT = 1002; 
+	public final static int SET_BACKGROUND = 1001; 
+	public final static int WAIT = 1002; 
 	//group number 2 Sound
-	public final static Integer PLAY_SOUND = 2001; 
+	public final static int PLAY_SOUND = 2001;
+	
+	//group number 9 Error
+	public final static int NOT_DEFINED = 9999;
+	
+	
+	
 }
