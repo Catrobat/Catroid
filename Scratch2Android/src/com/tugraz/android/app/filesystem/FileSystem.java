60520cc397a8232c30210a63b0a66321a6d770de
Nikolaus Koller <koller.nikolaus@gmail.com>
2010-07-13 14:16:39 +0200

    Task 12: finished test, changed .hgignore

---
+++ Scratch2Android/src/com/tugraz/android/app/filesystem/FileSystem.java
@@ -0,0 +1,17 @@
+package com.tugraz.android.app.filesystem;
+
+public class FileSystem {
+	
+	public String createOrOpenFile(String name){
+		
+		return null;
+	}
+	
+	public boolean deleteFile(String name){
+		
+		return true;
+	}
+	
+	
+
+}
