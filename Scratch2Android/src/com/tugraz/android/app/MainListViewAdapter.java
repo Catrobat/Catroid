f7f10811132e1f9bcf0c4566eb1ae56c6fab38ca
Alexander Kalchauer <kalchauer@gmail.com>
2010-07-13 10:17:09 +0200

    Baustelle Task4

--- Scratch2Android/src/com/tugraz/android/app/MainListViewAdapter.java
+++ Scratch2Android/src/com/tugraz/android/app/MainListViewAdapter.java
@@ -2,54 +2,100 @@ package com.tugraz.android.app;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainListViewAdapter extends BaseAdapter{
     private Context mCtx;
     private ArrayList<HashMap<String, String>> mList;
-    private HashMap<Integer, Integer> mViewElementMap;
-    private HashMap<Integer, String[]> mFromMap;
-    private HashMap<Integer, int[]> mToMap;
+
     
 	public MainListViewAdapter(Context context,
-			ArrayList<HashMap<String, String>> data, HashMap<Integer, Integer> viewElementMap, HashMap<Integer, String[]> fromMap,
-					HashMap<Integer, int[]> toMap) {
+			ArrayList<HashMap<String, String>> data) {
 		mCtx = context;
-		mList = data;
-		mViewElementMap = viewElementMap;
-		mFromMap = fromMap;
-		mToMap = toMap;		
+		mList = data;	
 	}
+	
+	
 
 	@Override
 	public int getCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return mList.size();
 	}
 
 	@Override
 	public Object getItem(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return mList.get(arg0);
 	}
 
 	@Override
 	public long getItemId(int position) {
-		// TODO Auto-generated method stub
-		return 0;
+		//Testfall schreiben
+		String type = mList.get(position).get(BrickDefine.BRICK_ID);
+		if(type == null)
+			return 0;
+		else
+			return Integer.valueOf(type).intValue();
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		return null;
+		//TODO check convertView
+		String type = mList.get(position).get(BrickDefine.BRICK_TYPE);
+		LayoutInflater inflater = (LayoutInflater)mCtx.getSystemService(
+	      Context.LAYOUT_INFLATER_SERVICE);
+		switch(Integer.valueOf(type).intValue()){
+		case (BrickDefine.SET_BACKGROUND): 
+		{
+			LinearLayout view = (LinearLayout) inflater.inflate(R.layout.mlve_two_labels, null);
+			TextView text1 = (TextView)view.getChildAt(0);
+			text1.setText("SET_BACKGROUND");
+			TextView text2 = (TextView)view.getChildAt(1);
+			text2.setText("BausteinSetBackground");
+			
+			return view;
+		}
+		case (BrickDefine.PLAY_SOUND): 
+		{
+		    LinearLayout view = (LinearLayout) inflater.inflate(R.layout.mlve_two_labels, null);
+			TextView text1 = (TextView)view.getChildAt(0);
+			text1.setText("PLAY_SOUND");
+			TextView text2 = (TextView)view.getChildAt(1);
+			text2.setText("BausteinPlaySound");
+			
+			return view;
+		}
+		case (BrickDefine.WAIT): 
+		{
+			LinearLayout view =  (LinearLayout)inflater.inflate(R.layout.mlve_two_labels_edit, null);
+			TextView text1 = (TextView)view.getChildAt(0);
+			text1.setText("WAIT");
+			  LinearLayout view2 = (LinearLayout)view.getChildAt(1);
+			  TextView text2 = (TextView) view2.getChildAt(0);
+	          text2.setText("BausteinWait");
+	          EditText etext = (EditText) view2.getChildAt(1);
+			return view;
+		}
+		case (BrickDefine.NOT_DEFINED):
+		{
+			return inflater.inflate(R.layout.mlve_two_labels, parent);
+		}
+		default: 
+		{
+			//TODO: Not defined Error
+			return null;
+	    }
+		
+		}
 	}
 	
 
