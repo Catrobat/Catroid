6aa680a67ba0c130ae32a0eb9a7e6126c2a1fda9
Nikolaus Koller <koller.nikolaus@gmail.com>
2010-07-13 18:09:48 +0200

    Task 16: Started Testing

--- Scratch2Android/src/com/tugraz/android/app/MainActivity.java
+++ Scratch2Android/src/com/tugraz/android/app/MainActivity.java
@@ -3,6 +3,8 @@ package com.tugraz.android.app;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Observable;
+import java.util.Observer;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -12,47 +14,31 @@ import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements Observer{
     /** Called when the activity is first created. */
 
 	
 	protected ListView mMainListView;
 	//TODO change public list and adapter
-	public ArrayList<HashMap<String, String>> mList = new ArrayList<HashMap<String,String>>(); 
-	public MainListViewAdapter adapter = new MainListViewAdapter(this, mList);
+	public ArrayList<HashMap<String, String>> mList = new ArrayList<HashMap<String,String>>();
+	
+	
+	private MainListViewAdapter mAdapter;
+	private ContentManager mContentManager;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        //Bsp.: List; Testdaten
-               
-        HashMap<String, String> map = new HashMap<String, String>();
-        map.put(BrickDefine.BRICK_ID, "1");
-        map.put(BrickDefine.BRICK_TYPE, String.valueOf(BrickDefine.SET_BACKGROUND));
-        map.put(BrickDefine.BRICK_NAME, "Test1");
-        map.put(BrickDefine.BRICK_VALUE, "bla");
-        mList.add(map);
-        map = new HashMap<String, String>();
-        map.put(BrickDefine.BRICK_ID, "2");
-        map.put(BrickDefine.BRICK_TYPE, String.valueOf(BrickDefine.PLAY_SOUND));
-        map.put(BrickDefine.BRICK_NAME, "Test2");
-        map.put(BrickDefine.BRICK_VALUE, "blabla1");
-        mList.add(map);
-        map = new HashMap<String, String>();
-        map.put(BrickDefine.BRICK_ID, "3");
-        map.put(BrickDefine.BRICK_TYPE, String.valueOf(BrickDefine.WAIT));
-        map.put(BrickDefine.BRICK_NAME, "Test3");
-        map.put(BrickDefine.BRICK_VALUE, "blabla2");
-        mList.add(map);
+        mContentManager = new ContentManager();
+        mContentManager.loadContent();
+        MainListViewAdapter mAdapter = new MainListViewAdapter(this, mContentManager.getContentArrayList());
               
         mMainListView = (ListView) findViewById(R.id.MainListView);
-        mMainListView.setAdapter(adapter);
+        mMainListView.setAdapter(mAdapter);
         this.registerForContextMenu(mMainListView);
       
     }
@@ -72,7 +58,7 @@ public class MainActivity extends Activity {
         } catch (ClassCastException e) {
             return;
         }
-        adapter.getItemId(info.position);
+        mAdapter.getItemId(info.position);
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.maincontextmenu, menu);
     }
@@ -82,7 +68,6 @@ public class MainActivity extends Activity {
     	AdapterView.AdapterContextMenuInfo info;
     	info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
     	mList.remove(info.position);
-        adapter.notifyDataSetChanged();
     	return true;
     };
 
@@ -99,11 +84,15 @@ public class MainActivity extends Activity {
             
         case R.id.reset:
         	mList.clear();
-        	adapter.notifyDataSetChanged();
+        	mAdapter.notifyDataSetChanged();
             return true;
    
         default:
             return super.onOptionsItemSelected(item);
         }
     }
+
+	public void update(Observable observable, Object data) {
+		mAdapter.notifyDataSetChanged();
+	}
  }
