8176713f5e56ad128cd93b538e463ae694d36d5e
Nikolaus Koller <koller.nikolaus@gmail.com>
2010-07-16 14:35:52 +0200

    Task 19: started

--- Scratch2Android/src/com/tugraz/android/app/filesystem/MediaFileLoader.java
+++ Scratch2Android/src/com/tugraz/android/app/filesystem/MediaFileLoader.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import android.content.Context;
 import android.database.Cursor;
 import android.provider.MediaStore;
+import android.util.Log;
 
 /**
  * this class provides reading media files like pictures from sd card
@@ -30,12 +31,39 @@ public class MediaFileLoader {
 	public void loadPictureContent(){
 		mPictureContent = new ArrayList<HashMap<String,String>>();
 		
+		
 		String[] projection = {
+				MediaStore.Images.Thumbnails.DATA,				
+				MediaStore.Images.Thumbnails.IMAGE_ID};
+
+
+		Cursor cursor = MediaStore.Images.Thumbnails.query(mCtx.getContentResolver(),
+				MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
+				projection);
+		//.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, null, null,null);   
+
+
+		int column_data_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Thumbnails.DATA);
+	//	int column_name_index = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.TITLE);
+		cursor.moveToFirst();
+
+		HashMap<String,String> map;
+		for(int i = 0; i < cursor.getCount(); i++){
+			map = new HashMap<String, String>();
+			map.put(PICTURE_NAME, "test");
+			map.put(PICTURE_PATH, cursor.getString(column_data_index));
+			mPictureContent.add(map);
+		}
+
+		cursor.close();
+		Log.d("TEST", mPictureContent.toString());
+		
+		/*			String[] projection = {
 				MediaStore.Images.Media.DATA,				
 				MediaStore.Images.ImageColumns.TITLE};
 		
 		
-		Cursor cursor = mCtx.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, null, null,null);   
+	Cursor cursor = mCtx.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, null, null,null);   
 		
 		
 		int column_data_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
@@ -47,11 +75,10 @@ public class MediaFileLoader {
 			map = new HashMap<String, String>();
 			map.put(PICTURE_NAME, cursor.getString(column_title_index));
 			map.put(PICTURE_PATH, cursor.getString(column_data_index));
-			
 			mPictureContent.add(map);
 		}
 		
-		cursor.close();
+		cursor.close();*/
 	}
 	
 	
