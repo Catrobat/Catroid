6a72ccc3aab8610615428e6560c99c751a84abd3
Nikolaus Koller <koller.nikolaus@gmail.com>
2010-08-18 17:51:38 +0200

    Task 3.11

--- Scratch2AndroidTest/src/at/tugraz/ist/s2a/test/utils/ImageContainerTest.java
+++ Scratch2AndroidTest/src/at/tugraz/ist/s2a/test/utils/ImageContainerTest.java
@@ -5,7 +5,6 @@ import java.io.File;
 
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
-import android.graphics.BitmapFactory;
 import at.tugraz.ist.s2a.utils.ImageContainer;
 import junit.framework.TestCase;
 
@@ -35,7 +34,7 @@ public class ImageContainerTest extends TestCase {
 		mImageContainer.saveBitmap("test2.png", bitmap2);
 		mImageContainer.deleteImage("test1.png");
 		
-		assertEquals(bitmap1, mImageContainer.getImage("test2.png"));
+		assertEquals(bitmap1.getNinePatchChunk(), mImageContainer.getImage("test2.png").getNinePatchChunk());
 		
 		boolean exists = false;
 		File rootFile = new File("/sdcard/");
@@ -49,8 +48,25 @@ public class ImageContainerTest extends TestCase {
     			
     	}
 		assertTrue(exists);
+		mImageContainer.deleteImage("test2.png");
 	}
-	public void saveBitmapOnSDCardAsPNG() throws Throwable{
+	public void testSaveBitmapOnSDCardAsPNG() throws Throwable{
+		Bitmap bitmap = Bitmap.createBitmap(10, 10, Config.ARGB_8888);
+		mImageContainer.saveBitmap("test1.png", bitmap);
+		
+		boolean exists = false;
+		File rootFile = new File("/sdcard/");
+		String[] rootFileList = rootFile.list();
+		
+		for(int i=0; i<rootFileList.length; i++)
+    	{
+    		if(rootFileList[i].contains("test1.png")){
+    			exists = true;
+    		}
+    			
+    	}
+		assertTrue(exists);
+		mImageContainer.deleteImage("test1.png");
 	}
 	
 	
