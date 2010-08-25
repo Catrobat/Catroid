8cb0d8c1ef5a48b14ae026c76d0e86546644c55c
Nikolaus Koller <koller.nikolaus@gmail.com>
2010-08-25 14:50:09 +0200

    Ticket 3.11: Finished Tests + Implementation of Sound RootFolder

--- Scratch2AndroidTest/src/at/tugraz/ist/s2a/test/utils/ImageContainerTest.java
+++ Scratch2AndroidTest/src/at/tugraz/ist/s2a/test/utils/ImageContainerTest.java
@@ -28,45 +28,49 @@ public class ImageContainerTest extends TestCase {
 	public void testInit() throws Throwable{
 	}
 	public void testSaveImageGetImage() throws Throwable{
-		Bitmap bitmap1 = Bitmap.createBitmap(10, 10, Config.ARGB_8888);
-		mImageContainer.saveBitmap("test1.png", bitmap1);
-		Bitmap bitmap2 = Bitmap.createBitmap(10, 10, Config.ARGB_8888);
-		mImageContainer.saveBitmap("test2.png", bitmap2);
-		mImageContainer.deleteImage("test1.png");
+//		Bitmap bitmap1 = Bitmap.createBitmap(10, 10, Config.ARGB_8888);
+//		mImageContainer.saveBitmap("test1.png", bitmap1);
+//		Bitmap bitmap2 = Bitmap.createBitmap(10, 10, Config.ARGB_8888);
+//		mImageContainer.saveBitmap("test2.png", bitmap2);
+//		mImageContainer.deleteImage("test1.png");
+//		
+//		assertEquals(bitmap1.getNinePatchChunk(), mImageContainer.getImage("test2.png").getNinePatchChunk());
+//		
+//		boolean exists = false;
+//		File rootFile = new File("/sdcard/");
+//		String[] rootFileList = rootFile.list();
+//		
+//		for(int i=0; i<rootFileList.length; i++)
+//    	{
+//    		if(rootFileList[i].contains("test2.png")){
+//    			exists = true;
+//    		}
+//    			
+//    	}
+//		assertTrue(exists);
+//		mImageContainer.deleteImage("test2.png");
 		
-		assertEquals(bitmap1.getNinePatchChunk(), mImageContainer.getImage("test2.png").getNinePatchChunk());
-		
-		boolean exists = false;
-		File rootFile = new File("/sdcard/");
-		String[] rootFileList = rootFile.list();
-		
-		for(int i=0; i<rootFileList.length; i++)
-    	{
-    		if(rootFileList[i].contains("test2.png")){
-    			exists = true;
-    		}
-    			
-    	}
-		assertTrue(exists);
-		mImageContainer.deleteImage("test2.png");
+		assertTrue(false);
 	}
 	public void testSaveBitmapOnSDCardAsPNG() throws Throwable{
-		Bitmap bitmap = Bitmap.createBitmap(10, 10, Config.ARGB_8888);
-		mImageContainer.saveBitmap("test1.png", bitmap);
+//		Bitmap bitmap = Bitmap.createBitmap(10, 10, Config.ARGB_8888);
+//		mImageContainer.saveBitmap("test1.png", bitmap);
+//		
+//		boolean exists = false;
+//		File rootFile = new File("/sdcard/");
+//		String[] rootFileList = rootFile.list();
+//		
+//		for(int i=0; i<rootFileList.length; i++)
+//    	{
+//    		if(rootFileList[i].contains("test1.png")){
+//    			exists = true;
+//    		}
+//    			
+//    	}
+//		assertTrue(exists);
+//		mImageContainer.deleteImage("test1.png");
 		
-		boolean exists = false;
-		File rootFile = new File("/sdcard/");
-		String[] rootFileList = rootFile.list();
-		
-		for(int i=0; i<rootFileList.length; i++)
-    	{
-    		if(rootFileList[i].contains("test1.png")){
-    			exists = true;
-    		}
-    			
-    	}
-		assertTrue(exists);
-		mImageContainer.deleteImage("test1.png");
+		assertTrue(false);
 	}
 	
 	
