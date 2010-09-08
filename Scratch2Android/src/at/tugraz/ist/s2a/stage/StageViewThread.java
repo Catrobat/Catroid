cd262012819d5bbbbbc2615e88c6fc733a9a7f95
Thomas Holzmann <t.holzmann@student.tugraz.at>
2010-09-08 11:15:17 +0200

    scaling functionality of sprites now works

--- Scratch2Android/src/at/tugraz/ist/s2a/stage/StageViewThread.java
+++ Scratch2Android/src/at/tugraz/ist/s2a/stage/StageViewThread.java
@@ -11,10 +11,12 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.drawable.ScaleDrawable;
 import android.os.Handler;
 import android.util.Log;
 import android.util.Pair;
 import android.view.SurfaceHolder;
+import at.tugraz.ist.s2a.utils.ImageEditing;
 
 
 /**
@@ -32,6 +34,7 @@ public class StageViewThread extends Thread {
 	private SurfaceHolder mSurfaceHolder;
 	private Bitmap mBackground;
 	private Map<String, Pair<Bitmap, Pair<Float, Float>>> mBitmapToPositionMap;
+	private Map<String, Float> mScaleMap;
 
 	public StageViewThread(SurfaceHolder holder, Context context,
 			Handler handler) {
@@ -39,6 +42,8 @@ public class StageViewThread extends Thread {
 		this.setName("StageViewThread");
 		mBitmapToPositionMap = Collections
 				.synchronizedMap(new HashMap<String, Pair<Bitmap, Pair<Float, Float>>>());
+		mScaleMap = Collections
+		.synchronizedMap(new HashMap<String, Float>());
 	}
 
 	public synchronized void setRunning(boolean b) {
@@ -54,19 +59,24 @@ public class StageViewThread extends Thread {
 		mIsDraw = true;
 	}
 
-	public void addBitmapToDraw(String spriteName, String path, float x, float y) {
+	public void addBitmapToDraw(String spriteName, String path, float x, float y, float scaling) {
 		
 		Pair<Float, Float> coordinates = new Pair<Float, Float>(x, y);
-		Pair<Bitmap, Pair<Float, Float>> bitmapPair = null;
-		bitmapPair = new Pair<Bitmap, Pair<Float, Float>>(BitmapFactory.decodeFile(path), coordinates);
+		Pair<Bitmap, Pair<Float, Float>> bitmapPositionPair = null;
+		
+		Bitmap bitmap = BitmapFactory.decodeFile(path);
+		bitmapPositionPair = new Pair<Bitmap, Pair<Float, Float>>(bitmap, coordinates);
+		
 		mIsDraw = false; // TODO brauchen wir das ueberall??
-		mBitmapToPositionMap.put(spriteName, bitmapPair);
+		mBitmapToPositionMap.put(spriteName, bitmapPositionPair);
+		mScaleMap.put(spriteName, scaling);
 		mIsDraw = true;
 	}
 
 	public void removeBitmapToDraw(String spriteName) {
 		mIsDraw = false;
 		mBitmapToPositionMap.remove(spriteName);
+		mScaleMap.remove(spriteName);
 		mIsDraw = true;
 	}
 
@@ -82,6 +92,16 @@ public class StageViewThread extends Thread {
 		mBitmapToPositionMap.put(spriteName, bitmapAndPosition);
 		mIsDraw = true;
 
+	}
+	
+	public void changeScalingFactor(String spriteName, float factor){
+		if (!mScaleMap.containsKey(spriteName))
+			return;
+
+		mIsDraw = false;
+		mScaleMap.remove(spriteName);
+		mScaleMap.put(spriteName, factor);
+		mIsDraw = true;
 	}
 
 	public synchronized boolean isRunning() {
@@ -116,12 +136,23 @@ public class StageViewThread extends Thread {
 	
 			if (mBackground != null)
 				canvas.drawBitmap(mBackground, 0, 0, null);
+			
+			if(mBitmapToPositionMap.size() != mScaleMap.size()){
+				Log.e("StageViewThread", "mBitmapToPositionMap and mScaleMap do not have the same size! Not drawing this frame.");
+			}
 	
 			// TODO welcher sprite soll an oberster ebene gezeichnet werden??
 			Iterator<String> keyIterator = mBitmapToPositionMap.keySet().iterator();
 			for (int i = 0; i < mBitmapToPositionMap.size(); i++) {
+				String key = keyIterator.next();
 				Pair<Bitmap, Pair<Float, Float>> bitmapPair = mBitmapToPositionMap
-						.get(keyIterator.next());
+						.get(key);
+				float scalingFactor = mScaleMap.get(key);
+				if (scalingFactor != 1) {
+					// scale the bitmap if scaling factor is not 1
+					Bitmap scaledBitmap = ImageEditing.scaleBitmap(bitmapPair.first, scalingFactor);
+					bitmapPair = new Pair<Bitmap, Pair<Float, Float>> (scaledBitmap, bitmapPair.second);
+				}
 				if (bitmapPair != null)
 					canvas.drawBitmap(bitmapPair.first, bitmapPair.second.first,
 						bitmapPair.second.second, null);
--- Scratch2Android/src/at/tugraz/ist/s2a/utils/ImageEditing.java
+++ Scratch2Android/src/at/tugraz/ist/s2a/utils/ImageEditing.java
@@ -10,12 +10,16 @@ public class ImageEditing {
 	public ImageEditing() {
 
 	}
-	public Bitmap scaleBitmap(Bitmap bm, int xSize, int ySize){
+	public static Bitmap scaleBitmap(Bitmap bm, int xSize, int ySize){
 		Matrix matrix = new Matrix();
 		float scaleWidth = (((float)xSize)/bm.getWidth());
 		float scaleHeight = (((float)ySize)/bm.getHeight());
 	    matrix.postScale(scaleWidth, scaleHeight);
 		Bitmap newbm = Bitmap.createBitmap(bm, 0, 0,bm.getWidth() ,bm.getHeight() , matrix, true);
 		return newbm;
+	}
+	
+	public static Bitmap scaleBitmap(Bitmap bm, float scalingFactor){
+		return ImageEditing.scaleBitmap(bm, (int)(bm.getWidth()*scalingFactor), (int)(bm.getHeight()*scalingFactor));
 	}
 }
