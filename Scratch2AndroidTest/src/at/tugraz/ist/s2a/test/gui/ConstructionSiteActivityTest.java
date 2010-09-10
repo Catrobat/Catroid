7fa29092d3171a6022a80b676297377737cb4897
Thomas Holzmann <t.holzmann@student.tugraz.at>
2010-09-10 12:12:24 +0200

    3.24: tried to implement the test though it's not completed yet

--- Scratch2AndroidTest/src/at/tugraz/ist/s2a/test/gui/ConstructionSiteActivityTest.java
+++ Scratch2AndroidTest/src/at/tugraz/ist/s2a/test/gui/ConstructionSiteActivityTest.java
@@ -1,18 +1,31 @@
 package at.tugraz.ist.s2a.test.gui;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.xmlpull.v1.XmlPullParserException;
+
 
 import android.app.Dialog;
+import android.content.res.XmlResourceParser;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.TouchUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.Xml;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import at.tugraz.ist.s2a.ConstructionSiteActivity;
+import at.tugraz.ist.s2a.R;
 import at.tugraz.ist.s2a.constructionSite.content.BrickDefine;
 
 public class ConstructionSiteActivityTest extends ActivityInstrumentationTestCase2<ConstructionSiteActivity>{
@@ -44,6 +57,70 @@ public class ConstructionSiteActivityTest extends ActivityInstrumentationTestCas
         
         mShowObjectDialogButton = (Button) mActivity.findViewById(at.tugraz.ist.s2a.R.id.toolbar_button);
         
+	}
+	
+	/**
+	 * tests if the scroll bar in the tool box dialog is set to show up as long as expected
+	 */
+	public void testToolBoxScrollBar(){
+
+		//first approach
+		
+//		CharSequence filePath = mActivity.getApplicationContext().getResources().getText(R.layout.dialog_toolbox);
+		
+		
+//		StringBuffer xmlStringBuffer = null;
+//		try {
+//			FileInputStream fis = new FileInputStream(filePath.toString()); 
+//			InputStreamReader in = new InputStreamReader(fis, "UTF-8");
+//	
+//			xmlStringBuffer = new StringBuffer(1024);
+//			BufferedReader reader = new BufferedReader(new FileReader(filePath.toString()));
+//					
+//			char[] chars = new char[1024];
+//			int numRead = 0;
+//			while( (numRead = reader.read(chars)) > -1){
+//				xmlStringBuffer.append(String.valueOf(chars));	
+//			}
+//	
+//			reader.close();
+//		}
+//		catch (Exception e){
+//			e.printStackTrace();
+//		}
+//		
+//		String xmlString = xmlStringBuffer.toString();
+//		
+//		assertTrue(xmlString.contains("android:scrollbarDefaultDelayBeforeFade=\"2000\""));
+
+		
+		//second approach
+		
+//		XmlResourceParser parser = mActivity.getApplicationContext().getResources().getXml(R.layout.dialog_toolbox);
+//		AttributeSet set = Xml.asAttributeSet(parser);
+//		
+//		
+//		int scrollbarDelay = 0;
+//		String namespace = parser.getNamespace();
+//		
+//		try {
+//			while(scrollbarDelay == 0){
+//				if (parser.getIdAttribute().equals(R.id.toolboxListView))
+//						scrollbarDelay = parser.getAttributeIntValue(parser.getNamespace(), "scrollbarDefaultDelayBeforeFade", 0);
+//				parser.next();
+//			}
+//		} catch (XmlPullParserException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (NullPointerException e) {
+//			Log.e("ConstructionSiteActivityTest", "did not find scrollbar attribute");
+//		}
+//		
+//		
+//		assertTrue(scrollbarDelay==2000);
 	}
 	
 //	public void testToolboxShowingUp() {
