2846efe11339cb07d7056ba357ef8348beba7aa1
Peter Treitler <peter.treitler@gmail.com>
2010-09-16 13:47:39 +0200

    Added UI tests for adding the "set background", "play sound" and "wait" bricks

---
+++ catroidUiTest/src/at/tugraz/ist/catroid/test/construction_site/ConstructionSiteTest.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package at.tugraz.ist.catroid.test.construction_site;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.Smoke;
+import at.tugraz.ist.catroid.ConstructionSiteActivity;
+import at.tugraz.ist.catroid.R;
+
+import com.jayway.android.robotium.solo.Solo;
+
+/**
+ * @author Peter Treitler
+ * 
+ * Tests the features of the construction site
+ */
+public class ConstructionSiteTest extends
+		ActivityInstrumentationTestCase2<ConstructionSiteActivity> {
+	private Solo solo;
+	
+	public ConstructionSiteTest() {
+		super("at.tugraz.ist.catroid.test.construction_site",
+				ConstructionSiteActivity.class);
+	}
+	
+	private void clearConstructionSite() {
+		solo.clickOnMenuItem(getActivity().getString(R.string.reset));
+	}
+	
+	private void addBrick(int brickTextId) {
+		solo.clickOnButton(getActivity().getString(R.string.toolbar));
+		solo.clickOnText(getActivity().getString(brickTextId));
+		
+		boolean foundText = solo.searchText(getActivity().getString(brickTextId));
+		assertTrue("Found brick in construction site", foundText);
+	}
+
+	public void setUp() throws Exception {
+		solo = new Solo(getInstrumentation(), getActivity());
+		clearConstructionSite();
+	}
+	
+	public void tearDown() throws Exception {
+		clearConstructionSite();
+		try {	
+			solo.finalize();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		getActivity().finish();
+		super.tearDown();
+	}
+	
+	@Smoke
+	public void testAddSetBackgroundBrick() {
+		addBrick(R.string.set_background_main_adapter);
+	}
+	
+	@Smoke
+	public void testAddPlaySoundBrick() {
+		addBrick(R.string.play_sound_main_adapter);
+	}
+	
+	@Smoke
+	public void testAddWaitBrick() {
+		addBrick(R.string.wait_main_adapter);
+	}
+}
