6dd1b12dbc6c0aed0acbea4f609c2b0cffa700a7
Thomas Holzmann <t.holzmann@student.tugraz.at>
2010-09-28 12:54:02 +0200

    4.17: continuing implementation

--- catroid/src/at/tugraz/ist/catroid/stage/BrickWait.java
+++ catroid/src/at/tugraz/ist/catroid/stage/BrickWait.java
@@ -5,39 +5,23 @@ import java.util.Observable;
 import android.os.Handler;
 import android.util.Log;
 
-public class BrickWait extends Observable{
+/**
+ * 
+ * @author Thomas Holzmann
+ *
+ */
+public class BrickWait extends Observable {
+	//TODO pause/start needs to be tested
+	
 	public int mWaitTime=0; //the time to wait in milliseconds
 	private int mAlreayWaited =0;
 	public boolean mIsWaiting = false;
-//	
-//    public void run() {
-//    	synchronized (this){
-//	    	try {
-//				wait((int)(mWaitTime*1000+1)); 
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//			setChanged();
-//	        notifyObservers( "waited" );
-//    	}
-//    	
-//    }
-	
-    
-    private Handler handler = new Handler();
-
-    private Runnable runnable = new Runnable() {
-
+    private Handler mHandler = new Handler();
+    private Runnable mRunnable = new Runnable() {
     	public void run() {
-
     		checkWaitTime();
-
-    		/*
-    		 * Now register it for running next time
-    		 */
-
     		if (mIsWaiting)
-    			handler.postDelayed(this, 100);
+    			mHandler.postDelayed(this, 100);
     	}
 
     };
@@ -45,7 +29,7 @@ public class BrickWait extends Observable{
     private void checkWaitTime(){
     	Log.i("BrickWait", "checking wait time...");
     	if (mAlreayWaited >= mWaitTime){
-    		handler.removeCallbacks(runnable);
+    		mHandler.removeCallbacks(mRunnable);
     		mAlreayWaited = 0;
     		mIsWaiting = false;
     		setChanged();
@@ -57,13 +41,13 @@ public class BrickWait extends Observable{
     }
     
     public void pause() {
-    	handler.removeCallbacks(runnable);
+    	mHandler.removeCallbacks(mRunnable);
     	
     }
     
     public void start() {
     	mIsWaiting = true;
-    	runnable.run();
+    	mRunnable.run();
     }
 
 	
