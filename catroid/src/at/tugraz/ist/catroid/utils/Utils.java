ab02e01f9cd2cdd8ece55c9dd52d4b023cc966f7
Peter Treitler <peter.treitler@gmail.com>
2010-09-28 14:02:13 +0200

    B5: Fixed SD card problem (app now shows an error message and exits when no SD card is present).

--- catroid/src/at/tugraz/ist/catroid/utils/Utils.java
+++ catroid/src/at/tugraz/ist/catroid/utils/Utils.java
@@ -6,140 +6,172 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.graphics.Bitmap;
 import android.util.Log;
 import at.tugraz.ist.catroid.ConstructionSiteActivity;
+import at.tugraz.ist.catroid.R;
 
 public class Utils {
-	
-	public static boolean copyFile(String from, String to){
+	private static boolean hasSdCard() {
+		return android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED);
+	}
+
+	/**
+	 * Checks whether the current device has an SD card. If it has none an error
+	 * message is displayed and the calling activity is finished.
+	 * TODO: A RuntimeException is thrown after the call to Activity.finish; find out why!
+	 * 
+	 * @param parentActivity
+	 *            the activity that calls this method
+	 */
+	public static boolean checkForSdCard(final Activity parentActivity) {
+		if (!hasSdCard()) {
+			Builder builder = new AlertDialog.Builder(parentActivity);
+
+			builder.setTitle(parentActivity.getString(R.string.error));
+			builder.setMessage(parentActivity.getString(R.string.error_no_sd_card));
+			builder.setNeutralButton(parentActivity.getString(R.string.close), new OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					// finish parent activity
+					parentActivity.finish();
+				}
+			});
+			builder.show();
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean copyFile(String from, String to) {
 		File fileFrom = new File(from);
 		File fileTo = new File(to);
-		
-		if(fileTo.exists())
+
+		if (fileTo.exists())
 			deleteFile(fileTo.getAbsolutePath());
 		try {
 			fileTo.createNewFile();
 		} catch (IOException e1) {
 			return false;
 		}
-		
-		if(!fileFrom.exists() || !fileTo.exists())
+
+		if (!fileFrom.exists() || !fileTo.exists())
 			return false;
-		
+
 		FileInputStream fis;
 		FileOutputStream fos;
 		try {
-			fis  = new FileInputStream(fileFrom);
+			fis = new FileInputStream(fileFrom);
 			fos = new FileOutputStream(fileTo);
-	    
-	        byte[] buf = new byte[1024];
-	        int i = 0;
-	        while ((i = fis.read(buf)) != -1) {
-	            fos.write(buf, 0, i);
-	        }
-	        
-	        if (fis != null) fis.close();
-	        if (fos != null) fos.close();
-	    } 
-	    catch (Exception e) {
-	    	return false;
-	    }
-	    
-	    return true;
+
+			byte[] buf = new byte[1024];
+			int i = 0;
+			while ((i = fis.read(buf)) != -1) {
+				fos.write(buf, 0, i);
+			}
+
+			if (fis != null)
+				fis.close();
+			if (fos != null)
+				fos.close();
+		} catch (Exception e) {
+			return false;
+		}
+
+		return true;
 	}
-	
-	public static boolean deleteFile(String path){
+
+	public static boolean deleteFile(String path) {
 		File fileFrom = new File(path);
 		return fileFrom.delete();
 	}
-	
-	public static boolean deleteFolder(String path){
+
+	public static boolean deleteFolder(String path) {
 		File fileFrom = new File(path);
-		  if (fileFrom.isDirectory()) {
-		    for (File c : fileFrom.listFiles()){
-		    	if(c.isDirectory())
-		    		deleteFolder(c.getAbsolutePath());
-		    	else
-		    		c.delete();
-		    }     
-		  }else
-			  fileFrom.delete();
-		  
-		  return true;
-		}
-	
-	public static boolean deleteFolder(String path, String ignoreFile){
-		File fileFrom = new File(path);
-		  if (fileFrom.isDirectory()) {
-		    for (File c : fileFrom.listFiles()){
-		    	if(c.isDirectory())
-		    		deleteFolder(c.getAbsolutePath(), ignoreFile);
-		    	else{
-					  if(!c.getName().equals(ignoreFile))
-						  c.delete();
-		    	}
-		    		
-		    }     
-		  }else{
-			  if(!fileFrom.getName().equals(ignoreFile))
-				  fileFrom.delete();
-		  }
-			  
-		  
-		  return true;
-		}
-	
-	public static String concatPaths(String first, String second){
-		if(first == null && second == null)
-			return null;
-		if(first == null)
-			return second;
-		if(second == null)
-			return first;
-		if(first.endsWith("/"))
-			if(second.startsWith("/"))
-				return first+second.replaceFirst("/", "");
-			else
-				return first+second;
-		else
-			if(second.startsWith("/"))
-				return first+second;
-			else
-				return first+"/"+second;
+		if (fileFrom.isDirectory()) {
+			for (File c : fileFrom.listFiles()) {
+				if (c.isDirectory())
+					deleteFolder(c.getAbsolutePath());
+				else
+					c.delete();
+			}
+		} else
+			fileFrom.delete();
+
+		return true;
 	}
-	
-	public static String addDefaultFileEnding(String filename){
-		if(!filename.endsWith(ConstructionSiteActivity.DEFAULT_FILE_ENDING))
+
+	public static boolean deleteFolder(String path, String ignoreFile) {
+		File fileFrom = new File(path);
+		if (fileFrom.isDirectory()) {
+			for (File c : fileFrom.listFiles()) {
+				if (c.isDirectory())
+					deleteFolder(c.getAbsolutePath(), ignoreFile);
+				else {
+					if (!c.getName().equals(ignoreFile))
+						c.delete();
+				}
+
+			}
+		} else {
+			if (!fileFrom.getName().equals(ignoreFile))
+				fileFrom.delete();
+		}
+
+		return true;
+	}
+
+	public static String concatPaths(String first, String second) {
+		if (first == null && second == null)
+			return null;
+		if (first == null)
+			return second;
+		if (second == null)
+			return first;
+		if (first.endsWith("/"))
+			if (second.startsWith("/"))
+				return first + second.replaceFirst("/", "");
+			else
+				return first + second;
+		else if (second.startsWith("/"))
+			return first + second;
+		else
+			return first + "/" + second;
+	}
+
+	public static String addDefaultFileEnding(String filename) {
+		if (!filename.endsWith(ConstructionSiteActivity.DEFAULT_FILE_ENDING))
 			return filename + ConstructionSiteActivity.DEFAULT_FILE_ENDING;
 		return filename;
 	}
-	
-	public static void saveBitmapOnSDCardAsPNG(String full_path, Bitmap bitmap){
-		 File file = new File(full_path);
-		 try {
+
+	public static void saveBitmapOnSDCardAsPNG(String full_path, Bitmap bitmap) {
+		File file = new File(full_path);
+		try {
 			FileOutputStream os = new FileOutputStream(file);
 			bitmap.compress(Bitmap.CompressFormat.PNG, 100, os);
 			os.close();
-		 } catch (FileNotFoundException e) {
-			 Log.e("UTILS", e.getMessage());
+		} catch (FileNotFoundException e) {
+			Log.e("UTILS", e.getMessage());
 			e.printStackTrace();
-		   } catch (IOException e) {
-			   Log.e("UTILS", e.getMessage());
+		} catch (IOException e) {
+			Log.e("UTILS", e.getMessage());
 			e.printStackTrace();
 		}
 	}
-	
-	public static String changeFileEndingToPng(String filename)
-	{
+
+	public static String changeFileEndingToPng(String filename) {
 		String newFileName;
-		
+
 		int beginOfFileEnding = filename.lastIndexOf(".");
 		newFileName = filename.replace(filename.substring(beginOfFileEnding), "");
-		
-	    newFileName = newFileName + ".png";
+
+		newFileName = newFileName + ".png";
 		return newFileName;
 	}
 
 }
-
