ea6bb8a2ec108616a7d3de4e9db6b39e78ed245d
Thomas Holzmann <t.holzmann@student.tugraz.at>
2010-09-29 10:35:51 +0200

    added missing files

---
+++ catroid/src/at/tugraz/ist/catroid/stage/CanvasDraw.java
@@ -0,0 +1,30 @@
+package at.tugraz.ist.catroid.stage;
+
+import android.graphics.Canvas;
+
+/**
+ * 
+ * Draws DrawObjects into a canvas.
+ * 
+ * @author Thomas Holzmann
+ *
+ */
+public class CanvasDraw implements IDraw {
+	private Canvas mCanvas;
+	
+	public CanvasDraw(){
+		super();
+		
+	}
+
+	public void draw(DrawObject drawObject) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
