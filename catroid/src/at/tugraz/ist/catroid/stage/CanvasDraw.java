b94ba11d7d9218548048558d1031d5081e261699
Thomas Holzmann <t.holzmann@student.tugraz.at>
2010-09-29 11:22:59 +0200

    continued implementing CanvasDraw

--- catroid/src/at/tugraz/ist/catroid/stage/CanvasDraw.java
+++ catroid/src/at/tugraz/ist/catroid/stage/CanvasDraw.java
@@ -1,6 +1,18 @@
 package at.tugraz.ist.catroid.stage;
 
+import java.util.Iterator;
+
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.Log;
+import android.util.Pair;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import at.tugraz.ist.catroid.utils.ImageEditing;
 
 /**
  * 
@@ -9,22 +21,48 @@ import android.graphics.Canvas;
  * @author Thomas Holzmann
  *
  */
-public class CanvasDraw implements IDraw {
-	private Canvas mCanvas;
+public class CanvasDraw implements IDraw, SurfaceHolder.Callback {
+	private Canvas mCanvas=null;
+	private SurfaceView mSurfaceView;
 	
 	public CanvasDraw(){
 		super();
+		mSurfaceView = StageActivity.mStage;
+		mSurfaceView.getHolder().addCallback(this);
 		
 	}
 
-	public void draw(DrawObject drawObject) {
+	public synchronized void draw(DrawObject drawObject) {
+		if (mCanvas != null && drawObject.mBitmap != null){
+			mCanvas.drawBitmap(drawObject.mBitmap, drawObject.mPosition.first, drawObject.mPosition.second, null); //TODO change to getter
+		}	
+	}
+
+	public synchronized void clear() {
 		// TODO Auto-generated method stub
 
 	}
 
-	public void clear() {
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
 		// TODO Auto-generated method stub
+		
+	}
 
+	public void surfaceCreated(SurfaceHolder holder) {
+		mCanvas = holder.lockCanvas();
+		
+		// we want to start with a white rectangle
+		Paint paint = new Paint();
+		paint.setStyle(Paint.Style.FILL);
+		paint.setColor(Color.WHITE);
+		mCanvas.drawRect(new Rect(0, 0, mCanvas.getWidth(), mCanvas.getHeight()),
+				paint);
+	}
+
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
