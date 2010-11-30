2abba72579650e07902c35d1685d5ee9e969e1cb
Daniel Burtscher <knut0025@gmail.com>
2010-11-30 18:04:46 +0100

    [db] 4.27 create Mock objects

--- catroidTest/src/at/tugraz/ist/catroid/test/web/UpAndDownloadTest.java
+++ catroidTest/src/at/tugraz/ist/catroid/test/web/UpAndDownloadTest.java
@@ -1,6 +1,8 @@
 package at.tugraz.ist.catroid.test.web;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 
 import android.test.AndroidTestCase;
 import at.tugraz.ist.catroid.ConstructionSiteActivity;
@@ -8,9 +10,12 @@ import at.tugraz.ist.catroid.constructionSite.tasks.ProjectUploadTask;
 import at.tugraz.ist.catroid.download.tasks.ProjectDownloadTask;
 import at.tugraz.ist.catroid.utils.UtilFile;
 import at.tugraz.ist.catroid.web.ConnectionWrapper;
+import at.tugraz.ist.catroid.web.WebconnectionException;
 
 public class UpAndDownloadTest extends AndroidTestCase {
 
+	private MockConnection mMockConnection;
+	
 	public UpAndDownloadTest() {
 		super();
 	}
@@ -19,17 +24,7 @@ public class UpAndDownloadTest extends AndroidTestCase {
 		super.setUp();
 		
 		// generate mock object
-		new ConnectionWrapper() {
-			@Override
-			public void testing() {
-				// TODO Auto-generated method stub
-				super.testing();
-			}
-		};
 		
-		ConnectionWrapper() {
-			
-		}
 	}
 
 	protected void tearDown() throws Exception {
@@ -40,26 +35,35 @@ public class UpAndDownloadTest extends AndroidTestCase {
 	}
 
 	public void testUpAndDownload() throws Throwable {
-//		String testProjectName = "UpAndDownloadTest"+System.currentTimeMillis();
-//		
-//		ProjectUploadTask uploadTask = new ProjectUploadTask(null, testProjectName,ConstructionSiteActivity.DEFAULT_ROOT, 
-//					ConstructionSiteActivity.TMP_PATH+"/tmp.zip");
-//		
-//		uploadTask.execute();		
-//		Thread.sleep(8000);
-//		String projectDownloadUrl = uploadTask.getResultString();
-//		
-//		assertTrue("No download url returned from the server. Connected to the Internt?", projectDownloadUrl != null && projectDownloadUrl.length() > 0);
-//		
-//		
-//		new ProjectDownloadTask(null, projectDownloadUrl, testProjectName,
-//					ConstructionSiteActivity.TMP_PATH+"/down.zip").execute();
-//		Thread.sleep(8000);
-//		
-//		File downloadProjectRoot = new File(ConstructionSiteActivity.DEFAULT_ROOT + "/"+testProjectName+"/");
-//		assertTrue("Download Project is not available.", downloadProjectRoot.exists());
-//		
-//		boolean spfFilePresent = false;
+		String testProjectName = "UpAndDownloadTest"+System.currentTimeMillis();
+		
+		ProjectUploadTask uploadTask = new ProjectUploadTask(null, testProjectName,
+				ConstructionSiteActivity.DEFAULT_ROOT+"/defaultSaveFile", 
+				ConstructionSiteActivity.TMP_PATH+"/tmp.zip") {
+			@Override
+			protected ConnectionWrapper createConnection() {
+				return mMockConnection;
+			}
+		};
+		ProjectDownloadTask downloadTask = new ProjectDownloadTask(null, 
+				"", testProjectName, ConstructionSiteActivity.TMP_PATH+"/down.zip") {
+			@Override
+			protected ConnectionWrapper createConnection() {
+				return mMockConnection;
+			}
+		};
+		
+		uploadTask.execute();		
+		//Thread.sleep(8000);
+		
+		
+		downloadTask.execute();
+		//Thread.sleep(8000);
+		
+		File downloadProjectRoot = new File(ConstructionSiteActivity.DEFAULT_ROOT + "/"+testProjectName+"/");
+		//assertTrue("Download Project is not available.", downloadProjectRoot.exists());
+		
+		boolean spfFilePresent = false;
 //		String[] projectFiles = downloadProjectRoot.list();
 //		for (String fileName : projectFiles) {
 //			if(fileName.endsWith(ConstructionSiteActivity.DEFAULT_FILE_ENDING))
@@ -70,4 +74,19 @@ public class UpAndDownloadTest extends AndroidTestCase {
 //		UtilFile.deleteDirectory(downloadProjectRoot);
 	}
 
+	private class MockConnection extends ConnectionWrapper {
+		@Override
+		public String doHttpPostFileUpload(String urlstring,
+				HashMap<String, String> postValues, String filetag,
+				String filePath) throws IOException, WebconnectionException {
+			
+			return "";
+		}
+		@Override
+		public void doHttpPostFileDownload(String urlstring,
+				HashMap<String, String> postValues, String filePath)
+				throws IOException {
+			
+		}
+	}
 }
