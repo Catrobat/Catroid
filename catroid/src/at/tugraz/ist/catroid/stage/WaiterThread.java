d8f710b58aa0fc0af1feef7844839b59e691b788
Peter Treitler <peter.treitler@gmail.com>
2010-12-17 17:54:55 +0100

    B18: Added unit tests. Accepted by WS. Pause / continue still not working properly!

--- catroid/src/at/tugraz/ist/catroid/stage/WaiterThread.java
+++ catroid/src/at/tugraz/ist/catroid/stage/WaiterThread.java
@@ -5,33 +5,31 @@ import android.util.Log;
 public class WaiterThread extends Thread {
 	private long timeToWaitInMilliseconds;
 	private long startTime;
-	private boolean isWaiting = false;
 	BrickWait owner;
-	
+
 	public WaiterThread(BrickWait owner, long timeToWaitInMilliseconds) {
 		this.owner = owner;
 		this.timeToWaitInMilliseconds = timeToWaitInMilliseconds;
 	}
-	
-	public boolean isWaiting() {
-		return isWaiting;
-	}
 
 	public void run() {
-		isWaiting = true;
 		try {
 			startTime = System.currentTimeMillis();
 			Log.d("WaiterThread", "Started waiting at " + startTime);
 			Thread.sleep(timeToWaitInMilliseconds);
 		} catch (InterruptedException e) {
+			// TODO: This catch block isn't reached, even after calling interrupt(). Find out why!
 			long interruptedTime = System.currentTimeMillis();
 			Log.d("WaiterThread", "Interrupted at " + interruptedTime);
 			long alreadyWaited = interruptedTime - startTime;
 			timeToWaitInMilliseconds -= alreadyWaited;
+			if (owner != null)
+				owner.threadInterrupted((int) timeToWaitInMilliseconds);
+			return;
 		}
 		Log.d("WaiterThread", "Done waiting at " + System.currentTimeMillis());
-		isWaiting = false;
-		owner.done();
+		if (owner != null)
+			owner.done();
 	}
 
 }
