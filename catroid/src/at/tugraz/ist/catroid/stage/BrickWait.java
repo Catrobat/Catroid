843b6fb612e3b4b1c1431f9c8b6eba39a07ce617
Peter Treitler <peter.treitler@gmail.com>
2010-12-17 17:54:55 +0100

    B18: Added unit tests. Accepted by WS. Pause / continue still not working properly!

--- catroid/src/at/tugraz/ist/catroid/stage/BrickWait.java
+++ catroid/src/at/tugraz/ist/catroid/stage/BrickWait.java
@@ -4,24 +4,33 @@ import java.util.Observable;
 
 public class BrickWait extends Observable {
 	private WaiterThread waiterThread;
+	private boolean isWaiting;
 
 	public BrickWait(int timeToWaitInMilliseconds) {
 		waiterThread = new WaiterThread(this, timeToWaitInMilliseconds);
+		isWaiting = false;
 	}
 
 	public void start() {
+		isWaiting = true;
 		waiterThread.start();
 	}
-	
+
 	public void pause() {
+		isWaiting = false;
 		waiterThread.interrupt();
 	}
 
 	public boolean isWaiting() {
-		return waiterThread.isAlive();
+		return isWaiting;
+	}
+
+	public void threadInterrupted(int timeLeftToWaitInMilliseconds) {
+		waiterThread = new WaiterThread(this, timeLeftToWaitInMilliseconds);
 	}
 
 	public void done() {
+		isWaiting = false;
 		setChanged();
 		notifyObservers();
 	}
