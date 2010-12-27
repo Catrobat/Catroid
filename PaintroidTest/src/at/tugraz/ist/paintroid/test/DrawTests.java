1ddab62731a575d851ef37d1978bad417087977f
Maximiliian Fellner <max.fellner@gmail.com>
2010-12-27 17:28:29 +0100

    Fixed a test and the version number.

--- PaintroidTest/src/at/tugraz/ist/paintroid/test/DrawTests.java
+++ PaintroidTest/src/at/tugraz/ist/paintroid/test/DrawTests.java
@@ -13,10 +13,10 @@ import com.jayway.android.robotium.solo.Solo;
 public class DrawTests extends ActivityInstrumentationTestCase2<MainActivity> {
 
 	private Solo solo;
-	
+
 	final int BRUSH = 3;
 	final int FILE = 7;
-	
+
 	public DrawTests() {
 		super("at.tugraz.ist.paintroid", MainActivity.class);
 
@@ -27,9 +27,9 @@ public class DrawTests extends ActivityInstrumentationTestCase2<MainActivity> {
 	}
 
 	/**
-	 * This test creates a new empty image, draws transparency onto it,
-	 * saves it, loads it again and checks, whether the same spot is
-	 * still transparent.
+	 * This test creates a new empty image, draws transparency onto it, saves
+	 * it, loads it again and checks, whether the same spot is still
+	 * transparent.
 	 * 
 	 */
 	@Smoke
@@ -37,44 +37,46 @@ public class DrawTests extends ActivityInstrumentationTestCase2<MainActivity> {
 		solo.clickOnImageButton(FILE);
 		solo.clickOnButton("New Drawing");
 		solo.clickOnImageButton(BRUSH);
-		
-		int screenWidth = solo.getCurrentActivity().getWindowManager().getDefaultDisplay().getWidth();
-		int screenHeight = solo.getCurrentActivity().getWindowManager().getDefaultDisplay().getHeight();
-		
-		int halfScreenWidth = (int)(screenWidth * 0.5);
-		int halfScreenHeight = (int)(screenHeight * 0.5);
-		
-		solo.drag(halfScreenWidth, halfScreenWidth, halfScreenHeight, halfScreenHeight, 1);
-		
+
+		int screenWidth = solo.getCurrentActivity().getWindowManager()
+				.getDefaultDisplay().getWidth();
+		int screenHeight = solo.getCurrentActivity().getWindowManager()
+				.getDefaultDisplay().getHeight();
+
+		int halfScreenWidth = (int) (screenWidth * 0.5);
+		int halfScreenHeight = (int) (screenHeight * 0.5);
+
+		solo.drag(halfScreenWidth, halfScreenWidth, halfScreenHeight,
+				halfScreenHeight, 1);
+
 		MainActivity mainActivity = (MainActivity) solo.getCurrentActivity();
-		
+
 		Bitmap currentImage = mainActivity.getCurrentImage();
-		
+
 		assertNotNull(currentImage);
-		
-		int halfImageWidth = (int)(currentImage.getWidth() * 0.5);
-		int halfImageHeight = (int)(currentImage.getHeight() * 0.5);
-		
+
+		int halfImageWidth = (int) (currentImage.getWidth() * 0.5);
+		int halfImageHeight = (int) (currentImage.getHeight() * 0.5);
+
 		int pixel = currentImage.getPixel(halfImageWidth, halfImageHeight);
-				
+
 		assertEquals(pixel, Color.TRANSPARENT);
-		
+
 		solo.clickOnImageButton(FILE);
 		solo.clickOnButton("Save");
 		solo.enterText(0, "robotium-test");
 		solo.clickOnButton("Done");
 		solo.clickOnMenuItem("Clear Drawing");
-		solo.clickOnImageButton(FILE);
-		solo.clickOnButton("Cancel");
-		
-//		Intent intent = new Intent();
-//		intent.putExtra("UriString", mainActivity.savedFile.toString());
-//		intent.putExtra("IntentReturnValue", "LOAD");
-//		
-//		mainActivity.onActivityResult(mainActivity.FILE_IO, Activity.RESULT_OK, intent);
-		
-//		mainActivity.loadNewImage(mainActivity.savedFile.toString());
-		
+
+		Intent intent = new Intent();
+		intent.putExtra("UriString", mainActivity.getSavedFileUriString());
+		intent.putExtra("IntentReturnValue", "LOAD");
+
+		mainActivity.onActivityResult(mainActivity.FILE_IO, Activity.RESULT_OK,
+				intent);
+
+		assertEquals(pixel, Color.TRANSPARENT);
+
 		solo.clickOnMenuItem("Quit");
 	}
 
