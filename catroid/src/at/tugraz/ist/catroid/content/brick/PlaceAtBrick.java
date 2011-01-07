f179d7d8122b9f410bfdc2f3c825b27656df8bc8
Peter Treitler <peter.treitler@gmail.com>
2011-01-07 16:22:37 +0100

    Refactoring: Tickets 5.1, 5.11 implemented. Some others started (skeleton classes to use there)

---
+++ catroid/src/at/tugraz/ist/catroid/content/brick/PlaceAtBrick.java
@@ -0,0 +1,41 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010  Catroid development team 
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.tugraz.ist.catroid.content.brick;
+
+import java.io.Serializable;
+
+import at.tugraz.ist.catroid.content.sprite.Sprite;
+
+public class PlaceAtBrick implements Brick, Serializable {
+	private int xPosition;
+	private int yPosition;
+	private transient Sprite sprite;
+	private static final long serialVersionUID = -1721024451054335050L;
+	
+	public PlaceAtBrick(int xPosition, int yPosition, Sprite sprite) {
+		this.xPosition = xPosition;
+		this.yPosition = yPosition;
+		this.sprite = sprite;
+	}
+	
+	public void execute() {
+		sprite.setPosition(xPosition, yPosition);
+	}
+
+}
---
+++ catroid/src/at/tugraz/ist/catroid/content/brick/ShowBrick.java
@@ -0,0 +1,38 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+    Copyright (C) 2010  Catroid development team 
+    (<http://code.google.com/p/catroid/wiki/Credits>)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package at.tugraz.ist.catroid.content.brick;
+
+import java.io.Serializable;
+
+import at.tugraz.ist.catroid.content.sprite.Sprite;
+
+public class ShowBrick implements Brick, Serializable {
+	private transient Sprite sprite;
+	private static final long serialVersionUID = 572499527545382155L;
+
+	public ShowBrick(Sprite sprite) {
+		this.sprite = sprite;
+	}
+	
+	public void execute() {
+		sprite.show();
+	}
+
+}
