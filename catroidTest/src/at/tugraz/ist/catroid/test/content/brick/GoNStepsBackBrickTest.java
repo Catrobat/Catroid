c362dcf8da04c234ee0c3cb4787f54522019fde5
Anton Rieder <aried3r@gmail.com>
2011-02-02 14:24:54 +0100

    [Catroid] Ticket 5.16, GoNStepsBackBrickTest implemented with Ainul, JiaLin, Denise. Waiting for Acceptance

---
+++ catroidTest/src/at/tugraz/ist/catroid/test/content/brick/GoNStepsBackBrickTest.java
@@ -0,0 +1,79 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010  Catroid development team 
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.tugraz.ist.catroid.test.content.brick;
+
+import android.test.AndroidTestCase;
+import at.tugraz.ist.catroid.content.brick.GoNStepsBackBrick;
+import at.tugraz.ist.catroid.content.sprite.Sprite;
+
+
+public class GoNStepsBackBrickTest extends AndroidTestCase {
+	
+	private int steps = 17;
+	private int oldPosition; 
+	
+	
+	public void testSteps() {
+		
+		
+		Sprite sprite = new Sprite("testSprite");
+		assertEquals("Unexpected initial sprite Z position", 0, sprite.getZPosition());
+		
+		oldPosition = sprite.getZPosition();
+		
+		GoNStepsBackBrick brick = new GoNStepsBackBrick(sprite, steps);
+		brick.execute();
+		assertEquals("Incorrect sprite Z position after GoNBackStepsBrick executed", (oldPosition - steps), sprite.getZPosition());		
+	}
+	
+	public void testNullSprite() {
+		GoNStepsBackBrick brick = new GoNStepsBackBrick(null, steps);
+		try {
+			brick.execute();
+			fail("Execution of GoNStepsBackBrick with null Sprite did not cause a NullPointerException to be thrown");
+		} catch(NullPointerException e) {
+			// expected behavior
+		}
+	}
+	
+	public void testBoundarySteps() {
+		Sprite sprite = new Sprite("testSprite");
+		
+		oldPosition = sprite.getZPosition();
+		
+		GoNStepsBackBrick brick = new GoNStepsBackBrick(sprite, Integer.MAX_VALUE);
+		brick.execute();
+		assertEquals("GoNStepsBackBrick failed to scale Sprite to maximum double value", (oldPosition - Integer.MAX_VALUE), sprite.getZPosition());
+		
+		brick = new GoNStepsBackBrick(sprite, Integer.MIN_VALUE);
+		try {
+			brick.execute();
+			fail("Execution of GoNStepsBackBrick with negative steps value did not cause a NumberFormatException to be thrown");
+		} catch(NumberFormatException e) {
+			// expected behavior
+		}
+		
+		brick = new GoNStepsBackBrick(sprite, Integer.MAX_VALUE);
+		brick.execute();
+		brick.execute();
+		assertEquals("An Integer underflow occured.", Integer.MIN_VALUE, sprite.getZPosition());
+		
+	}
+
+}
