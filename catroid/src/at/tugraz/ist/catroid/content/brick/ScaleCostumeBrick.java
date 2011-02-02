1cbc06d9b4a8f11da3543a1b190ff799862ad02a
Anton Rieder <aried3r@gmail.com>
2011-02-02 12:26:35 +0100

    [Catroid] Ticket: 5.13, ScaleCostumeBrick implemented with Ainul Husna, waiting for acceptance

---
+++ catroid/src/at/tugraz/ist/catroid/content/brick/ScaleCostumeBrick.java
@@ -0,0 +1,42 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010  Catroid development team 
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.tugraz.ist.catroid.content.brick;
+
+import java.io.Serializable;
+import at.tugraz.ist.catroid.content.sprite.Sprite;
+
+/**
+ * @author Anton Rieder, Ainul Husna
+ *
+ */
+public class ScaleCostumeBrick implements Brick, Serializable {
+	private transient Sprite sprite;
+	private double scale;
+	private static final long serialVersionUID = 1L;
+
+	public ScaleCostumeBrick(Sprite sprite, double scale) {
+		this.sprite = sprite;
+		this.scale  = scale;
+	}
+
+	public void execute() {
+		sprite.setScale(scale);
+	}
+
+}
