bd845b9694ca35176df38d7cf52bb10fdc3df39c
Daniel Markart <daniel.markart@gmail.com>
2011-02-03 21:12:25 +0100

    [Catroid] 5.23 Costume Test start

---
+++ catroidTest/src/at/tugraz/ist/catroid/test/content/sprite/CustomeTest.java
@@ -0,0 +1,51 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010  Catroid development team 
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.tugraz.ist.catroid.test.content.sprite;
+
+import android.test.AndroidTestCase;
+import at.tugraz.ist.catroid.content.sprite.Costume;
+
+public class CustomeTest extends AndroidTestCase{
+    
+    public void testDefaultConstructor(){
+        Costume costume = new Costume();
+        assertEquals("Unexpected ImagePath", "", costume.imagePath);
+        assertEquals("Unexpected ThumbnailPath", "", costume.thumbnailPath);
+    }
+    
+    public void testConstructor(){
+        final String imagePath = "imagePath";
+        final String thumbnailPath = "thumbPath";
+        Costume costume = new Costume(imagePath,thumbnailPath);
+        assertEquals("Unexpected ImagePath", imagePath, costume.imagePath);
+        assertEquals("Unexpected ThumbnailPath", thumbnailPath, costume.thumbnailPath);
+    }
+    
+    public void testChangePath() {
+        final String imagePath = "imagePath";
+        final String thumbnailPath = "thumbPath";
+        final String changedImagePath = "changed imagePath";
+        final String changedThumbnailPath = "changed thumbPath";
+        Costume costume = new Costume(imagePath,thumbnailPath);
+        costume.imagePath = changedImagePath;
+        costume.thumbnailPath = changedThumbnailPath;
+        assertEquals("Unexpected ImagePath", changedImagePath, costume.imagePath);
+        assertEquals("Unexpected ThumbnailPath", changedThumbnailPath, costume.thumbnailPath);
+    }
+}
