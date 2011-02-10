444265344544b1e61185c8eee1b8d081d41eb1b5
David Reisenberger <threesn.b@gmail.com>
2011-02-10 17:11:39 +0100

    5.6 Wait Brick, Sprite merged, Script merged, unfinished

--- catroid/src/at/tugraz/ist/catroid/content/brick/WaitBrick.java
+++ catroid/src/at/tugraz/ist/catroid/content/brick/WaitBrick.java
@@ -1,21 +1,34 @@
 package at.tugraz.ist.catroid.content.brick;
 
+import android.util.Log;
 import at.tugraz.ist.catroid.content.sprite.Sprite;
 
 public class WaitBrick implements Brick {
 	private static final long serialVersionUID = 1L;
-
-	public WaitBrick(int timeToWaitInMilliseconds) {
-		
+    private long timeToWaitInMilliseconds;
+    
+	public WaitBrick(long timeToWaitInMilliseconds) {
+		this.timeToWaitInMilliseconds = timeToWaitInMilliseconds;
 	}
 
 	public void execute() {
-		// TODO Auto-generated method stub
+		long startTime = 0;
+		try {
+			Log.d("WaitBrick ", "Starting to wait for " + this.timeToWaitInMilliseconds);
+			startTime = System.currentTimeMillis();
+			Thread.sleep(timeToWaitInMilliseconds);
+		} catch (InterruptedException e) {
+			Log.d("WaitBrick ", "Interrupted at " + System.currentTimeMillis());
+			timeToWaitInMilliseconds -= System.currentTimeMillis() - startTime;
+			Log.d("WaitBrick ", "remainingWaitingTime is " + timeToWaitInMilliseconds);
+		}
 		
 	}
 
 	public Sprite getSprite() {
 		return null;
 	}
+	
+	
 
 }
