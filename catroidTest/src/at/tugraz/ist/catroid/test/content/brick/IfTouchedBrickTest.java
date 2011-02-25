5f9ec27435fc6ed9f3b4cf0cbf3c527bd36b5e6d
Ainul Husna <ainulhusna@gmail.com>
2011-02-25 13:20:42 +0100

    Ticket 5.15 IfTouchedBrickTest done and accepted.

---
+++ catroidTest/src/at/tugraz/ist/catroid/test/content/brick/IfTouchedBrickTest.java
@@ -0,0 +1,30 @@
+package at.tugraz.ist.catroid.test.content.brick;
+
+import android.test.AndroidTestCase;
+import at.tugraz.ist.catroid.content.script.Script;
+import at.tugraz.ist.catroid.content.sprite.Sprite;
+import at.tugraz.ist.catroid.content.brick.gui.IfTouchedBrick;
+
+public class IfTouchedBrickTest extends AndroidTestCase {
+
+	Script touchScript = new Script();
+
+	public void testIfTouch() {
+
+		Sprite sprite = new Sprite("new sprite");
+		assertFalse("Unexpected default value", touchScript.isTouchScript());
+		IfTouchedBrick ifTouchedBrick = new IfTouchedBrick(sprite, touchScript);
+		ifTouchedBrick.execute();
+		assertTrue(
+				"The default value for the script is true after IfTouchedBrick execute.",
+				touchScript.isTouchScript());
+	}
+
+	public void testNullSprite() {
+		IfTouchedBrick ifTouchedBrick = new IfTouchedBrick(null, touchScript);
+
+		ifTouchedBrick.execute();
+		/*nothing happen because there is no sprite*/
+	}
+
+}
