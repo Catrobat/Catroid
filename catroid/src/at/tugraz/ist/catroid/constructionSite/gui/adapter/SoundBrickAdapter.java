0864cc62034568bc139e0e77f370b6a41c78c757
Daniel Markart <daniel.markart@gmail.com>
2011-03-02 17:42:40 +0100

    [Catroid] added further threading Tests

--- catroid/src/at/tugraz/ist/catroid/constructionSite/gui/adapter/SoundBrickAdapter.java
+++ catroid/src/at/tugraz/ist/catroid/constructionSite/gui/adapter/SoundBrickAdapter.java
@@ -47,7 +47,6 @@ public class SoundBrickAdapter extends BaseAdapter{
 
 	@Override
 	public void notifyDataSetChanged() {
-		// TODO Auto-generated method stub
 		super.notifyDataSetChanged();
 	}
 	
