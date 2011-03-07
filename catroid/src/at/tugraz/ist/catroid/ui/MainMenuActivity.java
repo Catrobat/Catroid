8cc252e7ca433606a8072ec8a1eb423cc06b28fd
Peter Treitler <peter.treitler@gmail.com>
2011-03-07 15:13:41 +0100

    Started MainMenuActivity

---
+++ catroid/src/at/tugraz/ist/catroid/ui/MainMenuActivity.java
@@ -0,0 +1,87 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010  Catroid development team 
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.tugraz.ist.catroid.ui;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import at.tugraz.ist.catroid.R;
+import at.tugraz.ist.catroid.content.project.Project;
+
+public class MainMenuActivity extends Activity {
+	private Project currentProject;
+	
+	
+	private void initListeners() {
+		final Context context = this;
+		Button resumeButton = (Button) this.findViewById(R.id.resumeButton);
+		resumeButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				if(currentProject != null) {
+					// TODO: Start new project activity with current project
+				}
+			}
+		});
+		
+		Button toStageButton = (Button) this.findViewById(R.id.toStageButton);
+		toStageButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				if(currentProject != null) {
+					// TODO: Start new stage activity with current project
+				}
+			}
+		});
+		
+		Button newProjectButton = (Button) this.findViewById(R.id.newProjectButton);
+		newProjectButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				try {
+					currentProject = new Project(context, "");
+				} catch (NameNotFoundException e) {
+					e.printStackTrace();
+				}
+				// TODO: Start new project activity
+			}
+		});
+	}
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.main_menu);
+		
+		// Try to load project
+		currentProject = null;
+		
+		if(currentProject == null) {
+			Button resumeButton = (Button) this.findViewById(R.id.resumeButton);
+			resumeButton.setEnabled(false);
+			Button toStageButton = (Button) this.findViewById(R.id.toStageButton);
+			toStageButton.setEnabled(false);
+			
+			TextView currentProjectTextView = (TextView) this.findViewById(R.id.currentProjectNameTextView);
+			currentProjectTextView.setText(getString(R.string.current_project) + " " + getString(R.string.no_project));
+		}
+		initListeners();
+	}
+}
