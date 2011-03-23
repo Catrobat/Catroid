e64f8e39202beb033bfaed2911e00f4f1b7cafd2
Anton Rieder <aried3r@gmail.com>
2011-03-23 17:41:34 +0100

    Deleted obsolete files, xml files need some lovin'

--- catroid/src/at/tugraz/ist/catroid/content/entities/SoundInfo.java
+++ catroid/src/at/tugraz/ist/catroid/content/entities/SoundInfo.java
@@ -18,37 +18,41 @@
  */
 package at.tugraz.ist.catroid.content.entities;
 
-
 public class SoundInfo {
 
-	private int id;
-	private String path;
-	private String title;
-	
-	public String getTitleWithPath() {
-		if(path.endsWith("/"))
-			return path+title;
-		else
-			return path+"/"+title;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getPath() {
-		return path;
-	}
-	public void setPath(String path) {
-		this.path = path;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	
-	
+    private int id;
+    private String path;
+    private String title;
+
+    public String getTitleWithPath() {
+        if (path.endsWith("/"))
+            return path + title;
+        else
+            return path + "/" + title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
 }
