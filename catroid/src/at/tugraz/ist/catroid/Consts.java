37efdba8a54a96d9303215f6ae0c1869e5c41820
Tom Spiss <spisst22@gmail.com>
2011-04-04 16:40:12 +0200

    Fixed ImageEditing tests to run on emulator and device again and added Ticket 5.67 code

--- catroid/src/at/tugraz/ist/catroid/Consts.java
+++ catroid/src/at/tugraz/ist/catroid/Consts.java
@@ -45,6 +45,7 @@ public final class Consts {
     public static final int THUMBNAIL_HEIGHT = 80;
     public static final int MAX_COSTUME_WIDTH = 1000;
     public static final int MAX_COSTUME_HEIGHT = 1000;
+    public static final int JPG_COMPRESSION_SETING = 95;
     
     
 }
