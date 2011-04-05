f429ca6029388731268ff9d37d6740716dccc363
Anton Rieder <aried3r@gmail.com>
2011-04-05 16:48:36 +0200

    Added License Text.

--- catroidUiTest/src/at/tugraz/ist/catroid/uitest/construction_site/AddBrickDialogTest.java
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/construction_site/AddBrickDialogTest.java
@@ -1,3 +1,22 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010  Catroid development team
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package at.tugraz.ist.catroid.uitest.construction_site;
 
 import java.io.File;
@@ -17,89 +36,89 @@ import at.tugraz.ist.catroid.utils.UtilFile;
 import com.jayway.android.robotium.solo.Solo;
 
 public class AddBrickDialogTest extends ActivityInstrumentationTestCase2<MainMenuActivity> {
-    private Solo solo;
-    private String testProject = "testProject";
+	private Solo solo;
+	private String testProject = "testProject";
 
-    public AddBrickDialogTest() {
-        super("at.tugraz.ist.catroid.ui", MainMenuActivity.class);
-    }
+	public AddBrickDialogTest() {
+		super("at.tugraz.ist.catroid.ui", MainMenuActivity.class);
+	}
 
-    @Override
-    public void setUp() throws Exception {
-        solo = new Solo(getInstrumentation(), getActivity());
-        createTestProject(testProject);
-    }
+	@Override
+	public void setUp() throws Exception {
+		solo = new Solo(getInstrumentation(), getActivity());
+		createTestProject(testProject);
+	}
 
-    @Override
-    public void tearDown() throws Exception {
-        try {
-            solo.finalize();
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
-        getActivity().finish();
+	@Override
+	public void tearDown() throws Exception {
+		try {
+			solo.finalize();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		getActivity().finish();
 
-        File directory = new File("/sdcard/catroid/" + testProject);
-        UtilFile.deleteDirectory(directory);
+		File directory = new File("/sdcard/catroid/" + testProject);
+		UtilFile.deleteDirectory(directory);
 
-        super.tearDown();
-    }
+		super.tearDown();
+	}
 
-    public void testAddBrickDialog() throws NameNotFoundException, IOException {
+	public void testAddBrickDialog() throws NameNotFoundException, IOException {
 
-        solo.clickOnButton(getActivity().getString(R.string.load_project));
+		solo.clickOnButton(getActivity().getString(R.string.load_project));
 
-        solo.clickOnText(testProject);
-        solo.clickInList(2);
+		solo.clickOnText(testProject);
+		solo.clickInList(2);
 
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2)); //Substring is needed because of the "+" sign in the button name.
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.play_sound_main_adapter));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2)); //Substring is needed because of the "+" sign in the button name.
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.play_sound_main_adapter));
 
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.wait_main_adapter));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.wait_main_adapter));
 
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.hide_main_adapter));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.hide_main_adapter));
 
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.show_main_adapter));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.show_main_adapter));
 
 		solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
 		solo.clickOnText(solo.getCurrentActivity().getString(R.string.goto_main_adapter));
-		
+
 		solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.set_x_main_adapter));
-        
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.set_y_main_adapter));
-        
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
-        solo.scrollDownList(1);
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.change_x_main_adapter));
-        
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.change_y_main_adapter));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.set_x_main_adapter));
 
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
-        solo.scrollDownList(1);
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.go_back_main_adapter));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.set_y_main_adapter));
 
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
-        solo.scrollDownList(1);
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.come_to_front_main_adapter));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
+		solo.scrollDownList(1);
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.change_x_main_adapter));
 
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.touched_main_adapter));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.change_y_main_adapter));
 
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.costume_main_adapter));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
+		solo.scrollDownList(1);
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.go_back_main_adapter));
 
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
-        solo.clickOnText(solo.getCurrentActivity().getString(R.string.scaleCustome));
-        
-        
-        
-        assertNotNull(solo.getString(R.string.play_sound_main_adapter));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
+		solo.scrollDownList(1);
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.come_to_front_main_adapter));
+
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.touched_main_adapter));
+
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.costume_main_adapter));
+
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.add_new_brick).substring(2));
+		solo.clickOnText(solo.getCurrentActivity().getString(R.string.scaleCustome));
+
+
+
+		assertNotNull(solo.getString(R.string.play_sound_main_adapter));
 		assertNotNull(solo.getString(R.string.wait_main_adapter));
 		assertNotNull(solo.getString(R.string.hide_main_adapter));
 		assertNotNull(solo.getString(R.string.show_main_adapter));
@@ -114,26 +133,26 @@ public class AddBrickDialogTest extends ActivityInstrumentationTestCase2<MainMen
 		assertNotNull(solo.getString(R.string.change_x_main_adapter));
 		assertNotNull(solo.getString(R.string.change_y_main_adapter));
 
-    }
+	}
 
 
-    private void createTestProject(String projectName) throws IOException {
+	private void createTestProject(String projectName) throws IOException {
 
-        StorageHandler storageHandler = StorageHandler.getInstance();
+		StorageHandler storageHandler = StorageHandler.getInstance();
 
-        Project project = new Project(getActivity(), projectName);
-        Sprite firstSprite = new Sprite("cat");
+		Project project = new Project(getActivity(), projectName);
+		Sprite firstSprite = new Sprite("cat");
 
-        Script testScript = new Script("ScriptTest", firstSprite);
+		Script testScript = new Script("ScriptTest", firstSprite);
 
-        firstSprite.getScriptList().add(testScript);
-        project.addSprite(firstSprite);
+		firstSprite.getScriptList().add(testScript);
+		project.addSprite(firstSprite);
 
-        ProjectManager.getInstance().setProject(project);
-        ProjectManager.getInstance().setCurrentSprite(firstSprite);
-        ProjectManager.getInstance().setCurrentScript(testScript);
+		ProjectManager.getInstance().setProject(project);
+		ProjectManager.getInstance().setCurrentSprite(firstSprite);
+		ProjectManager.getInstance().setCurrentScript(testScript);
 
-        storageHandler.saveProject(project);
-    }
+		storageHandler.saveProject(project);
+	}
 
 }
--- catroidUiTest/src/at/tugraz/ist/catroid/uitest/construction_site/script_adapter/SoundBrickTest.java
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/construction_site/script_adapter/SoundBrickTest.java
@@ -1,3 +1,21 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010  Catroid development team
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package at.tugraz.ist.catroid.uitest.construction_site.script_adapter;
 
 import java.io.IOException;
@@ -35,48 +53,48 @@ public class SoundBrickTest extends ActivityInstrumentationTestCase2<ScriptActiv
 		super("at.tugraz.ist.catroid",
 				ScriptActivity.class);
 	}
-	
+
 	@Override
-    public void setUp() throws Exception {
+	public void setUp() throws Exception {
 		createProject();
 		solo = new Solo(getInstrumentation(), getActivity());
 	}
-	
+
 	@Override
-    public void tearDown() throws Exception {	
-		try {	
+	public void tearDown() throws Exception {
+		try {
 			solo.finalize();
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
-		
+
 		getActivity().finish();
 		super.tearDown();
 	}
-	
+
 	@Smoke
 	public void testPlaySoundBrick() throws Throwable {
 		int childrenCount = getActivity().getAdapter().getChildCountFromLastGroup();
 		int groupCount = getActivity().getAdapter().getGroupCount();
 		assertEquals("Incorrect number of bricks.", 2, solo.getCurrentListViews().get(0).getChildCount());
 		assertEquals("Incorrect number of bricks.", 1, childrenCount);
-		
+
 		ArrayList<Brick> projectBrickList = project.getSpriteList().get(0).getScriptList().get(0).getBrickList();
 		assertEquals("Incorrect number of bricks.", 1, projectBrickList.size());
-		
+
 		assertEquals("Wrong Brick instance.", projectBrickList.get(0), getActivity().getAdapter().getChild(groupCount-1, 0));
 		assertNotNull("TextView does not exist.", solo.getText(getActivity().getString(R.string.play_sound_main_adapter)));
-		
+
 		assertTrue("Wrong title selected", solo.searchText(selectedTitle));
 		assertTrue("Wrong title selected", solo.searchText(selectedTitle));
-		
+
 		solo.clickOnButton(selectedTitle);
 		solo.clickInList(2);
 		Thread.sleep(500);
 		assertTrue("Wrong title selected", solo.searchText(title));
-		
+
 	}
-	
+
 	private void createProject() throws IOException {
 		title = "myTitle";
 		path = "path/to/sound/";
@@ -98,19 +116,19 @@ public class SoundBrickTest extends ActivityInstrumentationTestCase2<ScriptActiv
 		soundInfo.setPath(path);
 		soundlist.add(soundInfo);
 		StorageHandler.getInstance().setSoundContent(soundlist);
-		
+
 		project = new Project(null, "testProject");
-        Sprite sprite = new Sprite("cat");
-        Script script = new Script("script", sprite);
-        soundBrick = new PlaySoundBrick(sprite, soundInfo.getTitleWithPath());
-        script.addBrick(soundBrick);
-        
-        sprite.getScriptList().add(script);
-        project.addSprite(sprite);
-        
-        ProjectManager.getInstance().setProject(project);
-        ProjectManager.getInstance().setCurrentSprite(sprite);
-        ProjectManager.getInstance().setCurrentScript(script);
+		Sprite sprite = new Sprite("cat");
+		Script script = new Script("script", sprite);
+		soundBrick = new PlaySoundBrick(sprite, soundInfo.getTitleWithPath());
+		script.addBrick(soundBrick);
+
+		sprite.getScriptList().add(script);
+		project.addSprite(sprite);
+
+		ProjectManager.getInstance().setProject(project);
+		ProjectManager.getInstance().setCurrentSprite(sprite);
+		ProjectManager.getInstance().setCurrentScript(script);
 	}
-	
+
 }
