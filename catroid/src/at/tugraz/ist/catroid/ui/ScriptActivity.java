6b2f87fb643a0aef42329e7c55667eedf801c2a0
David Reisenberger <threesn.b@gmail.com>
2011-04-05 17:30:13 +0200

    Changed "delete" to resource string

--- catroid/src/at/tugraz/ist/catroid/ui/ScriptActivity.java
+++ catroid/src/at/tugraz/ist/catroid/ui/ScriptActivity.java
@@ -1,6 +1,6 @@
 /**
  *  Catroid: An on-device graphical programming language for Android devices
- *  Copyright (C) 2010  Catroid development team
+ *  Copyright (C) 2010  Catroid development team 
  *  (<http://code.google.com/p/catroid/wiki/Credits>)
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -53,178 +53,177 @@ import at.tugraz.ist.catroid.ui.dialogs.AddBrickDialog;
 import at.tugraz.ist.catroid.ui.dragndrop.DragNDropListView;
 
 public class ScriptActivity extends Activity implements OnDismissListener, OnCancelListener {
-	private BrickAdapter adapter;
-	private DragNDropListView listView;
-	private Sprite sprite;
-	private Script scriptToEdit;
+    private BrickAdapter adapter;
+    private DragNDropListView listView;
+    private Sprite sprite;
+    private Script scriptToEdit;
 
 
-	private void initListeners() {
-		sprite = ProjectManager.getInstance().getCurrentSprite();
-		listView = (DragNDropListView) findViewById(R.id.brickListView);
-		adapter = new BrickAdapter(this, ProjectManager.getInstance().getCurrentSprite(), listView);
-		if(adapter.getGroupCount() > 0)
-			ProjectManager.getInstance().setCurrentScript(adapter.getGroup(adapter.getGroupCount()-1));
+    private void initListeners() {
+        sprite = ProjectManager.getInstance().getCurrentSprite();
+        listView = (DragNDropListView) findViewById(R.id.brickListView);
+        adapter = new BrickAdapter(this, ProjectManager.getInstance().getCurrentSprite(), listView);
+        if(adapter.getGroupCount() > 0)
+            ProjectManager.getInstance().setCurrentScript(adapter.getGroup(adapter.getGroupCount()-1));
+        
+        listView.setTrashView((ImageView)findViewById(R.id.trash));
+        listView.setOnCreateContextMenuListener(this);
+        listView.setOnDropListener(adapter);
+        listView.setOnRemoveListener(adapter);
+        listView.setAdapter(adapter);
+        listView.setTranscriptMode(ListView.TRANSCRIPT_MODE_NORMAL);
+        listView.setGroupIndicator(null);
+        listView.setOnGroupClickListener(adapter);
+        registerForContextMenu(listView);
+    
 
-		listView.setTrashView((ImageView)findViewById(R.id.trash));
-		listView.setOnCreateContextMenuListener(this);
-		listView.setOnDropListener(adapter);
-		listView.setOnRemoveListener(adapter);
-		listView.setAdapter(adapter);
-		listView.setTranscriptMode(ListView.TRANSCRIPT_MODE_NORMAL);
-		listView.setGroupIndicator(null);
-		listView.setOnGroupClickListener(adapter);
-		registerForContextMenu(listView);
+        Button mainMenuButton = (Button) findViewById(R.id.mainMenuButton);
+        mainMenuButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(ScriptActivity.this, MainMenuActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
 
+        Button addBrickButton = (Button) findViewById(R.id.addBrickButton);
+        addBrickButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showDialog(Consts.DIALOG_ADD_BRICK);
+            }
+        });
 
-		Button mainMenuButton = (Button) findViewById(R.id.mainMenuButton);
-		mainMenuButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				Intent intent = new Intent(ScriptActivity.this, MainMenuActivity.class);
-				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-				startActivity(intent);
-			}
-		});
+    }
 
-		Button addBrickButton = (Button) findViewById(R.id.addBrickButton);
-		addBrickButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				showDialog(Consts.DIALOG_ADD_BRICK);
-			}
-		});
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.script_activity);
+        initListeners();
+        if(adapter.getGroupCount()>0)
+            listView.expandGroup(adapter.getGroupCount()-1);
+    }
 
-	}
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        Dialog dialog;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.script_activity);
-		initListeners();
-		if(adapter.getGroupCount()>0)
-			listView.expandGroup(adapter.getGroupCount()-1);
-	}
+        switch (id) {
+        case Consts.DIALOG_ADD_BRICK:
+            dialog = new AddBrickDialog(this, sprite);
+            dialog.setOnDismissListener(this);
+            break;
+        default:
+            dialog = null;
+            break;
+        }
+        return dialog;
+    }
 
-	@Override
-	protected Dialog onCreateDialog(int id) {
-		Dialog dialog;
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ProjectManager projectManager = ProjectManager.getInstance();
+        if (projectManager.getCurrentProject() != null) {
+            projectManager.saveProject(this);
+        }
+    }
+    
+    public void onDismiss(DialogInterface dialog) {
+        System.out.println("DialogDismiss");
+        for(int i=0;i<adapter.getGroupCount()-1;++i)
+            listView.collapseGroup(i);
+        
+        adapter.notifyDataSetChanged();
+        if(adapter.getGroupCount() > 0)
+            listView.expandGroup(adapter.getGroupCount()-1);
+    }
+    
+    public void onCancel(DialogInterface arg0) {
+        adapter.notifyDataSetChanged();
+        
+    }
+	
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-		switch (id) {
-		case Consts.DIALOG_ADD_BRICK:
-			dialog = new AddBrickDialog(this, sprite);
-			dialog.setOnDismissListener(this);
-			break;
-		default:
-			dialog = null;
-			break;
-		}
-		return dialog;
-	}
+        /*
+         * This is used for getting the results of the gallery intent when the
+         * user selected an image. requestCode holds the ID / position of the
+         * brick that issued the request. If and when we have different kinds of
+         * intents we need to find a better way.
+         */
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		ProjectManager projectManager = ProjectManager.getInstance();
-		if (projectManager.getCurrentProject() != null) {
-			projectManager.saveProject(this);
-		}
-	}
+        if (resultCode == RESULT_OK) {
+            SetCostumeBrick affectedBrick = (SetCostumeBrick) adapter.getChild(adapter.getGroupCount()-1, requestCode);
+            if (affectedBrick != null) {
+                Uri selectedImageUri = data.getData();
+                String selectedImagePath = getPathFromContentUri(selectedImageUri);
+                try {
+                    File outputFile = StorageHandler.getInstance().copyImage(ProjectManager.getInstance().getCurrentProject().getName(),
+                            selectedImagePath);
+                    if (outputFile != null) {
+                        affectedBrick.setCostume(outputFile.getAbsolutePath());
+                        adapter.notifyDataSetChanged();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-	public void onDismiss(DialogInterface dialog) {
-		System.out.println("DialogDismiss");
-		for(int i=0;i<adapter.getGroupCount()-1;++i)
-			listView.collapseGroup(i);
+            }
+        }
 
-		adapter.notifyDataSetChanged();
-		if(adapter.getGroupCount() > 0)
-			listView.expandGroup(adapter.getGroupCount()-1);
-	}
+    }
 
-	public void onCancel(DialogInterface arg0) {
-		adapter.notifyDataSetChanged();
+    public String getPathFromContentUri(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(columnIndex);
+    }
 
-	}
+    public BrickAdapter getAdapter() {
+        return adapter;
+    }
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
+ 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenuInfo menuInfo) {
+        if (view.getId() == R.id.brickListView) {
+            ExpandableListView.ExpandableListContextMenuInfo info = (ExpandableListView.ExpandableListContextMenuInfo) menuInfo;
+            menu.setHeaderTitle("Script Menu");
+            
+            if (ExpandableListView.getPackedPositionType(info.packedPosition) == ExpandableListView.PACKED_POSITION_TYPE_CHILD)
+                return;
+            
+            int position = ExpandableListView.getPackedPositionGroup(info.packedPosition);
+            scriptToEdit = adapter.getGroup(position);
+            
+            menu.add(Menu.NONE, 0, 0, this.getString(R.string.delete_script_button));
+        }
+    }
 
-		/*
-		 * This is used for getting the results of the gallery intent when the
-		 * user selected an image. requestCode holds the ID / position of the
-		 * brick that issued the request. If and when we have different kinds of
-		 * intents we need to find a better way.
-		 */
-
-		if (resultCode == RESULT_OK) {
-			SetCostumeBrick affectedBrick = (SetCostumeBrick) adapter.getChild(adapter.getGroupCount()-1, requestCode);
-			if (affectedBrick != null) {
-				Uri selectedImageUri = data.getData();
-				String selectedImagePath = getPathFromContentUri(selectedImageUri);
-				try {
-					File outputFile = StorageHandler.getInstance().copyImage(ProjectManager.getInstance().getCurrentProject().getName(),
-							selectedImagePath);
-					if (outputFile != null) {
-						affectedBrick.setCostume(outputFile.getAbsolutePath());
-						adapter.notifyDataSetChanged();
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-			}
-		}
-
-	}
-
-	public String getPathFromContentUri(Uri uri) {
-		String[] projection = { MediaStore.Images.Media.DATA };
-		Cursor cursor = managedQuery(uri, projection, null, null, null);
-		int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-		cursor.moveToFirst();
-		return cursor.getString(columnIndex);
-	}
-
-	public BrickAdapter getAdapter() {
-		return adapter;
-	}
-
-
-	@Override
-	public void onCreateContextMenu(ContextMenu menu, View view, ContextMenuInfo menuInfo) {
-		if (view.getId() == R.id.brickListView) {
-			ExpandableListView.ExpandableListContextMenuInfo info = (ExpandableListView.ExpandableListContextMenuInfo) menuInfo;
-			menu.setHeaderTitle("Script Menu");
-
-			if (ExpandableListView.getPackedPositionType(info.packedPosition) == ExpandableListView.PACKED_POSITION_TYPE_CHILD)
-				return;
-
-			int position = ExpandableListView.getPackedPositionGroup(info.packedPosition);
-			scriptToEdit = adapter.getGroup(position);
-
-			menu.add(Menu.NONE, 0, 0, this.getString(R.string.delete_script_button));
-		}
-	}
-
-	@Override
-	public boolean onContextItemSelected(MenuItem item) {
-		int menuItemIndex = item.getItemId();
-		switch (menuItemIndex) {
-		case 0: // delete
-			sprite.getScriptList().remove(scriptToEdit);
-			if (sprite.getScriptList().isEmpty()) {
-				ProjectManager.getInstance().setCurrentScript(null);
-				adapter.notifyDataSetChanged();
-				return false;
-			}
-			int lastScriptIndex = sprite.getScriptList().size() - 1;
-			Script lastScript = sprite.getScriptList().get(lastScriptIndex);
-			ProjectManager.getInstance().setCurrentScript(lastScript);
-			adapter.notifyDataSetChanged();
-			listView.expandGroup(adapter.getGroupCount() - 1);
-		}
-		return true;
-	}
-
-
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        int menuItemIndex = item.getItemId();
+        switch (menuItemIndex) {
+        case 0: // delete
+           sprite.getScriptList().remove(scriptToEdit);
+           if (sprite.getScriptList().isEmpty()) {
+               ProjectManager.getInstance().setCurrentScript(null);
+               adapter.notifyDataSetChanged();
+               return false;
+           }
+           int lastScriptIndex = sprite.getScriptList().size() - 1;
+           Script lastScript = sprite.getScriptList().get(lastScriptIndex);
+           ProjectManager.getInstance().setCurrentScript(lastScript);
+           adapter.notifyDataSetChanged();
+           listView.expandGroup(adapter.getGroupCount() - 1);
+        }
+        return true;
+    }
+    
+    
 
 }
