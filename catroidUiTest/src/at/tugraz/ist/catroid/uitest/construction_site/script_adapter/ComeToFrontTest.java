43f69aa70145f9b4c97b913058ea50639bf5692b
Ainul Husna <ainulhusna@gmail.com>
2011-04-06 11:53:32 +0200

    Apply new coding style for ComeToFrontTest

--- catroidUiTest/src/at/tugraz/ist/catroid/uitest/construction_site/script_adapter/ComeToFrontTest.java
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/construction_site/script_adapter/ComeToFrontTest.java
@@ -76,7 +76,8 @@ public class ComeToFrontTest extends ActivityInstrumentationTestCase2<ScriptActi
 		ArrayList<Brick> projectBrickList = project.getSpriteList().get(0).getScriptList().get(0).getBrickList();
 		assertEquals("Incorrect number of bricks.", 1, projectBrickList.size());
 		
-		assertEquals("Wrong Brick instance.", projectBrickList.get(0), getActivity().getAdapter().getChild(groupCount-1, 0));
+		assertEquals("Wrong Brick instance.", projectBrickList.get(0), getActivity().getAdapter().getChild(groupCount-1,
+				      0));
 		assertNotNull("TextView does not exist.", solo.getText(getActivity().getString(R.string.come_to_front_main_adapter)));
 	}
 	
