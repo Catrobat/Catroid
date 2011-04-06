980dbe98eb14b05234b9f5ac33e470b24781313b
Ainul Husna <ainulhusna@gmail.com>
2011-04-06 13:22:25 +0200

    Robotium Test for SetYBrick

---
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/construction_site/script_adapter/SetYBrickTest.java
@@ -0,0 +1,87 @@
+package at.tugraz.ist.catroid.uitest.construction_site.script_adapter;
+
+import java.util.ArrayList;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.Smoke;
+import at.tugraz.ist.catroid.R;
+import at.tugraz.ist.catroid.constructionSite.content.ProjectManager;
+import at.tugraz.ist.catroid.content.brick.Brick;
+import at.tugraz.ist.catroid.content.brick.SetYBrick;
+import at.tugraz.ist.catroid.content.project.Project;
+import at.tugraz.ist.catroid.content.script.Script;
+import at.tugraz.ist.catroid.content.sprite.Sprite;
+import at.tugraz.ist.catroid.ui.ScriptActivity;
+
+import com.jayway.android.robotium.solo.Solo;
+
+public class SetYBrickTest extends ActivityInstrumentationTestCase2<ScriptActivity>{
+	private Solo solo;
+	private Project project;
+	private SetYBrick setYBrick;
+	private int setY;
+
+	public SetYBrickTest() {
+		super("at.tugraz.ist.catroid",ScriptActivity.class);
+	}
+	
+	@Override
+    public void setUp() throws Exception {
+		createProject();
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+	
+	@Override
+    public void tearDown() throws Exception {	
+		try {	
+			solo.finalize();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		
+		getActivity().finish();
+		super.tearDown();
+	}
+	
+	@Smoke
+	public void testChangeXByBrick() throws Throwable {
+		int childrenCount = getActivity().getAdapter().getChildCountFromLastGroup();
+		int groupCount = getActivity().getAdapter().getGroupCount();
+
+		assertEquals("Incorrect number of bricks.", 2, solo.getCurrentListViews().get(0).getChildCount());
+		assertEquals("Incorrect number of bricks.", 1, childrenCount);
+		
+		ArrayList<Brick> projectBrickList = project.getSpriteList().get(0).getScriptList().get(0).getBrickList();
+		assertEquals("Incorrect number of bricks.", 1, projectBrickList.size());
+		
+		assertEquals("Wrong Brick instance.", projectBrickList.get(0), getActivity().getAdapter().getChild(groupCount-1,
+				      0));
+		assertNotNull("TextView does not exist.", solo.getText(getActivity().getString(R.string.set_y_main_adapter)));
+		
+		solo.clickOnEditText(0);
+		solo.clearEditText(0);
+		solo.enterText(0, setY + "");
+		solo.clickOnButton(0);
+		
+		Thread.sleep(300);
+		assertEquals("Wrong text in field.", setY, setYBrick.getYPosition());
+		assertEquals("Value in Brick is not updated.", setY +"", solo.getEditText(0).getText().toString());
+	}
+	
+	private void createProject() {
+		setY = 17;
+		project = new Project(null, "testProject");
+        Sprite sprite = new Sprite("cat");
+        Script script = new Script("script", sprite);
+        setYBrick = new SetYBrick(sprite, 0);
+        script.addBrick(setYBrick);
+
+        sprite.getScriptList().add(script);
+        project.addSprite(sprite);
+        
+        ProjectManager.getInstance().setProject(project);
+        ProjectManager.getInstance().setCurrentSprite(sprite);
+        ProjectManager.getInstance().setCurrentScript(script);
+	}
+	
+}
