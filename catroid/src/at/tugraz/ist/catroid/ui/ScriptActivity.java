227d1b7971f74336c4f6e205cc16f9da5f7a5419
Ainul Husna <ainulhusna@gmail.com>
2011-04-07 13:37:42 +0200

    Add toStageButton code

--- catroid/src/at/tugraz/ist/catroid/ui/ScriptActivity.java
+++ catroid/src/at/tugraz/ist/catroid/ui/ScriptActivity.java
@@ -25,18 +25,18 @@ import java.io.IOException;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.DialogInterface.OnCancelListener;
 import android.content.DialogInterface.OnDismissListener;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
@@ -48,6 +48,7 @@ import at.tugraz.ist.catroid.content.brick.SetCostumeBrick;
 import at.tugraz.ist.catroid.content.script.Script;
 import at.tugraz.ist.catroid.content.sprite.Sprite;
 import at.tugraz.ist.catroid.io.StorageHandler;
+import at.tugraz.ist.catroid.stage.StageActivity;
 import at.tugraz.ist.catroid.ui.adapter.BrickAdapter;
 import at.tugraz.ist.catroid.ui.dialogs.AddBrickDialog;
 import at.tugraz.ist.catroid.ui.dragndrop.DragNDropListView;
@@ -81,6 +82,14 @@ public class ScriptActivity extends Activity implements OnDismissListener, OnCan
 			public void onClick(View v) {
 				Intent intent = new Intent(ScriptActivity.this, MainMenuActivity.class);
 				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(intent);
+			}
+		});
+
+		Button toStageButton = (Button) findViewById(R.id.toStageButton);
+		toStageButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v){
+				Intent intent = new Intent(ScriptActivity.this, StageActivity.class);
 				startActivity(intent);
 			}
 		});
