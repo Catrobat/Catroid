83f5946f0aff23b137c527337d53fedd2095da01
Florian Hubner <FlorianHubner88@gmail.com>
2011-04-26 13:07:26 +0200

    Bug 5.19c scaling test cases added

--- catroidTest/src/at/tugraz/ist/catroid/test/content/brick/ScaleCostumeBrickTest.java
+++ catroidTest/src/at/tugraz/ist/catroid/test/content/brick/ScaleCostumeBrickTest.java
@@ -1,6 +1,6 @@
 /**
  *  Catroid: An on-device graphical programming language for Android devices
- *  Copyright (C) 2010  Catroid development team 
+ *  Copyright (C) 2010  Catroid development team
  *  (<http://code.google.com/p/catroid/wiki/Credits>)
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -18,17 +18,61 @@
  */
 package at.tugraz.ist.catroid.test.content.brick;
 
-import android.test.AndroidTestCase;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.graphics.BitmapFactory;
+import android.test.InstrumentationTestCase;
+import at.tugraz.ist.catroid.Values;
 import at.tugraz.ist.catroid.content.Sprite;
 import at.tugraz.ist.catroid.content.bricks.ScaleCostumeBrick;
+import at.tugraz.ist.catroid.test.R;
 
-public class ScaleCostumeBrickTest extends AndroidTestCase {
-   
+public class ScaleCostumeBrickTest extends InstrumentationTestCase {
+
 	private double scale = 70;
+	private final double scaleToBig = 1000000.;
+	private final double scaleToSmall = .00001;
+
+	private static final int IMAGE_FILE_ID = R.raw.icon;
+	private File testImage;
+	int width;
+	int height;
+
+	@Override
+	protected void setUp() throws Exception {
+		final int fileSize = 4147;
+		final String imagePath = "/mnt/sdcard/catroid/testImage.png";
+		testImage = new File(imagePath);
+		if (!testImage.exists()) {
+			testImage.createNewFile();
+		}
+		InputStream in   = getInstrumentation().getContext().getResources().openRawResource(IMAGE_FILE_ID);
+		OutputStream out = new BufferedOutputStream(new FileOutputStream(testImage), fileSize);
+		byte[] buffer = new byte[fileSize];
+		int length = 0;
+		while ((length = in.read(buffer)) > 0) {
+			out.write(buffer, 0, length);
+		}
+
+		in.close();
+		out.flush();
+		out.close();
+
+		BitmapFactory.Options o = new BitmapFactory.Options();
+		o.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(imagePath, o);
+
+		width = o.outWidth;
+		height = o.outHeight;
+	}
 
 	public void testScale() {
 		Sprite sprite = new Sprite("testSprite");
-        assertEquals("Unexpected initial sprite scale value", 100.0, sprite.getScale());
+		assertEquals("Unexpected initial sprite scale value", 100.0, sprite.getScale());
 
 		ScaleCostumeBrick brick = new ScaleCostumeBrick(sprite, scale);
 		brick.execute();
@@ -38,11 +82,11 @@ public class ScaleCostumeBrickTest extends AndroidTestCase {
 
 	public void testNullSprite() {
 		ScaleCostumeBrick brick = new ScaleCostumeBrick(null, scale);
-		
+
 		try {
 			brick.execute();
 			fail("Execution of ScaleCostumeBrick with null Sprite did not cause " +
-					"a NullPointerException to be thrown");
+			"a NullPointerException to be thrown");
 		} catch (NullPointerException e) {
 			// expected behavior
 		}
@@ -70,7 +114,7 @@ public class ScaleCostumeBrickTest extends AndroidTestCase {
 		try {
 			brick.execute();
 			fail("Execution of ScaleCostumeBrick with 0.0 scale did not cause a " +
-					"IllegalArgumentException to be thrown.");
+			"IllegalArgumentException to be thrown.");
 		} catch (IllegalArgumentException e) {
 			// expected behavior
 		}
@@ -83,10 +127,46 @@ public class ScaleCostumeBrickTest extends AndroidTestCase {
 		try {
 			brick.execute();
 			fail("Execution of ScaleCostumeBrick with negative scale did not cause" +
-					" a IllegalArgumentException to be thrown.");
+			" a IllegalArgumentException to be thrown.");
 		} catch (IllegalArgumentException e) {
 			// expected behavior
 		}
+	}
+
+	public void testCostumeToBig() {
+		Values.SCREEN_HEIGHT = 800;
+		Values.SCREEN_WIDTH = 480;
+
+		Sprite sprite = new Sprite("testSprite");
+		sprite.getCostume().setImagePath(testImage.getAbsolutePath());
+
+		ScaleCostumeBrick brick = new ScaleCostumeBrick(sprite, scaleToBig);
+
+		brick.execute();
+
+		int newWidth = sprite.getCostume().getImageWidthHeight().first;
+		int newHeight= sprite.getCostume().getImageWidthHeight().second;
+
+		assertTrue("ScaleCostumeBrick width of scaled image to big", newWidth <= 1000);
+		assertTrue("ScaleCostumeBrick height of scaled image to big", newHeight <= 1000);
+	}
+
+	public void testCostumeToSmall() {
+		Values.SCREEN_HEIGHT = 800;
+		Values.SCREEN_WIDTH = 480;
+
+		Sprite sprite = new Sprite("testSprite");
+		sprite.getCostume().setImagePath(testImage.getAbsolutePath());
+
+		ScaleCostumeBrick brick = new ScaleCostumeBrick(sprite, scaleToSmall);
+
+		brick.execute();
+
+		int newWidth = sprite.getCostume().getImageWidthHeight().first;
+		int newHeight= sprite.getCostume().getImageWidthHeight().second;
+
+		assertTrue("ScaleCostumeBrick width of scaled image to small", newWidth > 0);
+		assertTrue("ScaleCostumeBrick height of scaled image to small", newHeight > 0);
 	}
 
 }
