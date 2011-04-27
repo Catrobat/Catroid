df02b0544676e77402989d4bdd3dc17cb49ae8a4
Jakob Strauss <oktolon@gmail.com>
2011-04-27 13:30:35 +0200

    SetCostumeBrickTest get image fixed

--- catroidTest/src/at/tugraz/ist/catroid/test/content/brick/SetCostumeBrickTest.java
+++ catroidTest/src/at/tugraz/ist/catroid/test/content/brick/SetCostumeBrickTest.java
@@ -27,6 +27,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.test.InstrumentationTestCase;
 import at.tugraz.ist.catroid.Values;
 import at.tugraz.ist.catroid.content.Sprite;
@@ -34,67 +35,71 @@ import at.tugraz.ist.catroid.content.bricks.SetCostumeBrick;
 import at.tugraz.ist.catroid.test.R;
 
 public class SetCostumeBrickTest extends InstrumentationTestCase {
-    
-    private static final int IMAGE_FILE_ID = R.raw.icon;
-    private File testImage;
-    int width;
-    int height;
 
-    @Override
-    protected void setUp() throws Exception {
-        final int fileSize = 4147;
-        final String imagePath = "/mnt/sdcard/catroid/testImage.png"; 
-        testImage = new File(imagePath);
-        if (!testImage.exists()) {
-            testImage.createNewFile();
-        }
-        InputStream in   = getInstrumentation().getContext().getResources().openRawResource(IMAGE_FILE_ID);
-        OutputStream out = new BufferedOutputStream(new FileOutputStream(testImage), fileSize);
-        byte[] buffer = new byte[fileSize];
-        int length = 0;
-        while ((length = in.read(buffer)) > 0) {
-            out.write(buffer, 0, length);
-        }
-        
-        in.close();
-        out.flush();
-        out.close();
-        
-        BitmapFactory.Options o = new BitmapFactory.Options();
-        o.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(imagePath, o);
+	private static final int IMAGE_FILE_ID = R.raw.icon;
+	private File testImage;
+	int width;
+	int height;
 
-        width = o.outWidth;
-        height = o.outHeight;
-    }
-    
-    @Override
-    protected void tearDown() throws Exception {
-        if (testImage != null && testImage.exists()){
-            testImage.delete();
-        }
-    }
-    
-    public void testSetCostume() throws IOException {
-        
-        Values.SCREEN_HEIGHT = 200;
-        Values.SCREEN_WIDTH = 200;
-        
-        Sprite sprite = new Sprite("new sprite");
-        SetCostumeBrick setCostumeBrick = new SetCostumeBrick(sprite);
-        setCostumeBrick.setCostume(testImage.getAbsolutePath());
-        setCostumeBrick.execute();
-        assertNotNull("current Costume is null", sprite.getCostume());
-        
-        assertEquals("the new Costume is not in the costumeList of the sprite", width, 
-        		sprite.getCostume().getBitmap().getWidth());
-        assertEquals("the new Costume is not in the costumeList of the sprite", height, 
-        		sprite.getCostume().getBitmap().getHeight());
-        setCostumeBrick.execute(); //now setting current costume
-        assertEquals("Width of loaded bitmap is not the same as width of original image",   width,  
-        		sprite.getCostume().getBitmap().getWidth());
-        assertEquals("Height of loaded bitmap is not the same as height of original image", height, 
-        		sprite.getCostume().getBitmap().getHeight());
-    }
+	@Override
+	protected void setUp() throws Exception {
+
+		File sdCard = Environment.getExternalStorageDirectory();
+		testImage = new File(sdCard.getAbsolutePath() + "/catroid/testImage.png");
+
+		if (!testImage.exists()) {
+			testImage.createNewFile();
+		}
+
+		InputStream in = getInstrumentation().getContext().getResources().openRawResource(IMAGE_FILE_ID);
+		OutputStream out = new BufferedOutputStream(new FileOutputStream(testImage));
+
+		byte[] buffer = new byte[1024];
+		int length = 0;
+		while ((length = in.read(buffer)) > 0) {
+			out.write(buffer, 0, length);
+		}
+
+		in.close();
+		out.flush();
+		out.close();
+
+		BitmapFactory.Options o = new BitmapFactory.Options();
+		o.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(sdCard.getAbsolutePath() + "/catroid/testImage.png", o);
+
+		width = o.outWidth;
+		height = o.outHeight;
+
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		if (testImage != null && testImage.exists()) {
+			testImage.delete();
+		}
+	}
+
+	public void testSetCostume() throws IOException {
+
+		Values.SCREEN_HEIGHT = 200;
+		Values.SCREEN_WIDTH = 200;
+
+		Sprite sprite = new Sprite("new sprite");
+		SetCostumeBrick setCostumeBrick = new SetCostumeBrick(sprite);
+		setCostumeBrick.setCostume(testImage.getAbsolutePath());
+		setCostumeBrick.execute();
+		assertNotNull("current Costume is null", sprite.getCostume());
+
+		assertEquals("the new Costume is not in the costumeList of the sprite", width,
+				sprite.getCostume().getBitmap().getWidth());
+		assertEquals("the new Costume is not in the costumeList of the sprite", height,
+				sprite.getCostume().getBitmap().getHeight());
+		setCostumeBrick.execute(); //now setting current costume
+		assertEquals("Width of loaded bitmap is not the same as width of original image", width,
+				sprite.getCostume().getBitmap().getWidth());
+		assertEquals("Height of loaded bitmap is not the same as height of original image", height,
+				sprite.getCostume().getBitmap().getHeight());
+	}
 
 }
