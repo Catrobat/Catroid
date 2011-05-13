ff8317d645ef8dae4cc1a4b6530e309a40a5cbd9
Daniel Markart <daniel.markart@gmail.com>
2011-05-14 01:29:08 +0200

    trying out flakytests in stageTest

--- catroidUiTest/src/at/tugraz/ist/catroid/uitest/stage/StageTest.java
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/stage/StageTest.java
@@ -26,6 +26,9 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.FlakyTest;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.util.Log;
 import at.tugraz.ist.catroid.ProjectManager;
 import at.tugraz.ist.catroid.common.Consts;
 import at.tugraz.ist.catroid.common.Values;
@@ -66,6 +69,7 @@ public class StageTest extends ActivityInstrumentationTestCase2<MainMenuActivity
 	private int image2Width;
 	private int image1Height;
 	private int image2Height;
+	public static int flakyCount = 0;
 
 	private static final int IMAGE_FILE_ID = R.raw.icon;
 	private static final int IMAGE_FILE_ID2 = R.raw.icon2;
@@ -295,14 +299,20 @@ public class StageTest extends ActivityInstrumentationTestCase2<MainMenuActivity
 		assertEquals("costume has wrong height", image2Height * 2, costume.getBitmap().getHeight());
 	}
 
+	@LargeTest
+	@FlakyTest(tolerance = 3)
 	public void testMediaPlayerPlaying() {
-
-		MediaPlayer mediaPlayer = SoundManager.getInstance().getMediaPlayer();
+		Utils.clearProject(projectName);
 
 		this.createTestProjectWithSound();
-		solo.clickOnButton(1);
+		MediaPlayer mediaPlayer = SoundManager.getInstance().getMediaPlayer();
+		solo.sleep(800);
+		if (flakyCount == 0) {
+			solo.clickOnButton(1);
+		}
 		solo.clickOnScreen(Values.SCREEN_WIDTH / 2, Values.SCREEN_HEIGHT / 2);
-		solo.sleep(200);
+		solo.sleep(350);
+		Log.e("FlakeyTestCase", "Execution Count:" + ++flakyCount);
 		assertTrue("Media player is not playing", mediaPlayer.isPlaying());
 	}
 
