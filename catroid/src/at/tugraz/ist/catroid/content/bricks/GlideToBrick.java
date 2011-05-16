c852709bd531676d410ea63d35903d52e9939933
Alexei Nicoara <lexmiir@gmail.com>
2011-05-16 17:02:53 +0200

    GlideBrick patch imported from Alex. Works like a charm. :)

---
+++ catroid/src/at/tugraz/ist/catroid/content/bricks/GlideToBrick.java
@@ -0,0 +1,153 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010  Catroid development team 
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.tugraz.ist.catroid.content.bricks;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.DialogInterface.OnDismissListener;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.EditText;
+import at.tugraz.ist.catroid.R;
+import at.tugraz.ist.catroid.content.Sprite;
+import at.tugraz.ist.catroid.exception.InterruptedRuntimeException;
+import at.tugraz.ist.catroid.ui.dialogs.EditDoubleDialog;
+import at.tugraz.ist.catroid.ui.dialogs.EditIntegerDialog;
+
+public class GlideToBrick implements Brick, OnDismissListener {
+	private static final long serialVersionUID = 1L;
+	private int xDestination;
+	private int yDestination;
+	private int durationInMilliSeconds;
+	private Sprite sprite;
+
+	public GlideToBrick(Sprite sprite, int xDestination, int yDestination, int durationInMilliSeconds) {
+		this.sprite = sprite;
+		this.xDestination = xDestination;
+		this.yDestination = yDestination;
+		this.durationInMilliSeconds = durationInMilliSeconds;
+	}
+
+	public void execute() {
+		long startTime = System.currentTimeMillis();
+		while (durationInMilliSeconds > 0) {
+			try {
+				Thread.sleep(33);
+
+				long currentTime = System.currentTimeMillis();
+				durationInMilliSeconds -= (int) (currentTime - startTime);
+
+				updatePositions((int) (currentTime - startTime));
+
+				startTime = currentTime;
+				sprite.setToDraw(true);
+			} catch (InterruptedException e) {
+				durationInMilliSeconds -= (int) (System.currentTimeMillis() - startTime);
+				throw new InterruptedRuntimeException("GlideToBrick was interrupted", e);
+			}
+		}
+		sprite.setXYPosition(xDestination, yDestination);
+		sprite.setToDraw(true);
+	}
+
+	private void updatePositions(int timePassed) {
+		int xPosition = sprite.getXPosition();
+		int yPosition = sprite.getYPosition();
+
+		xPosition += ((float) timePassed / durationInMilliSeconds) * (xDestination - xPosition);
+		yPosition += ((float) timePassed / durationInMilliSeconds) * (yDestination - yPosition);
+
+		sprite.setXYPosition(xPosition, yPosition);
+	}
+
+	public Sprite getSprite() {
+		return this.sprite;
+	}
+
+	public int getXDestination() {
+		return xDestination;
+	}
+
+	public int getYDestination() {
+		return yDestination;
+	}
+
+	public int getDurationInMilliSeconds() {
+		return durationInMilliSeconds;
+	}
+
+	public View getView(Context context, int brickId, BaseExpandableListAdapter adapter) {
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View brickView = inflater.inflate(R.layout.construction_brick_glide_to, null);
+
+		EditText editX = (EditText) brickView.findViewById(R.id.InputValueEditTextX);
+		editX.setText(String.valueOf(xDestination));
+		EditIntegerDialog dialogX = new EditIntegerDialog(context, editX, xDestination, true);
+		dialogX.setOnDismissListener(this);
+		dialogX.setOnCancelListener((OnCancelListener) context);
+		editX.setOnClickListener(dialogX);
+
+		EditText editY = (EditText) brickView.findViewById(R.id.InputValueEditTextY);
+		editY.setText(String.valueOf(yDestination));
+		EditIntegerDialog dialogY = new EditIntegerDialog(context, editY, yDestination, true);
+		dialogY.setOnDismissListener(this);
+		dialogY.setOnCancelListener((OnCancelListener) context);
+		editY.setOnClickListener(dialogY);
+
+		EditText editDuration = (EditText) brickView.findViewById(R.id.InputValueEditTextDuration);
+		editDuration.setText(String.valueOf(durationInMilliSeconds / 1000.0));
+		EditDoubleDialog dialogDuration = new EditDoubleDialog(context, editDuration, durationInMilliSeconds / 1000.0);
+		dialogDuration.setOnDismissListener(this);
+		dialogDuration.setOnCancelListener((OnCancelListener) context);
+		editDuration.setOnClickListener(dialogDuration);
+
+		return brickView;
+	}
+
+	public View getPrototypeView(Context context) {
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View brickView = inflater.inflate(R.layout.toolbox_brick_glide_to, null);
+		return brickView;
+	}
+
+	@Override
+	public Brick clone() {
+		return new GlideToBrick(getSprite(), getXDestination(), getYDestination(), getDurationInMilliSeconds());
+	}
+
+	public void onDismiss(DialogInterface dialog) {
+		if (dialog instanceof EditIntegerDialog) {
+			EditIntegerDialog inputDialog = (EditIntegerDialog) dialog;
+			if (inputDialog.getRefernecedEditTextId() == R.id.InputValueEditTextX) {
+				xDestination = inputDialog.getValue();
+			} else if (inputDialog.getRefernecedEditTextId() == R.id.InputValueEditTextY) {
+				yDestination = inputDialog.getValue();
+			} else {
+				throw new RuntimeException("Received illegal id from EditText: "
+						+ inputDialog.getRefernecedEditTextId());
+			}
+		} else if (dialog instanceof EditDoubleDialog) {
+			durationInMilliSeconds = (int) Math.round(((EditDoubleDialog) dialog).getValue() * 1000);
+		}
+		dialog.cancel();
+	}
+
+}
---
+++ catroidTest/src/at/tugraz/ist/catroid/test/content/brick/GlideToBrickTest.java
@@ -0,0 +1,164 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010  Catroid development team 
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.tugraz.ist.catroid.test.content.brick;
+
+import android.test.AndroidTestCase;
+import at.tugraz.ist.catroid.content.Script;
+import at.tugraz.ist.catroid.content.Sprite;
+import at.tugraz.ist.catroid.content.bricks.GlideToBrick;
+import at.tugraz.ist.catroid.content.bricks.HideBrick;
+import at.tugraz.ist.catroid.content.bricks.PlaceAtBrick;
+import at.tugraz.ist.catroid.content.bricks.ShowBrick;
+
+public class GlideToBrickTest extends AndroidTestCase {
+
+	int xPosition = 100;
+	int yPosition = 100;
+	int duration = 1000;
+
+	public void testNormalBehavior() {
+		Sprite sprite = new Sprite("testSprite");
+		assertEquals("Unexpected initial sprite x position", 0, sprite.getXPosition());
+		assertEquals("Unexpected initial sprite y position", 0, sprite.getYPosition());
+
+		GlideToBrick glideToBrick = new GlideToBrick(sprite, xPosition, yPosition, duration);
+		glideToBrick.execute();
+
+		try {
+			Thread.sleep(1100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		assertEquals("Incorrect sprite x position after GlideToBrick executed",
+				xPosition, sprite.getXPosition());
+		assertEquals("Incorrect sprite y position after GlideToBrick executed",
+				yPosition, sprite.getYPosition());
+	}
+
+	public void testNullSprite() {
+		GlideToBrick glideToBrick = new GlideToBrick(null, xPosition, yPosition, duration);
+		try {
+			glideToBrick.execute();
+			fail("Execution of GlideToBrick with null Sprite did not cause a " +
+					"NullPointerException to be thrown");
+		} catch (NullPointerException e) {
+			// expected behavior
+		}
+	}
+
+	public void testBoundaryPositions() {
+		Sprite sprite = new Sprite("testSprite");
+
+		PlaceAtBrick brick = new PlaceAtBrick(sprite, Integer.MAX_VALUE, Integer.MAX_VALUE);
+		brick.execute();
+
+		assertEquals("PlaceAtBrick failed to place Sprite at maximum x integer value",
+				Integer.MAX_VALUE, sprite.getXPosition());
+		assertEquals("PlaceAtBrick failed to place Sprite at maximum y integer value",
+				Integer.MAX_VALUE, sprite.getYPosition());
+
+		brick = new PlaceAtBrick(sprite, Integer.MIN_VALUE, Integer.MIN_VALUE);
+		brick.execute();
+
+		assertEquals("PlaceAtBrick failed to place Sprite at minimum x integer value",
+				Integer.MIN_VALUE, sprite.getXPosition());
+		assertEquals("PlaceAtBrick failed to place Sprite at minimum y integer value",
+				Integer.MIN_VALUE, sprite.getYPosition());
+	}
+
+	public void testTime() {
+		Sprite sprite = new Sprite("testSprite");
+		Script script = new Script("testScript", sprite);
+		HideBrick hideBrick = new HideBrick(sprite);
+		GlideToBrick glideToBrick = new GlideToBrick(sprite, 0, 0, 1000);
+		ShowBrick showBrick = new ShowBrick(sprite);
+
+		script.addBrick(hideBrick);
+		script.addBrick(glideToBrick);
+		script.addBrick(showBrick);
+
+		sprite.getScriptList().add(script);
+
+		sprite.startScripts();
+
+		try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		assertFalse("Unexpected visibility of test sprite", sprite.isVisible());
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		assertTrue("Unexpected visibility of test sprite", sprite.isVisible());
+	}
+
+	public void testPauseResume() {
+		Sprite sprite = new Sprite("testSprite");
+		Script script = new Script("testScript", sprite);
+		HideBrick hideBrick = new HideBrick(sprite);
+		GlideToBrick glideToBrick = new GlideToBrick(sprite, 0, 0, 3000);
+		ShowBrick showBrick = new ShowBrick(sprite);
+
+		script.addBrick(hideBrick);
+		script.addBrick(glideToBrick);
+		script.addBrick(showBrick);
+
+		sprite.getScriptList().add(script);
+
+		sprite.startScripts();
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		assertFalse("Unexpected visibility of test sprite", sprite.isVisible());
+
+		sprite.pause();
+		try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		assertFalse("Unexpected visibility of test sprite", sprite.isVisible());
+
+		sprite.resume();
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		assertFalse("Unexpected visibility of testSprite", sprite.isVisible());
+
+		try {
+			Thread.sleep(1200);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		assertTrue("Unexpected visibility of testSprite", sprite.isVisible());
+	}
+}
