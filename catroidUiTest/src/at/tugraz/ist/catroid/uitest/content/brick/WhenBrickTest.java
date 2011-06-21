3fa4a059766f99d770367b7ff423b0f12c605a05
Pei Ying Hoo <denisehooanita@gmail.com>
2011-06-21 14:09:59 +0200

    WhenBrickTest

---
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/content/brick/WhenBrickTest.java
@@ -0,0 +1,90 @@
+package at.tugraz.ist.catroid.uitest.content.brick;
+
+import java.util.ArrayList;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.Smoke;
+import android.widget.ArrayAdapter;
+import at.tugraz.ist.catroid.ProjectManager;
+import at.tugraz.ist.catroid.R;
+import at.tugraz.ist.catroid.content.Project;
+import at.tugraz.ist.catroid.content.Script;
+import at.tugraz.ist.catroid.content.Sprite;
+import at.tugraz.ist.catroid.content.WhenScript;
+import at.tugraz.ist.catroid.content.bricks.Brick;
+import at.tugraz.ist.catroid.content.bricks.WhenBrick;
+import at.tugraz.ist.catroid.ui.ScriptActivity;
+
+import com.jayway.android.robotium.solo.Solo;
+
+public class WhenBrickTest extends ActivityInstrumentationTestCase2<ScriptActivity> {
+	private Solo solo;
+	private Project project;
+
+	//	private WhenBrick whenBrick;
+	//	private String selectedTitle;
+
+	public WhenBrickTest() {
+		super("at.tugraz.ist.catroid", ScriptActivity.class);
+	}
+
+	@Override
+	public void setUp() throws Exception {
+		createProject();
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		try {
+			solo.finalize();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+
+		getActivity().finish();
+		super.tearDown();
+	}
+
+	@Smoke
+	public void testWhenBrick() {
+		int childrenCount = getActivity().getAdapter().getChildCountFromLastGroup();
+		int groupCount = getActivity().getAdapter().getGroupCount();
+
+		assertEquals("Incorrect number of bricks.", 2, solo.getCurrentListViews().get(0).getChildCount());
+		assertEquals("Incorrect number of bricks.", 1, childrenCount);
+
+		ArrayList<Brick> projectBrickList = project.getSpriteList().get(0).getScriptList().get(0).getBrickList();
+		assertEquals("Incorrect number of bricks.", 1, projectBrickList.size());
+
+		assertEquals("Wrong Brick instance.", projectBrickList.get(0),
+				getActivity().getAdapter().getChild(groupCount - 1, 0));
+		assertNotNull("TextView does not exist", solo.getText(getActivity().getString(R.string.brick_when)));
+		//		solo.clickOnButton(name);
+		solo.clickInList(0);
+		solo.sleep(500);
+	}
+
+	private void createProject() {
+		ArrayAdapter<CharSequence> spinnerAdapter = new ArrayAdapter<CharSequence>(
+				null, android.R.layout.simple_spinner_item);
+		spinnerAdapter.add("Tapped");
+		spinnerAdapter.add("Double Tapped");
+		spinnerAdapter.add("Long Pressed");
+		spinnerAdapter.add("Touching Starts");
+		spinnerAdapter.add("Touching Stops");
+
+		project = new Project(null, "testProject");
+		Sprite sprite = new Sprite("cat");
+		Script script = new WhenScript("script", sprite);
+		script.addBrick(new WhenBrick(sprite, script));
+
+		sprite.getScriptList().add(script);
+		project.addSprite(sprite);
+
+		ProjectManager.getInstance().setProject(project);
+		ProjectManager.getInstance().setCurrentSprite(sprite);
+		ProjectManager.getInstance().setCurrentScript(script);
+	}
+
+}
