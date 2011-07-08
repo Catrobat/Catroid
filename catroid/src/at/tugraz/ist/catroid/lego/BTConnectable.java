b8a434fad4e408912d4e2088ee630b883388e406
Smiai Oussama <smiai.oussama@gmail.com>
2011-07-08 12:08:48 +0200

    bluetooth

---
+++ catroid/src/at/tugraz/ist/catroid/lego/BTConnectable.java
@@ -0,0 +1,29 @@
+/**
+ *   Copyright 2010 Guenther Hoelzl
+ *
+ *   This file is part of MINDdroid.
+ *
+ *   MINDdroid is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   MINDdroid is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with MINDdroid.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+package at.tugraz.ist.catroid.lego;
+
+public interface BTConnectable {
+
+    /**
+     * @return true, when currently pairing 
+     */
+    public boolean isPairing();
+
+}
