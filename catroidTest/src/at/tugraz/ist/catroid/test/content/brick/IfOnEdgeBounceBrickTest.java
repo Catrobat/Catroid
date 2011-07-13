2778924bb8dcf62c76486b2088f943e8eb39c07e
Florian Hubner <FlorianHubner88@gmail.com>
2011-07-13 12:57:09 +0200

    Tests for IfOnEdgeBounceBrick

--- catroidTest/src/at/tugraz/ist/catroid/test/content/brick/IfOnEdgeBounceBrickTest.java
+++ catroidTest/src/at/tugraz/ist/catroid/test/content/brick/IfOnEdgeBounceBrickTest.java
@@ -1,16 +1,51 @@
 package at.tugraz.ist.catroid.test.content.brick;
 
-import android.test.AndroidTestCase;
+import java.io.File;
+
+import android.test.InstrumentationTestCase;
+import at.tugraz.ist.catroid.ProjectManager;
 import at.tugraz.ist.catroid.common.Consts;
+import at.tugraz.ist.catroid.common.Values;
+import at.tugraz.ist.catroid.content.Project;
 import at.tugraz.ist.catroid.content.Sprite;
 import at.tugraz.ist.catroid.content.bricks.IfOnEdgeBounceBrick;
+import at.tugraz.ist.catroid.io.StorageHandler;
+import at.tugraz.ist.catroid.test.R;
+import at.tugraz.ist.catroid.test.utils.TestUtils;
+import at.tugraz.ist.catroid.utils.UtilFile;
 
-public class IfOnEdgeBounceBrickTest extends AndroidTestCase {
+public class IfOnEdgeBounceBrickTest extends InstrumentationTestCase {
 
 	private static final int BOUNCE_LEFT_POS = -(Consts.MAX_REL_COORDINATES + 50);
 	private static final int BOUNCE_RIGHT_POS = Consts.MAX_REL_COORDINATES + 50;
 	private static final int BOUNCE_DOWN_POS = -(Consts.MAX_REL_COORDINATES + 50);
 	private static final int BOUNCE_UP_POS = Consts.MAX_REL_COORDINATES + 50;
+
+	private static final int IMAGE_FILE_ID = R.raw.icon;
+
+	private final String projectName = "testProject";
+	private File testImage;
+
+	@Override
+	public void setUp() throws Exception {
+
+		File defProject = new File(Consts.DEFAULT_ROOT + "/" + projectName);
+
+		if (defProject.exists()) {
+			UtilFile.deleteDirectory(defProject);
+		}
+
+		Project project = new Project(getInstrumentation().getTargetContext(), projectName);
+		StorageHandler.getInstance().saveProject(project);
+		ProjectManager.getInstance().setProject(project);
+
+		testImage = TestUtils.saveFileToProject(this.projectName, "testImage.png", IMAGE_FILE_ID, getInstrumentation()
+				.getContext(), TestUtils.TYPE_IMAGE_FILE);
+
+		Values.SCREEN_HEIGHT = 800;
+		Values.SCREEN_WIDTH = 480;
+
+	}
 
 	public void testNoBounce() {
 
@@ -21,50 +56,105 @@ public class IfOnEdgeBounceBrickTest extends AndroidTestCase {
 		brick.execute();
 		assertEquals("Wrong X-Position!", 0, sprite.getXPosition());
 		assertEquals("Wrong Y-Position!", 0, sprite.getYPosition());
-		assertEquals("Wrong direction", 0.0, sprite.getDirection(), 1e-3);
+		assertEquals("Wrong direction", 90., sprite.getDirection(), 1e-3);
 
 	}
 
 	public void testBounceStraight() {
 
 		Sprite sprite = new Sprite("testSprite");
+		sprite.getCostume().setImagePath(testImage.getAbsolutePath());
+
 		IfOnEdgeBounceBrick brick = new IfOnEdgeBounceBrick(sprite);
 
-		sprite.setDirection(0);
+		sprite.setDirection(90);
 		sprite.setXYPosition(BOUNCE_RIGHT_POS, 0);
-
 		brick.execute();
-		assertEquals("Wrong X-Position!", Consts.MAX_REL_COORDINATES, sprite.getXPosition());
+
+		double width = sprite.getCostume().getRelBoundingBoxWidth();
+		double height = sprite.getCostume().getRelBoudingBoxHeight();
+
+		assertEquals("Wrong X-Position!", Consts.MAX_REL_COORDINATES - (int) (width / 2), sprite.getXPosition());
 		assertEquals("Wrong Y-Position!", 0, sprite.getYPosition());
-		assertEquals("Wrong direction", 180.0, sprite.getDirection(), 1e-3);
+		assertEquals("Wrong direction", -90, sprite.getDirection(), 1e-3);
+
+		sprite.setDirection(-90);
+		sprite.setXYPosition(BOUNCE_LEFT_POS, 0);
+		brick.execute();
+
+		width = sprite.getCostume().getRelBoundingBoxWidth();
+		height = sprite.getCostume().getRelBoudingBoxHeight();
+
+		assertEquals("Wrong X-Position!", -Consts.MAX_REL_COORDINATES + (int) (width / 2), sprite.getXPosition());
+		assertEquals("Wrong Y-Position!", 0, sprite.getYPosition());
+		assertEquals("Wrong direction", 90, sprite.getDirection(), 1e-3);
+
+		sprite.setDirection(0);
+		sprite.setXYPosition(0, BOUNCE_UP_POS);
+		brick.execute();
+
+		width = sprite.getCostume().getRelBoundingBoxWidth();
+		height = sprite.getCostume().getRelBoudingBoxHeight();
+		assertEquals("Wrong X-Position!", 0, sprite.getXPosition());
+		assertEquals("Wrong Y-Position!", Consts.MAX_REL_COORDINATES - (int) (height / 2), sprite.getYPosition());
+		assertEquals("Wrong direction", 180, sprite.getDirection(), 1e-3);
 
 		sprite.setDirection(180);
-		sprite.setXYPosition(BOUNCE_LEFT_POS, 0);
-
-		brick.execute();
-		assertEquals("Wrong X-Position!", -Consts.MAX_REL_COORDINATES, sprite.getXPosition());
-		assertEquals("Wrong Y-Position!", 0, sprite.getYPosition());
-		assertEquals("Wrong direction", 0., sprite.getDirection(), 1e-3);
-
-		sprite.setDirection(90);
-		sprite.setXYPosition(0, BOUNCE_UP_POS);
-
-		brick.execute();
-		assertEquals("Wrong X-Position!", 0, sprite.getXPosition());
-		assertEquals("Wrong Y-Position!", Consts.MAX_REL_COORDINATES, sprite.getYPosition());
-		assertEquals("Wrong direction", 270.0, sprite.getDirection(), 1e-3);
-
-		sprite.setDirection(270);
 		sprite.setXYPosition(0, BOUNCE_DOWN_POS);
-
 		brick.execute();
+
+		width = sprite.getCostume().getRelBoundingBoxWidth();
+		height = sprite.getCostume().getRelBoudingBoxHeight();
 		assertEquals("Wrong X-Position!", 0, sprite.getXPosition());
-		assertEquals("Wrong Y-Position!", -Consts.MAX_REL_COORDINATES, sprite.getYPosition());
-		assertEquals("Wrong direction", 90., sprite.getDirection(), 1e-3);
+		assertEquals("Wrong Y-Position!", -Consts.MAX_REL_COORDINATES + (int) (height / 2), sprite.getYPosition());
+		assertEquals("Wrong direction", 0, sprite.getDirection(), 1e-3);
 	}
 
-	public void testZeroSizeSprite() {
+	public void testBounceDiagonal() {
+		Sprite sprite = new Sprite("testSprite");
+		sprite.getCostume().setImagePath(testImage.getAbsolutePath());
 
+		IfOnEdgeBounceBrick brick = new IfOnEdgeBounceBrick(sprite);
+
+		sprite.setDirection(30);
+		sprite.setXYPosition(BOUNCE_RIGHT_POS, 0);
+		brick.execute();
+
+		double width = sprite.getCostume().getRelBoundingBoxWidth();
+		double height = sprite.getCostume().getRelBoudingBoxHeight();
+		assertEquals("Wrong X-Position!", Consts.MAX_REL_COORDINATES - (int) (width / 2), sprite.getXPosition());
+		assertEquals("Wrong Y-Position!", 0, sprite.getYPosition());
+		assertEquals("Wrong direction", -30, sprite.getDirection(), 1e-3);
+
+		sprite.setDirection(30);
+		sprite.setXYPosition(0, BOUNCE_UP_POS);
+		brick.execute();
+
+		width = sprite.getCostume().getRelBoundingBoxWidth();
+		height = sprite.getCostume().getRelBoudingBoxHeight();
+		assertEquals("Wrong X-Position!", 0, sprite.getXPosition());
+		assertEquals("Wrong Y-Position!", Consts.MAX_REL_COORDINATES - (int) (height / 2), sprite.getYPosition());
+		assertEquals("Wrong direction", 150, sprite.getDirection(), 1e-3);
+
+		sprite.setDirection(30);
+		sprite.setXYPosition(BOUNCE_LEFT_POS, 0);
+		brick.execute();
+
+		width = sprite.getCostume().getRelBoundingBoxWidth();
+		height = sprite.getCostume().getRelBoudingBoxHeight();
+		assertEquals("Wrong X-Position!", -Consts.MAX_REL_COORDINATES + (int) (width / 2), sprite.getXPosition());
+		assertEquals("Wrong Y-Position!", 0, sprite.getYPosition());
+		assertEquals("Wrong direction", -30, sprite.getDirection(), 1e-3);
+
+		sprite.setDirection(30);
+		sprite.setXYPosition(0, BOUNCE_DOWN_POS);
+		brick.execute();
+
+		width = sprite.getCostume().getRelBoundingBoxWidth();
+		height = sprite.getCostume().getRelBoudingBoxHeight();
+		assertEquals("Wrong X-Position!", 0, sprite.getXPosition());
+		assertEquals("Wrong Y-Position!", -Consts.MAX_REL_COORDINATES + (int) (height / 2), sprite.getYPosition());
+		assertEquals("Wrong direction", 150, sprite.getDirection(), 1e-3);
 	}
 
 }
