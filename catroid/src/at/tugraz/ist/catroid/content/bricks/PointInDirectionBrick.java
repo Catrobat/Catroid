6acb239522cffb3e63af8fc290123b6140d2f5ae
David Kikelj <gkkiscus@gmail.com>
2011-07-18 12:07:58 +0200

    PointInDirectinBrick added

---
+++ catroid/src/at/tugraz/ist/catroid/content/bricks/PointInDirectionBrick.java
@@ -0,0 +1,76 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010  Catroid development team 
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.tugraz.ist.catroid.content.bricks;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnDismissListener;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.BaseExpandableListAdapter;
+import at.tugraz.ist.catroid.R;
+import at.tugraz.ist.catroid.content.Sprite;
+import at.tugraz.ist.catroid.ui.dialogs.EditDoubleDialog;
+
+public class PointInDirectionBrick implements Brick, OnDismissListener {
+
+	private static final long serialVersionUID = 1L;
+	private Sprite sprite;
+	private double direction;
+
+	public PointInDirectionBrick(Sprite sprite, double direction) {
+		this.sprite = sprite;
+		this.direction = direction;
+	}
+
+	public void execute() {
+		sprite.setDirection(direction);
+	}
+
+	public Sprite getSprite() {
+		return this.sprite;
+	}
+
+	public double getDirection() {
+		return direction;
+	}
+
+	public View getView(Context context, int brickId, BaseExpandableListAdapter adapter) {
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View brickView = inflater.inflate(R.layout.construction_brick_broadcast_receive, null);
+
+		return null;
+	}
+
+	public View getPrototypeView(Context context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Brick clone() {
+		return new MoveNStepsBrick(getSprite(), getDirection());
+	}
+
+	public void onDismiss(DialogInterface dialog) {
+		direction = ((EditDoubleDialog) dialog).getValue();
+		dialog.cancel();
+	}
+
+}
