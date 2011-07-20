002c7f6be3ae1dca03d4f4ea1c638a6fd93b7761
Pei Ying Hoo <denisehooanita@gmail.com>
2011-07-20 19:09:05 +0200

    changes

--- catroidTest/src/at/tugraz/ist/catroid/test/content/sprite/SpriteTest.java
+++ catroidTest/src/at/tugraz/ist/catroid/test/content/sprite/SpriteTest.java
@@ -273,4 +273,69 @@ public class SpriteTest extends AndroidTestCase {
 		assertEquals("Sprite1 is not behind Sprite2.", Integer.MAX_VALUE, sprite1.compareTo(sprite3));
 		assertEquals("Sprite1 is not in front of Sprite2.", Integer.MIN_VALUE, sprite1.compareTo(sprite4));
 	}
+
+	public void testSetVolume() {
+		Sprite sprite = new Sprite("new sprite");
+		final double volume = 60;
+		sprite.setVolume(volume);
+		assertEquals("Unexpected volume", volume, sprite.getVolume());
+
+		final double hugeVolume = 10.0e100;
+		sprite.setVolume(hugeVolume);
+		assertEquals("Failed to size sprite to a very large volume", hugeVolume, sprite.getVolume());
+
+		final double negativeVolume = -10.0e100;
+		sprite.setVolume(negativeVolume);
+		assertEquals("Failed to size sprite to a negative volume", negativeVolume, sprite.getVolume());
+	}
+
+	public void testSetBrightness() {
+		Sprite sprite = new Sprite("new sprite");
+		final double brightness = 70;
+		sprite.setBrightnessValue(brightness);
+		assertEquals("Unexpected brightness", brightness, sprite.getBrightnessValue());
+
+		final double hugeBrightness = 10.0e100;
+		sprite.setBrightnessValue(hugeBrightness);
+		assertEquals("Failed to set sprite to a very high brightness", hugeBrightness, sprite.getBrightnessValue());
+
+		final double negativeBrightness = -10.0e100;
+		sprite.setBrightnessValue(negativeBrightness);
+		assertEquals("Failed to set sprite to a negative brightness", negativeBrightness, sprite.getBrightnessValue());
+	}
+
+	public void testSetGhostEffect() {
+		Sprite sprite = new Sprite("new sprite");
+		final double effect = 70;
+		sprite.setGhostEffectValue(effect);
+		assertEquals("Unexpected effect", effect, sprite.getGhostEffectValue());
+
+		final double hugeEffect = 10.0e100;
+		sprite.setGhostEffectValue(hugeEffect);
+		assertEquals("Failed to set sprite to a very high transparent", hugeEffect, sprite.getGhostEffectValue());
+
+		final double negativeEffect = -10.0e100;
+		try {
+			sprite.setGhostEffectValue(negativeEffect);
+			assertNotSame("Failed to change negative effect value to positive", negativeEffect,
+					sprite.getGhostEffectValue());
+		} catch (IllegalArgumentException e) {
+			// expected behavior
+		}
+	}
+
+	public void testClearGraphicEffect() {
+		Sprite sprite = new Sprite("new sprite");
+		final double effect = 70;
+		sprite.setGhostEffectValue(effect);
+		assertEquals("Unexpected effect", effect, sprite.getGhostEffectValue());
+
+		final double brightness = 70;
+		sprite.setBrightnessValue(brightness);
+		assertEquals("Unexpected brightness", brightness, sprite.getBrightnessValue());
+
+		sprite.clearGraphicEffect();
+		assertEquals("Unexpected brightness", 0.0, sprite.getBrightnessValue());
+		assertEquals("Unexpected effect", 0.0, sprite.getGhostEffectValue());
+	}
 }
