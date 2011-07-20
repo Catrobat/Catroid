62670057ecd876a858a0f6ec395c9528a857dd1a
Pei Ying Hoo <denisehooanita@gmail.com>
2011-07-20 19:08:49 +0200

    changes

--- catroidUiTest/src/at/tugraz/ist/catroid/uitest/content/brick/WhenBrickTest.java
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/content/brick/WhenBrickTest.java
@@ -11,7 +11,7 @@ import at.tugraz.ist.catroid.content.Script;
 import at.tugraz.ist.catroid.content.Sprite;
 import at.tugraz.ist.catroid.content.WhenScript;
 import at.tugraz.ist.catroid.content.bricks.Brick;
-import at.tugraz.ist.catroid.content.bricks.WhenBrick;
+import at.tugraz.ist.catroid.content.bricks.PlaceAtBrick;
 import at.tugraz.ist.catroid.ui.ScriptActivity;
 
 import com.jayway.android.robotium.solo.Solo;
@@ -44,11 +44,9 @@ public class WhenBrickTest extends ActivityInstrumentationTestCase2<ScriptActivi
 
 	@Smoke
 	public void testWhenBrick() {
-		int childrenCount = getActivity().getAdapter().getChildCountFromLastGroup();
 		int groupCount = getActivity().getAdapter().getGroupCount();
 
-		assertEquals("Incorrect number of bricks.", 2, solo.getCurrentListViews().get(0).getChildCount());
-		assertEquals("Incorrect number of bricks.", 1, childrenCount);
+		assertEquals("Incorrect number of bricks.", 2, solo.getCurrentListViews().get(0).getCount());
 
 		ArrayList<Brick> projectBrickList = project.getSpriteList().get(0).getScript(0).getBrickList();
 		assertEquals("Incorrect number of bricks.", 1, projectBrickList.size());
@@ -57,8 +55,11 @@ public class WhenBrickTest extends ActivityInstrumentationTestCase2<ScriptActivi
 				getActivity().getAdapter().getChild(groupCount - 1, 0));
 		assertNotNull("TextView does not exist", solo.getText(getActivity().getString(R.string.brick_when)));
 
-		solo.pressSpinnerItem(0, 2);
+		solo.pressSpinnerItem(0, 1);
 		solo.sleep(1000);
+		System.out.println(solo.getCurrentSpinners().get(0).getSelectedItemPosition());
+		System.out.println(solo.getCurrentSpinners().get(0).getSelectedItem().toString());
+		assertEquals("Wrong event selected!", 0, solo.getCurrentSpinners().get(0).getSelectedItemPosition());
 	}
 
 	private void createProject() {
@@ -66,11 +67,10 @@ public class WhenBrickTest extends ActivityInstrumentationTestCase2<ScriptActivi
 		project = new Project(null, "testProject");
 		Sprite sprite = new Sprite("cat");
 		Script script = new WhenScript("script", sprite);
-		WhenBrick whenBrick = new WhenBrick(sprite, script);
-		script.addBrick(whenBrick);
+		Brick placeAtBrick = new PlaceAtBrick(sprite, 100, 100);
+		script.addBrick(placeAtBrick);
 		sprite.addScript(script);
 
-		sprite.addScript(script);
 		project.addSprite(sprite);
 
 		ProjectManager.getInstance().setProject(project);
