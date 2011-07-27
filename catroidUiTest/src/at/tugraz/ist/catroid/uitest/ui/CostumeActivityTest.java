d7ee6bad35f48ab9df969dd6086a2652fba974f0
Ainul Husna <ainulhusna@gmail.com>
2011-07-27 16:41:48 +0200

    Edit costumeActivity Test, Done with SetCostumeBrick

--- catroidUiTest/src/at/tugraz/ist/catroid/uitest/ui/CostumeActivityTest.java
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/ui/CostumeActivityTest.java
@@ -1,11 +1,23 @@
 package at.tugraz.ist.catroid.uitest.ui;
 
+import java.io.File;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import at.tugraz.ist.catroid.R;
+import at.tugraz.ist.catroid.common.Consts;
+import at.tugraz.ist.catroid.test.utils.TestUtils;
 import at.tugraz.ist.catroid.ui.CostumeActivity;
 import at.tugraz.ist.catroid.ui.MainMenuActivity;
 import at.tugraz.ist.catroid.uitest.util.UiTestUtils;
+import at.tugraz.ist.catroid.utils.UtilFile;
 
 import com.jayway.android.robotium.solo.Solo;
 
@@ -13,7 +25,8 @@ public class CostumeActivityTest extends ActivityInstrumentationTestCase2<Costum
 	private Solo solo;
 	private String testingcostume = "testingcostume";
 	private int SELECT_IMAGE = 0;
-	private ImageView setCostumeImageView;
+	private ImageButton addCostumeButton;
+	private File imageFile;
 
 	public CostumeActivityTest() {
 		super("at.tugraz.ist.catroid", CostumeActivity.class);
@@ -25,20 +38,28 @@ public class CostumeActivityTest extends ActivityInstrumentationTestCase2<Costum
 		UiTestUtils.createTestProject();
 		solo = new Solo(getInstrumentation(), getActivity());
 
-		//		UiTestUtils.clickOnImageButton(solo, R.id.btn_action_add_sprite);
-		//
-		//		// Override OnClickListener to launch MockGalleryActivity
-		//		OnClickListener listener = new OnClickListener() {
-		//			public void onClick(View v) {
-		//				Intent intent = new Intent(getInstrumentation().getContext(),
-		//						at.tugraz.ist.catroid.uitest.mockups.MockGalleryActivity.class);
-		//				getActivity().startActivityForResult(intent, SELECT_IMAGE);
-		//			}
-		//		};
-		//
-		//		setCostumeImageView = (ImageView) solo.getView(R.id.costume_image);
-		//
-		//		assertNotNull("ImageView of the costume was not found", setCostumeImageView);
+		addCostumeButton = (ImageButton) solo.getCurrentActivity().findViewById(R.id.btn_action_add_sprite);
+
+		final int RESOURCE_LOCATION = R.drawable.catroid_sunglasses;
+		String imageFilePath = Consts.DEFAULT_ROOT + "/" + UiTestUtils.DEFAULT_TEST_PROJECT_NAME
+				+ Consts.IMAGE_DIRECTORY + "/" + "catroid_sunglasses.png";
+		imageFile = UtilFile.saveFileToProject(UiTestUtils.DEFAULT_TEST_PROJECT_NAME, "catroid_sunglasses.png",
+				RESOURCE_LOCATION, getActivity(), TestUtils.TYPE_IMAGE_FILE);
+
+		final File imageFileReference = imageFile;
+
+		// Override OnClickListener to launch MockGalleryActivity
+		OnClickListener listener = new OnClickListener() {
+			public void onClick(View v) {
+				final Intent intent = new Intent("android.intent.action.MAIN");
+				intent.setComponent(new ComponentName("at.tugraz.ist.catroid.uitest",
+						"at.tugraz.ist.catroid.uitest.mockups.MockGalleryActivity"));
+				intent.putExtra("filePath", imageFileReference.getAbsolutePath());
+				solo.getActivityMonitor().getLastActivity().startActivityForResult(intent, SELECT_IMAGE);
+			}
+		};
+
+		addCostumeButton.setOnClickListener(listener);
 	}
 
 	@Override
@@ -57,6 +78,22 @@ public class CostumeActivityTest extends ActivityInstrumentationTestCase2<Costum
 		UiTestUtils.clickOnImageButton(solo, R.id.btn_action_home);
 		assertTrue("Clicking on main menu button did not cause main menu to be displayed",
 				solo.getCurrentActivity() instanceof MainMenuActivity);
+	}
+
+	public void testAddCostume() {
+		UiTestUtils.clickOnImageButton(solo, R.id.btn_action_add_sprite);
+
+		ImageView costumeView = (ImageView) solo.getCurrentActivity().findViewById(R.id.costume_image);
+		Bitmap bitmapToTest = costumeView.getDrawingCache();
+		assertNotNull("Bitmap of costume ImageView is null", bitmapToTest);
+		solo.sleep(5000);
+
+		Bitmap originalBitmap = BitmapFactory.decodeResource(getInstrumentation().getContext().getResources(),
+				at.tugraz.ist.catroid.uitest.R.drawable.catroid_sunglasses);
+		assertNotNull("Decoding the costume resource failed", originalBitmap);
+
+		solo.sleep(1000);
+
 	}
 
 	public void testCopyButton() {
@@ -79,7 +116,6 @@ public class CostumeActivityTest extends ActivityInstrumentationTestCase2<Costum
 		solo.sleep(100);
 		solo.enterText(R.id.edit_costume, testingcostume);
 		solo.sleep(100);
-		solo.clickOnButton("Done");
 		//check the editText
 		//assertEquals("The name of the costume is not change", solo.getCurrentActivity() instanceof MainMenuActivity);
 	}
