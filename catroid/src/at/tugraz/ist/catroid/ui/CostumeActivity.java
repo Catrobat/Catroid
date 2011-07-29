e55bc3846ad80760b622e2cdd7ec477cef176d60
Daniel Markart <daniel.markart@gmail.com>
2011-07-30 01:34:10 +0200

    fixed defaultProject, testing some costume adapter tweaks, costume activity now with icons (not finished)

--- catroid/src/at/tugraz/ist/catroid/ui/CostumeActivity.java
+++ catroid/src/at/tugraz/ist/catroid/ui/CostumeActivity.java
@@ -1,6 +1,6 @@
 /**
  *  Catroid: An on-device graphical programming language for Android devices
- *  Copyright (C) 2010  Catroid development team
+ *  Copyright (C) 2010  Catroid development team 
  *  (<http://code.google.com/p/catroid/wiki/Credits>)
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -16,7 +16,6 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package at.tugraz.ist.catroid.ui;
 
 import java.io.File;
@@ -26,72 +25,91 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore.MediaColumns;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import at.tugraz.ist.catroid.ProjectManager;
 import at.tugraz.ist.catroid.R;
 import at.tugraz.ist.catroid.common.Consts;
 import at.tugraz.ist.catroid.common.CostumeData;
-import at.tugraz.ist.catroid.content.Sprite;
 import at.tugraz.ist.catroid.io.StorageHandler;
-import at.tugraz.ist.catroid.ui.adapter.CostumeAdapter;
 import at.tugraz.ist.catroid.ui.dialogs.RenameCostumeDialog;
 import at.tugraz.ist.catroid.utils.ActivityHelper;
 import at.tugraz.ist.catroid.utils.Utils;
 
 public class CostumeActivity extends ListActivity {
-	private Sprite sprite;
-	private ArrayList<CostumeData> costumeData;
-	public CostumeData selectedCostumeInfo;
+	public CostumeData selectedCostumeData;
 	private RenameCostumeDialog renameCostumeDialog;
-	private Runnable viewCostumes;
-	Intent intent = null;
-	String filemanagerstring, selectedImagePath, imagePath, costumeName, absolutePath, costume, costumeFormat,
-			costumeDisplayName;
-	int counter, splitAt, costumeId, column_index;
-	Cursor cursor;
-	private static final int SELECT_IMAGE = 1;
+	private ArrayList<CostumeData> costumeDataList;
+
+	private final int REQUEST_SELECT_IMAGE = 0;
+	private static final int CONTEXT_MENU_ITEM_RENAME = 0;
+	private static final int CONTEXT_MENU_ITEM_EDIT = 1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_costume);
+		costumeDataList = ProjectManager.getInstance().getCurrentSprite().getCostumeDataList();
+
+		setListAdapter(new CostumeAdapter2(this, R.layout.activity_costume_costumelist_item, costumeDataList));
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (!Utils.checkForSdCard(this)) {
+			return;
+		}
+
+		//		this.costumeDataList = ProjectManager.getInstance().getCurrentSprite().getSoundList();
+		//		setListAdapter(new SoundAdapter(this, R.layout.activity_sound_soundlist_item, costumeDataList));
+		//		((SoundAdapter) getListAdapter()).notifyDataSetChanged();
+
+		//change actionbar:
+		ScriptTabActivity scriptTabActivity = (ScriptTabActivity) getParent();
+		ActivityHelper activityHelper = scriptTabActivity.activityHelper;
+		if (activityHelper != null) {
+			//set new functionality for actionbar add button:
+			activityHelper.changeClickListener(R.id.btn_action_add_sprite, createAddCostumeClickListener());
+			//set new icon for actionbar plus button:
+			activityHelper.changeButtonIcon(R.id.btn_action_add_sprite, R.drawable.ic_plus_black);
+		}
+
+	}
 
 	private View.OnClickListener createAddCostumeClickListener() {
 		return new View.OnClickListener() {
 			public void onClick(View v) {
 				Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
 				intent.setType("image/*");
-				startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_IMAGE);
+				startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQUEST_SELECT_IMAGE);
 			}
 		};
-	}
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_costume);
-
-		sprite = ProjectManager.getInstance().getCurrentSprite();
-		costumeData = sprite.getCostumeDataList();
-		setListAdapter(new CostumeAdapter(this, R.layout.activity_costumelist, costumeData, sprite));
-		getListView().setTextFilterEnabled(true);
-	}
-
-	@Override
-	protected void onStart() {
-		super.onStart();
 	}
 
 	@Override
 	protected Dialog onCreateDialog(int id) {
 		final Dialog dialog;
 		switch (id) {
-			case Consts.DIALOG_RENAME_COSTUME:
-				if (selectedCostumeInfo == null) {
+			case Consts.DIALOG_RENAME_SOUND:
+				if (selectedCostumeData == null) {
 					dialog = null;
 				} else {
 					renameCostumeDialog = new RenameCostumeDialog(this);
-					dialog = renameCostumeDialog.createDialog(selectedCostumeInfo);
+					dialog = renameCostumeDialog.createDialog(selectedCostumeData);
 				}
 				break;
 			default:
@@ -110,81 +128,21 @@ public class CostumeActivity extends ListActivity {
 		}
 	}
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		if (!Utils.checkForSdCard(this)) {
-			return;
-		}
+	private void updateCostumeAdapter(String name, String fileName) {
+		CostumeData costumeData = new CostumeData();
+		costumeData.setCostumeFilename(fileName);
+		costumeData.setCostumeName(name);
+		costumeDataList.add(costumeData);
+		((CostumeAdapter2) getListAdapter()).notifyDataSetChanged();
 
-		ScriptTabActivity scriptTabActivity = (ScriptTabActivity) getParent();
-		ActivityHelper activityHelper = scriptTabActivity.activityHelper;
-		if (activityHelper != null) {
-			activityHelper.changeClickListener(R.id.btn_action_add_sprite, createAddCostumeClickListener());
-			scriptTabActivity.activityHelper.changeButtonIcon(R.id.btn_action_add_sprite, R.drawable.ic_plus_black);
-		}
-	}
-
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (resultCode == Activity.RESULT_OK) {
-			if (requestCode == SELECT_IMAGE) {
-				Uri selectedImageUri = data.getData();
-
-				//OI FILE Manager
-				filemanagerstring = selectedImageUri.getPath();
-
-				//MEDIA GALLERY
-				selectedImagePath = getPath(selectedImageUri);
-
-				if (selectedImagePath == null) {
-					Utils.displayErrorMessage(this, getString(R.string.error_load_image));
-					return;
+		//scroll down the list to the new item:
+		{
+			final ListView listView = getListView();
+			listView.post(new Runnable() {
+				public void run() {
+					listView.setSelection(listView.getCount() - 1);
 				}
-				try {
-					File outputFile = StorageHandler.getInstance().copyImage(
-							ProjectManager.getInstance().getCurrentProject().getName(), selectedImagePath, null);
-					if (outputFile != null) {
-
-						absolutePath = outputFile.getAbsolutePath();
-						costume = absolutePath.substring(33);
-						int length = costume.length();
-						for (int i = length - 1; i > 0; i--) {
-							if (costume.charAt(i) == '.') {
-								splitAt = i;
-							}
-						}
-						costumeName = outputFile.getName();
-						costumeFormat = costume.substring(splitAt);
-						costumeId = 0;
-						for (int i = 0; i < sprite.getCostumeDataList().size(); i++) {
-							if (costumeName.equals(sprite.getCostumeDataList().get(i).getCostumeName())) {
-								if (costumeId <= sprite.getCostumeDataList().get(i).getCostumeId()) {
-									costumeId = sprite.getCostumeDataList().get(i).getCostumeId();
-								}
-							}
-						}
-
-						costumeId = costumeId + 1;
-
-						costumeDisplayName = costumeName.substring(33, costumeName.length() - 4);
-
-						((CostumeAdapter) getListAdapter()).notifyDataSetChanged();
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-				viewCostumes = new Runnable() {
-					public void run() {
-						getCostumes();
-					}
-				};
-
-				Thread thread = new Thread(null, viewCostumes, "MagentoBackground");
-				thread.start();
-
-			}
+			});
 		}
 	}
 
@@ -196,42 +154,97 @@ public class CostumeActivity extends ListActivity {
 		renameCostumeDialog.renameDialog.cancel();
 	}
 
-	public String getPath(Uri uri) {
-		String[] projection = { MediaColumns.DATA };
-		Cursor cursor = managedQuery(uri, projection, null, null, null);
-		column_index = cursor.getColumnIndexOrThrow(MediaColumns.DATA);
-		cursor.moveToFirst();
-		imagePath = cursor.getString(column_index);
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		//when new sound title is selected and ready to be added to the catroid project
+		if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_SELECT_IMAGE) {
+			String imagePath = "";
+			//get path of image --------------------------
+			{
+				Uri imageUri = data.getData();
+				String[] projection = { MediaStore.MediaColumns.DATA };
+				Cursor cursor = managedQuery(imageUri, projection, null, null, null);
+				int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+				cursor.moveToFirst();
+				imagePath = cursor.getString(column_index);
+			}
+			//-----------------------------------------------------
 
-		return cursor.getString(column_index);
+			//copy image to catroid:
+			try {
+				if (imagePath.equalsIgnoreCase("")) {
+					throw new IOException();
+				}
+				File imageFile = StorageHandler.getInstance().copyImage(
+						ProjectManager.getInstance().getCurrentProject().getName(), imagePath, "testname");
+				String imageName = imageFile.getName().substring(33, imageFile.getName().length() - 4);
+				String imageFileName = imageFile.getName();
+				updateCostumeAdapter(imageName, imageFileName);
+			} catch (IOException e) {
+				Utils.displayErrorMessage(this, this.getString(R.string.error_load_image));
+			}
+		}
 	}
 
-	private Runnable returnRes = new Runnable() {
+	private class CostumeAdapter2 extends ArrayAdapter<CostumeData> {
+		protected ArrayList<CostumeData> costumeDataItems;
+		protected CostumeActivity activity;
 
-		public void run() {
-			if (costumeData != null && costumeData.size() > 0) {
-				((CostumeAdapter) getListAdapter()).notifyDataSetChanged();
-				for (int i = 0; i < costumeData.size(); i++) {
-					((CostumeAdapter) getListAdapter()).add(costumeData.get(i));
-				}
+		public CostumeAdapter2(final CostumeActivity activity, int textViewResourceId, ArrayList<CostumeData> items) {
+			super(activity, textViewResourceId, items);
+			this.activity = activity;
+			costumeDataItems = items;
+		}
+
+		@Override
+		public View getView(final int position, View convView, ViewGroup parent) {
+
+			View convertView = convView;
+			if (convertView == null) {
+				LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				convertView = inflater.inflate(R.layout.activity_costume_costumelist_item, null);
 			}
-			((CostumeAdapter) getListAdapter()).notifyDataSetChanged();
-		}
-	};
 
-	private void getCostumes() {
-		try {
+			final CostumeData costumeData = costumeDataItems.get(position);
 
-			this.costumeData = new ArrayList<CostumeData>();
-			CostumeData newCostumeData = new CostumeData();
-			newCostumeData.setCostumeFilename(costumeName);
-			newCostumeData.setCostumeName(costumeDisplayName);
-			newCostumeData.setCostumeDataId(costumeId);
-			costumeData.add(newCostumeData);
-			//sprite.getCostumeDataList().add(newCostumeData);
-		} catch (Exception e) {
-			e.printStackTrace();
+			if (costumeData != null) {
+				final ImageView costumeImage = (ImageView) convertView.findViewById(R.id.costume_image);
+				final TextView costumeNameTextField = (TextView) convertView.findViewById(R.id.costume_name);
+				final Button editCostumeButton = (Button) convertView.findViewById(R.id.btn_costume_edit);
+				final Button copyCostumeButton = (Button) convertView.findViewById(R.id.btn_costume_copy);
+				Button deleteCostumeButton = (Button) convertView.findViewById(R.id.btn_costume_delete);
+
+				copyCostumeButton.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_media_play, 0, 0);
+				editCostumeButton.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_media_stop, 0, 0);
+				deleteCostumeButton.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_trash, 0, 0);
+
+				costumeImage.setImageBitmap(costumeData.getThumbnailBitmap());
+				costumeNameTextField.setText(costumeData.getCostumeName());
+
+				copyCostumeButton.setOnClickListener(new View.OnClickListener() {
+					public void onClick(View v) {
+
+						notifyDataSetChanged();
+					}
+				});
+
+				editCostumeButton.setOnClickListener(new View.OnClickListener() {
+					public void onClick(View v) {
+						//TODO: call context menu
+					}
+				});
+
+				deleteCostumeButton.setOnClickListener(new View.OnClickListener() {
+					public void onClick(View v) {
+						costumeDataItems.remove(costumeData);
+						StorageHandler.getInstance().deleteFile(costumeData.getAbsolutePath());
+						notifyDataSetChanged();
+					}
+				});
+
+			}
+			return convertView;
 		}
-		runOnUiThread(returnRes);
 	}
 }
--- catroid/src/at/tugraz/ist/catroid/ui/adapter/CostumeAdapter.java
+++ catroid/src/at/tugraz/ist/catroid/ui/adapter/CostumeAdapter.java
@@ -1,150 +1,150 @@
-/**
- *  Catroid: An on-device graphical programming language for Android devices
- *  Copyright (C) 2010  Catroid development team 
- *  (<http://code.google.com/p/catroid/wiki/Credits>)
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-package at.tugraz.ist.catroid.ui.adapter;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import at.tugraz.ist.catroid.ProjectManager;
-import at.tugraz.ist.catroid.R;
-import at.tugraz.ist.catroid.common.Consts;
-import at.tugraz.ist.catroid.common.CostumeData;
-import at.tugraz.ist.catroid.content.Sprite;
-import at.tugraz.ist.catroid.io.StorageHandler;
-import at.tugraz.ist.catroid.ui.CostumeActivity;
-
-/**
- * @author ainulhusna
- * 
- */
-public class CostumeAdapter extends ArrayAdapter<CostumeData> {
-
-	private ArrayList<CostumeData> items;
-	private CostumeActivity activity;
-	String filemanagerstring, selectedImagePath, imagePath, costumeName, absolutePath, costume, costumeFormat,
-			costumeDisplayName;
-	private Sprite sprite;
-
-	/**
-	 * @param activity2
-	 * @param activityCostumelist
-	 * @param costumeData
-	 * @param sprite2
-	 */
-	public CostumeAdapter(CostumeActivity activity, int activityCostumelist, ArrayList<CostumeData> items, Sprite sprite) {
-		super(activity, activityCostumelist, items);
-		this.sprite = sprite;
-		this.items = items;
-		this.activity = activity;
-	}
-
-	@Override
-	public View getView(final int position, View convertView, ViewGroup parent) {
-		View v = convertView;
-		if (v == null) {
-			LayoutInflater vi = activity.getLayoutInflater();
-			v = vi.inflate(R.layout.activity_costumelist, null);
-		}
-
-		final CostumeData costumeData = items.get(position);
-		if (costumeData != null) {
-			TextView costumeNameTextView = (TextView) v.findViewById(R.id.costume_edit_name);
-			costumeNameTextView.setText(costumeData.getCostumeName());
-
-			Button copyCostume = (Button) v.findViewById(R.id.copy_costume);
-			copyCostume.setOnClickListener(new View.OnClickListener() {
-
-				public void onClick(View v) {
-					int costumeId = 0;
-					absolutePath = costumeData.getAbsolutePath();
-					String costumeName = costumeData.getCostumeName();
-					String costumeFormat = costumeData.getFileExtension();
-
-					for (int i = 0; i < sprite.getCostumeDataList().size(); i++) {
-						if (costumeName.equals(sprite.getCostumeDataList().get(i).getCostumeName())) {
-							if (costumeId <= sprite.getCostumeDataList().get(i).getCostumeId()) {
-								costumeId = sprite.getCostumeDataList().get(i).getCostumeId();
-							}
-						}
-					}
-
-					costumeId = costumeId + 1;
-
-					String costumeDisplayName = costumeName.concat("_" + costumeId);
-					try {
-						File copyFile = StorageHandler.getInstance().copyImage(
-								ProjectManager.getInstance().getCurrentProject().getName(), absolutePath,
-								costumeDisplayName + costumeFormat);
-
-						if (copyFile != null) {
-							absolutePath = copyFile.getName();
-						}
-
-						CostumeData costumeData = new CostumeData();
-						costumeData.setCostumeFilename(copyFile.getName());
-						costumeData.setCostumeName(costumeDisplayName);
-						costumeData.setCostumeDataId(costumeId);
-						items.add(costumeData);
-						notifyDataSetChanged();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-
-				}
-			});
-
-			Button deleteCostume = (Button) v.findViewById(R.id.delete_button);
-			deleteCostume.setOnClickListener(new OnClickListener() {
-
-				public void onClick(View arg0) {
-					if (items.size() > 1) {
-						items.remove(costumeData);
-						//sprite.getCostumeDataList().remove(costumeData);
-						notifyDataSetChanged();
-					}
-				}
-			});
-
-			Button renameCostume = (Button) v.findViewById(R.id.rename_button);
-			renameCostume.setOnClickListener(new OnClickListener() {
-
-				public void onClick(View v) {
-					activity.selectedCostumeInfo = costumeData;
-					activity.showDialog(Consts.DIALOG_RENAME_COSTUME);
-				}
-			});
-
-			ImageView costumeImage = (ImageView) v.findViewById(R.id.costume_image);
-			if (costumeImage != null) {
-				costumeImage.setImageBitmap(costumeData.getThumbnailBitmap());
-			}
-		}
-		return v;
-	}
-
-}
+///**
+// *  Catroid: An on-device graphical programming language for Android devices
+// *  Copyright (C) 2010  Catroid development team 
+// *  (<http://code.google.com/p/catroid/wiki/Credits>)
+// *
+// *  This program is free software: you can redistribute it and/or modify
+// *  it under the terms of the GNU General Public License as published by
+// *  the Free Software Foundation, either version 3 of the License, or
+// *  (at your option) any later version.
+// *
+// *  This program is distributed in the hope that it will be useful,
+// *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+// *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// *  GNU General Public License for more details.
+// *
+// *  You should have received a copy of the GNU General Public License
+// *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// */
+//package at.tugraz.ist.catroid.ui.adapter;
+//
+//import java.io.File;
+//import java.io.IOException;
+//import java.util.ArrayList;
+//
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.View.OnClickListener;
+//import android.view.ViewGroup;
+//import android.widget.ArrayAdapter;
+//import android.widget.Button;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//import at.tugraz.ist.catroid.ProjectManager;
+//import at.tugraz.ist.catroid.R;
+//import at.tugraz.ist.catroid.common.Consts;
+//import at.tugraz.ist.catroid.common.CostumeData;
+//import at.tugraz.ist.catroid.content.Sprite;
+//import at.tugraz.ist.catroid.io.StorageHandler;
+//import at.tugraz.ist.catroid.ui.CostumeActivity;
+//
+///**
+// * @author ainulhusna
+// * 
+// */
+//public class CostumeAdapter extends ArrayAdapter<CostumeData> {
+//
+//	private ArrayList<CostumeData> items;
+//	private CostumeActivity activity;
+//	String filemanagerstring, selectedImagePath, imagePath, costumeName, absolutePath, costume, costumeFormat,
+//			costumeDisplayName;
+//	private Sprite sprite;
+//
+//	/**
+//	 * @param activity2
+//	 * @param activityCostumelist
+//	 * @param costumeData
+//	 * @param sprite2
+//	 */
+//	public CostumeAdapter(CostumeActivity activity, int activityCostumelist, ArrayList<CostumeData> items, Sprite sprite) {
+//		super(activity, activityCostumelist, items);
+//		this.sprite = sprite;
+//		this.items = items;
+//		this.activity = activity;
+//	}
+//
+//	@Override
+//	public View getView(final int position, View convertView, ViewGroup parent) {
+//		View v = convertView;
+//		if (v == null) {
+//			LayoutInflater vi = activity.getLayoutInflater();
+//			v = vi.inflate(R.layout.activity_costumelist, null);
+//		}
+//
+//		final CostumeData costumeData = items.get(position);
+//		if (costumeData != null) {
+//			TextView costumeNameTextView = (TextView) v.findViewById(R.id.costume_edit_name);
+//			costumeNameTextView.setText(costumeData.getCostumeName());
+//
+//			Button copyCostume = (Button) v.findViewById(R.id.copy_costume);
+//			copyCostume.setOnClickListener(new View.OnClickListener() {
+//
+//				public void onClick(View v) {
+//					int costumeId = 0;
+//					absolutePath = costumeData.getAbsolutePath();
+//					String costumeName = costumeData.getCostumeName();
+//					String costumeFormat = costumeData.getFileExtension();
+//
+//					for (int i = 0; i < sprite.getCostumeDataList().size(); i++) {
+//						if (costumeName.equals(sprite.getCostumeDataList().get(i).getCostumeName())) {
+//							if (costumeId <= sprite.getCostumeDataList().get(i).getCostumeId()) {
+//								costumeId = sprite.getCostumeDataList().get(i).getCostumeId();
+//							}
+//						}
+//					}
+//
+//					costumeId = costumeId + 1;
+//
+//					String costumeDisplayName = costumeName.concat("_" + costumeId);
+//					try {
+//						File copyFile = StorageHandler.getInstance().copyImage(
+//								ProjectManager.getInstance().getCurrentProject().getName(), absolutePath,
+//								costumeDisplayName + costumeFormat);
+//
+//						if (copyFile != null) {
+//							absolutePath = copyFile.getName();
+//						}
+//
+//						CostumeData costumeData = new CostumeData();
+//						costumeData.setCostumeFilename(copyFile.getName());
+//						costumeData.setCostumeName(costumeDisplayName);
+//						costumeData.setCostumeDataId(costumeId);
+//						items.add(costumeData);
+//						notifyDataSetChanged();
+//					} catch (IOException e) {
+//						e.printStackTrace();
+//					}
+//
+//				}
+//			});
+//
+//			Button deleteCostume = (Button) v.findViewById(R.id.delete_button);
+//			deleteCostume.setOnClickListener(new OnClickListener() {
+//
+//				public void onClick(View arg0) {
+//					if (items.size() > 1) {
+//						items.remove(costumeData);
+//						//sprite.getCostumeDataList().remove(costumeData);
+//						notifyDataSetChanged();
+//					}
+//				}
+//			});
+//
+//			Button renameCostume = (Button) v.findViewById(R.id.rename_button);
+//			renameCostume.setOnClickListener(new OnClickListener() {
+//
+//				public void onClick(View v) {
+//					activity.selectedCostumeInfo = costumeData;
+//					activity.showDialog(Consts.DIALOG_RENAME_COSTUME);
+//				}
+//			});
+//
+//			ImageView costumeImage = (ImageView) v.findViewById(R.id.costume_image);
+//			if (costumeImage != null) {
+//				costumeImage.setImageBitmap(costumeData.getThumbnailBitmap());
+//			}
+//		}
+//		return v;
+//	}
+//
+//}
