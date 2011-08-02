747c7407acd67c5d8506262a7c21d9402c30c6dc
Daniel Markart <daniel.markart@gmail.com>
2011-08-02 18:39:56 +0200

    refactoring of some tests - soundactivity, costumeactivity, playsoundbrick, setcostumebrick

--- catroidUiTest/src/at/tugraz/ist/catroid/uitest/content/brick/SetCostumeBrickTest.java
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/content/brick/SetCostumeBrickTest.java
@@ -1,118 +1,133 @@
-///**
-// *  Catroid: An on-device graphical programming language for Android devices
-// *  Copyright (C) 2010  Catroid development team
-// *  (<http://code.google.com/p/catroid/wiki/Credits>)
-// *
-// *  This program is free software: you can redistribute it and/or modify
-// *  it under the terms of the GNU General Public License as published by
-// *  the Free Software Foundation, either version 3 of the License, or
-// *  (at your option) any later version.
-// *
-// *  This program is distributed in the hope that it will be useful,
-// *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-// *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// *  GNU General Public License for more details.
-// *
-// *  You should have received a copy of the GNU General Public License
-// *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// */
-//package at.tugraz.ist.catroid.uitest.content.brick;
-//
-//import java.io.File;
-//import java.util.List;
-//
-//import android.content.ComponentName;
-//import android.content.Intent;
-//import android.graphics.Bitmap;
-//import android.graphics.BitmapFactory;
-//import android.test.ActivityInstrumentationTestCase2;
-//import android.view.View;
-//import android.view.View.OnClickListener;
-//import android.widget.ImageView;
-//import at.tugraz.ist.catroid.R;
-//import at.tugraz.ist.catroid.content.bricks.Brick;
-//import at.tugraz.ist.catroid.ui.ScriptTabActivity;
-//import at.tugraz.ist.catroid.uitest.util.UiTestUtils;
-//import at.tugraz.ist.catroid.utils.UtilFile;
-//
-//import com.jayway.android.robotium.solo.Solo;
-//
-//public class SetCostumeBrickTest extends ActivityInstrumentationTestCase2<ScriptTabActivity> {
-//	private Solo solo;
-//	private ImageView setCostumeImageView;
-//	private File imageFile;
-//
-//	public SetCostumeBrickTest() {
-//		super("at.tugraz.ist.catroid", ScriptTabActivity.class);
-//	}
-//
-//	@Override
-//	public void setUp() throws Exception {
-//		super.setUp();
-//		List<Brick> brickList = UiTestUtils.createTestProject();
-//
-//		solo = new Solo(getInstrumentation(), getActivity());
-//		solo.clickOnText("Costumes");
-//		solo.clickOnText("Script");
-//		solo.sleep(100);
-//		UiTestUtils.addNewBrickAndScrollDown(solo, R.string.brick_set_costume);
-//		final int setCostumeBrickIndex = brickList.size();
-//
-//		//setCostumeImageView = (ImageView) solo.getCurrentActivity().findViewById(R.id.costume_image_view);
-//		//setCostumeImageView.setDrawingCacheEnabled(true);
-//
-//		// Copy test image to be used
-//
-//		final int RESOURCE_LOCATION = R.drawable.catroid_sunglasses;
-//		imageFile = UtilFile.saveFileToProject(UiTestUtils.DEFAULT_TEST_PROJECT_NAME, "catroid_sunglasses.png",
-//				RESOURCE_LOCATION, getActivity(), UiTestUtils.TYPE_IMAGE_FILE);
-//
-//		final File imageFileReference = imageFile;
-//
-//		// Override OnClickListener to launch MockGalleryActivity
-//		OnClickListener listener = new OnClickListener() {
-//			public void onClick(View v) {
-//				final Intent intent = new Intent("android.intent.action.MAIN");
-//				intent.setComponent(new ComponentName("at.tugraz.ist.catroid.uitest",
-//						"at.tugraz.ist.catroid.uitest.mockups.MockGalleryActivity"));
-//				intent.putExtra("filePath", imageFileReference.getAbsolutePath());
-//				solo.getActivityMonitor().getLastActivity().startActivityForResult(intent, setCostumeBrickIndex);
-//			}
-//		};
-//
-//		assertNotNull("ImageView of change costume brick was not found", setCostumeImageView);
-//		setCostumeImageView.setOnClickListener(listener);
-//	}
-//
-//	@Override
-//	public void tearDown() throws Exception {
-//		if (imageFile.exists()) {
-//			imageFile.delete();
-//		}
-//
-//		try {
-//			solo.finalize();
-//		} catch (Throwable e) {
-//			e.printStackTrace();
-//		}
-//
-//		getActivity().finish();
-//		super.tearDown();
-//	}
-//
-//	public void testChangeCostume() {
-//		solo.sleep(1000);
-//		solo.clickOnView(setCostumeImageView);
-//		solo.sleep(5000);
-//
-//		Bitmap bitmapToTest = setCostumeImageView.getDrawingCache();
-//		assertNotNull("Bitmap of costume ImageView is null", bitmapToTest);
-//
-//		Bitmap originalBitmap = BitmapFactory.decodeResource(getInstrumentation().getContext().getResources(),
-//				at.tugraz.ist.catroid.uitest.R.drawable.catroid_sunglasses);
-//		assertNotNull("Decoding the costume resource failed", originalBitmap);
-//
-//		solo.sleep(1000);
-//
-//	}
-//}
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010  Catroid development team
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.tugraz.ist.catroid.uitest.content.brick;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import android.test.ActivityInstrumentationTestCase2;
+import at.tugraz.ist.catroid.ProjectManager;
+import at.tugraz.ist.catroid.R;
+import at.tugraz.ist.catroid.common.CostumeData;
+import at.tugraz.ist.catroid.content.Costume;
+import at.tugraz.ist.catroid.content.Project;
+import at.tugraz.ist.catroid.content.Script;
+import at.tugraz.ist.catroid.content.Sprite;
+import at.tugraz.ist.catroid.content.StartScript;
+import at.tugraz.ist.catroid.content.bricks.SetCostumeBrick;
+import at.tugraz.ist.catroid.ui.ScriptTabActivity;
+import at.tugraz.ist.catroid.uitest.util.UiTestUtils;
+
+import com.jayway.android.robotium.solo.Solo;
+
+public class SetCostumeBrickTest extends ActivityInstrumentationTestCase2<ScriptTabActivity> {
+	private Solo solo;
+	private String costumeName = "testCostume1";
+	private String costumeName2 = "testCostume2";
+	private File costumeFile;
+	private File costumeFile2;
+	private ArrayList<CostumeData> costumeDataList;
+	private final int RESOURCE_COSTUME = at.tugraz.ist.catroid.uitest.R.raw.icon;
+	private final int RESOURCE_COSTUME2 = at.tugraz.ist.catroid.uitest.R.raw.icon2;
+
+	public SetCostumeBrickTest() {
+		super("at.tugraz.ist.catroid", ScriptTabActivity.class);
+	}
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		UiTestUtils.clearAllUtilTestProjects();
+
+		ProjectManager projectManager = ProjectManager.getInstance();
+		Project project = new Project(null, UiTestUtils.DEFAULT_TEST_PROJECT_NAME);
+		Sprite firstSprite = new Sprite("cat");
+		Script testScript = new StartScript("testscript", firstSprite);
+
+		SetCostumeBrick setCostumeBrick = new SetCostumeBrick(firstSprite);
+		testScript.addBrick(setCostumeBrick);
+
+		firstSprite.addScript(testScript);
+		project.addSprite(firstSprite);
+
+		projectManager.setProject(project);
+		projectManager.setCurrentSprite(firstSprite);
+		projectManager.setCurrentScript(testScript);
+		costumeDataList = projectManager.getCurrentSprite().getCostumeDataList();
+
+		costumeFile = UiTestUtils.saveFileToProject(UiTestUtils.DEFAULT_TEST_PROJECT_NAME, "image.png",
+				RESOURCE_COSTUME, getInstrumentation().getContext(), UiTestUtils.TYPE_IMAGE_FILE);
+		CostumeData costumeData = new CostumeData();
+		costumeData.setCostumeFilename(costumeFile.getName());
+		costumeData.setCostumeName(costumeName);
+
+		costumeFile2 = UiTestUtils.saveFileToProject(UiTestUtils.DEFAULT_TEST_PROJECT_NAME, "image2.png",
+				RESOURCE_COSTUME2, getInstrumentation().getContext(), UiTestUtils.TYPE_IMAGE_FILE);
+		CostumeData costumeData2 = new CostumeData();
+		costumeData2.setCostumeFilename(costumeFile2.getName());
+		costumeData2.setCostumeName(costumeName2);
+
+		costumeDataList.add(costumeData);
+		costumeDataList.add(costumeData2);
+		ProjectManager.getInstance().fileChecksumContainer.addChecksum(costumeData.getChecksum(),
+				costumeData.getAbsolutePath());
+		ProjectManager.getInstance().fileChecksumContainer.addChecksum(costumeData2.getChecksum(),
+				costumeData2.getAbsolutePath());
+
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		try {
+			solo.finalize();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		getActivity().finish();
+		UiTestUtils.clearAllUtilTestProjects();
+		super.tearDown();
+	}
+
+	public void testSelectCostumeAndPlay() {
+		solo.sleep(100);
+		solo.clickOnText(getActivity().getString(R.string.broadcast_nothing_selected));
+		solo.sleep(100);
+		solo.clickOnText(costumeName);
+		solo.sleep(100);
+		assertTrue(costumeName + " is not selected in Spinner", solo.searchText(costumeName));
+		UiTestUtils.clickOnImageButton(solo, R.id.btn_action_play);
+		solo.sleep(7000);
+		Costume costume = ProjectManager.getInstance().getCurrentProject().getSpriteList().get(0).getCostume();
+		assertEquals("costume not set", costume.getImagePath(), costumeDataList.get(0).getAbsolutePath());
+		solo.goBack();
+		solo.sleep(200);
+
+		//changing le sound
+		solo.clickOnText(costumeName);
+		solo.sleep(100);
+		solo.clickOnText(costumeName2);
+		solo.sleep(100);
+		assertTrue(costumeName2 + " is not selected in Spinner", solo.searchText(costumeName2));
+		UiTestUtils.clickOnImageButton(solo, R.id.btn_action_play);
+		solo.sleep(7000);
+		costume = ProjectManager.getInstance().getCurrentProject().getSpriteList().get(0).getCostume();
+		assertEquals("costume not set", costume.getImagePath(), costumeDataList.get(1).getAbsolutePath());
+	}
+}
