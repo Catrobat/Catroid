66362f851b8078efb69e7e90c284fa1a69ca915f
Daniel Markart <daniel.markart@gmail.com>
2011-08-05 00:19:52 +0200

    small changes in tab layout

--- catroid/src/at/tugraz/ist/catroid/ui/adapter/CustomIconContextMenu.java
+++ catroid/src/at/tugraz/ist/catroid/ui/adapter/CustomIconContextMenu.java
@@ -1,8 +1,6 @@
-/*
- *  Copyright (C) 2010 Tani Group 
- *
+/**
  *  Catroid: An on-device graphical programming language for Android devices
- *  Copyright (C) 2010  Catroid development team 
+ *  Copyright (C) 2010  Catroid development team
  *  (<http://code.google.com/p/catroid/wiki/Credits>)
  *
  *  This program is free software: you can redistribute it and/or modify
