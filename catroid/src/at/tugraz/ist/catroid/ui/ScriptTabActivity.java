297df2dfc1cef8dbd1d4439d246f4641272ff30c
Daniel Markart <daniel.markart@gmail.com>
2011-08-11 18:31:05 +0200

    Starting 'marki2' branch (with some google code snippets (won't compile))

--- catroid/src/at/tugraz/ist/catroid/ui/ScriptTabActivity.java
+++ catroid/src/at/tugraz/ist/catroid/ui/ScriptTabActivity.java
@@ -19,14 +19,23 @@
 
 package at.tugraz.ist.catroid.ui;
 
+import java.util.TimeZone;
+
 import android.app.Dialog;
 import android.app.TabActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.util.Pair;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
@@ -177,6 +186,95 @@ public class ScriptTabActivity extends TabActivity {
 
 	public void handleNegativeButtonRenameCostume(View v) {
 		dismissDialog(Consts.DIALOG_RENAME_COSTUME);
+	}
+
+	public static class FlingableTabHost extends TabHost {
+		GestureDetector mGestureDetector;
+
+		Animation mRightInAnimation;
+		Animation mRightOutAnimation;
+		Animation mLeftInAnimation;
+		Animation mLeftOutAnimation;
+
+		public FlingableTabHost(Context context, AttributeSet attrs) {
+			super(context, attrs);
+
+			mRightInAnimation = AnimationUtils.loadAnimation(context, R.anim.slide_right_in);
+			mRightOutAnimation = AnimationUtils.loadAnimation(context, R.anim.slide_right_out);
+			mLeftInAnimation = AnimationUtils.loadAnimation(context, R.anim.slide_left_in);
+			mLeftOutAnimation = AnimationUtils.loadAnimation(context, R.anim.slide_left_out);
+
+			final int minScaledFlingVelocity = ViewConfiguration.get(context).getScaledMinimumFlingVelocity() * 10; // 10 = fudge by experimentation
+
+			mGestureDetector = new GestureDetector(new GestureDetector.SimpleOnGestureListener() {
+				@Override
+				public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+					int tabCount = getTabWidget().getTabCount();
+					int currentTab = getCurrentTab();
+					if (Math.abs(velocityX) > minScaledFlingVelocity && Math.abs(velocityY) < minScaledFlingVelocity) {
+
+						final boolean right = velocityX < 0;
+						final int newTab = MathUtils.constrain(currentTab + (right ? 1 : -1), 0, tabCount - 1);
+						if (newTab != currentTab) {
+							// Somewhat hacky, depends on current implementation of TabHost:
+							// http://android.git.kernel.org/?p=platform/frameworks/base.git;a=blob;
+							// f=core/java/android/widget/TabHost.java
+							View currentView = getCurrentView();
+							setCurrentTab(newTab);
+							View newView = getCurrentView();
+
+							newView.startAnimation(right ? mRightInAnimation : mLeftInAnimation);
+							currentView.startAnimation(right ? mRightOutAnimation : mLeftOutAnimation);
+						}
+					}
+					return super.onFling(e1, e2, velocityX, velocityY);
+				}
+			});
+		}
+
+		@Override
+		public boolean onInterceptTouchEvent(MotionEvent ev) {
+			if (mGestureDetector.onTouchEvent(ev)) {
+				return true;
+			}
+			return super.onInterceptTouchEvent(ev);
+		}
+	}
+
+	private void setupBlocksTab(String tag, long startMillis) {
+		final TabHost host = getTabHost();
+
+		final long endMillis = startMillis + DateUtils.DAY_IN_MILLIS;
+		final Uri blocksBetweenDirUri = Blocks.buildBlocksBetweenDirUri(startMillis, endMillis);
+
+		final Intent intent = new Intent(Intent.ACTION_VIEW, blocksBetweenDirUri);
+		intent.addCategory(Intent.CATEGORY_TAB);
+
+		intent.putExtra(BlocksActivity.EXTRA_TIME_START, startMillis);
+		intent.putExtra(BlocksActivity.EXTRA_TIME_END, endMillis);
+
+		TimeZone.setDefault(UIUtils.CONFERENCE_TIME_ZONE);
+		final String label = DateUtils.formatDateTime(this, startMillis, TIME_FLAGS);
+		host.addTab(host.newTabSpec(tag).setIndicator(buildIndicator(label)).setContent(intent));
+	}
+
+	/**
+	 * Build a {@link View} to be used as a tab indicator, setting the requested
+	 * string resource as its label.
+	 */
+	private View buildIndicator(String text) {
+		final TextView indicator = (TextView) getLayoutInflater()
+				.inflate(R.layout.tab_indicator, getTabWidget(), false);
+		indicator.setText(text);
+		return indicator;
+	}
+
+	public void onHomeClick(View v) {
+		UIUtils.goHome(this);
+	}
+
+	public void onSearchClick(View v) {
+		UIUtils.goSearch(this);
 	}
 
 }
