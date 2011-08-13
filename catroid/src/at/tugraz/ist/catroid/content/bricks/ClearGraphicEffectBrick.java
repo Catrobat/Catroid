53ffd6c961bb62d864a7600d7502ee9c5574d873
Johannes Iber <johannes.iber@gmail.com>
2011-08-14 00:46:24 +0200

    added brightness support and some small fixes

--- catroid/src/at/tugraz/ist/catroid/content/bricks/ClearGraphicEffectBrick.java
+++ catroid/src/at/tugraz/ist/catroid/content/bricks/ClearGraphicEffectBrick.java
@@ -34,6 +34,7 @@ public class ClearGraphicEffectBrick implements Brick {
 	}
 
 	public void execute() {
+		sprite.costume.setBrightness(1f);
 		sprite.costume.setAlphaValue(1f);
 		sprite.costume.setSize(1f);
 	}
