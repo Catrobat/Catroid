777cabc6a79f7a4f5b18f7a5219bc44746200882
Fatin Ghazi <fatintastic@gmail.com>
2011-09-08 10:33:16 +0200

    NextCostumeBrick

--- catroid/src/at/tugraz/ist/catroid/content/Sprite.java
+++ catroid/src/at/tugraz/ist/catroid/content/Sprite.java
@@ -83,7 +83,7 @@ public class Sprite implements Serializable, Comparable<Sprite> {
 		isPaused = false;
 		isFinished = false;
 		speechBubble = new SpeechBubble();
-		
+
 		if (soundList == null) {
 			soundList = new ArrayList<SoundInfo>();
 		}
