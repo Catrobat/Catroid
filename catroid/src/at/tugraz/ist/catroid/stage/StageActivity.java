894ede07c0d11b0db7f788fd9c2fb38255b3ac21
Peter Tielsch <p.tielsch@gmail.com>
2011-09-09 12:21:56 +0200

    blub2

--- catroid/src/at/tugraz/ist/catroid/stage/StageActivity.java
+++ catroid/src/at/tugraz/ist/catroid/stage/StageActivity.java
@@ -44,6 +44,7 @@ import at.tugraz.ist.catroid.ProjectManager;
 import at.tugraz.ist.catroid.R;
 import at.tugraz.ist.catroid.LegoNXT.LegoNXT;
 import at.tugraz.ist.catroid.LegoNXT.LegoNXTBtCommunicator;
+import at.tugraz.ist.catroid.arduino.Arduino;
 import at.tugraz.ist.catroid.bluetooth.BluetoothManager;
 import at.tugraz.ist.catroid.bluetooth.DeviceListActivity;
 import at.tugraz.ist.catroid.io.SoundManager;
