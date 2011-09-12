32d8cef670cf092cb966ad20fb8cbc91bf37978f
Peter Tielsch <p.tielsch@gmail.com>
2011-09-12 16:53:25 +0200

    Improved Functionality and usability of LegoNXT Bluetooth Test system. added library for ubuntu compatibility

--- catroidLegoNXTBTTest/src/at/tugraz/ist/catroid/LegoNXTBTTestServer/BTServer.java
+++ catroidLegoNXTBTTest/src/at/tugraz/ist/catroid/LegoNXTBTTestServer/BTServer.java
@@ -1,11 +1,6 @@
 package at.tugraz.ist.catroid.LegoNXTBTTestServer;
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
  
 import javax.bluetooth.*;
 import javax.microedition.io.*;
@@ -16,102 +11,47 @@ import javax.microedition.io.*;
  */
 public class BTServer {
     
+
+	private boolean run = true;
+	static StreamConnection connection = null;
+	static BTServer btServer;
 	
-	public static byte DIRECT_COMMAND_REPLY = 0x00;
-	public static byte SYSTEM_COMMAND_REPLY = 0x01;
-	public static byte REPLY_COMMAND = 0x02;
-	public static byte DIRECT_COMMAND_NOREPLY = (byte) 0x80; // Avoids ~100ms latency
-	public static byte SYSTEM_COMMAND_NOREPLY = (byte) 0x81; // Avoids ~100ms latency
-	public static final byte SET_OUTPUT_STATE = 0x04;
-	public static final byte SET_INPUT_MODE = 0x05;
-	public static final byte GET_OUTPUT_STATE = 0x06;
-	public static final byte GET_INPUT_VALUES = 0x07;
-    //start server
     private void startServer() throws IOException{
  
         //Create a UUID for SPP
         UUID uuid = new UUID("1101", true);
         //Create the servicve url
-        String connectionString = "btspp://localhost:" + uuid +";name=NXT Test Server";
+        String connectionString = "btspp://localhost:" + uuid +";name=BT Test Server";
         
         //open server url
         StreamConnectionNotifier streamConnNotifier = (StreamConnectionNotifier)Connector.open( connectionString );
         
         //Wait for client connection
-        System.out.println("\nServer Started. Waiting for NXT Test client...");
-        StreamConnection connection=streamConnNotifier.acceptAndOpen();
- 
-        RemoteDevice dev = RemoteDevice.getRemoteDevice(connection);
-        System.out.println("Remote device address: "+dev.getBluetoothAddress());
-        System.out.println("Remote device name: "+dev.getFriendlyName(true));
+        GUI.writeMessage("Bluetooth Server started. Waiting for Bluetooth test clients... \n");
         
-        //read string from spp client
-        InputStream inStream=connection.openInputStream();
-        BufferedReader bReader=new BufferedReader(new InputStreamReader(inStream));
-        
-        OutputStream outStream=connection.openOutputStream();
-        PrintWriter pWriter=new PrintWriter(new OutputStreamWriter(outStream));
-        
-        char[] lastMessage = null;
-        int messageLength = 0;
-        int firstLenByte = 0;
-        int secondLenByte = 0;
-        while((firstLenByte = bReader.read()) != -1){
-        	secondLenByte = (bReader.read() << 8);
-        	messageLength = firstLenByte + secondLenByte;
-        	System.out.println("Received Message, length: " + messageLength);
-        	char[] buf = new char[messageLength];
-        	char[] reply = null;
-        	bReader.read(buf);
-        	
-        	//reply required?
-        	if((messageLength == 3) && (buf[0] == DIRECT_COMMAND_REPLY) && (buf[1] == GET_OUTPUT_STATE)){
-        		reply = getReplyMessage(lastMessage);
-        	}
-            
-        	if(buf[0] == DIRECT_COMMAND_REPLY){
-        		System.out.println("sending reply");
-	        	pWriter.write(reply.length);
-	            pWriter.write(0);
-	            pWriter.write(reply);
-	            pWriter.flush();
-        	}
-            lastMessage = buf;
+        while(run){
+        	connection=streamConnNotifier.acceptAndOpen();
+        	BTClientHandler btc = new BTClientHandler(connection);
+        	btc.start();
         }
- 
-        pWriter.close();
+        
         streamConnNotifier.close();
- 
     }
- 
-    public char[] getReplyMessage(char[] lastMessage){
-    	
-		char[] reply = new char[32];
-		reply[0] = (char)REPLY_COMMAND;
-		reply[1] = (char)GET_OUTPUT_STATE;
-		reply[3] = lastMessage[2]; //used motor
-		reply[2] = 0; //status 0 = no error
-		reply[4] = lastMessage[3]; //speed
-		reply[5] = lastMessage[4];
-		reply[6] = lastMessage[5];
-		reply[7] = lastMessage[6];
-		reply[8] = lastMessage[7];
-		reply[9] = lastMessage[8];
-		reply[10] = lastMessage[9];
-		reply[11] = lastMessage[10];
-    	return reply;
-    }
- 
+
     public static void main(String[] args){
         
     	try{
+    		GUI.startGUI();
 	        //display local device address and name
 	        LocalDevice localDevice = LocalDevice.getLocalDevice();
-	        System.out.println("Address: "+localDevice.getBluetoothAddress());
-	        System.out.println("Name: "+localDevice.getFriendlyName());
+	        GUI.writeMessage("Local System:\n");
+	        GUI.writeMessage("Address: "+localDevice.getBluetoothAddress() + "\n");
+	        GUI.writeMessage("Name: "+localDevice.getFriendlyName() + "\n");
 	        
-	        BTServer btServer=new BTServer();
+	        
+	        btServer = new BTServer();
 	        btServer.startServer();
+	        
     	}
     	catch(IOException e){
     		e.printStackTrace();
