4347ff95260b2b2af70a67a667b6492029b5b96d
Johannes Iber <johannes.iber@gmail.com>
2011-09-28 19:03:39 +0200

    added stage tests

---
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/stage/SimpleStageTest.java
@@ -0,0 +1,89 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010  Catroid development team 
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package at.tugraz.ist.catroid.uitest.stage;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import at.tugraz.ist.catroid.ProjectManager;
+
+import at.tugraz.ist.catroid.common.Values;
+import at.tugraz.ist.catroid.content.Project;
+import at.tugraz.ist.catroid.io.StorageHandler;
+import at.tugraz.ist.catroid.stage.StageActivity;
+
+import com.jayway.android.robotium.solo.Solo;
+
+public class SimpleStageTest extends ActivityInstrumentationTestCase2<StageActivity> {
+	private Solo solo;
+
+	public SimpleStageTest() {
+		super("at.tugraz.ist.catroid", StageActivity.class);
+	}
+
+	@Override
+	public void setUp() throws Exception {
+		createProject();
+		solo = new Solo(getInstrumentation(), getActivity());
+		super.setUp();
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		try {
+			solo.finalize();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		getActivity().finish();
+
+		super.tearDown();
+	}
+
+	public void testSimple() {
+		solo.waitForActivity("StageActivity");
+		byte[] result = StageActivity.stageListener.getPixels(0, 0, 1, 1);
+		assertEquals("Color not white!", (byte) 255, result[0]);
+		assertEquals("Color not white!", (byte) 251, result[1]);
+		assertEquals("Color not white!", (byte) 255, result[2]);
+		assertEquals("Color not white!", (byte) 255, result[3]);
+
+		result = StageActivity.stageListener.getPixels(19, 19, 1, 1);
+		assertEquals("Color not white!", (byte) 255, result[0]);
+		assertEquals("Color not white!", (byte) 255, result[1]);
+		assertEquals("Color not white!", (byte) 255, result[2]);
+		assertEquals("Color not white!", (byte) 255, result[3]);
+
+		result = StageActivity.stageListener.getPixels(-1, -1, 1, 1);
+		assertEquals("Color not black!", (byte) 0, result[0]);
+		assertEquals("Color not black!", (byte) 0, result[1]);
+		assertEquals("Color not black!", (byte) 0, result[2]);
+		assertEquals("Color not black!", (byte) 0, result[3]);
+	}
+
+	private void createProject() {
+		Values.SCREEN_HEIGHT = 20;
+		Values.SCREEN_WIDTH = 20;
+		Project project = new Project(null, "simpleStageProject");
+
+		ProjectManager.getInstance().setProject(project);
+		StorageHandler.getInstance().saveProject(project);
+	}
+
+}
