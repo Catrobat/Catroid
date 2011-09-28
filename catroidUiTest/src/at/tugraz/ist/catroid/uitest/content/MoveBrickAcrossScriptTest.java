fbd79992cefe4e92d36343b669814cc1be38bda3
Christoph Kahr <christophk27@gmail.com>
2011-09-28 17:01:36 +0200

    add catroidUiTest MoveBrickAcrossScriptTest

---
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/content/MoveBrickAcrossScriptTest.java
@@ -0,0 +1,77 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010  Catroid development team 
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package at.tugraz.ist.catroid.uitest.content;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.Smoke;
+import at.tugraz.ist.catroid.ProjectManager;
+import at.tugraz.ist.catroid.content.Project;
+import at.tugraz.ist.catroid.content.Script;
+import at.tugraz.ist.catroid.content.Sprite;
+import at.tugraz.ist.catroid.content.StartScript;
+import at.tugraz.ist.catroid.ui.ScriptActivity;
+
+import com.jayway.android.robotium.solo.Solo;
+
+public class MoveBrickAcrossScriptTest extends ActivityInstrumentationTestCase2<ScriptActivity> {
+	private Solo solo;
+	private Project project;
+
+	public MoveBrickAcrossScriptTest() {
+		super("at.tugraz.ist.catroid", ScriptActivity.class);
+	}
+
+	@Override
+	public void setUp() throws Exception {
+		createProject();
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		try {
+			solo.finalize();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+
+		getActivity().finish();
+		super.tearDown();
+	}
+
+	private void createProject() {
+		project = new Project(null, "testProject");
+		Sprite sprite = new Sprite("cat");
+		Script script = new StartScript("script", sprite);
+
+		sprite.addScript(script);
+		project.addSprite(sprite);
+
+		ProjectManager.getInstance().setProject(project);
+		ProjectManager.getInstance().setCurrentSprite(sprite);
+		ProjectManager.getInstance().setCurrentScript(script);
+	}
+
+	@Smoke
+	public void testMoveBrickAcrossScriptbrick() {
+		// To do
+	}
+
+}
