71e95fcee234cf5e0158f7d2f082b2ee9f9eebc4
Christoph Kahr <christophk27@gmail.com>
2011-09-29 17:35:02 +0200

    fixed uiTests

--- catroidUiTest/src/at/tugraz/ist/catroid/uitest/ui/ProjectActivityTest.java
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/ui/ProjectActivityTest.java
@@ -90,6 +90,7 @@ public class ProjectActivityTest extends ActivityInstrumentationTestCase2<MainMe
 		assertTrue("EditText field got cleared after changing orientation", solo.searchText(sometext));
 		solo.sleep(600);
 		solo.setActivityOrientation(Solo.PORTRAIT);
+		solo.goBack();
 		solo.clickOnButton(0);
 		solo.sleep(100);
 
@@ -145,6 +146,7 @@ public class ProjectActivityTest extends ActivityInstrumentationTestCase2<MainMe
 		solo.setActivityOrientation(Solo.LANDSCAPE);
 		solo.sleep(500);
 		solo.setActivityOrientation(Solo.PORTRAIT);
+		solo.goBack();
 		solo.clickOnButton(0);
 		solo.sleep(50);
 
@@ -314,7 +316,6 @@ public class ProjectActivityTest extends ActivityInstrumentationTestCase2<MainMe
 		solo.setActivityOrientation(Solo.LANDSCAPE);
 		solo.sleep(600);
 		solo.setActivityOrientation(Solo.PORTRAIT);
-		solo.goBack();
 		solo.clickOnButton(0);
 		assertTrue("not in NewSpriteDialog", solo.searchText(getActivity().getString(R.string.new_sprite_dialog_title)));
 	}
