f0e9d4e180f9e438f2f5690a51e6557c2e4c54ca
Johannes Iber <johannes.iber@gmail.com>
2011-10-02 22:10:00 +0200

    changed screenshot method, now using android bitmap and also file size is much lower

--- catroid/src/at/tugraz/ist/catroid/stage/StageListener.java
+++ catroid/src/at/tugraz/ist/catroid/stage/StageListener.java
@@ -25,6 +25,9 @@ import java.util.Comparator;
 import java.util.List;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Color;
 import at.tugraz.ist.catroid.ProjectManager;
 import at.tugraz.ist.catroid.common.Consts;
 import at.tugraz.ist.catroid.common.Values;
@@ -32,7 +35,6 @@ import at.tugraz.ist.catroid.content.Project;
 import at.tugraz.ist.catroid.content.Sprite;
 import at.tugraz.ist.catroid.io.SoundManager;
 import at.tugraz.ist.catroid.ui.dialogs.StageDialog;
-import at.tugraz.ist.catroid.utils.RGBA8888ToPngEncoder;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
@@ -293,7 +295,7 @@ public class StageListener implements ApplicationListener {
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-				this.saveThumbnail();
+				this.makeThumbnail();
 			}
 			makeFirstScreenshot = false;
 		}
@@ -364,18 +366,10 @@ public class StageListener implements ApplicationListener {
 		}
 	}
 
-	private void saveThumbnail() {
+	private void makeThumbnail() {
 		byte[] screenshot = ScreenUtils.getFrameBufferPixels(screenshotX, screenshotY, screenshotWidth,
 				screenshotHeight, true);
-		FileHandle image = Gdx.files.absolute(pathForScreenshot + Consts.SCREENSHOT_FILE_NAME);
-		OutputStream stream = image.write(false);
-		try {
-			byte[] bytes = RGBA8888ToPngEncoder.toPNG(screenshot, screenshotWidth, screenshotHeight);
-			stream.write(bytes);
-			stream.close();
-		} catch (IOException e) {
-		}
-
+		this.saveScreenshot(screenshot);
 	}
 
 	public boolean makeScreenshot() {
@@ -383,11 +377,25 @@ public class StageListener implements ApplicationListener {
 		while (makeScreenshot) {
 			Thread.yield();
 		}
+		return this.saveScreenshot(this.screenshot);
+	}
+
+	private boolean saveScreenshot(byte[] screenshot) {
+		int length = screenshot.length;
+		int[] colors = new int[length / 4];
+
+		for (int i = 0; i < length; i += 4) {
+			colors[i / 4] = Color.argb(255, screenshot[i + 0] & 0xFF, screenshot[i + 1] & 0xFF,
+					screenshot[i + 2] & 0xFF);
+		}
+
+		Bitmap bitmap = Bitmap.createBitmap(colors, 0, screenshotWidth, screenshotWidth, screenshotHeight,
+				Config.ARGB_8888);
+
 		FileHandle image = Gdx.files.absolute(pathForScreenshot + Consts.SCREENSHOT_FILE_NAME);
 		OutputStream stream = image.write(false);
 		try {
-			byte[] bytes = RGBA8888ToPngEncoder.toPNG(screenshot, screenshotWidth, screenshotHeight);
-			stream.write(bytes);
+			bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
 			stream.close();
 		} catch (IOException e) {
 			return false;
