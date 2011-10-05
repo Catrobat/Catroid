1f072de73d47e278373b2eb39281f0cf96996661
David Reisenberger <threesn.b@gmail.com>
2011-10-05 12:25:08 +0200

    License text updated

--- catroid/src/at/tugraz/ist/catroid/bluetooth/BTConnectable.java
+++ catroid/src/at/tugraz/ist/catroid/bluetooth/BTConnectable.java
@@ -1,31 +1,33 @@
-/*    Catroid: An on-device graphical programming language for Android devices
- *    Copyright (C) 2010  Catroid development team
- *    (<http://code.google.com/p/catroid/wiki/Credits>)
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010-2011 The Catroid Team
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *    
- *    This file incorporates work covered by the following copyright and  
- *    permission notice: 
+ *  This file incorporates work covered by the following copyright and  
+ *  permission notice: 
  *    
- *		   	Copyright 2010 Guenther Hoelzl, Shawn Brown
+ *		  Copyright 2010 Guenther Hoelzl, Shawn Brown
  *
- *		   	This file is part of MINDdroid.
+ *		  This file is part of MINDdroid.
  *
- * 		  	MINDdroid is free software: you can redistribute it and/or modify
- * 		  	it under the terms of the GNU General Public License as published by
- * 		  	the Free Software Foundation, either version 3 of the License, or
- *   		(at your option) any later version.
+ * 		  MINDdroid is free software: you can redistribute it and/or modify
+ * 		  it under the terms of the GNU General Public License as published by
+ * 		  the Free Software Foundation, either version 3 of the License, or
+ *   	  (at your option) any later version.
  *
  *   		MINDdroid is distributed in the hope that it will be useful,
  *   		but WITHOUT ANY WARRANTY; without even the implied warranty of
