942114be742df0bc9798044969134707c0b1c18c
Daniel Markart <daniel.markart@gmail.com>
2011-10-06 22:34:44 +0200

    fixed whenbricktest and setsizetobricktest (pretty awesome)

--- catroidTest/src/at/tugraz/ist/catroid/test/content/brick/WhenBrickTest.java
+++ catroidTest/src/at/tugraz/ist/catroid/test/content/brick/WhenBrickTest.java
@@ -23,23 +23,27 @@
 package at.tugraz.ist.catroid.test.content.brick;
 
 import android.test.AndroidTestCase;
+import at.tugraz.ist.catroid.content.Sprite;
+import at.tugraz.ist.catroid.content.WhenScript;
+import at.tugraz.ist.catroid.content.bricks.Brick;
+import at.tugraz.ist.catroid.content.bricks.PlaceAtBrick;
 
 public class WhenBrickTest extends AndroidTestCase {
 
-//	public void testWhenBrick() throws InterruptedException {
-//		int testPosition = 100;
-//
-//		Sprite sprite = new Sprite("new sprite");
-//		WhenScript whenScript = new WhenScript("script", sprite);
-//		whenScript.setAction(1);
-//		Brick placeAtBrick = new PlaceAtBrick(sprite, testPosition, testPosition);
-//		whenScript.addBrick(placeAtBrick);
-//		sprite.addScript(whenScript);
-//		sprite.startWhenScripts(whenScript.getAction());
-//
-//		Thread.sleep(1000);
-//
-//		assertEquals("Simple broadcast failed", testPosition, sprite.getXPosition());
-//	}
+	public void testWhenBrick() throws InterruptedException {
+		int testPosition = 100;
+
+		Sprite sprite = new Sprite("new sprite");
+		WhenScript whenScript = new WhenScript("script", sprite);
+		whenScript.setAction(1);
+		Brick placeAtBrick = new PlaceAtBrick(sprite, testPosition, testPosition);
+		whenScript.addBrick(placeAtBrick);
+		sprite.addScript(whenScript);
+		sprite.startWhenScripts(whenScript.getAction());
+
+		Thread.sleep(1000);
+
+		assertEquals("Simple broadcast failed", (float) testPosition, sprite.costume.x);
+	}
 
 }
