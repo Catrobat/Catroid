87b6fd1f881d91b0bc5cad189d3bcd80ffdb2394
Fatin Ghazi <fatintastic@gmail.com>
2011-10-11 14:39:49 +0200

    Changes to NextCostumeBrickTest

--- catroidUiTest/src/at/tugraz/ist/catroid/uitest/content/brick/NextCostumeBrickTest.java
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/content/brick/NextCostumeBrickTest.java
@@ -1,3 +1,22 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010-2011 The Catroid Team
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package at.tugraz.ist.catroid.uitest.content.brick;
 
 import java.util.ArrayList;
@@ -13,6 +32,7 @@ import at.tugraz.ist.catroid.content.StartScript;
 import at.tugraz.ist.catroid.content.bricks.Brick;
 import at.tugraz.ist.catroid.content.bricks.NextCostumeBrick;
 import at.tugraz.ist.catroid.ui.ScriptActivity;
+import at.tugraz.ist.catroid.uitest.util.UiTestUtils;
 
 import com.jayway.android.robotium.solo.Solo;
 
@@ -27,6 +47,7 @@ public class NextCostumeBrickTest extends ActivityInstrumentationTestCase2<Scrip
 	@Override
 	public void setUp() throws Exception {
 		createProject();
+		UiTestUtils.createTestProject();
 		solo = new Solo(getInstrumentation(), getActivity());
 	}
 
