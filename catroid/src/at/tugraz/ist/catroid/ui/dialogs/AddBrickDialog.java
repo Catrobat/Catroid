4334a8e216915aabdacf1f10eebbcfd352704029
Daniel Burtscher <knut0025@gmail.com>
2011-12-18 19:08:41 +0100

    fix Issue 40; PointInDirectionBrick xml is wrong

--- catroid/src/at/tugraz/ist/catroid/ui/dialogs/AddBrickDialog.java
+++ catroid/src/at/tugraz/ist/catroid/ui/dialogs/AddBrickDialog.java
@@ -72,6 +72,7 @@ import at.tugraz.ist.catroid.content.bricks.NoteBrick;
 import at.tugraz.ist.catroid.content.bricks.PlaceAtBrick;
 import at.tugraz.ist.catroid.content.bricks.PlaySoundBrick;
 import at.tugraz.ist.catroid.content.bricks.PointInDirectionBrick;
+import at.tugraz.ist.catroid.content.bricks.PointInDirectionBrick.Direction;
 import at.tugraz.ist.catroid.content.bricks.PointToBrick;
 import at.tugraz.ist.catroid.content.bricks.RepeatBrick;
 import at.tugraz.ist.catroid.content.bricks.SetBrightnessBrick;
@@ -121,7 +122,7 @@ public class AddBrickDialog extends Dialog {
 		motionBrickList.add(new MoveNStepsBrick(sprite, 10));
 		motionBrickList.add(new TurnLeftBrick(sprite, 15));
 		motionBrickList.add(new TurnRightBrick(sprite, 15));
-		motionBrickList.add(new PointInDirectionBrick(sprite, 0));
+		motionBrickList.add(new PointInDirectionBrick(sprite, Direction.DIRECTION_RIGHT));
 		motionBrickList.add(new PointToBrick(sprite, null));
 		motionBrickList.add(new GlideToBrick(sprite, 800, 0, 1000));
 		if (!isBackground(sprite)) {
