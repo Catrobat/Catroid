7fef87d5a57f4f5ed3ced0693b287c8ce4f5ef08
Hans-Peter Gugl <hanspeter.gugl@gmail.com>
2012-02-02 21:12:14 +0100

    adding scripts like bricks nearly finished

--- catroid/src/at/tugraz/ist/catroid/ProjectManager.java
+++ catroid/src/at/tugraz/ist/catroid/ProjectManager.java
@@ -183,10 +183,6 @@ public class ProjectManager {
 		currentSprite.addScript(script);
 	}
 
-	public void addScript(Script script, int index) {
-		currentSprite.addScript(index, script);
-	}
-
 	public boolean spriteExists(String spriteName) {
 		for (Sprite tempSprite : project.getSpriteList()) {
 			if (tempSprite.getName().equalsIgnoreCase(spriteName)) {
