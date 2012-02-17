7b25436b94c34c963a588230ebc016fabeb88666
Daniel Burtscher <knut0025@gmail.com>
2012-02-17 20:50:44 +0100

    add first implementation of the sound recorder

---
+++ catroid/src/at/tugraz/ist/catroid/soundrecorder/SoundRecorder.java
@@ -0,0 +1,89 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010  Catroid development team 
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.tugraz.ist.catroid.soundrecorder;
+
+import java.io.File;
+import java.io.IOException;
+
+import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.Environment;
+
+public class SoundRecorder {
+
+	final MediaRecorder recorder = new MediaRecorder();
+	final String path;
+
+	/**
+	 * Creates a new audio recording at the given path (relative to root of SD card).
+	 */
+	public SoundRecorder(String path) {
+		this.path = sanitizePath(path);
+	}
+
+	private String sanitizePath(String path) {
+		if (!path.startsWith("/")) {
+			path = "/" + path;
+		}
+		if (!path.contains(".")) {
+			path += ".mp3";
+		}
+		return Environment.getExternalStorageDirectory().getAbsolutePath() + path;
+	}
+
+	/**
+	 * Starts a new recording.
+	 */
+	public void start() throws IOException {
+		String state = android.os.Environment.getExternalStorageState();
+		if (!state.equals(android.os.Environment.MEDIA_MOUNTED)) {
+			throw new IOException("SD Card is not mounted.  It is " + state + ".");
+		}
+
+		File soundFile = new File(path);
+		if (soundFile.exists()) {
+			soundFile.delete();
+		}
+		// make sure the directory we plan to store the recording in exists
+		File directory = soundFile.getParentFile();
+		if (!directory.exists() && !directory.mkdirs()) {
+			throw new IOException("Path to file could not be created.");
+		}
+
+		recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+		recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+		recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+		recorder.setOutputFile(path);
+		recorder.prepare();
+		recorder.start();
+	}
+
+	/**
+	 * Stops a recording that has been previously started.
+	 */
+	public void stop() throws IOException {
+		recorder.stop();
+		recorder.release();
+	}
+
+	public Uri getPath() {
+		return Uri.fromFile(new File(path));
+	}
+
+}
