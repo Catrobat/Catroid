2ef78e717e7e6739650722e13cb3a46bca0e15c5
Markus Hobl <markus.hobl@gmail.com>
2012-03-10 11:04:43 +0100

    Issue 122: adapted test cases (MainMenuActivityTest & MyProjectsActivityTest)

--- catroid/src/at/tugraz/ist/catroid/ProjectManager.java
+++ catroid/src/at/tugraz/ist/catroid/ProjectManager.java
@@ -26,6 +26,7 @@ import java.io.File;
 import java.io.IOException;
 
 import android.content.Context;
+import at.tugraz.ist.catroid.common.Consts;
 import at.tugraz.ist.catroid.common.FileChecksumContainer;
 import at.tugraz.ist.catroid.common.MessageContainer;
 import at.tugraz.ist.catroid.common.StandardProjectHandler;
