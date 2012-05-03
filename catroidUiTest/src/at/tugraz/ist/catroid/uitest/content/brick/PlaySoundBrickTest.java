eda06ff9e61d0a6aacf91fc51091875540c47418
Majda Osmic <mobearette@gmail.com>
2012-05-03 16:57:13 +0200

    Issue confirmation dialog 7.14C: Adapted some costume delete tests to work with the new dialog

--- catroidUiTest/src/at/tugraz/ist/catroid/uitest/content/brick/PlaySoundBrickTest.java
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/content/brick/PlaySoundBrickTest.java
@@ -155,6 +155,8 @@ public class PlaySoundBrickTest extends ActivityInstrumentationTestCase2<ScriptT
 		solo.sleep(300);
 		solo.clickOnButton(getActivity().getString(R.string.sound_delete));
 		solo.sleep(300);
+		solo.clickOnButton(getActivity().getString(R.string.ok));
+		solo.sleep(1000);
 		solo.clickOnText(getActivity().getString(R.string.scripts));
 		solo.sleep(300);
 		solo.clickOnText(getActivity().getString(R.string.broadcast_nothing_selected));
