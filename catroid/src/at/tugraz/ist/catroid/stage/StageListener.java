b823e7ac3a112be1f046ce2ca2161bdcbe467b9a
Anton Rieder <aried3r@gmail.com>
2012-05-06 19:30:08 +0200

    Update to libgdx 0.9.3

--- catroid/src/at/tugraz/ist/catroid/stage/StageListener.java
+++ catroid/src/at/tugraz/ist/catroid/stage/StageListener.java
@@ -56,7 +56,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class StageListener implements ApplicationListener {
-	private static final boolean DEBUG = false;
+	private static final boolean DEBUG = true;
 	public static final String SCREENSHOT_FILE_NAME = "screenshot.png";
 	private FPSLogger fpsLogger;
 
