e078ab104aa2274f4ae3b2bc7f45f87c81a38a4c
Thomas Kaufmann <kaufmaster@gmail.com>
2012-05-10 16:28:12 +0200

    7.32C: revert last changes

--- catroidUiTest/src/at/tugraz/ist/catroid/uitest/ui/MainMenuActivityTest.java
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/ui/MainMenuActivityTest.java
@@ -71,7 +71,6 @@ public class MainMenuActivityTest extends ActivityInstrumentationTestCase2<MainM
 	@Override
 	public void tearDown() throws Exception {
 		solo.finishOpenedActivities();
-		solo.finishInactiveActivities();
 		UiTestUtils.clearAllUtilTestProjects();
 		UtilFile.deleteDirectory(new File(Utils.buildProjectPath(projectNameWithBlacklistedCharacters)));
 		UtilFile.deleteDirectory(new File(Utils.buildProjectPath(projectNameWithWhitelistedCharacters)));
