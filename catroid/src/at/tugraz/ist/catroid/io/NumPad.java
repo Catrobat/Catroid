add86ee7aac5680ffe1008ad180581539d96eb60
obusher <grunztiger@gmail.com>
2012-07-03 17:58:56 +0200

    Num Pad Classes, xml files and png files added

---
+++ catroid/src/at/tugraz/ist/catroid/io/NumPad.java
@@ -0,0 +1,94 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010  Catroid development team 
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.tugraz.ist.catroid.io;
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.KeyboardView.OnKeyboardActionListener;
+
+/**
+ * @author obusher
+ *
+ */
+public class NumPad extends InputMethodService implements OnKeyboardActionListener {
+
+	/* (non-Javadoc)
+	 * @see android.inputmethodservice.KeyboardView.OnKeyboardActionListener#onKey(int, int[])
+	 */
+	public void onKey(int arg0, int[] arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see android.inputmethodservice.KeyboardView.OnKeyboardActionListener#onPress(int)
+	 */
+	public void onPress(int primaryCode) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see android.inputmethodservice.KeyboardView.OnKeyboardActionListener#onRelease(int)
+	 */
+	public void onRelease(int primaryCode) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see android.inputmethodservice.KeyboardView.OnKeyboardActionListener#onText(java.lang.CharSequence)
+	 */
+	public void onText(CharSequence text) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see android.inputmethodservice.KeyboardView.OnKeyboardActionListener#swipeDown()
+	 */
+	public void swipeDown() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see android.inputmethodservice.KeyboardView.OnKeyboardActionListener#swipeLeft()
+	 */
+	public void swipeLeft() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see android.inputmethodservice.KeyboardView.OnKeyboardActionListener#swipeRight()
+	 */
+	public void swipeRight() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see android.inputmethodservice.KeyboardView.OnKeyboardActionListener#swipeUp()
+	 */
+	public void swipeUp() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
