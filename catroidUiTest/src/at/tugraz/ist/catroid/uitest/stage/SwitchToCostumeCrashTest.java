6a5570507cbf36738bbba83e8f59d310ef5ab901
Thomas Kaufmann <kaufmaster@gmail.com>
2012-07-16 15:08:03 +0200

    Issue145: first testCase

---
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/stage/SwitchToCostumeCrashTest.java
@@ -0,0 +1,172 @@
+/**
+ *  Catroid: An on-device graphical programming language for Android devices
+ *  Copyright (C) 2010-2011 The Catroid Team
+ *  (<http://code.google.com/p/catroid/wiki/Credits>)
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  An additional term exception under section 7 of the GNU Affero
+ *  General Public License, version 3, is available at
+ *  http://www.catroid.org/catroid_license_additional_term
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.tugraz.ist.catroid.uitest.stage;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.test.ActivityInstrumentationTestCase2;
+import at.tugraz.ist.catroid.ProjectManager;
+import at.tugraz.ist.catroid.R;
+import at.tugraz.ist.catroid.common.Constants;
+import at.tugraz.ist.catroid.common.CostumeData;
+import at.tugraz.ist.catroid.common.FileChecksumContainer;
+import at.tugraz.ist.catroid.content.Project;
+import at.tugraz.ist.catroid.content.Script;
+import at.tugraz.ist.catroid.content.Sprite;
+import at.tugraz.ist.catroid.content.StartScript;
+import at.tugraz.ist.catroid.content.bricks.SetCostumeBrick;
+import at.tugraz.ist.catroid.io.StorageHandler;
+import at.tugraz.ist.catroid.stage.StageActivity;
+import at.tugraz.ist.catroid.ui.CostumeActivity;
+import at.tugraz.ist.catroid.ui.MainMenuActivity;
+import at.tugraz.ist.catroid.ui.ProjectActivity;
+import at.tugraz.ist.catroid.ui.ScriptTabActivity;
+import at.tugraz.ist.catroid.uitest.util.UiTestUtils;
+import at.tugraz.ist.catroid.utils.Utils;
+
+import com.badlogic.gdx.utils.GdxRuntimeException;
+import com.jayway.android.robotium.solo.Solo;
+
+public class SwitchToCostumeCrashTest extends ActivityInstrumentationTestCase2<MainMenuActivity> {
+	private Solo solo;
+	private ProjectManager projectManager = ProjectManager.getInstance();
+
+	public SwitchToCostumeCrashTest() {
+		super("at.tugraz.ist.catroid", MainMenuActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		UiTestUtils.clearAllUtilTestProjects();
+		UiTestUtils.createTestProject();
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		try {
+			solo.finalize();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		getActivity().finish();
+		UiTestUtils.clearAllUtilTestProjects();
+		super.tearDown();
+	}
+
+	public void testSwitchToCostumeCrashPNG() throws IOException {
+		solo.clickOnButton(solo.getString(R.string.current_project_button));
+		createProject();
+		solo.waitForActivity(ProjectActivity.class.getSimpleName());
+		solo.sleep(200);
+
+		solo.clickOnText(solo.getString(R.string.background));
+		solo.waitForActivity(ScriptTabActivity.class.getSimpleName());
+		solo.clickOnText(solo.getString(R.string.backgrounds));
+		solo.sleep(2000);
+
+		String nyanCatPath = "";
+		try {
+			InputStream inputStream = getInstrumentation().getContext().getResources().getAssets()
+					.open("nyancat_crash.png");
+			nyanCatPath = Utils.buildPath(Utils.buildProjectPath(UiTestUtils.DEFAULT_TEST_PROJECT_NAME),
+					Constants.IMAGE_DIRECTORY, "nyancat.png");
+
+			FileOutputStream nyanCatFileOutputStream = new FileOutputStream(nyanCatPath);
+			byte[] buffer = new byte[inputStream.available()];
+			inputStream.read(buffer);
+			inputStream.close();
+			nyanCatFileOutputStream.write(buffer);
+			nyanCatFileOutputStream.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			fail("Image not loaded from Assets");
+		}
+
+		File nyanCatPngFile = new File(nyanCatPath);
+		Bundle bundleForGallery = new Bundle();
+		bundleForGallery.putString("filePath", nyanCatPngFile.getAbsolutePath());
+		Intent intent = new Intent(getInstrumentation().getContext(),
+				at.tugraz.ist.catroid.uitest.mockups.MockGalleryActivity.class);
+		intent.putExtras(bundleForGallery);
+
+		solo.getCurrentActivity().startActivityForResult(intent, CostumeActivity.REQUEST_SELECT_IMAGE);
+		solo.sleep(200);
+		assertTrue("Testfile not added from mockActivity", solo.searchText("nyancat"));
+
+		String checksumNyanCatImageFile = Utils.md5Checksum(nyanCatPngFile);
+		assertTrue("Checksum not in checksumcontainer",
+				projectManager.fileChecksumContainer.containsChecksum(checksumNyanCatImageFile));
+
+		boolean isInCostumeDataList = false;
+		for (CostumeData costumeData : projectManager.getCurrentSprite().getCostumeDataList()) {
+			if (costumeData.getChecksum().equalsIgnoreCase(checksumNyanCatImageFile)) {
+				isInCostumeDataList = true;
+			}
+		}
+		if (!isInCostumeDataList) {
+			fail("File not added in CostumeDataList");
+		}
+
+		solo.clickOnText(solo.getString(R.string.scripts));
+		solo.clickOnText(solo.getString(R.string.broadcast_nothing_selected));
+		solo.clickOnText("nyancat");
+
+		try {
+			UiTestUtils.clickOnLinearLayout(solo, R.id.btn_action_play);
+			solo.waitForActivity(StageActivity.class.getSimpleName());
+			solo.sleep(5000);
+		} catch (GdxRuntimeException e) {
+			e.printStackTrace();
+			fail("GdxException was thrown");
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			fail("Exception was thrown");
+		}
+	}
+
+	private void createProject() {
+		StorageHandler storageHandler = StorageHandler.getInstance();
+		Project project = new Project(getActivity(), UiTestUtils.DEFAULT_TEST_PROJECT_NAME);
+
+		Sprite backgroundSprite = project.getSpriteList().get(0);
+		Script startScript = new StartScript(backgroundSprite);
+		SetCostumeBrick setCostumeBrick = new SetCostumeBrick(backgroundSprite);
+
+		startScript.addBrick(setCostumeBrick);
+		backgroundSprite.addScript(startScript);
+		project.addSprite(backgroundSprite);
+
+		projectManager.fileChecksumContainer = new FileChecksumContainer();
+		projectManager.setProject(project);
+		projectManager.setCurrentSprite(backgroundSprite);
+		projectManager.setCurrentScript(startScript);
+		storageHandler.saveProject(project);
+	}
+}
