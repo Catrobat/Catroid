060aa84945d85f8fa5ce495c3ec3ac3e67f1688a
Jakob Unterkofler <jakob.unterkofler@student.tugraz.at>
2012-07-20 17:46:16 +0200

    Issue 94 updated

--- catroid/src/at/tugraz/ist/catroid/ui/MainMenuActivity.java
+++ catroid/src/at/tugraz/ist/catroid/ui/MainMenuActivity.java
@@ -131,7 +131,7 @@ public class MainMenuActivity extends Activity {
 	protected Dialog onCreateDialog(int id) {
 		Dialog dialog;
 		if (projectManager.getCurrentProject() != null
-				&& StorageHandler.getInstance().projectExists(projectManager.getCurrentProject().getName())) {
+				&& StorageHandler.getInstance().projectExistsCheckCase(projectManager.getCurrentProject().getName())) {
 			projectManager.saveProject();
 		}
 
@@ -169,8 +169,8 @@ public class MainMenuActivity extends Activity {
 				EditText projectDescriptionField = (EditText) dialog.findViewById(R.id.project_description_upload);
 				EditText projectUploadName = (EditText) dialog.findViewById(R.id.project_upload_name);
 				TextView sizeOfProject = (TextView) dialog.findViewById(R.id.dialog_upload_size_of_project);
-				sizeOfProject.setText(UtilFile
-						.getSizeAsString(new File(Constants.DEFAULT_ROOT + "/" + currentProjectName)));
+				sizeOfProject.setText(UtilFile.getSizeAsString(new File(Constants.DEFAULT_ROOT + "/"
+						+ currentProjectName)));
 
 				projectRename.setVisibility(View.GONE);
 				projectUploadName.setText(ProjectManager.getInstance().getCurrentProject().getName());
