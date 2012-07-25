10445dbe87d05abb36f4046de55845736c2a18b0
obusher <grunztiger@gmail.com>
2012-07-25 17:54:21 +0200

    update test

--- catroidUiTest/src/at/tugraz/ist/catroid/uitest/formulaeditor/CatKeyboardTest.java
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/formulaeditor/CatKeyboardTest.java
@@ -99,7 +99,7 @@ public class CatKeyboardTest extends android.test.ActivityInstrumentationTestCas
 		UiTestUtils.clearAllUtilTestProjects();
 		this.project = null;
 		super.tearDown();
-		solo.sleep(1000);
+		Thread.sleep(1000);
 	}
 
 	@Smoke
@@ -108,17 +108,6 @@ public class CatKeyboardTest extends android.test.ActivityInstrumentationTestCas
 		solo.clickOnEditText(0);
 		solo.clickOnEditText(0);
 		solo.clickOnEditText(1);// View.performclick()
-		//		solo.clickOnEditText(1);
-		//		int size = solo.getViews().size();
-		//		Log.i("info", "solo.getViews().size():" + size);
-		//		solo.getViews().get(1).performClick();
-		//		solo.getViews().get(1).performClick();
-
-		//Didnt worked to test the keyboard:
-		//				solo.sendKey(8); 
-		//		solo.sleep(1000);
-		//		solo.clickOnText("+");
-		//		solo.clickOnImage(0);
 
 		this.clickOnKey("del");
 		this.clickOnKey("del");
@@ -189,11 +178,6 @@ public class CatKeyboardTest extends android.test.ActivityInstrumentationTestCas
 		assertEquals("Wrong button clicked", "^", text.getText().toString().substring(0, 1));
 		this.clickOnKey("del");
 
-		//Test the 3 Buttons with this methods:
-		//		solo.clickOnButton(0); // Ok-Button
-		//		solo.clickOnButton(1); // UNDO - Button
-		//		solo.clickOnButton(2); // Cancel - Button 
-
 		solo.clickOnButton(2);
 
 	}
@@ -242,10 +226,10 @@ public class CatKeyboardTest extends android.test.ActivityInstrumentationTestCas
 				text.getText().toString().substring(0, "rand( 0 , 1 )".length()));
 		this.clickOnKey("del");
 
-		//		this.clickOnKey("pi");
-		//		assertEquals("Wrong button clicked", "pi", text.getText().toString().substring(0, "pi".length()));
-		//		this.clickOnKey("del");
-		//		this.clickOnKey("del");
+		this.clickOnKey("pi");
+		assertEquals("Wrong button clicked", "pi", text.getText().toString().substring(0, "pi".length()));
+		this.clickOnKey("del");
+		this.clickOnKey("del");
 
 		this.clickOnKey("e");
 		assertEquals("Wrong button clicked", "e", text.getText().toString().substring(0, "e".length()));
@@ -271,29 +255,28 @@ public class CatKeyboardTest extends android.test.ActivityInstrumentationTestCas
 		this.clickOnKey("keyboardswitch");
 		this.clickOnKey("keyboardswitch");
 		this.clickOnKey("x-accel");
-		assertEquals("Wrong button clicked", "X_Accelerometer",
-				text.getText().toString().substring(0, "X_Accelerometer".length()));
+		assertEquals("Wrong button clicked", "XACC_", text.getText().toString().substring(0, "XACC_".length()));
 		this.clickOnKey("del");
 
-		//		this.clickOnKey("y-accel");
-		//		assertEquals("Wrong button clicked", "Y_Accelerometer", text.getText().toString().substring(0, "Y_Accelerometer".length()));
-		//		this.clickOnKey("del");
-		//
-		//		this.clickOnKey("z-accel");
-		//		assertEquals("Wrong button clicked", "Z_Accelerometer", text.getText().toString().substring(0, "Z_Accelerometer".length()));
-		//		this.clickOnKey("del");
-		//
-		//		this.clickOnKey("pitch");
-		//		assertEquals("Wrong button clicked", "Pitch_Orientation", text.getText().toString().substring(0, "Pitch_Orientation".length()));
-		//		this.clickOnKey("del");
-		//
-		//		this.clickOnKey("roll");
-		//		assertEquals("Wrong button clicked", "'Roll_Orientation", text.getText().toString().substring(0, "Roll_Orientation".length()));
-		//		this.clickOnKey("del");
-		//
-		//		this.clickOnKey("azimuth");
-		//		assertEquals("Wrong button clicked", "'Azimuth_Orientation", text.getText().toString().substring(0, "Azimuth_Orientation".length()));
-		//		this.clickOnKey("del");
+		this.clickOnKey("y-accel");
+		assertEquals("Wrong button clicked", "YACC_", text.getText().toString().substring(0, "YACC_".length()));
+		this.clickOnKey("del");
+
+		this.clickOnKey("z-accel");
+		assertEquals("Wrong button clicked", "ZACC_", text.getText().toString().substring(0, "ZACC_".length()));
+		this.clickOnKey("del");
+
+		this.clickOnKey("pitch");
+		assertEquals("Wrong button clicked", "PITCH_", text.getText().toString().substring(0, "PITCH_".length()));
+		this.clickOnKey("del");
+
+		this.clickOnKey("roll");
+		assertEquals("Wrong button clicked", "ROLL_", text.getText().toString().substring(0, "ROLL_".length()));
+		this.clickOnKey("del");
+
+		this.clickOnKey("azimuth");
+		assertEquals("Wrong button clicked", "AZIM_", text.getText().toString().substring(0, "AZIM_".length()));
+		this.clickOnKey("del");
 
 		solo.clickOnButton(2);
 	}
