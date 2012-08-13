9ee717fbe5b72fe3d3772cb07e20bc4de41bf604
mhobl <markus.hobl@gmail.com>
2012-08-13 11:06:41 +0200

    Revert to original version

--- catroid/src/at/tugraz/ist/catroid/ProjectManager.java
+++ catroid/src/at/tugraz/ist/catroid/ProjectManager.java
@@ -84,6 +84,7 @@ public class ProjectManager {
 				Utils.displayErrorMessage(context, context.getString(R.string.error_load_project));
 			}
 			return false;
+
 		} else if (!Utils.isApplicationDebuggable(context)
 				&& project.getCatroidVersionCode() > Utils.getVersionCode(context)) {
 			project = oldProject;
@@ -92,6 +93,7 @@ public class ProjectManager {
 				// TODO show dialog to download latest catroid version instead
 			}
 			return false;
+
 		} else {
 			// Set generic localized name on background sprite and move it to the back.
 			if (project.getSpriteList().size() > 0) {
