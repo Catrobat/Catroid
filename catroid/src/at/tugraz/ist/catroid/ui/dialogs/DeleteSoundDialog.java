eee5dab2c51621cf6fe253f83de20aab1f0d5cee
Artur Termenji <atermenji@gmail.com>
2012-08-14 16:24:21 +0300

    renamed some static final String variables

--- catroid/src/at/tugraz/ist/catroid/ui/dialogs/DeleteSoundDialog.java
+++ catroid/src/at/tugraz/ist/catroid/ui/dialogs/DeleteSoundDialog.java
@@ -39,14 +39,14 @@ import at.tugraz.ist.catroid.ui.ScriptTabActivity;
 
 public class DeleteSoundDialog extends DialogFragment {
 
-	private static final String ARGS_SELECTED_POSITION = "selected_position";
+	private static final String BUNDLE_ARGS_SELECTED_POSITION = "selected_position";
 	public static final String DIALOG_FRAGMENT_TAG = "dialog_delete_sound";
 
 	public static DeleteSoundDialog newInstance(int selectedPosition) {
 		DeleteSoundDialog dialog = new DeleteSoundDialog();
 
 		Bundle args = new Bundle();
-		args.putInt(ARGS_SELECTED_POSITION, selectedPosition);
+		args.putInt(BUNDLE_ARGS_SELECTED_POSITION, selectedPosition);
 		dialog.setArguments(args);
 
 		return dialog;
@@ -54,7 +54,7 @@ public class DeleteSoundDialog extends DialogFragment {
 
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		final int selectedPosition = getArguments().getInt(ARGS_SELECTED_POSITION);
+		final int selectedPosition = getArguments().getInt(BUNDLE_ARGS_SELECTED_POSITION);
 
 		Dialog dialog = new AlertDialog.Builder(getActivity()).setTitle(R.string.delete_sound_dialog)
 				.setNegativeButton(R.string.cancel_button, new OnClickListener() {
