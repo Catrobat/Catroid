1fdbbe76829df41a9dd5df787b5c58708834cde4
mhobl <markus.hobl@gmail.com>
2012-08-20 09:48:14 +0200

    Bugfix Issue 207 & 208

--- catroid/src/at/tugraz/ist/catroid/ui/ScriptTabActivity.java
+++ catroid/src/at/tugraz/ist/catroid/ui/ScriptTabActivity.java
@@ -127,7 +127,15 @@ public class ScriptTabActivity extends SherlockFragmentActivity {
 
 		if (requestCode == PreStageActivity.REQUEST_RESOURCES_INIT && resultCode == RESULT_OK) {
 			Intent intent = new Intent(ScriptTabActivity.this, StageActivity.class);
-			startActivity(intent);
+			startActivityForResult(intent, StageActivity.STAGE_ACTIVITY_FINISH);
+		}
+		if (requestCode == StageActivity.STAGE_ACTIVITY_FINISH) {
+			ProjectManager projectManager = ProjectManager.getInstance();
+			int currentSpritePos = projectManager.getCurrentSpritePosition();
+			int currentScriptPos = projectManager.getCurrentScriptPosition();
+			projectManager.loadProject(projectManager.getCurrentProject().getName(), this, false);
+			projectManager.setCurrentSpriteWithPosition(currentSpritePos);
+			projectManager.setCurrentScriptWithPosition(currentScriptPos);
 		}
 	}
 
