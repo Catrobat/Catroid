648d41a8eeba61922eae03c62f96a954ad29b27f
Thomas Kaufmann <kaufmaster@gmail.com>
2012-08-27 13:30:33 +0200

    added comment

--- catroidUiTest/src/at/tugraz/ist/catroid/uitest/stage/SwitchToCostumeCrashTest.java
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/stage/SwitchToCostumeCrashTest.java
@@ -79,6 +79,8 @@ public class SwitchToCostumeCrashTest extends ActivityInstrumentationTestCase2<M
 		String nyanCat = "nyancat";
 		String nyanCatPng = "nyancat_crash.png";
 		try {
+			// need to load image from assets, not from resources folder
+			// this way, the image is loaded similar to catroid, when importing an costume
 			InputStream inputStream = getInstrumentation().getContext().getResources().getAssets().open(nyanCatPng);
 			nyanCatPath = Utils.buildPath(Utils.buildProjectPath(UiTestUtils.DEFAULT_TEST_PROJECT_NAME),
 					Constants.IMAGE_DIRECTORY, nyanCatPng);
@@ -129,6 +131,8 @@ public class SwitchToCostumeCrashTest extends ActivityInstrumentationTestCase2<M
 		String manImage = "man_crash";
 		String manImageJpg = "man_crash.jpg";
 		try {
+			// need to load image from assets, not from resources folder
+			// this way, the image is loaded similar to catroid, when importing an costume
 			InputStream inputStream = getInstrumentation().getContext().getResources().getAssets().open(manImageJpg);
 			manImagePath = Utils.buildPath(Utils.buildProjectPath(UiTestUtils.DEFAULT_TEST_PROJECT_NAME),
 					Constants.IMAGE_DIRECTORY, manImageJpg);
