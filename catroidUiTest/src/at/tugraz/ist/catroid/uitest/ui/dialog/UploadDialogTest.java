2c2aaca25169b69f4cb374c366185d8cf9480765
Thomas Kaufmann <kaufmaster@gmail.com>
2012-09-01 22:29:16 +0200

    refactoring of UploadDialogTest should be more stable now

--- catroidUiTest/src/at/tugraz/ist/catroid/uitest/ui/dialog/UploadDialogTest.java
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/ui/dialog/UploadDialogTest.java
@@ -24,6 +24,7 @@ package at.tugraz.ist.catroid.uitest.ui.dialog;
 
 import java.io.File;
 
+import junit.framework.AssertionFailedError;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.test.ActivityInstrumentationTestCase2;
@@ -135,6 +136,8 @@ public class UploadDialogTest extends ActivityInstrumentationTestCase2<MainMenuA
 
 	public void testUploadingProjectDescriptionDefaultValue() throws Throwable {
 		String testDescription = "Test description";
+		String actionSetDescriptionText = solo.getString(R.string.set_description);
+		String setDescriptionDialogTitle = solo.getString(R.string.description);
 		Project uploadProject = new Project(getActivity(), testProject);
 		ProjectManager.INSTANCE.setProject(uploadProject);
 		ProjectManager.INSTANCE.saveProject();
@@ -145,18 +148,31 @@ public class UploadDialogTest extends ActivityInstrumentationTestCase2<MainMenuA
 		solo.clickOnButton(solo.getString(R.string.my_projects));
 		solo.waitForActivity(MyProjectsActivity.class.getSimpleName());
 		UiTestUtils.longClickOnTextInList(solo, uploadProject.getName());
-		solo.clickInList(2);
-		solo.sleep(200);
+		assertTrue("context menu not loaded in 5 seconds", solo.waitForText(actionSetDescriptionText, 0, 5000));
+		solo.clickOnText(actionSetDescriptionText);
+		assertTrue("dialog not loaded in 5 seconds", solo.waitForText(setDescriptionDialogTitle, 0, 5000));
+		solo.clearEditText(0);
 		solo.enterText(0, testDescription);
-		solo.clickOnView(solo.getCurrentButtons().get(0));
-		solo.waitForDialogToClose(500);
+		solo.sleep(200);
+		solo.setActivityOrientation(Solo.LANDSCAPE);
 		solo.sleep(300);
+		solo.setActivityOrientation(Solo.PORTRAIT);
+		assertTrue("dialog not loaded in 5 seconds", solo.waitForText(setDescriptionDialogTitle, 0, 5000));
+		solo.sleep(300);
+
+		String buttonPositiveText = solo.getString(R.string.ok);
+		// if keyboard is there, hide it and click ok
+		try {
+			solo.clickOnText(buttonPositiveText);
+		} catch (AssertionFailedError e) {
+			solo.goBack();
+			solo.clickOnText(buttonPositiveText);
+		}
+		solo.waitForDialogToClose(500);
 		solo.goBack();
 		solo.waitForActivity(MainMenuActivity.class.getSimpleName());
 
-		solo.sleep(200);
 		solo.clickOnText(solo.getString(R.string.upload_project));
-
 		assertTrue("upload project dialog not shown",
 				solo.waitForText(solo.getString(R.string.upload_project_dialog_title), 0, 5000));
 		EditText uploadDescriptionView = (EditText) solo.getView(R.id.project_description_upload);
