20eec1831ea1106c09f5f094ccf68f8a8b9f3aba
Daniel Fritzsch <d.fritzsch@student.tugraz.at>
2012-10-08 20:32:14 +0200

    testing if title appears

--- catroidUiTest/src/org/catrobat/catroid/uitest/ui/SettingsActivityTest.java
+++ catroidUiTest/src/org/catrobat/catroid/uitest/ui/SettingsActivityTest.java
@@ -58,20 +58,18 @@ public class SettingsActivityTest extends ActivityInstrumentationTestCase2<MainM
 	}
 
 	public void testToggleMindstormBricks() {
+		int actionBarIconIndex = 0;
 		String currentProject = solo.getString(R.string.current_project_button);
 		String background = solo.getString(R.string.background);
 		String settings = solo.getString(R.string.settings);
 		String prefMsBricks = solo.getString(R.string.pref_enable_ms_bricks);
 		String categoryLegoNXTLabel = solo.getString(R.string.category_lego_nxt);
-		SharedPreferences preferances = PreferenceManager.getDefaultSharedPreferences(getActivity());
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
 		//disable mindstorm bricks, if enabled at start
-		if (preferances.getBoolean("setting_mindstorm_bricks", false)) {
+		if (preferences.getBoolean("setting_mindstorm_bricks", false)) {
 			solo.clickOnText(settings);
 			solo.assertCurrentActivity("Wrong Activity", SettingsActivity.class);
-
-			assertTrue("Wrong title", solo.searchText(solo.getString(R.string.pref_title)));
-
 			solo.clickOnText(prefMsBricks);
 			solo.goBack();
 		}
@@ -87,8 +85,11 @@ public class SettingsActivityTest extends ActivityInstrumentationTestCase2<MainM
 
 		solo.clickOnText(settings);
 		solo.waitForActivity(SettingsActivity.class.getSimpleName());
+
+		assertTrue("Wrong title", solo.searchText(solo.getString(R.string.pref_title)));
+
 		solo.clickOnText(prefMsBricks);
-		solo.goBack();
+		solo.clickOnImage(actionBarIconIndex);
 		solo.waitForActivity(MainMenuActivity.class.getSimpleName());
 		solo.clickOnText(currentProject);
 		solo.waitForActivity(ProjectActivity.class.getSimpleName());
