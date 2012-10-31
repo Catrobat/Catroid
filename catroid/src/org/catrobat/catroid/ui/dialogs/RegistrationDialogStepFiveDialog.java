14a98e7547afdd1a1b476748bf195212f8bb7cab
Stefan Jaindl <jaindl.stefan@gmail.com>
2012-10-31 13:16:23 +0100

    dialogs refactored and country dialog included

--- catroid/src/org/catrobat/catroid/ui/dialogs/RegistrationDialogStepFiveDialog.java
+++ catroid/src/org/catrobat/catroid/ui/dialogs/RegistrationDialogStepFiveDialog.java
@@ -23,51 +23,116 @@
 package org.catrobat.catroid.ui.dialogs;
 
 import org.catrobat.catroid.R;
+import org.catrobat.catroid.transfers.RegistrationData;
+import org.catrobat.catroid.transfers.RegistrationTask;
 import org.catrobat.catroid.transfers.RegistrationTask.OnRegistrationCompleteListener;
 
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnShowListener;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class RegistrationDialogStepFiveDialog extends DialogFragment implements OnRegistrationCompleteListener {
 
 	public static final String DIALOG_FRAGMENT_TAG = "dialog_register_step5";
 
-	private Button uploadButton;
+	private EditText usernameEditText;
+	private EditText passwordEditText;
+	private EditText passwordConfirmationEditText;
+	private CheckBox showPassword;
+	private Button registerButton;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.dialog_register_completed, container);
+		View rootView = inflater.inflate(R.layout.dialog_register_username_password, container);
 
-		uploadButton = (Button) rootView.findViewById(R.id.upload_button);
+		usernameEditText = (EditText) rootView.findViewById(R.id.username);
+		passwordEditText = (EditText) rootView.findViewById(R.id.password);
+		passwordConfirmationEditText = (EditText) rootView.findViewById(R.id.password_confirmation);
+		showPassword = (CheckBox) rootView.findViewById(R.id.show_password);
+		registerButton = (Button) rootView.findViewById(R.id.register_button);
 
-		uploadButton.setOnClickListener(new OnClickListener() {
+		usernameEditText.setText("");
+		passwordEditText.setText("");
+		passwordConfirmationEditText.setText("");
+		showPassword.setChecked(false);
+
+		showPassword.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				handleUploadButtonClick();
+				if (showPassword.isChecked()) {
+					passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT);
+					passwordConfirmationEditText.setInputType(InputType.TYPE_CLASS_TEXT);
+				} else {
+					passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+					passwordConfirmationEditText.setInputType(InputType.TYPE_CLASS_TEXT
+							| InputType.TYPE_TEXT_VARIATION_PASSWORD);
+				}
 			}
 		});
 
-		getDialog().setTitle(R.string.upload_project_dialog_title);
+		registerButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				handleRegisterButtonClick();
+			}
+		});
+
+		getDialog().setTitle(R.string.register_dialog_title);
 		getDialog().setCanceledOnTouchOutside(true);
 		getDialog().getWindow().setLayout(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+
+		getDialog().setOnShowListener(new OnShowListener() {
+			@Override
+			public void onShow(DialogInterface dialog) {
+				InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(
+						Context.INPUT_METHOD_SERVICE);
+				inputManager.showSoftInput(usernameEditText, InputMethodManager.SHOW_IMPLICIT);
+			}
+		});
 
 		return rootView;
 	}
 
-	private void handleUploadButtonClick() {
-		UploadProjectDialog uploadProjectDialog = new UploadProjectDialog();
-		uploadProjectDialog.show(getFragmentManager(), UploadProjectDialog.DIALOG_FRAGMENT_TAG);
-		dismiss();
+	private void handleRegisterButtonClick() {
+
+		String username = usernameEditText.getText().toString();
+		String password = passwordEditText.getText().toString();
+		String passwordConfirmation = passwordConfirmationEditText.getText().toString();
+
+		if (!password.equals(passwordConfirmation)) {
+			new Builder(getActivity()).setTitle(R.string.register_error)
+					.setMessage(R.string.register_password_mismatch).setPositiveButton("OK", null).show();
+			return;
+		}
+		RegistrationTask registrationTask = new RegistrationTask(getActivity(), username, password);
+		registrationTask.setOnRegistrationCompleteListener(this);
+		registrationTask.execute();
 	}
 
 	@Override
 	public void onRegistrationComplete() {
+		String username = usernameEditText.getText().toString();
+		String password = passwordEditText.getText().toString();
+		RegistrationData.INSTANCE.setUserName(username);
+		RegistrationData.INSTANCE.setPassword(password);
+
+		RegistrationDialogStepSixDialog registerStepSixDialog = new RegistrationDialogStepSixDialog();
 		dismiss();
+		registerStepSixDialog.show(getActivity().getSupportFragmentManager(),
+				RegistrationDialogStepOneDialog.DIALOG_FRAGMENT_TAG);
 	}
 }
