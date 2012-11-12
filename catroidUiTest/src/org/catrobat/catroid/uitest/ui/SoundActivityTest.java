b89b207fcf8bd0c2c3678623b36aef8afbbfa362
Daniel Fritzsch <d.fritzsch@student.tugraz.at>
2012-11-12 18:49:19 +0100

    tests for SoundActivity

--- catroidUiTest/src/org/catrobat/catroid/uitest/ui/SoundActivityTest.java
+++ catroidUiTest/src/org/catrobat/catroid/uitest/ui/SoundActivityTest.java
@@ -1,7 +1,15 @@
 package org.catrobat.catroid.uitest.ui;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import org.catrobat.catroid.ProjectManager;
+import org.catrobat.catroid.R;
+import org.catrobat.catroid.common.SoundInfo;
+import org.catrobat.catroid.stage.StageActivity;
+import org.catrobat.catroid.ui.MainMenuActivity;
 import org.catrobat.catroid.ui.ProgramMenuActivity;
+import org.catrobat.catroid.ui.ScriptTabActivity;
 import org.catrobat.catroid.ui.SoundActivity;
 import org.catrobat.catroid.uitest.util.UiTestUtils;
 
@@ -10,12 +18,17 @@ import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 
 import com.jayway.android.robotium.solo.Solo;
 
 public class SoundActivityTest extends ActivityInstrumentationTestCase2<SoundActivity> {
+	private final int RESOURCE_SOUND = org.catrobat.catroid.uitest.R.raw.longsound;
 
 	private Solo solo = null;
+	private ArrayList<SoundInfo> soundInfoList;
+	private ProjectManager projectManager = ProjectManager.getInstance();
+	private File soundFile;
 
 	public SoundActivityTest() {
 		super(SoundActivity.class);
@@ -25,6 +38,21 @@ public class SoundActivityTest extends ActivityInstrumentationTestCase2<SoundAct
 	public void setUp() throws Exception {
 		super.setUp();
 		UiTestUtils.createTestProject();
+		soundInfoList = projectManager.getCurrentSprite().getSoundList();
+		soundFile = UiTestUtils.saveFileToProject(UiTestUtils.DEFAULT_TEST_PROJECT_NAME, "longsound.mp3",
+				RESOURCE_SOUND, getInstrumentation().getContext(), UiTestUtils.FileTypes.SOUND);
+
+		if (soundFile == null) {
+			Log.d("CATROID", "File NULL!!!");
+		} else {
+			Log.d("CATROID", "not null :)");
+			SoundInfo soundInfo = new SoundInfo();
+			soundInfo.setSoundFileName(soundFile.getName());
+			soundInfo.setTitle("Test Sound");
+			soundInfoList.add(soundInfo);
+			projectManager.getFileChecksumContainer().addChecksum(soundInfo.getChecksum(), soundInfo.getAbsolutePath());
+		}
+
 		solo = new Solo(getInstrumentation(), getActivity());
 	}
 
@@ -35,7 +63,7 @@ public class SoundActivityTest extends ActivityInstrumentationTestCase2<SoundAct
 		UiTestUtils.clearAllUtilTestProjects();
 		super.tearDown();
 		solo = null;
-		ProjectManager.getInstance().deleteCurrentProject();
+		//		projectManager.deleteCurrentProject();
 	}
 
 	public void testOrientation() throws NameNotFoundException {
@@ -55,5 +83,83 @@ public class SoundActivityTest extends ActivityInstrumentationTestCase2<SoundAct
 		assertEquals(ProgramMenuActivity.class.getSimpleName()
 				+ " not set to be in portrait mode in AndroidManifest.xml!", ActivityInfo.SCREEN_ORIENTATION_PORTRAIT,
 				activityInfo.screenOrientation);
+	}
+
+	public void testMainMenuButton() {
+		int upImageIndex = 0;
+		solo.clickOnImage(upImageIndex);
+		solo.waitForActivity(MainMenuActivity.class.getSimpleName());
+		solo.assertCurrentActivity("Clicking on main menu button did not cause main menu to be displayed",
+				MainMenuActivity.class);
+	}
+
+	public void testPlayProgramButton() {
+		soundInfoList = projectManager.getCurrentSprite().getSoundList();
+		int numberOfSoundsBeforePlayingProgram = soundInfoList.size();
+
+		UiTestUtils.clickOnBottomBar(solo, R.id.btn_play);
+		solo.waitForActivity(StageActivity.class.getSimpleName());
+		solo.assertCurrentActivity("Not in StageActivity", StageActivity.class);
+
+		solo.goBack();
+		solo.goBack();
+
+		solo.waitForActivity(SoundActivity.class.getSimpleName());
+		solo.assertCurrentActivity("Not in SoundActivity", SoundActivity.class);
+
+		soundInfoList = projectManager.getCurrentSprite().getSoundList();
+		int numberOfSoundsAfterPlayingProgram = soundInfoList.size();
+		assertEquals("Size of the soundlist has changed", numberOfSoundsBeforePlayingProgram,
+				numberOfSoundsAfterPlayingProgram);
+	}
+
+	public void testChangeViaSpinner() {
+		int numberOfSoundsBeforeSpinning = soundInfoList.size();
+
+		String sounds = solo.getString(R.string.sounds);
+		clickOnSpinnerItem(sounds);
+		solo.sleep(1000);
+		solo.waitForActivity(SoundActivity.class.getSimpleName());
+
+		soundInfoList = projectManager.getCurrentSprite().getSoundList();
+		assertEquals("Number of sounds has changed after clicking on Sounds", numberOfSoundsBeforeSpinning,
+				soundInfoList.size());
+
+		String scripts = solo.getString(R.string.scripts);
+		clickOnSpinnerItem(scripts);
+		solo.sleep(1000);
+
+		//TODO CHANGE TO SCRIPTACTIVITY!
+		solo.waitForActivity(ScriptTabActivity.class.getSimpleName());
+		solo.goBack();
+		solo.waitForActivity(SoundActivity.class.getSimpleName());
+		solo.waitForText(scripts);
+
+		soundInfoList = projectManager.getCurrentSprite().getSoundList();
+		assertEquals("Number of sounds has changed after clicking on Scripts", numberOfSoundsBeforeSpinning,
+				soundInfoList.size());
+
+		String looks = solo.getString(R.string.category_looks);
+		clickOnSpinnerItem(looks);
+		solo.sleep(1000);
+
+		//TODO CHANGE TO LOOKACTIVITY!
+		solo.waitForActivity(ScriptTabActivity.class.getSimpleName());
+		solo.goBack();
+		solo.waitForActivity(SoundActivity.class.getSimpleName());
+		solo.waitForText(scripts);
+
+		soundInfoList = projectManager.getCurrentSprite().getSoundList();
+		assertEquals("Number of sounds has changed after clicking on Scripts", numberOfSoundsBeforeSpinning,
+				soundInfoList.size());
+	}
+
+	private void clickOnSpinnerItem(String itemName) {
+		String sounds = solo.getString(R.string.sounds);
+		solo.waitForText(sounds);
+		solo.clickOnText(sounds);
+		solo.sleep(200);
+		solo.waitForText(itemName);
+		solo.clickOnText(itemName);
 	}
 }
