218822ee0ad2489c727c4dfb6cd9930cc2414185
sjaindl <stefan.jaindl@student.tugraz.at>
2013-01-08 20:21:53 +0100

    license text updated

--- catroid/src/org/catrobat/catroid/ui/dialogs/RegistrationDialogStepThreeDialog.java
+++ catroid/src/org/catrobat/catroid/ui/dialogs/RegistrationDialogStepThreeDialog.java
@@ -1,6 +1,6 @@
 /**
  *  Catroid: An on-device visual programming system for Android devices
- *  Copyright (C) 2010-2012 The Catrobat Team
+ *  Copyright (C) 2010-2013 The Catrobat Team
  *  (<http://developer.catrobat.org/credits>)
  *  
  *  This program is free software: you can redistribute it and/or modify
@@ -20,6 +20,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.catrobat.catroid.ui.dialogs;
 
 import org.catrobat.catroid.R;
