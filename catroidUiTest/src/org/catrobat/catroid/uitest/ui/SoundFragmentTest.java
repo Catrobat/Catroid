0bcb6b066d70f7799c01cb7ec8b43f96f0df2d5b
Daniel Pail <daniel.pail@student.tugraz.at>
2013-02-07 21:09:27 +0100

    Removed unnecessary waitForText in test

--- catroidUiTest/src/org/catrobat/catroid/uitest/ui/SoundFragmentTest.java
+++ catroidUiTest/src/org/catrobat/catroid/uitest/ui/SoundFragmentTest.java
@@ -515,7 +515,6 @@ public class SoundFragmentTest extends ActivityInstrumentationTestCase2<MainMenu
 		solo.sleep(timeToWait);
 		assertFalse("Mediaplayer continues playing even if context menu has been opened", soundInfo.isPlaying);
 		solo.goBack();
-		solo.waitForText(solo.getString(R.string.sounds), 1, timeToWait, false, true);
 		checkVisibilityOfViews(VISIBLE, GONE, VISIBLE, GONE, VISIBLE, GONE, GONE);
 
 		solo.clickOnView(playImageButton);
@@ -524,7 +523,6 @@ public class SoundFragmentTest extends ActivityInstrumentationTestCase2<MainMenu
 		solo.sleep(timeToWait);
 		assertFalse("Mediaplayer continues playing even if rename action has been opened", soundInfo.isPlaying);
 		solo.goBack();
-		solo.waitForText(solo.getString(R.string.sounds), 1, timeToWait, false, true);
 		checkVisibilityOfViews(VISIBLE, GONE, VISIBLE, GONE, VISIBLE, GONE, GONE);
 
 		solo.clickOnView(playImageButton);
@@ -533,7 +531,6 @@ public class SoundFragmentTest extends ActivityInstrumentationTestCase2<MainMenu
 		solo.sleep(timeToWait);
 		assertFalse("Mediaplayer continues playing even if delete action has been opened", soundInfo.isPlaying);
 		solo.goBack();
-		solo.waitForText(solo.getString(R.string.sounds), 1, timeToWait, false, true);
 		checkVisibilityOfViews(VISIBLE, GONE, VISIBLE, GONE, VISIBLE, GONE, GONE);
 
 		UiTestUtils.openActionMode(solo, null, R.id.delete);
