0d25fe459346e8f45d341e04d4d9eb39dcedaacb
Daniel Fritzsch <d.fritzsch@student.tugraz.at>
2013-02-07 10:36:04 +0100

    remove unused method

--- catroid/src/org/catrobat/catroid/ui/fragment/SoundFragment.java
+++ catroid/src/org/catrobat/catroid/ui/fragment/SoundFragment.java
@@ -109,11 +109,6 @@ public class SoundFragment extends ScriptActivityFragment implements OnSoundEdit
 	private boolean isRenameActionMode;
 
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
-
-	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_sound, null);
 		return rootView;
