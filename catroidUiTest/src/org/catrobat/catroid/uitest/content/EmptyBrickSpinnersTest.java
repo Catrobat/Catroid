c492fd7391d2f5182a4045f8b7da1495d340d20b
Thomas Kaufmann <kaufmaster@gmail.com>
2013-02-21 01:01:17 +0100

    remove warning - unused String

--- catroidUiTest/src/org/catrobat/catroid/uitest/content/EmptyBrickSpinnersTest.java
+++ catroidUiTest/src/org/catrobat/catroid/uitest/content/EmptyBrickSpinnersTest.java
@@ -27,7 +27,6 @@ import java.lang.reflect.InvocationTargetException;
 
 import org.catrobat.catroid.ProjectManager;
 import org.catrobat.catroid.R;
-import org.catrobat.catroid.common.Constants;
 import org.catrobat.catroid.common.LookData;
 import org.catrobat.catroid.common.SoundInfo;
 import org.catrobat.catroid.content.Project;
@@ -42,7 +41,6 @@ import org.catrobat.catroid.content.bricks.SetLookBrick;
 import org.catrobat.catroid.ui.ProjectActivity;
 import org.catrobat.catroid.ui.ScriptActivity;
 import org.catrobat.catroid.uitest.util.UiTestUtils;
-import org.catrobat.catroid.utils.Utils;
 import org.json.JSONException;
 
 import android.test.ActivityInstrumentationTestCase2;
@@ -108,9 +106,9 @@ public class EmptyBrickSpinnersTest extends ActivityInstrumentationTestCase2<Scr
 		solo.goBack();
 		solo.waitForActivity(ProjectActivity.class.getSimpleName());
 
-		String projectXMLPath = Utils.buildPath(Utils.buildProjectPath(testProjectName), Constants.PROJECTCODE_NAME);
 		// TODO: add XML validation based on xsd
-		//		XMLValidationUtil.sendProjectXMLToServerForValidating(projectXMLPath);
+		//	String projectXMLPath = Utils.buildPath(Utils.buildProjectPath(testProjectName), Constants.PROJECTCODE_NAME);
+		//	XMLValidationUtil.sendProjectXMLToServerForValidating(projectXMLPath);
 	}
 
 	private void createSpinnerProject() {
