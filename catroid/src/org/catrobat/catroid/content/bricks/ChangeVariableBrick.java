227f5177525492b29969988c79a99f8cd789de7e
Daniel P <xorrizon@gmail.com>
2013-02-27 10:18:25 +0100

    Initial commit of ChangeVariableBy brick

---
+++ catroid/src/org/catrobat/catroid/content/bricks/ChangeVariableBrick.java
@@ -0,0 +1,127 @@
+/**
+ *  Catroid: An on-device visual programming system for Android devices
+ *  Copyright (C) 2010-2013 The Catrobat Team
+ *  (<http://developer.catrobat.org/credits>)
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  An additional term exception under section 7 of the GNU Affero
+ *  General Public License, version 3, is available at
+ *  http://developer.catrobat.org/license_additional_term
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.catrobat.catroid.content.bricks;
+
+import android.content.Context;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.*;
+import android.widget.AdapterView.OnItemSelectedListener;
+import org.catrobat.catroid.ProjectManager;
+import org.catrobat.catroid.R;
+import org.catrobat.catroid.content.Sprite;
+import org.catrobat.catroid.formulaeditor.Formula;
+import org.catrobat.catroid.formulaeditor.UserVariable;
+import org.catrobat.catroid.ui.adapter.UserVariableAdapter;
+import org.catrobat.catroid.ui.fragment.FormulaEditorFragment;
+
+public class ChangeVariableBrick implements Brick, OnClickListener {
+	private static final long serialVersionUID = 1L;
+	private Sprite sprite;
+	private UserVariable userVariable;
+	private transient View view;
+	private Formula variable_formula;
+
+	public ChangeVariableBrick(Sprite sprite, Formula variable_formula) {
+		this.sprite = sprite;
+		this.variable_formula = variable_formula;
+	}
+
+	public ChangeVariableBrick(Sprite sprite, double value) {
+		this.sprite = sprite;
+		this.variable_formula = new Formula(Double.toString(value));
+	}
+
+	@Override
+	public int getRequiredResources() {
+		return NO_RESOURCES;
+	}
+
+	@Override
+	public void execute() {
+		if (userVariable == null) {
+			return;
+		}
+		double original_value = userVariable.getValue();
+		double value = variable_formula.interpretFloat();
+		userVariable.setValue(original_value + value);
+	}
+
+	@Override
+	public Sprite getSprite() {
+		return sprite;
+	}
+
+	@Override
+	public View getView(final Context context, int brickId, BaseAdapter adapter) {
+
+		view = View.inflate(context, R.layout.brick_change_variable_by, null);
+
+		TextView prototype_text = (TextView) view.findViewById(R.id.brick_change_variable_prototype_view);
+		EditText edit_text = (EditText) view.findViewById(R.id.brick_change_variable_edit_text);
+		prototype_text.setVisibility(View.GONE);
+		variable_formula.setTextFieldId(R.id.brick_change_variable_edit_text);
+		variable_formula.refreshTextField(view);
+		edit_text.setVisibility(View.VISIBLE);
+		edit_text.setOnClickListener(this);
+
+		Spinner variableSpinner = (Spinner) view.findViewById(R.id.variable_spinner);
+		UserVariableAdapter variabeAdapter = ProjectManager.getInstance().getCurrentProject().getUserVariables().createUserVariableAdapter(context, sprite.getName());
+		variabeAdapter.setItemLayout(android.R.layout.simple_spinner_item, android.R.id.text1);
+		variableSpinner.setAdapter(variabeAdapter);
+
+		variableSpinner.setClickable(true);
+		variableSpinner.setFocusable(true);
+
+		variableSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+				userVariable = (UserVariable) parent.getItemAtPosition(position);
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				userVariable = null;
+			}
+		});
+
+		return view;
+	}
+
+	@Override
+	public View getPrototypeView(Context context) {
+		return View.inflate(context, R.layout.brick_change_variable_by, null);
+	}
+
+	@Override
+	public Brick clone() {
+		ChangeVariableBrick clonedBrick = new ChangeVariableBrick(getSprite(), variable_formula);
+		return clonedBrick;
+	}
+
+	@Override
+	public void onClick(View view) {
+		FormulaEditorFragment.showFragment(view, this, variable_formula);
+	}
+
+}
