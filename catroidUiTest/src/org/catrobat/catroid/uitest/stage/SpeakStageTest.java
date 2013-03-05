a7eab95283c40f20b4b0f60304ca09ad577d6a7c
mhobl <markus.hobl@gmail.com>
2013-03-05 13:49:47 +0100

    changed testSuccessiveBehaviour in SpeakStageTest

--- catroidUiTest/src/org/catrobat/catroid/uitest/stage/SpeakStageTest.java
+++ catroidUiTest/src/org/catrobat/catroid/uitest/stage/SpeakStageTest.java
@@ -130,7 +130,7 @@ public class SpeakStageTest extends ActivityInstrumentationTestCase2<MainMenuAct
 		assertEquals("TextToSpeech exectuted with wrong utterance id", "0",
 				textToSpeechMock.parameters.get(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID));
 		assertEquals("TextToSpeech executed with wrong text", textMessageTest, textToSpeechMock.text);
-		solo.sleep(6000);
+		solo.sleep(2000);
 		assertEquals("TextToSpeech executed with wrong parameter", TextToSpeech.QUEUE_FLUSH, textToSpeechMock.queueMode);
 		assertEquals("TextToSpeech exectuted with wrong utterance id", "1",
 				textToSpeechMock.parameters.get(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID));
@@ -177,8 +177,8 @@ public class SpeakStageTest extends ActivityInstrumentationTestCase2<MainMenuAct
 		broadcastBrickNormal.setSelectedMessage("normal");
 		startScriptNormal.addBrick(waitBrickNormal);
 		startScriptNormal.addBrick(speakBrickNormal);
-		WaitBrick waitBrickNormal2 = new WaitBrick(spriteNormal, 2000);
-		startScriptNormal.addBrick(waitBrickNormal2);
+		//		WaitBrick waitBrickNormal2 = new WaitBrick(spriteNormal, 2000);
+		//		startScriptNormal.addBrick(waitBrickNormal2);
 		SpeakBrick speakBrickNormal2 = new SpeakBrick(spriteNormal, textMessageHello);
 		startScriptNormal.addBrick(speakBrickNormal2);
 		//		startScriptNormal.addBrick(broadcastBrickNormal);
