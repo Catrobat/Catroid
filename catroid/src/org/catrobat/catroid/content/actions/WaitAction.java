956832b0fb5c93ef5cc8b5f38e1c27dddfc6f709
Franz Schreiner <grunztiger@gmail.com>
2013-03-06 18:47:29 +0100

    WaitAction add.

---
+++ catroid/src/org/catrobat/catroid/content/actions/WaitAction.java
@@ -0,0 +1,68 @@
+/**
+ *  Catroid: An on-device visual programming system for Android devices
+ *  Copyright (C) 2010-2013 The Catrobat Team
+ *  (<http://developer.catrobat.org/credits>)
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  An additional term exception under section 7 of the GNU Affero
+ *  General Public License, version 3, is available at
+ *  http://developer.catrobat.org/license_additional_term
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.catrobat.catroid.content.actions;
+
+import org.catrobat.catroid.content.Sprite;
+import org.catrobat.catroid.formulaeditor.Formula;
+
+import com.badlogic.gdx.scenes.scene2d.Action;
+
+public class WaitAction extends Action {
+
+	private Sprite sprite;
+	private Formula delay;
+	private float time;
+
+	@Override
+	public boolean act(float delta) {
+
+		if (time < delay.interpretFloat(sprite)) {
+			time += delta;
+
+		}
+
+		if (time < delay.interpretFloat(sprite)) {
+			return false;
+		}
+
+		return true;
+
+	}
+
+	public void setDelay(Formula delay) {
+		this.delay = delay;
+	}
+
+	public void setSprite(Sprite sprite) {
+		this.sprite = sprite;
+	}
+
+	public Sprite getSprite() {
+		return sprite;
+	}
+
+	public Formula getDelay() {
+		return delay;
+	}
+
+}
