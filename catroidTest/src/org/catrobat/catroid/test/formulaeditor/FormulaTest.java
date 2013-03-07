ba69feb5ec5033b0a02af955ce6ed096735366b8
bernhard <bernhardsp@gmail.com>
2013-03-07 15:39:58 +0100

    Test for Formula

---
+++ catroidTest/src/org/catrobat/catroid/test/formulaeditor/FormulaTest.java
@@ -0,0 +1,48 @@
+/**
+ *  Catroid: An on-device visual programming system for Android devices
+ *  Copyright (C) 2010-2013 The Catrobat Team
+ *  (<http://developer.catrobat.org/credits>)
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  An additional term exception under section 7 of the GNU Affero
+ *  General Public License, version 3, is available at
+ *  http://developer.catrobat.org/license_additional_term
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.catrobat.catroid.test.formulaeditor;
+
+import org.catrobat.catroid.formulaeditor.Formula;
+
+import android.test.InstrumentationTestCase;
+
+public class FormulaTest extends InstrumentationTestCase {
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testIsSingleNumberFormula() {
+
+		Formula formula = new Formula(1);
+		assertTrue("Formula should be single number formula", formula.isSingleNumberFormula());
+	}
+
+}
