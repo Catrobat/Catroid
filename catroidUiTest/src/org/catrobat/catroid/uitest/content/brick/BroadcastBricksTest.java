775fda7eb326bfdc9081763cf3f99db3736f8d9f
mhobl <markus.hobl@gmail.com>
2013-04-18 13:47:45 +0200

    added Focus to the BroadcastBricksTest

--- catroidUiTest/src/org/catrobat/catroid/uitest/content/brick/BroadcastBricksTest.java
+++ catroidUiTest/src/org/catrobat/catroid/uitest/content/brick/BroadcastBricksTest.java
@@ -174,6 +174,12 @@ public class BroadcastBricksTest extends ActivityInstrumentationTestCase2<Script
 
 		solo.sleep(500);
 
+		// just to get focus
+		solo.clickOnText(brickBroadcastString);
+		if (solo.searchText(solo.getString(R.string.brick_context_dialog_move_brick), true)) {
+			solo.goBack();
+		}
+
 		assertEquals("Wrong selection", solo.getString(R.string.brick_broadcast_default_value),
 				((Spinner) solo.getView(R.id.brick_broadcast_spinner)).getSelectedItem().toString());
 
