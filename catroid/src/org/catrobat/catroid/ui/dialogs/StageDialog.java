8f1f5e88342c007705ac4d20843c66dd5021b5cf
sjaindl <jaindl.stefan@gmail.com>
2013-04-22 14:01:57 +0200

    Revert "redesigned stage menu"
    This reverts commit 50b68c982fc45d35f04f682be9dc9981a0828247.

--- catroid/src/org/catrobat/catroid/ui/dialogs/StageDialog.java
+++ catroid/src/org/catrobat/catroid/ui/dialogs/StageDialog.java
@@ -31,6 +31,7 @@ import android.app.Dialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -50,7 +51,7 @@ public class StageDialog extends Dialog implements View.OnClickListener {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.dialog_stage);
-		//getWindow().setGravity(Gravity.BOTTOM);
+		getWindow().setGravity(Gravity.BOTTOM);
 		getWindow().setBackgroundDrawableResource(R.color.transparent);
 
 		((Button) findViewById(R.id.stage_dialog_button_back)).setOnClickListener(this);
@@ -58,6 +59,11 @@ public class StageDialog extends Dialog implements View.OnClickListener {
 		((Button) findViewById(R.id.stage_dialog_button_restart)).setOnClickListener(this);
 		((Button) findViewById(R.id.stage_dialog_button_toggle_axes)).setOnClickListener(this);
 		((Button) findViewById(R.id.stage_dialog_button_screenshot)).setOnClickListener(this);
+		//		if (stageActivity.getResizePossible()) {
+		//			((Button) findViewById(R.id.stage_dialog_button_maximize)).setOnClickListener(this);
+		//		} else {
+		//			((Button) findViewById(R.id.stage_dialog_button_maximize)).setVisibility(View.GONE);
+		//		}
 	}
 
 	@Override
@@ -77,6 +83,9 @@ public class StageDialog extends Dialog implements View.OnClickListener {
 			case R.id.stage_dialog_button_toggle_axes:
 				toggleAxes();
 				break;
+			//			case R.id.stage_dialog_button_maximize:
+			//				stageListener.changeScreenSize();
+			//				break;
 			case R.id.stage_dialog_button_screenshot:
 				makeScreenshot();
 				break;
