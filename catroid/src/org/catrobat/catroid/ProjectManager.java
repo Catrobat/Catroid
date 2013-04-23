290cc1b6ad2f3235bf51a20e8d8c73251ad5ac79
mhobl <markus.hobl@gmail.com>
2013-04-23 13:30:59 +0200

    refactor code

--- catroid/src/org/catrobat/catroid/ProjectManager.java
+++ catroid/src/org/catrobat/catroid/ProjectManager.java
@@ -22,13 +22,9 @@
  */
 package org.catrobat.catroid;
 
-import java.io.File;
-import java.io.IOException;
-import java.security.MessageDigest;
-import java.util.concurrent.*;
-import java.util.concurrent.locks.ReentrantLock;
-
+import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 import org.catrobat.catroid.common.Constants;
 import org.catrobat.catroid.common.FileChecksumContainer;
 import org.catrobat.catroid.common.MessageContainer;
@@ -39,8 +35,8 @@ import org.catrobat.catroid.content.Sprite;
 import org.catrobat.catroid.io.StorageHandler;
 import org.catrobat.catroid.utils.Utils;
 
-import android.content.Context;
-import android.util.Log;
+import java.io.File;
+import java.io.IOException;
 
 public class ProjectManager {
 
