dbe3e02d75f75f55976351571e75642d9d194365
threesn <threesn.b@gmail.com>
2013-04-25 12:36:22 +0200

    stabilized yet another test

--- catroidUiTest/src/org/catrobat/catroid/uitest/ui/MyProjectsActivityTest.java
+++ catroidUiTest/src/org/catrobat/catroid/uitest/ui/MyProjectsActivityTest.java
@@ -720,6 +720,7 @@ public class MyProjectsActivityTest extends ActivityInstrumentationTestCase2<Mai
 		solo.sleep(300);
 		assertTrue("rename wasnt successfull", solo.searchText(UiTestUtils.PROJECTNAME3, 1, true));
 		solo.goBack();
+		solo.sleep(300);
 		assertEquals("current project not updated", UiTestUtils.PROJECTNAME3, ProjectManager.getInstance()
 				.getCurrentProject().getName());
 	}
