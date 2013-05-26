fd6c64e03b0aa2fafa956bf620360b2355f054bb
Franz Schreiner <grunztiger@gmail.com>
2013-05-26 19:27:40 +0200

    Catrobat Language and Version Name update

--- catroid/src/org/catrobat/catroid/common/Constants.java
+++ catroid/src/org/catrobat/catroid/common/Constants.java
@@ -26,7 +26,7 @@ import android.os.Environment;
 
 public final class Constants {
 
-	public static final float SUPPORTED_CATROBAT_LANGUAGE_VERSION = 0.7f;
+	public static final float SUPPORTED_CATROBAT_LANGUAGE_VERSION = 0.8f;
 	public static final String PLATFORM_NAME = "Android";
 	public static final int APPLICATION_BUILD_NUMBER = 0; // updated from jenkins nightly/release build
 	public static final String APPLICATION_BUILD_NAME = ""; // updated from jenkins nightly/release build
