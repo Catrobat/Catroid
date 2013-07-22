6a09540202bd0236ffcb77a0d0a8894bd75313ea
mhobl <markus.hobl@gmail.com>
2013-07-22 11:26:54 +0200

    Fix a small bug

--- catroid/src/org/catrobat/catroid/utils/UtilFile.java
+++ catroid/src/org/catrobat/catroid/utils/UtilFile.java
@@ -153,7 +153,7 @@ public class UtilFile {
 
 	public static void createStandardProjectIfRootDirectoryIsEmpty(Context context) {
 		File rootDirectory = new File(Constants.DEFAULT_ROOT);
-		if (rootDirectory == null || rootDirectory.listFiles().length == 0) {
+		if (rootDirectory == null || rootDirectory.listFiles() == null || rootDirectory.listFiles().length == 0) {
 			ProjectManager.INSTANCE.initializeDefaultProject(context);
 		}
 	}
