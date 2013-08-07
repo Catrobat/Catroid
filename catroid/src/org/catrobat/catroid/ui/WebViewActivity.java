f92555a48a50a1ffccb950663ed0fdd649ca5976
Sercan Akpolat <sercan.akpolat@gmail.com>
2013-08-07 23:17:37 +0200

    ...

--- catroid/src/org/catrobat/catroid/ui/WebViewActivity.java
+++ catroid/src/org/catrobat/catroid/ui/WebViewActivity.java
@@ -33,10 +33,6 @@ import android.webkit.WebViewClient;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 
-/**
- * @author sercan
- * 
- */
 public class WebViewActivity extends SherlockFragmentActivity {
 
 	private WebView webView;
@@ -56,9 +52,9 @@ public class WebViewActivity extends SherlockFragmentActivity {
 			@Override
 			public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype,
 					long contentLength) {
-				Intent i = new Intent(Intent.ACTION_VIEW);
-				i.setData(Uri.parse(url));
-				startActivity(i);
+				Intent intent = new Intent(Intent.ACTION_VIEW);
+				intent.setData(Uri.parse(url));
+				startActivity(intent);
 			}
 		});
 
