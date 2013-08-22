6d31e3a5b9ed34dae452c15def30ca5404ad96e1
Ajdin Vihric <avihric@gmail.com>
2013-08-22 17:46:57 +0200

    WebView improvements Actionbar is hidden; History implemented (by pressing Back button); Return to PocketCode by pressing PocketCode logo in WebView

--- catroid/src/org/catrobat/catroid/common/Constants.java
+++ catroid/src/org/catrobat/catroid/common/Constants.java
@@ -46,7 +46,6 @@ public final class Constants {
 
 	//Web:
 	public static final String BASE_URL_HTTPS = "https://www.pocketcode.org/";
-	public static final String CATROBAT_WEBVIEW_URL = BASE_URL_HTTPS + "?inapp";
 	public static final String CATROBAT_TERMS_OF_USE_URL = BASE_URL_HTTPS + "termsOfUse";
 
 	public static final String CATROBAT_ABOUT_URL = "http://developer.catrobat.org/";
