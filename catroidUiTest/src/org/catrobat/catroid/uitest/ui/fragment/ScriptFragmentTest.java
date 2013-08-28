92c8cc017208275047670262faa8fd260658a173
Manuel Wallner <wallner.manuel@gmail.com>
2013-08-28 12:08:17 +0200

    Fix for the NullPointerException upon copying a brick that was added with no script brick present

--- catroidUiTest/src/org/catrobat/catroid/uitest/ui/fragment/ScriptFragmentTest.java
+++ catroidUiTest/src/org/catrobat/catroid/uitest/ui/fragment/ScriptFragmentTest.java
@@ -121,6 +121,31 @@ public class ScriptFragmentTest extends BaseActivityInstrumentationTestCase<Main
 
 	}
 
+	public void testCopyAddedBrickWithoutAddedScript() {
+		UiTestUtils.createTestProject();
+		UiTestUtils.getIntoScriptActivityFromMainMenu(solo);
+
+		UiTestUtils.openActionMode(solo, solo.getString(R.string.delete), R.id.delete, getActivity());
+		solo.clickOnCheckBox(0);
+
+		UiTestUtils.acceptAndCloseActionMode(solo);
+		solo.clickOnButton(solo.getString(R.string.yes));
+
+		UiTestUtils.addNewBrick(solo, R.string.brick_wait);
+		solo.clickOnText(solo.getString(R.string.brick_when_started));
+		solo.sleep(100);
+
+		UiTestUtils.openActionMode(solo, solo.getString(R.string.copy), R.id.copy, getActivity());
+		solo.clickOnCheckBox(1);
+
+		UiTestUtils.acceptAndCloseActionMode(solo);
+
+		int numberOfBricks = ProjectManager.getInstance().getCurrentProject().getSpriteList().get(0)
+				.getNumberOfBricks();
+
+		assertEquals("No brick has been copied!", 2, numberOfBricks);
+	}
+
 	public void testCreateNewBrickButton() {
 		List<Brick> brickListToCheck = UiTestUtils.createTestProject();
 		UiTestUtils.getIntoScriptActivityFromMainMenu(solo);
