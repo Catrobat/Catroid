aa4a39b8abe01784ea883a939a16ede56f496059
Andreas Schmidhofer <aschmidhofer@gmail.com>
2013-09-22 21:47:37 +0200

    issue SOR-3 added camera test as UI test

--- catroidTest/src/org/catrobat/catroid/test/camera/CameraManagerTest.java
+++ catroidTest/src/org/catrobat/catroid/test/camera/CameraManagerTest.java
@@ -1,17 +1,12 @@
 package org.catrobat.catroid.test.camera;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 
 import junit.framework.TestCase;
 
 import org.catrobat.catroid.camera.CameraManager;
-import org.catrobat.catroid.camera.JpgPreviewCallback;
 
 public class CameraManagerTest extends TestCase {
-
-	private static final int MAX_FRAME_DELAY_IN_MS = 1000;
 
 	public void testCameraNotAvailable() {
 		Camera camera = null;
@@ -56,29 +51,30 @@ public class CameraManagerTest extends TestCase {
 		CameraManager.getInstance().releaseCamera();
 	}
 
-	public void testJpgPreviewFrameCallback() {
-		final int[] calls = new int[1];
-		calls[0] = 0;
-		JpgPreviewCallback callback = new JpgPreviewCallback() {
-			public void onJpgPreviewFrame(byte[] jpgData) {
-				calls[0]++;
-				if (calls[0] == 1) {
-					Bitmap bitmap = BitmapFactory.decodeByteArray(jpgData, 0, jpgData.length);
-					assertNotNull("Could not create bitmap from data - wrong format?", bitmap);
-				}
-			}
-		};
-		CameraManager.getInstance().addOnJpgPreviewFrameCallback(callback);
-		boolean success = CameraManager.getInstance().startCamera();
-		assertTrue("Camera was not started properly", success);
-		try {
-			Thread.sleep(MAX_FRAME_DELAY_IN_MS);
-		} catch (InterruptedException e) {
-		}
-		assertTrue("Did not receive frame data from camera", calls[0] > 0);
-		CameraManager.getInstance().removeOnJpgPreviewFrameCallback(callback);
-		CameraManager.getInstance().releaseCamera();
-	}
+	// MOVED TO UI TESTS
+	//	public void testJpgPreviewFrameCallback() {
+	//		final int[] calls = new int[1];
+	//		calls[0] = 0;
+	//		JpgPreviewCallback callback = new JpgPreviewCallback() {
+	//			public void onJpgPreviewFrame(byte[] jpgData) {
+	//				calls[0]++;
+	//				if (calls[0] == 1) {
+	//					Bitmap bitmap = BitmapFactory.decodeByteArray(jpgData, 0, jpgData.length);
+	//					assertNotNull("Could not create bitmap from data - wrong format?", bitmap);
+	//				}
+	//			}
+	//		};
+	//		CameraManager.getInstance().addOnJpgPreviewFrameCallback(callback);
+	//		boolean success = CameraManager.getInstance().startCamera();
+	//		assertTrue("Camera was not started properly", success);
+	//		try {
+	//			Thread.sleep(MAX_FRAME_DELAY_IN_MS);
+	//		} catch (InterruptedException e) {
+	//		}
+	//		assertTrue("Did not receive frame data from camera", calls[0] > 0);
+	//		CameraManager.getInstance().removeOnJpgPreviewFrameCallback(callback);
+	//		CameraManager.getInstance().releaseCamera();
+	//	}
 
 	public void testGetInstance() {
 		assertNotNull("Could not get instance of CameraManager", CameraManager.getInstance());
