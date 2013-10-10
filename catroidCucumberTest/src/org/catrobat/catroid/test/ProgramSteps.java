ebd942bd80c7f1fbca45fa4e97ea3916da12cbe6
Maximilian Fellner <max.fellner@gmail.com>
2013-10-10 14:40:15 +0200

    Reformatted steps classes.

--- catroidCucumberTest/src/org/catrobat/catroid/test/ProgramSteps.java
+++ catroidCucumberTest/src/org/catrobat/catroid/test/ProgramSteps.java
@@ -19,85 +19,85 @@ import org.catrobat.catroid.ui.ProjectActivity;
 import java.io.IOException;
 
 public class ProgramSteps extends AndroidTestCase {
-    @Given("^I have a default program$")
-    public void I_have_default_program() {
-        ProjectManager pm = ProjectManager.getInstance();
-        pm.deleteCurrentProject();
-        pm.initializeDefaultProject(getContext());
-        Project project = pm.getCurrentProject();
-        Cucumber.put(Cucumber.KEY_PROJECT, project);
-    }
+	@Given("^I have a default program$")
+	public void I_have_default_program() {
+		ProjectManager pm = ProjectManager.getInstance();
+		pm.deleteCurrentProject();
+		pm.initializeDefaultProject(getContext());
+		Project project = pm.getCurrentProject();
+		Cucumber.put(Cucumber.KEY_PROJECT, project);
+	}
 
-    @Given("^I have a program with the name '(\\w+)'$")
-    public void I_have_program_with_name(String name) {
-        try {
-            ProjectManager pm = ProjectManager.getInstance();
-            pm.initializeNewProject(name, getContext());
-            Project project = pm.getCurrentProject();
-            project.getSpriteList().clear();
-            Cucumber.put(Cucumber.KEY_PROJECT, project);
-        } catch (IOException e) {
-            fail(e.getMessage());
-        }
-    }
+	@Given("^I have a program with the name '(\\w+)'$")
+	public void I_have_program_with_name(String name) {
+		try {
+			ProjectManager pm = ProjectManager.getInstance();
+			pm.initializeNewProject(name, getContext(), /*empty*/ true);
+			Project project = pm.getCurrentProject();
+			project.getSpriteList().clear();
+			Cucumber.put(Cucumber.KEY_PROJECT, project);
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+	}
 
-    @Given("^an empty program$")
-    public void empty_program() {
-        try {
-            String name = System.currentTimeMillis() + "_cucumber";
-            ProjectManager pm = ProjectManager.getInstance();
-            pm.initializeNewProject(name, getContext());
-            Project project = pm.getCurrentProject();
-            Cucumber.put(Cucumber.KEY_PROJECT, project);
-            project.getSpriteList().clear();
-            project.addSprite(newBackgroundObject());
-        } catch (IOException e) {
-            fail(e.getMessage());
-        }
-    }
+	@Given("^an empty program$")
+	public void empty_program() {
+		try {
+			String name = System.currentTimeMillis() + "_cucumber";
+			ProjectManager pm = ProjectManager.getInstance();
+			pm.initializeNewProject(name, getContext(), /*empty*/ true);
+			Project project = pm.getCurrentProject();
+			Cucumber.put(Cucumber.KEY_PROJECT, project);
+			project.getSpriteList().clear();
+			project.addSprite(newBackgroundObject());
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+	}
 
-    private Sprite newBackgroundObject() {
-        Sprite background = new Sprite("background");
-        background.look.setZIndex(0);
+	private Sprite newBackgroundObject() {
+		Sprite background = new Sprite("background");
+		background.look.setZIndex(0);
 
-        StartScript startScript = new StartScript(background);
-        SetLookBrick setLookBrick = new SetLookBrick(background);
-        startScript.addBrick(setLookBrick);
-        background.addScript(startScript);
+		StartScript startScript = new StartScript(background);
+		SetLookBrick setLookBrick = new SetLookBrick(background);
+		startScript.addBrick(setLookBrick);
+		background.addScript(startScript);
 
-        LookData lookData = Util.newLookData("background", Util.createBackgroundImage(getContext(), "background"));
-        background.getLookDataList().add(lookData);
-        setLookBrick.setLook(lookData);
-        return background;
-    }
+		LookData lookData = Util.newLookData("background", Util.createBackgroundImage(getContext(), "background"));
+		background.getLookDataList().add(lookData);
+		setLookBrick.setLook(lookData);
+		return background;
+	}
 
-    @When("^I start the program$")
-    public void I_start_the_program() {
-        Solo solo = (Solo) Cucumber.get(Cucumber.KEY_SOLO);
-        assertEquals(MainMenuActivity.class, solo.getCurrentActivity().getClass());
-        solo.clickOnView(solo.getView(org.catrobat.catroid.R.id.main_menu_button_continue));
-        solo.waitForActivity(ProjectActivity.class.getSimpleName(), 3000);
-        assertEquals(ProjectActivity.class, solo.getCurrentActivity().getClass());
-        solo.clickOnView(solo.getView(org.catrobat.catroid.R.id.button_play));
-        solo.waitForActivity(StageActivity.class.getSimpleName(), 3000);
-        assertEquals(StageActivity.class, solo.getCurrentActivity().getClass());
-    }
+	@When("^I start the program$")
+	public void I_start_the_program() {
+		Solo solo = (Solo) Cucumber.get(Cucumber.KEY_SOLO);
+		assertEquals(MainMenuActivity.class, solo.getCurrentActivity().getClass());
+		solo.clickOnView(solo.getView(org.catrobat.catroid.R.id.main_menu_button_continue));
+		solo.waitForActivity(ProjectActivity.class.getSimpleName(), 3000);
+		assertEquals(ProjectActivity.class, solo.getCurrentActivity().getClass());
+		solo.clickOnView(solo.getView(org.catrobat.catroid.R.id.button_play));
+		solo.waitForActivity(StageActivity.class.getSimpleName(), 3000);
+		assertEquals(StageActivity.class, solo.getCurrentActivity().getClass());
+	}
 
-    @Then("^I wait (\\d+) milliseconds$")
-    public void I_wait_d_milliseconds(int time) {
-        Solo solo = (Solo) Cucumber.get(Cucumber.KEY_SOLO);
-        solo.sleep(time);
-    }
+	@Then("^I wait (\\d+) milliseconds$")
+	public void I_wait_d_milliseconds(int time) {
+		Solo solo = (Solo) Cucumber.get(Cucumber.KEY_SOLO);
+		solo.sleep(time);
+	}
 
-    @Then("^the program is being executed$")
-    public void program_being_executed() {
-        ProjectManager pm = ProjectManager.getInstance();
-        for (Sprite sprite : pm.getCurrentProject().getSpriteList()) {
-            assertFalse("Sprite shouldn't be paused.", sprite.isPaused);
-            for (int i = 0; i < sprite.getNumberOfScripts(); i++) {
-                Script script = sprite.getScript(i);
-                assertFalse("Script shouldn't be paused.", script.isPaused());
-            }
-        }
-    }
+	@Then("^the program is being executed$")
+	public void program_being_executed() {
+		ProjectManager pm = ProjectManager.getInstance();
+		for (Sprite sprite : pm.getCurrentProject().getSpriteList()) {
+			assertFalse("Sprite shouldn't be paused.", sprite.isPaused);
+			for (int i = 0; i < sprite.getNumberOfScripts(); i++) {
+				Script script = sprite.getScript(i);
+				assertFalse("Script shouldn't be paused.", script.isPaused());
+			}
+		}
+	}
 }
--- catroidCucumberTest/src/org/catrobat/catroid/test/ScriptSteps.java
+++ catroidCucumberTest/src/org/catrobat/catroid/test/ScriptSteps.java
@@ -22,179 +22,179 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 public class ScriptSteps extends AndroidTestCase {
-    private final Semaphore mScriptEndWaitLock = new Semaphore(0);
+	private final Semaphore mScriptEndWaitLock = new Semaphore(0);
 
-    @Given("^a StartScript$")
-    public void start_script() {
-        Sprite object = (Sprite) Cucumber.get(Cucumber.KEY_CURRENT_OBJECT);
-        StartScript script = new StartScript(object);
-        object.addScript(script);
-        Cucumber.put(Cucumber.KEY_CURRENT_SCRIPT, script);
-    }
+	@Given("^a StartScript$")
+	public void start_script() {
+		Sprite object = (Sprite) Cucumber.get(Cucumber.KEY_CURRENT_OBJECT);
+		StartScript script = new StartScript(object);
+		object.addScript(script);
+		Cucumber.put(Cucumber.KEY_CURRENT_SCRIPT, script);
+	}
 
-    @Given("^a WhenScript '(\\w+)'$")
-    public void when_script(String name) {
-        Sprite object = (Sprite) Cucumber.get(Cucumber.KEY_CURRENT_OBJECT);
-        WhenScript script = new WhenScript(object);
-        Map<String, Integer> actions = new HashMap<String, Integer>();
-        actions.put("tapped", 0);
-        actions.put("doubletapped", 1);
-        actions.put("longpressed", 2);
-        actions.put("swipeup", 3);
-        actions.put("swipedown", 4);
-        actions.put("swipeleft", 5);
-        actions.put("swiperight", 6);
-        script.setAction(actions.get(name));
-        object.addScript(script);
-        Cucumber.put(Cucumber.KEY_CURRENT_SCRIPT, script);
-    }
+	@Given("^a WhenScript '(\\w+)'$")
+	public void when_script(String name) {
+		Sprite object = (Sprite) Cucumber.get(Cucumber.KEY_CURRENT_OBJECT);
+		WhenScript script = new WhenScript(object);
+		Map<String, Integer> actions = new HashMap<String, Integer>();
+		actions.put("tapped", 0);
+		actions.put("doubletapped", 1);
+		actions.put("longpressed", 2);
+		actions.put("swipeup", 3);
+		actions.put("swipedown", 4);
+		actions.put("swipeleft", 5);
+		actions.put("swiperight", 6);
+		script.setAction(actions.get(name));
+		object.addScript(script);
+		Cucumber.put(Cucumber.KEY_CURRENT_SCRIPT, script);
+	}
 
-    @Given("^a BroadcastScript '(\\w+)'$")
-    public void broadcast_script(String message) {
-        Sprite object = (Sprite) Cucumber.get(Cucumber.KEY_CURRENT_OBJECT);
-        BroadcastScript script = new BroadcastScript(object, message);
-        object.addScript(script);
-        Cucumber.put(Cucumber.KEY_CURRENT_SCRIPT, script);
-    }
+	@Given("^a BroadcastScript '(\\w+)'$")
+	public void broadcast_script(String message) {
+		Sprite object = (Sprite) Cucumber.get(Cucumber.KEY_CURRENT_OBJECT);
+		BroadcastScript script = new BroadcastScript(object, message);
+		object.addScript(script);
+		Cucumber.put(Cucumber.KEY_CURRENT_SCRIPT, script);
+	}
 
-    @When("^the script terminates$")
-    public void script_terminates() {
-        try {
-            mScriptEndWaitLock.tryAcquire(1, 30, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            fail(e.getMessage());
-        }
-    }
+	@When("^the script terminates$")
+	public void script_terminates() {
+		try {
+			mScriptEndWaitLock.tryAcquire(1, 30, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			fail(e.getMessage());
+		}
+	}
 
-    @When("^the script is executed$")
-    public void script_is_executed() {
-        Solo solo = (Solo) Cucumber.get(Cucumber.KEY_SOLO);
-        assertEquals(MainMenuActivity.class, solo.getCurrentActivity().getClass());
+	@When("^the script is executed$")
+	public void script_is_executed() {
+		Solo solo = (Solo) Cucumber.get(Cucumber.KEY_SOLO);
+		assertEquals(MainMenuActivity.class, solo.getCurrentActivity().getClass());
 
-        Script script = (Script) Cucumber.get(Cucumber.KEY_CURRENT_SCRIPT);
-        Sprite object = script.getScriptBrick().getSprite();
-        script.addBrick(new ScriptEndBrick(object, new ScriptEndCallback() {
-            @Override
-            public void onScriptEnd() {
-                long time = System.nanoTime();
-                Log.d(CucumberInstrumentation.TAG, String.format("Stop time: %d", time));
-                Cucumber.put(Cucumber.KEY_STOP_TIME_NANO, time);
-                mScriptEndWaitLock.release();
-            }
-        }));
+		Script script = (Script) Cucumber.get(Cucumber.KEY_CURRENT_SCRIPT);
+		Sprite object = script.getScriptBrick().getSprite();
+		script.addBrick(new ScriptEndBrick(object, new ScriptEndCallback() {
+			@Override
+			public void onScriptEnd() {
+				long time = System.nanoTime();
+				Log.d(CucumberInstrumentation.TAG, String.format("Stop time: %d", time));
+				Cucumber.put(Cucumber.KEY_STOP_TIME_NANO, time);
+				mScriptEndWaitLock.release();
+			}
+		}));
 
-        solo.clickOnView(solo.getView(org.catrobat.catroid.R.id.main_menu_button_continue));
-        solo.waitForActivity(ProjectActivity.class.getSimpleName(), 5000);
-        assertEquals(ProjectActivity.class, solo.getCurrentActivity().getClass());
+		solo.clickOnView(solo.getView(org.catrobat.catroid.R.id.main_menu_button_continue));
+		solo.waitForActivity(ProjectActivity.class.getSimpleName(), 5000);
+		assertEquals(ProjectActivity.class, solo.getCurrentActivity().getClass());
 
-        solo.clickOnView(solo.getView(org.catrobat.catroid.R.id.button_play));
-        solo.waitForActivity(StageActivity.class.getSimpleName(), 5000);
-        GdxListener gdxListener = new GdxListener();
-        StageActivity.stageListener.setStageListenerDelegate(gdxListener);
-        gdxListener.waitForStageToRender(10000);
-        assertEquals(StageActivity.class, solo.getCurrentActivity().getClass());
+		solo.clickOnView(solo.getView(org.catrobat.catroid.R.id.button_play));
+		solo.waitForActivity(StageActivity.class.getSimpleName(), 5000);
+		GdxListener gdxListener = new GdxListener();
+		StageActivity.stageListener.setStageListenerDelegate(gdxListener);
+		gdxListener.waitForStageToRender(10000);
+		assertEquals(StageActivity.class, solo.getCurrentActivity().getClass());
 
-        if (script instanceof StartScript) {
-            executeStartScript((StartScript) script);
-        } else if (script instanceof WhenScript) {
-            executeWhenScript((WhenScript) script);
-        } else if (script instanceof BroadcastScript) {
-            executeBroadcastScript((BroadcastScript) script);
-        } else {
-            fail("Unsupported script class.");
-        }
-    }
+		if (script instanceof StartScript) {
+			executeStartScript((StartScript) script);
+		} else if (script instanceof WhenScript) {
+			executeWhenScript((WhenScript) script);
+		} else if (script instanceof BroadcastScript) {
+			executeBroadcastScript((BroadcastScript) script);
+		} else {
+			fail("Unsupported script class.");
+		}
+	}
 
-    private void executeStartScript(StartScript script) {
-        // start scripts should start with the stage activity
-    }
+	private void executeStartScript(StartScript script) {
+		// start scripts should start with the stage activity
+	}
 
-    private void executeWhenScript(WhenScript script) {
-        String action = script.getAction();
-        Sprite object = script.getScriptBrick().getSprite();
-        if ("Tapped".equals(action)) { // action names should be public in WhenScript
-            object.look.doTouchDown(0, 0, 0);
-        } else {
-            fail(String.format("Unsupported when script action: %s", action));
-        }
-        Cucumber.put(Cucumber.KEY_START_TIME_NANO, System.nanoTime());
-    }
+	private void executeWhenScript(WhenScript script) {
+		String action = script.getAction();
+		Sprite object = script.getScriptBrick().getSprite();
+		if ("Tapped".equals(action)) { // action names should be public in WhenScript
+			object.look.doTouchDown(0, 0, 0);
+		} else {
+			fail(String.format("Unsupported when script action: %s", action));
+		}
+		Cucumber.put(Cucumber.KEY_START_TIME_NANO, System.nanoTime());
+	}
 
-    private void executeBroadcastScript(BroadcastScript script) {
-        Cucumber.put(Cucumber.KEY_START_TIME_NANO, System.nanoTime());
-        fail("BroadcastScript is not yet supported.");
-    }
+	private void executeBroadcastScript(BroadcastScript script) {
+		Cucumber.put(Cucumber.KEY_START_TIME_NANO, System.nanoTime());
+		fail("BroadcastScript is not yet supported.");
+	}
 
-    private static final class GdxListener implements ApplicationListener {
-        private final Semaphore mLock = new Semaphore(0);
+	public static interface ScriptEndCallback {
+		public void onScriptEnd();
+	}
 
-        public void waitForStageToRender(int timeout) {
-            try {
-                mLock.tryAcquire(1, timeout, TimeUnit.MILLISECONDS);
-            } catch (InterruptedException e) {
-                fail(e.getMessage());
-            }
-            long time = System.nanoTime();
-            Log.d(CucumberInstrumentation.TAG, String.format("Start time: %d", time));
-            Cucumber.put(Cucumber.KEY_START_TIME_NANO, time);
-        }
+	private static final class GdxListener implements ApplicationListener {
+		private final Semaphore mLock = new Semaphore(0);
 
-        @Override
-        public void render() {
-            mLock.release();
-        }
+		public void waitForStageToRender(int timeout) {
+			try {
+				mLock.tryAcquire(1, timeout, TimeUnit.MILLISECONDS);
+			} catch (InterruptedException e) {
+				fail(e.getMessage());
+			}
+			long time = System.nanoTime();
+			Log.d(CucumberInstrumentation.TAG, String.format("Start time: %d", time));
+			Cucumber.put(Cucumber.KEY_START_TIME_NANO, time);
+		}
 
-        @Override
-        public void create() {
-        }
+		@Override
+		public void render() {
+			mLock.release();
+		}
 
-        @Override
-        public void resize(int i, int i2) {
-        }
+		@Override
+		public void create() {
+		}
 
-        @Override
-        public void pause() {
-        }
+		@Override
+		public void resize(int i, int i2) {
+		}
 
-        @Override
-        public void resume() {
-        }
+		@Override
+		public void pause() {
+		}
 
-        @Override
-        public void dispose() {
-        }
-    }
+		@Override
+		public void resume() {
+		}
 
-    public static interface ScriptEndCallback {
-        public void onScriptEnd();
-    }
+		@Override
+		public void dispose() {
+		}
+	}
 
-    public static final class ScriptEndBrick extends ShowBrick {
-        private final transient ScriptEndCallback mCallback;
+	public static final class ScriptEndBrick extends ShowBrick {
+		private final transient ScriptEndCallback mCallback;
 
-        public ScriptEndBrick(Sprite sprite, ScriptEndCallback callback) {
-            this.sprite = sprite;
-            mCallback = callback;
-        }
+		public ScriptEndBrick(Sprite sprite, ScriptEndCallback callback) {
+			this.sprite = sprite;
+			mCallback = callback;
+		}
 
-        @Override
-        public List<SequenceAction> addActionToSequence(SequenceAction sequence) {
-            sequence.addAction(new ScriptEndAction(mCallback));
-            return null;
-        }
-    }
+		@Override
+		public List<SequenceAction> addActionToSequence(SequenceAction sequence) {
+			sequence.addAction(new ScriptEndAction(mCallback));
+			return null;
+		}
+	}
 
-    public static final class ScriptEndAction extends Action {
-        private final transient ScriptEndCallback mCallback;
+	public static final class ScriptEndAction extends Action {
+		private final transient ScriptEndCallback mCallback;
 
-        public ScriptEndAction(ScriptEndCallback callback) {
-            mCallback = callback;
-        }
+		public ScriptEndAction(ScriptEndCallback callback) {
+			mCallback = callback;
+		}
 
-        @Override
-        public boolean act(float delta) {
-            mCallback.onScriptEnd();
-            return true;
-        }
-    }
+		@Override
+		public boolean act(float delta) {
+			mCallback.onScriptEnd();
+			return true;
+		}
+	}
 }
