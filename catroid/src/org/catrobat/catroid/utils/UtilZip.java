6fae4544ef16fe6daea852eeeda2ef3bb29453f8
Tobias Ibounig <tobias.ibounig@outlook.com>
2014-02-14 14:02:05 +0100

    - Solved Warning "Resource leak: 'zipFile' is never closed" - Better Handling of closing process (using finally) - Some Renaming

--- catroid/src/org/catrobat/catroid/utils/UtilZip.java
+++ catroid/src/org/catrobat/catroid/utils/UtilZip.java
@@ -2,42 +2,47 @@
  *  Catroid: An on-device visual programming system for Android devices
  *  Copyright (C) 2010-2013 The Catrobat Team
  *  (<http://developer.catrobat.org/credits>)
- *  
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as
  *  published by the Free Software Foundation, either version 3 of the
  *  License, or (at your option) any later version.
- *  
+ *
  *  An additional term exception under section 7 of the GNU Affero
  *  General Public License, version 3, is available at
  *  http://developer.catrobat.org/license_additional_term
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *  GNU Affero General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.catrobat.catroid.utils;
 
+import android.util.Log;
+
 import org.catrobat.catroid.common.Constants;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Enumeration;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public final class UtilZip {
 	private static final int QUICKEST_COMPRESSION = 0;
+	private static final String TAG = UtilZip.class.getSimpleName();
+	private static final String DIRECTORY_LEVEL_UP = "../";
+	private static final String ERROR_FOLDER_NOT_CREATED = "Folder not created";
+	private static final String ERROR_CLOSING_STREAM = "Error closing stream";
 
 	private static ZipOutputStream zipOutputStream;
 
@@ -61,10 +66,17 @@ public final class UtilZip {
 				}
 			}
 
-			zipOutputStream.close();
 			return true;
-		} catch (IOException e) {
-			e.printStackTrace();
+		} catch (IOException ioException) {
+			Log.e(TAG, ioException.getMessage(), ioException);
+		} finally {
+			try {
+				if (zipOutputStream != null) {
+					zipOutputStream.close();
+				}
+			} catch (IOException ignoredException) {
+				Log.e(TAG, ERROR_CLOSING_STREAM, ignoredException);
+			}
 		}
 
 		return false;
@@ -83,7 +95,7 @@ public final class UtilZip {
 
 	private static void writeFileToZip(File file, String zipEntryPath) throws IOException {
 		byte[] readBuffer = new byte[Constants.BUFFER_8K];
-		int bytesIn = 0;
+		int bytesIn;
 
 		FileInputStream fileInputStream = new FileInputStream(file);
 		ZipEntry zipEntry = new ZipEntry(zipEntryPath + file.getName());
@@ -96,47 +108,60 @@ public final class UtilZip {
 		fileInputStream.close();
 	}
 
-	public static boolean unZipFile(String zipFile, String outDirectory) {
+	public static boolean unZipFile(String zipFileName, String outDirectory) {
+		File file;
+		BufferedOutputStream destinationOutputStream = null;
+		ZipInputStream zipInputStream = null;
 		try {
-			ZipEntry zipEntry = null;
-
-			BufferedOutputStream destinationOutputStream = null;
 			byte[] data = new byte[Constants.BUFFER_8K];
-			ZipFile zipfile = new ZipFile(zipFile);
-			Enumeration<? extends ZipEntry> e = zipfile.entries();
-			while (e.hasMoreElements()) {
+			file = new File(zipFileName);
+			zipInputStream = new ZipInputStream(new BufferedInputStream(new FileInputStream(file)));
+			ZipEntry zipEntry;
 
-				zipEntry = e.nextElement();
-				InputStream zipInputStream = zipfile.getInputStream(zipEntry);
+			while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+				if (zipEntry.getName().contains(DIRECTORY_LEVEL_UP)) {
+					Log.d(TAG, "Path traversal attack prevented");
+					continue;
+				}
 
 				if (zipEntry.isDirectory()) {
-					File file = new File(Utils.buildPath(outDirectory, zipEntry.getName()));
-					file.mkdir();
+					File entryFile = new File(Utils.buildPath(outDirectory, zipEntry.getName()));
+					if (!entryFile.mkdir() && !entryFile.isDirectory()) {
+						throw new IOException(ERROR_FOLDER_NOT_CREATED);
+					}
 					zipInputStream.close();
 					continue;
 				}
-				File file = new File(Utils.buildPath(outDirectory, zipEntry.getName()));
-				file.getParentFile().mkdirs();
-				FileOutputStream fileOutputStream = new FileOutputStream(file);
 
+				File entryFile = new File(Utils.buildPath(outDirectory, zipEntry.getName()));
+				if (!entryFile.getParentFile().mkdirs() && !entryFile.getParentFile().isDirectory()) {
+					throw new IOException(ERROR_FOLDER_NOT_CREATED);
+				}
+				FileOutputStream fileOutputStream = new FileOutputStream(entryFile);
 				int count;
 				destinationOutputStream = new BufferedOutputStream(fileOutputStream, Constants.BUFFER_8K);
 				while ((count = zipInputStream.read(data, 0, Constants.BUFFER_8K)) != -1) {
 					destinationOutputStream.write(data, 0, count);
 				}
 				destinationOutputStream.flush();
-				destinationOutputStream.close();
-				zipInputStream.close();
-
 			}
-
 			return true;
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		} catch (FileNotFoundException fileNotFoundException) {
+			Log.e(TAG, fileNotFoundException.getMessage(), fileNotFoundException);
+		} catch (IOException ioException) {
+			Log.e(TAG, ioException.getMessage(), ioException);
+		} finally {
+			try {
+				if (destinationOutputStream != null) {
+					destinationOutputStream.close();
+				}
+				if (zipInputStream != null) {
+					zipInputStream.close();
+				}
+			} catch (IOException ignoredException) {
+				Log.e(TAG, ERROR_CLOSING_STREAM, ignoredException);
+			}
 		}
 		return false;
 	}
-
 }
