846eab61927c29fc55d39826dd516458a9d36a3e
tafphil <TafPhil@gmail.com>
2014-02-25 20:27:07 +0100

    first step to provide physic-var in FormulaEditor

--- catroid/src/org/catrobat/catroid/stage/PreStageActivity.java
+++ catroid/src/org/catrobat/catroid/stage/PreStageActivity.java
@@ -306,9 +306,6 @@ public class PreStageActivity extends BaseActivity {
 				sprite.setActionFactory(actionPhysicsFactory);
 				ressources &= ~Brick.PHYSIC;
 			}
-			//			else {
-			//				sprite.setActionFactory(actionFactory);
-			//			}
 		}
 
 		return ressources;
