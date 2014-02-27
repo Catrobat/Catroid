886c626666124cd15dacdd8d30b8081c6d521308
Armin Wieser <armin.wieser@gmail.com>
2014-02-27 10:55:40 +0100

    thread loop for led
    Signed-off-by: Armin Wieser <armin.wieser@gmail.com>

--- catroid/src/org/catrobat/catroid/utils/LEDUtil.java
+++ catroid/src/org/catrobat/catroid/utils/LEDUtil.java
@@ -5,6 +5,8 @@ import android.hardware.Camera;
 import android.os.Build;
 import android.util.Log;
 
+import java.util.concurrent.Semaphore;
+
 
 /**
  * Created by bernd on 2/21/14.
@@ -19,25 +21,41 @@ public class LEDUtil {
     private static boolean ledValue = false;
     private static boolean paused = false;
 
-    private static Thread lightThread;
+    private static boolean lightThreadActive = true;
+    private static Semaphore lightThreadSemaphore = new Semaphore(1);
+    private static Thread lightThread = new Thread(new Runnable() {
+
+        @Override
+        public void run() {
+            while (lightThreadActive) {
+                try {
+                    lightThreadSemaphore.acquire();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                setLED(ledValue);
+            }
+        }
+    });
+
 
     public static boolean getLEDValue() {
         return ledValue;
     }
 
-    public static void setLEDValue( boolean val ) {
+    public static void setLEDValue(boolean val) {
         //Log.d(LOG_TAG, "setLEDValue");
         // TODO: Wake up thread
         ledValue = val;
-        lightThread.run();
+        lightThreadSemaphore.release();
     }
 
     public static void pauseLED() {
         Log.d(LOG_TAG, "pauseLED");
-        if ( !paused ) {
+        if (!paused) {
             paused = true;
-            if ( lightON == true ) {
-                setLEDValue( false );
+            if (lightON == true) {
+                setLEDValue(false);
                 previousLightOn = true;
             } else {
                 previousLightOn = false;
@@ -47,7 +65,7 @@ public class LEDUtil {
 
     public static void resumeLED() {
         Log.d(LOG_TAG, "resumeLED()");
-        setLEDValue( previousLightOn );
+        setLEDValue(previousLightOn);
         paused = false;
     }
 
@@ -57,19 +75,22 @@ public class LEDUtil {
 
     public static void activateLEDThread() {
         Log.d(LOG_TAG, "activateLEDThread()");
-        lightThread = new Thread( new Runnable() {
-            @Override
-            public void run() {
-                setLED( ledValue );
-            }
-        });
         lightThread.setName("lightThread");
-        lightThread.start();
+
+        if (!lightThread.isAlive()) {
+
+            lightThread.start();
+            try {
+                lightThreadSemaphore.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    private synchronized static void setLED( boolean ledValue ) {
+    private synchronized static void setLED(boolean ledValue) {
         Log.d(LOG_TAG, "setLED()");
-        if ( ledValue )
+        if (ledValue)
             ledON();
         else
             ledOFF();
@@ -83,21 +104,21 @@ public class LEDUtil {
         try {
             cam = Camera.open();
 
-            if ( cam != null ) {
+            if (cam != null) {
                 Camera.Parameters params = cam.getParameters();
                 params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
 
-                if ( android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.GINGERBREAD_MR1 ) {
-                    cam.setPreviewTexture( new SurfaceTexture( 0 ) );
+                if (android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.GINGERBREAD_MR1) {
+                    cam.setPreviewTexture(new SurfaceTexture(0));
                 }
 
                 cam.setParameters(params);
                 cam.startPreview();
                 lightON = true;
             }
-        } catch( Exception e ) {
+        } catch (Exception e) {
             // TODO: Toast message
-            Log.d( LOG_TAG, e.getMessage() );
+            Log.d(LOG_TAG, e.getMessage());
         }
     }
 
@@ -107,14 +128,14 @@ public class LEDUtil {
 
         Log.d(LOG_TAG, "ledOFF()");
         try {
-            if ( cam != null ) {
-                cam.stopPreview();;
+            if (cam != null) {
+                cam.stopPreview();
                 cam.release();
                 cam = null;
                 lightON = false;
             }
-        } catch ( Exception e ) {
-            Log.d( LOG_TAG, e.getMessage() );
+        } catch (Exception e) {
+            Log.d(LOG_TAG, e.getMessage());
         }
     }
 }
