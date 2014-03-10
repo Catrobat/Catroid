732fa87cd8f5bb3b73efd764523eef4775813d9c
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2014-03-10 18:53:51 +0100

    fixed PMDWarnings.

--- catroidSourceTest/src/org/catrobat/catroid/test/utils/ReflectionTest.java
+++ catroidSourceTest/src/org/catrobat/catroid/test/utils/ReflectionTest.java
@@ -42,7 +42,9 @@ public class ReflectionTest extends TestCase {
 			File file = new File(fileString);
 
 			reader = new BufferedReader(new FileReader(file));
-			while (!reader.readLine().contains("class Reflection")) {
+			boolean readUntilClassReflection = true;
+			while (readUntilClassReflection) {
+				readUntilClassReflection = !reader.readLine().contains("class Reflection");
 			}
 
 			StringBuilder builder = new StringBuilder();
@@ -63,6 +65,18 @@ public class ReflectionTest extends TestCase {
 			SmartFileContent smartFileContent = (SmartFileContent) object;
 			return context.compareTo(smartFileContent.context) == 0;
 		}
+
+		@Override
+		public int hashCode()
+		{
+			int result = 37;
+			int prime = 41;
+			if (context != null) {
+				result = prime * result + context.hashCode();
+			}
+			return result;
+		}
+
 	}
 
 	public void testIdenticalReflectionClassInTestProjects() throws IOException {
