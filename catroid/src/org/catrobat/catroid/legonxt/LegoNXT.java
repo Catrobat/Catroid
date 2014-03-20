57f2e1f200d128c65fca690b0edab2c5a0dd1d75
Tobias Ibounig <tobias.ibounig@outlook.com>
2014-03-20 12:38:31 +0100

    Refactor Bluetooth connection Refactor the bluetooth socket connection. The code to establish a socket is not trivial. It should be easy to reuse it for future projects (Albert, Arduino..)

--- catroid/src/org/catrobat/catroid/legonxt/LegoNXT.java
+++ catroid/src/org/catrobat/catroid/legonxt/LegoNXT.java
@@ -43,7 +43,6 @@
 package org.catrobat.catroid.legonxt;
 
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -119,8 +118,7 @@ public class LegoNXT implements BTConnectable {
 			}
 		}
 
-		myNXTCommunicator = new LegoNXTBtCommunicator(this, recieverHandler, BluetoothAdapter.getDefaultAdapter(),
-				activity.getResources());
+		myNXTCommunicator = new LegoNXTBtCommunicator(recieverHandler, activity.getResources());
 		btcHandler = myNXTCommunicator.getHandler();
 
 		((LegoNXTBtCommunicator) myNXTCommunicator).setMACAddress(macAddress);
