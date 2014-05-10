6d69d54aca27b7d4bd800ebca2d437138a68d293
BerndBaumann <berndbaumann42@gmail.com>
2014-05-11 00:25:01 +0200

    import order

--- catroidTest/src/org/catrobat/catroid/uitest/util/SensorServerUtils.java
+++ catroidTest/src/org/catrobat/catroid/uitest/util/SensorServerUtils.java
@@ -24,7 +24,8 @@ package org.catrobat.catroid.uitest.util;
 
 import android.util.Log;
 
-import junit.framework.AssertionFailedError;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -32,9 +33,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertTrue;
-
+import junit.framework.AssertionFailedError;
 
 public final class SensorServerUtils {
 	private static final String LOG_TEST = "SensorServerUtils::";
