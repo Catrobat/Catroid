7d915f50e90bb08d2e2dbe9512cba8ecebe3f825
Jayaruwan Mannapperuma <mj7007@gmail.com>
2014-05-17 08:49:42 +0530

    Revert "Revert "UtilFile.java/loadExistingOrCreateStandardDroneProject updated""
    This reverts commit d2381b454a4ccbc6c71aac8c5a458750abbb0482.

--- catroidTest/src/org/catrobat/catroid/uitest/drone/DroneBrickLayoutTest.java
+++ catroidTest/src/org/catrobat/catroid/uitest/drone/DroneBrickLayoutTest.java
@@ -27,6 +27,7 @@ import android.preference.PreferenceManager;
 import android.widget.ListView;
 
 import org.catrobat.catroid.R;
+import org.catrobat.catroid.exceptions.ProjectException;
 import org.catrobat.catroid.ui.MainMenuActivity;
 import org.catrobat.catroid.ui.SettingsActivity;
 import org.catrobat.catroid.uitest.annotation.Device;
@@ -58,7 +59,11 @@ public class DroneBrickLayoutTest extends BaseActivityInstrumentationTestCase<Ma
 		boolean droneEnabled = preferences.getBoolean(SettingsActivity.SETTINGS_QUADCOPTER_BRICKS, false);
 		assertTrue("Drone Bricks must be anabled to pass this test, check the Constructor and setup.", droneEnabled);
 
-		UtilFile.loadExistingOrCreateStandardDroneProject(getActivity());
+		try {
+			UtilFile.loadExistingOrCreateStandardDroneProject(getActivity());
+		} catch (ProjectException cannotCreateDroneProjectException) {
+			fail("Error loading standard drone project");
+		}
 		solo.waitForActivity(MainMenuActivity.class);
 		solo.clickOnText(solo.getString(R.string.main_menu_continue));
 		solo.waitForText(solo.getString(R.string.default_drone_project_sprites_takeoff));
