f00c95662122c634a00219d5622b070308aa0240
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2014-05-22 16:42:45 +0200

    checkboxes removed after delete dialog is cancelled.

--- catroidTest/src/org/catrobat/catroid/uitest/ui/fragment/SoundFragmentTest.java
+++ catroidTest/src/org/catrobat/catroid/uitest/ui/fragment/SoundFragmentTest.java
@@ -1145,7 +1145,29 @@ public class SoundFragmentTest extends BaseActivityInstrumentationTestCase<MainM
 		StorageHandler.getInstance().saveProject(projectManager.getCurrentProject());
 	}
 
+	public void testOpenDeleteDialogAndGoBack()
+	{
+		int viewAmountBeforeDeleteMode = solo.getCurrentViews().size();
+		UiTestUtils.openActionMode(solo, delete, R.id.delete, getActivity());
 
+		assertTrue("Bottom bar is visible", solo.getView(R.id.bottom_bar).getVisibility() == View.GONE);
+
+		int[] checkboxIndicesToCheck = { solo.getCurrentViews(CheckBox.class).size() - 1, 0, 2 };
+
+		solo.scrollDown();
+		solo.clickOnCheckBox(checkboxIndicesToCheck[0]);
+		solo.scrollToTop();
+
+		UiTestUtils.acceptAndCloseActionMode(solo);
+		solo.clickOnButton(solo.getString(R.string.no));
+
+		solo.sleep(300);
+		int viewAmountAfterDeleteMode = solo.getCurrentViews().size();
+
+		assertTrue("checkboxes or other delete elements are still visible",viewAmountBeforeDeleteMode==viewAmountAfterDeleteMode);
+
+	}
+	
 	private void renameSound(String soundToRename, String newSoundName) {
 		clickOnContextMenuItem(soundToRename, solo.getString(R.string.rename));
 		assertTrue("Wrong title of dialog", solo.searchText(renameDialogTitle));
