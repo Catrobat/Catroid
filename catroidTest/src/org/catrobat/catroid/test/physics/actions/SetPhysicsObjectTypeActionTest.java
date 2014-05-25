d0c8d6550ab648ea8c8eea3d58b9f130cc182219
Phillip Goriup <phillip.goriup@appinxs.com>
2014-05-25 16:26:18 +0200

    Add different physics action tests

---
+++ catroidTest/src/org/catrobat/catroid/test/physics/actions/SetPhysicsObjectTypeActionTest.java
@@ -0,0 +1,47 @@
+package org.catrobat.catroid.test.physics.actions;
+
+import org.catrobat.catroid.physics.PhysicsObject;
+import org.catrobat.catroid.physics.content.actions.SetPhysicsObjectTypeAction;
+
+public class SetPhysicsObjectTypeActionTest extends PhysicsActionTestCase {
+
+	public void testPhysicsTypeNone() {
+		PhysicsObject.Type type = PhysicsObject.Type.NONE;
+		SetPhysicsObjectTypeAction setPhysicsObjectTypeAction = new SetPhysicsObjectTypeAction();
+		setPhysicsObjectTypeAction.setPhysicsObject(physicsObject);
+		setPhysicsObjectTypeAction.setType(type);
+
+		assertEquals("Unexpected physics object type", PhysicsObject.Type.NONE, physicsObject.getType());
+
+		setPhysicsObjectTypeAction.act(1.0f);
+
+		assertEquals("Unexpected physics object type", type, physicsObject.getType());
+	}
+
+	public void testPhysicsTypeDynamic() {
+		PhysicsObject.Type type = PhysicsObject.Type.DYNAMIC;
+		SetPhysicsObjectTypeAction setPhysicsObjectTypeAction = new SetPhysicsObjectTypeAction();
+		setPhysicsObjectTypeAction.setPhysicsObject(physicsObject);
+		setPhysicsObjectTypeAction.setType(type);
+
+		assertEquals("Unexpected physics object type", PhysicsObject.Type.NONE, physicsObject.getType());
+
+		setPhysicsObjectTypeAction.act(1.0f);
+
+		assertEquals("Unexpected physics object type", type, physicsObject.getType());
+	}
+
+	public void testPhysicsTypeFixed() {
+		PhysicsObject.Type type = PhysicsObject.Type.FIXED;
+		SetPhysicsObjectTypeAction setPhysicsObjectTypeAction = new SetPhysicsObjectTypeAction();
+		setPhysicsObjectTypeAction.setPhysicsObject(physicsObject);
+		setPhysicsObjectTypeAction.setType(type);
+
+		assertEquals("Unexpected physics object type", PhysicsObject.Type.NONE, physicsObject.getType());
+
+		setPhysicsObjectTypeAction.act(1.0f);
+
+		assertEquals("Unexpected physics object type", type, physicsObject.getType());
+	}
+
+}
---
+++ catroidTest/src/org/catrobat/catroid/test/physics/actions/TurnLeftSpeedActionTest.java
@@ -0,0 +1,53 @@
+package org.catrobat.catroid.test.physics.actions;
+
+import org.catrobat.catroid.formulaeditor.Formula;
+import org.catrobat.catroid.physics.content.actions.TurnLeftSpeedAction;
+
+public class TurnLeftSpeedActionTest extends PhysicsActionTestCase {
+
+	public void testNormalBehavior() {
+		float speed = 45.55f;
+		Formula speedFormula = new Formula(speed);
+		TurnLeftSpeedAction turnLeftSpeedAction = new TurnLeftSpeedAction();
+		turnLeftSpeedAction.setSprite(sprite);
+		turnLeftSpeedAction.setPhysicsObject(physicsObject);
+		turnLeftSpeedAction.setSpeed(speedFormula);
+
+		assertEquals("Unexpected rotation speed value", 0.0f, physicsObject.getRotationSpeed());
+
+		turnLeftSpeedAction.act(1.0f);
+
+		assertEquals("Unexpected rotation speed value", speed, physicsObject.getRotationSpeed());
+	}
+
+	public void testNegativeValue() {
+		float speed = -45.55f;
+		Formula speedFormula = new Formula(speed);
+		TurnLeftSpeedAction turnLeftSpeedAction = new TurnLeftSpeedAction();
+		turnLeftSpeedAction.setSprite(sprite);
+		turnLeftSpeedAction.setPhysicsObject(physicsObject);
+		turnLeftSpeedAction.setSpeed(speedFormula);
+
+		assertEquals("Unexpected rotation speed value", 0.0f, physicsObject.getRotationSpeed());
+
+		turnLeftSpeedAction.act(1.0f);
+
+		assertEquals("Unexpected rotation speed value", speed, physicsObject.getRotationSpeed());
+	}
+
+	public void testZeroValue() {
+		float speed = 0f;
+		Formula speedFormula = new Formula(speed);
+		TurnLeftSpeedAction turnLeftSpeedAction = new TurnLeftSpeedAction();
+		turnLeftSpeedAction.setSprite(sprite);
+		turnLeftSpeedAction.setPhysicsObject(physicsObject);
+		turnLeftSpeedAction.setSpeed(speedFormula);
+
+		assertEquals("Unexpected rotation speed value", 0.0f, physicsObject.getRotationSpeed());
+
+		turnLeftSpeedAction.act(1.0f);
+
+		assertEquals("Unexpected rotation speed value", speed, physicsObject.getRotationSpeed());
+	}
+
+}
