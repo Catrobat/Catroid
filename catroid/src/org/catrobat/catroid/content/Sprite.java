4cebd7b629beff833c58815162bf911a05a7cb28
Joerg Hofer <joerg.hofer@student.tugraz.at>
2014-07-05 13:02:17 +0200

    Add VelocityActionTest

--- catroid/src/org/catrobat/catroid/content/Sprite.java
+++ catroid/src/org/catrobat/catroid/content/Sprite.java
@@ -419,9 +419,6 @@ public class Sprite implements Serializable, Cloneable {
 
 	public void setLookDataList(ArrayList<LookData> list) {
 		lookList = list;
-		if (list.get(0) != null) {
-
-		}
 	}
 
 	public ArrayList<SoundInfo> getSoundList() {
