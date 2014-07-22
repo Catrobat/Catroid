ca85bce64a43732a5941efaf347c7cd48927960b
Roland Urbano <urbano@student.tugraz.at>
2014-07-22 13:43:27 +0200

    test buildStandalone task
    added license to ManifestTests
    
    improved tests, fixed file paths to catroid

--- catroidSourceTest/src/test/groovy/ManifestTests.groovy
+++ catroidSourceTest/src/test/groovy/ManifestTests.groovy
@@ -1,4 +1,25 @@
-
+/**
+ *  Catroid: An on-device visual programming system for Android devices
+ *  Copyright (C) 2010-2013 The Catrobat Team
+ *  (<http://developer.catrobat.org/credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  An additional term exception under section 7 of the GNU Affero
+ *  General Public License, version 3, is available at
+ *  http://developer.catrobat.org/license_additional_term
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import org.junit.After
 import org.junit.Before
@@ -43,5 +64,31 @@ public class ManifestTests {
         programName = programName.replaceAll(" ", "")
         println manifestXML.attribute('package') + ' vs ' + ("org.catrobat.catroid." + programName)
         assert manifestXML.attribute('package') == ("org.catrobat.catroid." + programName) //change this!
+    }
+
+    @Test
+    public void testLaunchIconExists() {
+        def icon = new File('../catroid/res/drawable/icon.png')
+        assert icon.exists(), "no icon.png found"
+    }
+
+    @Test
+    public void testSplashScreenExists() {
+        def splashscreen = new File('../catroid/res/drawable/splash_screen.png')
+        assert splashscreen.exists(), "no splash_screen.png found"
+    }
+
+    @Test
+    public void testDownloadSucceeded() {
+        def zipfile = new File('../catroid/assets/Galaxy War.zip') //TODO maybe use BuildConfig?
+        assert zipfile.exists(), "Galaxy War.zip not found in assets"
+    }
+
+    @Test
+    public void testUnzipSuceeded() {
+        def unzipfolder = new File('../catroid/assets/standalone')
+        assert unzipfolder.exists(), "standalone/ directory not found"
+        assert unzipfolder.isDirectory()
+
     }
 }
