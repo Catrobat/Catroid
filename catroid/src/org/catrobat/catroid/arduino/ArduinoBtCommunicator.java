b4376c393e9e33b73498e8025ade7667af906f79
Adrian Schnedlitz <a.schnedlitz@student.tugraz.at>
2014-07-24 19:04:19 +0200

    added Arduino_Send_Brick into the Master branch ToDo: Functionality + Arduino Function Implementation

--- catroid/src/org/catrobat/catroid/arduino/ArduinoBtCommunicator.java
+++ catroid/src/org/catrobat/catroid/arduino/ArduinoBtCommunicator.java
@@ -2,21 +2,21 @@
  *  Catroid: An on-device visual programming system for Android devices
  *  Copyright (C) 2010-2013 The Catrobat Team
  *  (<http://developer.catrobat.org/credits>)
- *  
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as
  *  published by the Free Software Foundation, either version 3 of the
  *  License, or (at your option) any later version.
- *  
+ *
  *  An additional term exception under section 7 of the GNU Affero
  *  General Public License, version 3, is available at
  *  http://developer.catrobat.org/license_additional_term
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *  GNU Affero General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -83,26 +83,26 @@ public class ArduinoBtCommunicator extends ArduinoCommunicator {
 
 	@Override
 	public void createArduinoConnection() throws IOException {
-			BluetoothConnection bluetoothConnection = new BluetoothConnection(macAddress,
-					SERIAL_PORT_SERVICE_CLASS_UUID);
-			BluetoothConnection.State state = bluetoothConnection.connect();
+		BluetoothConnection bluetoothConnection = new BluetoothConnection(macAddress,
+				SERIAL_PORT_SERVICE_CLASS_UUID);
+		BluetoothConnection.State state = bluetoothConnection.connect();
 
-			switch (state) {
-				case CONNECTED:
-					break;
-				case ERROR_NOT_BONDED:
-				case ERROR_STILL_BONDING:
-					sendToast(resources.getString(R.string.no_paired_nxt)); //change stringS
-				default:
-					sendState(STATE_CONNECTERROR);
-					throw new IOException("Bluetooth connecting error " + state.name());
-			}
+		switch (state) {
+			case CONNECTED:
+				break;
+			case ERROR_NOT_BONDED:
+			case ERROR_STILL_BONDING:
+				sendToast(resources.getString(R.string.no_paired_nxt)); //change stringS
+			default:
+				sendState(STATE_CONNECTERROR);
+				throw new IOException("Bluetooth connecting error " + state.name());
+		}
 
-			btSocket = bluetoothConnection.getBluetoothSocket();
-			arduinoInputStream = btSocket.getInputStream();
-			arduinoOutputStream = btSocket.getOutputStream();
-			isConnected = true;
-			sendState(STATE_CONNECTED);
+		btSocket = bluetoothConnection.getBluetoothSocket();
+		arduinoInputStream = btSocket.getInputStream();
+		arduinoOutputStream = btSocket.getOutputStream();
+		isConnected = true;
+		sendState(STATE_CONNECTED);
 	}
 
 	@Override
@@ -130,7 +130,7 @@ public class ArduinoBtCommunicator extends ArduinoCommunicator {
 
 	/**
 	 * Sends a message on the opened OutputStream
-	 * 
+	 *
 	 * @param message , the message as a byte array
 	 */
 	@Override
@@ -148,7 +148,7 @@ public class ArduinoBtCommunicator extends ArduinoCommunicator {
 
 	/**
 	 * Receives a message on the opened InputStream
-	 * 
+	 *
 	 * @return the message
 	 */
 	@Override
