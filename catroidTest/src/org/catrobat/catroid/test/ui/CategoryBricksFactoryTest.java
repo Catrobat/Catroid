83b5a1a3a5e051ab0f1bbc0cc587e687562fe415
BerndBaumann <berndbaumann42@gmail.com>
2014-07-28 18:01:06 +0200

    wrong number of motion bricks fixed

--- catroidTest/src/org/catrobat/catroid/test/ui/CategoryBricksFactoryTest.java
+++ catroidTest/src/org/catrobat/catroid/test/ui/CategoryBricksFactoryTest.java
@@ -59,9 +59,9 @@ public class CategoryBricksFactoryTest extends AndroidTestCase {
 	}
 
 	public void testMotionBricks() {
-		final int expectedBackgroundBrickCount = 12;
+		final int expectedBackgroundBrickCount = 11;
 		checkBrickCountInCategory(R.string.category_motion, background, expectedBackgroundBrickCount);
-		final int expectedSpriteBrickCount = 15;
+		final int expectedSpriteBrickCount = 14;
 		checkBrickCountInCategory(R.string.category_motion, sprite, expectedSpriteBrickCount);
 	}
 
@@ -72,7 +72,7 @@ public class CategoryBricksFactoryTest extends AndroidTestCase {
 	}
 
 	public void testLooksBricks() {
-		final int expectedBrickCount = 13;
+		final int expectedBrickCount = 14;
 		checkBrickCountInCategory(R.string.category_looks, background, expectedBrickCount);
 		checkBrickCountInCategory(R.string.category_looks, sprite, expectedBrickCount);
 	}
