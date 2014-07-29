8a05073985714100201664afb923b0c73702f581
Phillip Goriup <phillip.goriup@appinxs.com>
2014-07-29 12:31:19 +0200

    implement CAT-1064 - When collision between add test and change testsuite for collision tests solve conflicts of rebaseing

--- catroid/src/org/catrobat/catroid/physics/PhysicsCollision.java
+++ catroid/src/org/catrobat/catroid/physics/PhysicsCollision.java
@@ -22,31 +22,65 @@
  */
 package org.catrobat.catroid.physics;
 
+import android.util.Log;
+
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Manifold;
 
-import org.catrobat.catroid.ProjectManager;
-import org.catrobat.catroid.content.BroadcastEvent;
-import org.catrobat.catroid.content.BroadcastEvent.BroadcastType;
 import org.catrobat.catroid.content.Sprite;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PhysicsCollision implements ContactListener {
-	//@SuppressWarnings("unused")
-	//private static final String TAG = PhysicsCollision.class.getSimpleName();
-	PhysicsWorld physicsWorld;
+	private static final String TAG = PhysicsCollision.class.getSimpleName();
+	private PhysicsWorld physicsWorld;
 
 	public PhysicsCollision(PhysicsWorld physicsWorld) {
 		this.physicsWorld = physicsWorld;
 	}
 
+	Map<String, PhysicsCollisionBroadcast> physicsCollisionBroadcasts = new HashMap<String, PhysicsCollisionBroadcast>();
+
+	private static String generateKey(Sprite sprite1, Sprite sprite2) {
+		String key = sprite1.getName().concat(sprite2.getName());
+		return key;
+	}
+
+	private void registerContact(Sprite sprite1, Sprite sprite2) {
+		String key1 = generateKey(sprite1, sprite2);
+		String key2 = generateKey(sprite2, sprite1);
+		if (!physicsCollisionBroadcasts.containsKey(key1)) {
+			PhysicsCollisionBroadcast physicsCollisionBroadcast = new PhysicsCollisionBroadcast(sprite1.getName(), sprite2.getName());
+			physicsCollisionBroadcasts.put(key1, physicsCollisionBroadcast);
+			physicsCollisionBroadcasts.put(key2, physicsCollisionBroadcast);
+		}
+		physicsCollisionBroadcasts.get(key1).increaseContactCounter();
+		Log.d(TAG, "registerContact:" + physicsCollisionBroadcasts.toString());
+	}
+
+	private void unregisterContact(Sprite sprite1, Sprite sprite2) {
+		String key1 = generateKey(sprite1, sprite2);
+		String key2 = generateKey(sprite2, sprite1);
+		if (physicsCollisionBroadcasts.containsKey(key1)) {
+			PhysicsCollisionBroadcast physicsCollisionBroadcast = physicsCollisionBroadcasts.get(key1);
+			physicsCollisionBroadcast.decreaseContactCounter();
+
+			if (physicsCollisionBroadcast.getContactCounter() == 0) {
+				physicsCollisionBroadcast.sendBroadcast();
+				Log.d(TAG, "remove contact:" + physicsCollisionBroadcasts.toString());
+				physicsCollisionBroadcasts.remove(key1);
+				physicsCollisionBroadcasts.remove(key2);
+			}
+		}
+	}
+
 	@Override
 	public void beginContact(Contact contact) {
-		//Log.d(TAG, "beginContact");
+		Log.d(TAG, "beginContact");
 
 		Body a = contact.getFixtureA().getBody();
 		Body b = contact.getFixtureB().getBody();
@@ -60,31 +94,22 @@ public class PhysicsCollision implements ContactListener {
 		} else if (a.getUserData() instanceof Sprite && b.getUserData() instanceof Sprite) {
 			Sprite sprite1 = (Sprite) a.getUserData();
 			Sprite sprite2 = (Sprite) b.getUserData();
-
-			physicsWorld.resetActiveInOnEdgeBounce(sprite1);
-			physicsWorld.resetActiveInOnEdgeBounce(sprite2);
-
-			//Log.d(TAG, "# COLLISION # :" + sprite1.getName() + "<->" + sprite2.getName());
-			fireEvent(sprite1.getName() + "<->" + sprite2.getName());
-			fireEvent(sprite2.getName() + "<->" + sprite1.getName());
-			fireEvent(sprite2.getName() + "<->anybody");
-			fireEvent(sprite2.getName() + "<->anybody");
-		}
-	}
-
-	private void fireEvent(String message) {
-		BroadcastEvent event = new BroadcastEvent();
-		event.setBroadcastMessage(message);
-		event.setType(BroadcastType.broadcast);
-		List<Sprite> sprites = ProjectManager.getInstance().getCurrentProject().getSpriteList();
-		for (Sprite spriteOfList : sprites) {
-			spriteOfList.look.fire(event);
+			registerContact(sprite1, sprite2);
 		}
 	}
 
 	@Override
 	public void endContact(Contact contact) {
-		//Log.d(TAG, "endContact");
+		Log.d(TAG, "endContact");
+
+		Body a = contact.getFixtureA().getBody();
+		Body b = contact.getFixtureB().getBody();
+
+		if (a.getUserData() instanceof Sprite && b.getUserData() instanceof Sprite) {
+			Sprite sprite1 = (Sprite) a.getUserData();
+			Sprite sprite2 = (Sprite) b.getUserData();
+			unregisterContact(sprite1, sprite2);
+		}
 	}
 
 	@Override
@@ -96,5 +121,4 @@ public class PhysicsCollision implements ContactListener {
 	public void postSolve(Contact contact, ContactImpulse impulse) {
 		//Log.d(TAG, "postSolve");
 	}
-
 }
---
+++ catroidTest/src/org/catrobat/catroid/test/physics/PhysicsCollisionBetweenTest.java
@@ -0,0 +1,76 @@
+/**
+ *  Catroid: An on-device visual programming system for Android devices
+ *  Copyright (C) 2010-2013 The Catrobat Team
+ *  (<http://developer.catrobat.org/credits>)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  An additional term exception under section 7 of the GNU Affero
+ *  General Public License, version 3, is available at
+ *  http://developer.catrobat.org/license_additional_term
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.catrobat.catroid.test.physics;
+
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Contact;
+
+import org.catrobat.catroid.physics.PhysicsCollision;
+import org.catrobat.catroid.physics.PhysicsCollisionBroadcast;
+import org.catrobat.catroid.physics.PhysicsObject;
+import org.catrobat.catroid.test.utils.Reflection;
+
+import java.util.Map;
+
+public class PhysicsCollisionBetweenTest extends PhysicsCollisionBaseTest {
+
+	public PhysicsCollisionBetweenTest() {
+		spritePosition = new Vector2(0.0f, 100.0f);
+		sprite2Position = new Vector2(0.0f, -200.0f);
+		physicsObject1Type = PhysicsObject.Type.DYNAMIC;
+		physicsObject2Type = PhysicsObject.Type.FIXED;
+	}
+
+	@Override
+	public void beginContactCallback(Contact contact) {
+		super.beginContactCallback(contact);
+		Map<Integer, PhysicsCollisionBroadcast> physicsCollisionBroadcasts = (Map<Integer, PhysicsCollisionBroadcast>) Reflection.getPrivateField(PhysicsCollision.class, physicsCollisionTestListener, "physicsCollisionBroadcasts");
+		assertTrue("Map must contain one element", physicsCollisionBroadcasts.size() == 2);
+		Object[] parameters = {sprite, sprite2};
+		Reflection.ParameterList paramList = new Reflection.ParameterList(parameters);
+		int key = (Integer) Reflection.invokeMethod(PhysicsCollision.class, physicsCollisionTestListener, "generateKey", paramList);
+		PhysicsCollisionBroadcast collisionBroadcast = physicsCollisionBroadcasts.get(key);
+		assertEquals("collision broadcast counter must be equal to beginCounter - endCounter", collisionBroadcast.getContactCounter(), getContactDifference());
+	}
+
+	@Override
+	public void endContactCallback(Contact contact) {
+		super.endContactCallback(contact);
+		Map<Integer, PhysicsCollisionBroadcast> physicsCollisionBroadcasts = (Map<Integer, PhysicsCollisionBroadcast>) Reflection.getPrivateField(PhysicsCollision.class, physicsCollisionTestListener, "physicsCollisionBroadcasts");
+		if (getContactDifference() == 0) {
+			assertTrue("Map must contain zero elements", physicsCollisionBroadcasts.size() == 0);
+		} else {
+			assertTrue("Map must contain one element", physicsCollisionBroadcasts.size() == 2);
+		}
+	}
+
+
+	public void testIfBroadcastsAreCorrectPreparedAndFired() {
+		assertTrue("collision rate is not zero before step", isContactRateOk());
+		assertTrue("no collision detected", simulateFullCollision());
+		assertTrue("collision rate is not zero after step", isContactRateOk());
+	}
+
+}
