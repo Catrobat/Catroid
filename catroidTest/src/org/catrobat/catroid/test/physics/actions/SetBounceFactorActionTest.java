f55c83984ae0e32d3b8694b3bb4db0c2604da694
Phillip Goriup <phillip.goriup@appinxs.com>
2014-08-11 12:37:59 +0200

    refactor SetBounceFactorActionTest for CAT-1009 and CAT-1008

--- catroidTest/src/org/catrobat/catroid/test/physics/actions/SetBounceFactorActionTest.java
+++ catroidTest/src/org/catrobat/catroid/test/physics/actions/SetBounceFactorActionTest.java
@@ -20,79 +20,91 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.catrobat.catroid.test.physics.actions;
 
-import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.math.Vector2;
 
+import org.catrobat.catroid.common.ScreenValues;
 import org.catrobat.catroid.formulaeditor.Formula;
 import org.catrobat.catroid.physics.PhysicsObject;
 import org.catrobat.catroid.physics.content.actions.SetBounceFactorAction;
-import org.catrobat.catroid.test.physics.PhysicsBaseTest;
-import org.catrobat.catroid.test.utils.Reflection;
+import org.catrobat.catroid.test.physics.PhysicsCollisionBaseTest;
 
-public class SetBounceFactorActionTest extends PhysicsBaseTest {
+public class SetBounceFactorActionTest extends PhysicsCollisionBaseTest {
 
-	public void testNormalBehavior() {
-		float bounceFactor = 45.55f;
-		PhysicsObject physicsObject = physicsWorld.getPhysicsObject(sprite);
-		Formula bounceFactorFormula = new Formula(bounceFactor);
-		SetBounceFactorAction setBounceFactorAction = new SetBounceFactorAction();
-		setBounceFactorAction.setSprite(sprite);
-		setBounceFactorAction.setPhysicsObject(physicsObject);
-		setBounceFactorAction.setBounceFactor(bounceFactorFormula);
-
-		float fixtureBouceFactor = ((FixtureDef) Reflection.getPrivateField(PhysicsObject.class, physicsObject,
-				"fixtureDef")).restitution;
-
-		assertEquals("Unexpected bounceFactor value", PhysicsObject.DEFAULT_BOUNCE_FACTOR, fixtureBouceFactor);
-
-		setBounceFactorAction.act(1.0f);
-
-		fixtureBouceFactor = ((FixtureDef) Reflection.getPrivateField(PhysicsObject.class, physicsObject, "fixtureDef")).restitution;
-
-		assertEquals("Unexpected bounceFactor value", bounceFactor / 100.0f, fixtureBouceFactor);
+	public SetBounceFactorActionTest() {
+		spritePosition = new Vector2(0.0f, 100.0f);
+		sprite2Position = new Vector2(0.0f, -200.0f);
+		physicsObject1Type = PhysicsObject.Type.DYNAMIC;
+		physicsObject2Type = PhysicsObject.Type.FIXED;
 	}
 
-	public void testNegativeValue() {
-		float bounceFactor = -45.55f;
-		PhysicsObject physicsObject = physicsWorld.getPhysicsObject(sprite);
-		Formula bounceFactorFormula = new Formula(bounceFactor);
-		SetBounceFactorAction setBounceFactorAction = new SetBounceFactorAction();
-		setBounceFactorAction.setSprite(sprite);
-		setBounceFactorAction.setPhysicsObject(physicsObject);
-		setBounceFactorAction.setBounceFactor(bounceFactorFormula);
-
-		float fixtureBouceFactor = ((FixtureDef) Reflection.getPrivateField(PhysicsObject.class, physicsObject,
-				"fixtureDef")).restitution;
-
-		assertEquals("Unexpected bounceFactor value", PhysicsObject.DEFAULT_BOUNCE_FACTOR, fixtureBouceFactor);
-
-		setBounceFactorAction.act(1.0f);
-
-		fixtureBouceFactor = ((FixtureDef) Reflection.getPrivateField(PhysicsObject.class, physicsObject, "fixtureDef")).restitution;
-
-		assertEquals("Unexpected bounceFactor value", PhysicsObject.MIN_BOUNCE_FACTOR, fixtureBouceFactor);
+	public void testDefaultBounceFactor() {
+		physicsWorld.step(1.0f);
+		assertEquals("Unexpected bounce factor", PhysicsObject.DEFAULT_BOUNCE_FACTOR,
+				physicsWorld.getPhysicsObject(sprite).getBounceFactor());
 	}
 
 	public void testZeroValue() {
-		float bounceFactor = 0f;
-		PhysicsObject physicsObject = physicsWorld.getPhysicsObject(sprite);
+		float bounceFactor = 0.0f;
+		initBounceFactor(bounceFactor);
+		assertEquals("Unexpected bounce factor", bounceFactor / 100.0f, physicsWorld.getPhysicsObject(sprite)
+				.getBounceFactor());
+	}
+
+	public void testHighValue() {
+		float bounceFactor = 250.0f;
+		initBounceFactor(bounceFactor);
+		assertEquals("Unexpected bounce factor", PhysicsObject.MAX_BOUNCE_FACTOR, physicsWorld.getPhysicsObject(sprite)
+				.getBounceFactor());
+	}
+
+	public void testNegativeValue() {
+		float bounceFactor = -50.0f;
+		initBounceFactor(bounceFactor);
+		assertEquals("Unexpected bounce factor", PhysicsObject.MIN_BOUNCE_FACTOR, physicsWorld.getPhysicsObject(sprite)
+				.getBounceFactor());
+	}
+
+	private void initBounceFactor(float bounceFactor) {
 		Formula bounceFactorFormula = new Formula(bounceFactor);
 		SetBounceFactorAction setBounceFactorAction = new SetBounceFactorAction();
 		setBounceFactorAction.setSprite(sprite);
-		setBounceFactorAction.setPhysicsObject(physicsObject);
+		setBounceFactorAction.setPhysicsObject(physicsWorld.getPhysicsObject(sprite));
 		setBounceFactorAction.setBounceFactor(bounceFactorFormula);
 
-		float fixtureBouceFactor = ((FixtureDef) Reflection.getPrivateField(PhysicsObject.class, physicsObject,
-				"fixtureDef")).restitution;
-
-		assertEquals("Unexpected bounceFactor value", PhysicsObject.DEFAULT_BOUNCE_FACTOR, fixtureBouceFactor);
-
 		setBounceFactorAction.act(1.0f);
+		physicsWorld.step(1.0f);
+	}
 
-		fixtureBouceFactor = ((FixtureDef) Reflection.getPrivateField(PhysicsObject.class, physicsObject, "fixtureDef")).restitution;
 
-		assertEquals("Unexpected bounceFactor value", bounceFactor / 100.0f, fixtureBouceFactor);
+	public void testBounceWithDifferentValues() {
+		float bounce01Height = bounce(0.1f);
+		float bounce06Height = bounce(0.6f);
+		assertTrue(
+				"Unexpected value: bounce06Height->bounce(0.6) should be greater then bounce01Height->bounce(0.1)",
+				bounce01Height < bounce06Height);
+	}
+
+	private float bounce(float bounceFactor) {
+		initializeSpritesForCollision();
+		physicsWorld.getPhysicsObject(sprite).setVelocity(0, 0);
+		physicsWorld.getPhysicsObject(sprite).setMass(20);
+		physicsWorld.getPhysicsObject(sprite).setBounceFactor(bounceFactor);
+		while (!collisionDetected()) {
+			physicsWorld.step(0.3f);
+		}
+
+		float y = physicsWorld.getPhysicsObject(sprite).getY() + (ScreenValues.SCREEN_HEIGHT / 2);
+		physicsWorld.step(0.3f);
+
+		while (y < (physicsWorld.getPhysicsObject(sprite).getY() + (ScreenValues.SCREEN_HEIGHT / 2))) {
+			y = physicsWorld.getPhysicsObject(sprite).getY() + (ScreenValues.SCREEN_HEIGHT / 2);
+			physicsWorld.step(0.3f);
+		}
+
+		return y;
 	}
 
 }
