0b3f3a93a507116e43cee2f8ca7f99f8131a56e4
Andrew D <andrew.deutschmann@student.tugraz.at>
2014-09-19 02:48:05 +0200

    CAT-1157 Physics Issue
    calculation of shapes / their resulting dimensions are more accurate now
    scaling of shapes in PhysicsShapeBuilder should be more accurate now aswell
    add getBoundaryBoxDimensions method to PhysicsObject
    add public static scaleCoordinate method for Vector2 and float
    physicsObject circumference stored as Box2DCoordinate, getter returns NormalCoordinate
    Adjust tests for new scaling calculations
    Comment unused private method in PhysicsShapeBuilder

--- catroid/src/org/catrobat/catroid/physics/PhysicsObject.java
+++ catroid/src/org/catrobat/catroid/physics/PhysicsObject.java
@@ -123,12 +123,10 @@ public class PhysicsObject {
 			return;
 		}
 
-		calculateAABB();
-		float max1 = Math.max(getMassCenter().dst(fixtureAABBUpperRight), getMassCenter().dst(fixtureAABBLowerLeft));
-		Vector2 aabbLowerRight = new Vector2(fixtureAABBUpperRight.x, fixtureAABBLowerLeft.y);
-		Vector2 aaabbUpperLeft = new Vector2(fixtureAABBLowerLeft.x, fixtureAABBUpperRight.y);
-		float max2 = Math.max(getMassCenter().dst(aabbLowerRight), getMassCenter().dst(aaabbUpperLeft));
-		circumference = Math.max(max1, max2);
+		Vector2 dimensions = getBoundaryBoxDimensions();
+		float w = dimensions.x / 2;
+		float h = dimensions.y / 2;
+		circumference = PhysicsWorldConverter.convertNormalToBox2dCoordinate((float) Math.sqrt(w * w + h * h));
 	}
 
 	public Type getType() {
@@ -183,7 +181,7 @@ public class PhysicsObject {
 	}
 
 	public float getCircumference() {
-		return circumference;
+		return PhysicsWorldConverter.convertBox2dToNormalCoordinate(circumference);
 	}
 
 	public Vector2 getPosition() {
@@ -341,6 +339,14 @@ public class PhysicsObject {
 		upperRight.x = PhysicsWorldConverter.convertBox2dToNormalVector(bodyAABBUpperRight).x;
 		upperRight.y = PhysicsWorldConverter.convertBox2dToNormalVector(bodyAABBUpperRight).y;
 	}
+
+	public Vector2 getBoundaryBoxDimensions() {
+		calculateAABB();
+		float aabbWidth = PhysicsWorldConverter.convertBox2dToNormalCoordinate(Math.abs(bodyAABBUpperRight.x - bodyAABBLowerLeft.x)) + 1.0f;
+		float aabbHeight = PhysicsWorldConverter.convertBox2dToNormalCoordinate(Math.abs(bodyAABBUpperRight.y - bodyAABBLowerLeft.y)) + 1.0f;
+		return new Vector2(aabbWidth, aabbHeight);
+	}
+
 
 	public void activateHangup() {
 		velocity = new Vector2(getVelocity());
