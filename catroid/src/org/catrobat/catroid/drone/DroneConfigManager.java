72385180f6a23e1252f57d03c0652e5104688303
Patrick R Radkohl <patrick.radkohl@student.tugraz.at>
2015-02-12 15:16:32 +0100

    merge CAT-1086

---
+++ catroid/src/org/catrobat/catroid/drone/DroneConfigManager.java
@@ -0,0 +1,52 @@
+/*
+ * Catroid: An on-device visual programming system for Android devices
+ * Copyright (C) 2010-2014 The Catrobat Team
+ * (<http://developer.catrobat.org/credits>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * An additional term exception under section 7 of the GNU Affero
+ * General Public License, version 3, is available at
+ * http://developer.catrobat.org/license_additional_term
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.catrobat.catroid.drone;
+
+import com.parrot.freeflight.service.DroneControlService;
+
+
+
+
+public class DroneConfigManager {
+
+	public static void setDefaultConfig(DroneControlService droneControlService){
+		droneControlService.resetConfigToDefaults();
+	}
+
+	//TODO: find the perfect settings for each profile
+
+
+	public static void setOutdoorConfig(DroneControlService droneControlService){
+		droneControlService.getDroneConfig().setOutdoorFlight(true);
+		//TODO: set other config params for outdoor flight
+	}
+
+	public void setConfigSetOne(DroneControlService droneControlService){
+		//TODO: create more configs
+	}
+
+	public void setConfigSetTwo(DroneControlService droneControlService){
+		//TODO: create more configs
+	}
+}
