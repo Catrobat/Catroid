6eaffa270a87240c97edc6146b9b5aa22e9f337e
Markus Hobisch <hobisch@student.tugraz.at>
2015-04-07 14:39:08 +0200

    TestBrick added
    Signed-off-by: Markus Hobisch <hobisch@student.tugraz.at>

--- catroid/src/org/catrobat/catroid/content/actions/ExtendedActions.java
+++ catroid/src/org/catrobat/catroid/content/actions/ExtendedActions.java
@@ -569,7 +569,7 @@ public class ExtendedActions extends Actions {
 
 		action.setPosition(x, y);
 		action.setText(text);
-		//action.setDuration(5);
+		action.setDuration(0);
 		action.setSprite(sprite);
 		return action;
 	}
