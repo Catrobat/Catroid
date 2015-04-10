38112b8894868362d43e5c3a98ca397f3a28a6cb
Markus Hobisch <hobisch@student.tugraz.at>
2015-04-10 18:14:42 +0200

    reconnecting little changes

--- catroid/src/org/catrobat/catroid/drone/DroneServiceWrapper.java
+++ catroid/src/org/catrobat/catroid/drone/DroneServiceWrapper.java
@@ -50,18 +50,33 @@ public final class DroneServiceWrapper {
 		droneControlService = service;
 	}
 
-	// first try to reconnect to drone and then check if droneControlService is not null
-	// if connection is successful droneControlService should not be null!
+	/* first we wait for autonomous reconnecting otherwise we try to reconnect to drone and then check if
+	 * droneControlService is not null.
+	   if connection is successful droneControlService should not be null!
+	*/
 	public DroneControlService getDroneService() {
 		if(droneControlService == null){
-		    DroneConnection droneConnection = new DroneConnection(CatroidApplication.getAppContext());
 
-			if (droneConnection != null) {
+			// polling: waiting for autonomous reconnecting
+			int counter = 0;
+			while(droneControlService == null && counter <= 15){
 				try {
-					droneConnection.initialise();
-				} catch (RuntimeException runtimeException) {
-					Toast.makeText(CatroidApplication.getAppContext(), R.string.error_no_drone_connected, Toast.LENGTH_LONG).show();
-					Log.e(getClass().getSimpleName(), "drone connection initialization was not successful!");
+					Thread.sleep(100);
+					counter++;
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			if(droneControlService == null) {
+				DroneConnection droneConnection = new DroneConnection(CatroidApplication.getAppContext());
+
+				if (droneConnection != null) {
+					try {
+						droneConnection.initialise();
+					} catch (RuntimeException runtimeException) {
+						Toast.makeText(CatroidApplication.getAppContext(), R.string.error_no_drone_connected, Toast.LENGTH_LONG).show();
+						Log.e(getClass().getSimpleName(), "drone connection initialization was not successful!");
+					}
 				}
 			}
 		}
