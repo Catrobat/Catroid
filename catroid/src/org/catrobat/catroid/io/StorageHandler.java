1751d55f629c02cd6eec6d3abe9847609fa6ecc9
LukasMayr <lukimayr@gmail.com>
2015-04-15 15:10:11 +0200

    resolving reference for bitmapfactory

--- catroid/src/org/catrobat/catroid/io/StorageHandler.java
+++ catroid/src/org/catrobat/catroid/io/StorageHandler.java
@@ -25,6 +25,7 @@ package org.catrobat.catroid.io;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 
 import com.google.common.base.Charsets;
