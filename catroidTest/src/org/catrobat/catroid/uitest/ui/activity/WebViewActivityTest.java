a21279f047daeea50fcf82b3b4e6cf8c67e4690c
Robert Painsi <robertpainsi@gmail.com>
2015-05-14 22:30:24 +0200

    CAT-1262 Add checkstyle rule AvoidEscapedUnicodeCharacters
    Restrict using Unicode escapes (e.g. \u221e).
    
    http://checkstyle.sourceforge.net/config_misc.html#AvoidEscapedUnicodeCharacters
    https://jira.catrob.at/browse/CAT-1262

--- catroidTest/src/org/catrobat/catroid/uitest/ui/activity/WebViewActivityTest.java
+++ catroidTest/src/org/catrobat/catroid/uitest/ui/activity/WebViewActivityTest.java
@@ -36,7 +36,6 @@ import org.catrobat.catroid.uitest.util.BaseActivityInstrumentationTestCase;
 import org.catrobat.catroid.web.ServerCalls;
 
 public class WebViewActivityTest extends BaseActivityInstrumentationTestCase<MainMenuActivity> {
-	private static final String COPYRIGHT_CHARACTER = "\u00A9";
 	private boolean containsSetting;
 	private boolean showWarning;
 	private SharedPreferences preferences;
@@ -84,7 +83,7 @@ public class WebViewActivityTest extends BaseActivityInstrumentationTestCase<Mai
 				}
 			});
 
-			assertTrue("website hasn't been loaded properly", solo.searchText(COPYRIGHT_CHARACTER + " Catrobat"));
+			assertTrue("website hasn't been loaded properly", solo.searchText("© Catrobat"));
 
 		} else {
 			applyWebViewOnOldDevices(webButtonText);
@@ -110,7 +109,7 @@ public class WebViewActivityTest extends BaseActivityInstrumentationTestCase<Mai
 				}
 			});
 
-			assertTrue("website hasn't been loaded properly", solo.searchText(COPYRIGHT_CHARACTER + " Catrobat"));
+			assertTrue("website hasn't been loaded properly", solo.searchText("© Catrobat"));
 
 		} else {
 			applyWebViewOnOldDevices(helpButtonText);
@@ -142,7 +141,7 @@ public class WebViewActivityTest extends BaseActivityInstrumentationTestCase<Mai
 				}
 			});
 
-			assertTrue("website hasn't been loaded properly", solo.searchText(COPYRIGHT_CHARACTER + " Catrobat"));
+			assertTrue("website hasn't been loaded properly", solo.searchText("© Catrobat"));
 
 		} else {
 			applyWebViewOnOldDevices(passwordForgottenButtonText);
