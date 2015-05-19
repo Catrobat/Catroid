689de28453417c363cf171bc34df00272355fb88
Christian Schindler <cschindler@ist.tugraz.at>
2015-05-19 17:16:24 +0200

    CAT-1316 Fix sensor issues

--- catroidTest/src/org/catrobat/catroid/test/formulaeditor/ParserTestSensors.java
+++ catroidTest/src/org/catrobat/catroid/test/formulaeditor/ParserTestSensors.java
@@ -202,7 +202,8 @@ public class ParserTestSensors extends InstrumentationTestCase {
 		SimulatedSensorManager simulatedSensorManager = new SimulatedSensorManager();
 		Reflection.setPrivateField(sensorHandler, "sensorManager", simulatedSensorManager);
 
-		Sensor accelerometerSensor = (Sensor) Reflection.getPrivateField(sensorHandler, "accelerometerSensor");
+		Sensor accelerometerSensor = (Sensor) Reflection.getPrivateField(sensorHandler,
+				"linearAccelerationSensor");
 		Sensor rotationVectorSensor = (Sensor) Reflection.getPrivateField(sensorHandler, "rotationVectorSensor");
 		SensorHandler.startSensorListener(getInstrumentation().getTargetContext());
 
@@ -222,9 +223,9 @@ public class ParserTestSensors extends InstrumentationTestCase {
 
 		float expectedLoudness = (Float) Reflection.getPrivateField(sensorHandler, "loudness");
 
-		float expectedXAcceleration = (Float) Reflection.getPrivateField(sensorHandler, "linearAcceleartionX");
-		float expectedYAcceleration = (Float) Reflection.getPrivateField(sensorHandler, "linearAcceleartionY");
-		float expectedZAcceleration = (Float) Reflection.getPrivateField(sensorHandler, "linearAcceleartionZ");
+		float expectedXAcceleration = (Float) Reflection.getPrivateField(sensorHandler, "linearAccelerationX");
+		float expectedYAcceleration = (Float) Reflection.getPrivateField(sensorHandler, "linearAccelerationY");
+		float expectedZAcceleration = (Float) Reflection.getPrivateField(sensorHandler, "linearAccelerationZ");
 
 		float[] rotationMatrix = new float[16];
 		float[] rotationVector = (float[]) Reflection.getPrivateField(sensorHandler, "rotationVector");
