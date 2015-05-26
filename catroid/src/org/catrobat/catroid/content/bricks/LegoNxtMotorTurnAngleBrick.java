64476312b5765fe6fe90dcafbe87a907fa823097
MightyMendor <christian.jung@student.tugraz.at>
2015-05-26 13:00:24 +0200

    CAT-1287 Fixed Layout of NXT_Bricks

--- catroid/src/org/catrobat/catroid/content/bricks/LegoNxtMotorTurnAngleBrick.java
+++ catroid/src/org/catrobat/catroid/content/bricks/LegoNxtMotorTurnAngleBrick.java
@@ -25,6 +25,7 @@ package org.catrobat.catroid.content.bricks;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.drawable.Drawable;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
@@ -110,6 +111,7 @@ public class LegoNxtMotorTurnAngleBrick extends FormulaBrick {
 
 		legoSpinner.setAdapter(motorAdapter);
 		legoSpinner.setSelection(motorEnum.ordinal());
+		legoSpinner.setGravity(Gravity.CENTER);
 		return prototypeView;
 	}
 
@@ -186,6 +188,7 @@ public class LegoNxtMotorTurnAngleBrick extends FormulaBrick {
 		});
 
 		motorSpinner.setSelection(motorEnum.ordinal());
+		motorSpinner.setGravity(Gravity.CENTER);
 
 		return view;
 	}
