9f968fe11f244189466168a1d6b808a7f334a532
Stefan Bürscher <stefan.buerscher@gmail.com>
2015-07-10 13:24:36 +0200

    CAT1440 Fixed: Cancelling creating a new variable leads to New…   Formatted

--- catroid/src/org/catrobat/catroid/ui/dialogs/NewDataDialog.java
+++ catroid/src/org/catrobat/catroid/ui/dialogs/NewDataDialog.java
@@ -90,7 +90,7 @@ public class NewDataDialog extends SherlockDialogFragment {
 	public void onCancel(DialogInterface dialog) {
 		super.onCancel(dialog);
 		userListDialogListenerListFinishNewUserListDialog(null);
-        variableDialogListenerListFinishNewVariableDialog(null);
+		variableDialogListenerListFinishNewVariableDialog(null);
 	}
 
 	@Override
