5c7158338b5fabf97ffd4ceec80ee593ac884c4e
Patrick R Radkohl <patrick.radkohl@student.tugraz.at>
2015-08-04 16:54:08 +0200

    CAT-1372-Wrote Test for VideoLook
    CAT-1372-Finalized DroneVideoLook visibility test
    
    CAT-1372-Cleaned imports up

--- catroidCucumberTest/src/org/catrobat/catroid/test/cucumber/ProgramSteps.java
+++ catroidCucumberTest/src/org/catrobat/catroid/test/cucumber/ProgramSteps.java
@@ -26,6 +26,11 @@ import android.test.AndroidTestCase;
 
 import com.robotium.solo.Solo;
 
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 import org.catrobat.catroid.ProjectManager;
 import org.catrobat.catroid.content.BroadcastScript;
 import org.catrobat.catroid.content.Project;
@@ -56,11 +61,6 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
-
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 
 import static org.catrobat.catroid.formulaeditor.FormulaElement.ElementType;
 import static org.hamcrest.MatcherAssert.assertThat;
