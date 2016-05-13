aa7b28342da37e2826490eddcfa95114ef011e54
Fabian Kofler <fabo.kofler@gmail.com>
2016-05-13 14:27:06 +0200

    CAT-1976 Fixed lint issues (#1693)

--- catroidTest/src/org/catrobat/catroid/uitest/ui/activity/ProgramMenuActivityTest.java
+++ catroidTest/src/org/catrobat/catroid/uitest/ui/activity/ProgramMenuActivityTest.java
@@ -89,7 +89,7 @@ public class ProgramMenuActivityTest extends BaseActivityInstrumentationTestCase
 		// https://developer.android.com/reference/android/content/pm/ActivityInfo.html
 		PackageManager packageManager = solo.getCurrentActivity().getPackageManager();
 		ActivityInfo activityInfo = packageManager.getActivityInfo(solo.getCurrentActivity().getComponentName(),
-				PackageManager.GET_ACTIVITIES);
+				PackageManager.GET_META_DATA);
 
 		// Note that the activity is _indeed_ rotated on your device/emulator!
 		// Robotium can _force_ the activity to be in landscapeMode mode (and so could we, programmatically)
