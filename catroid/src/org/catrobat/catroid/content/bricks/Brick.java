9c8af388bcc0fd2138f5dcf95fcf759e8eb48a82
Manuel Haid <manuel.haid@student.tugraz.at>
2016-05-14 14:33:52 +0200

    CAT-1970 VideoBrick did not resume

--- catroid/src/org/catrobat/catroid/content/bricks/Brick.java
+++ catroid/src/org/catrobat/catroid/content/bricks/Brick.java
@@ -82,6 +82,7 @@ public interface Brick extends Serializable, Cloneable {
 	int SENSOR_INCLINATION = 0x4000;
 	int SENSOR_COMPASS = 0x8000;
 	int NFC_ADAPTER = 0x10000;
+	int VIDEO = 0x20000;
 	//	public static final int BLUETOOTH_ARDUINO = 0x20000;
 
 	List<SequenceAction> addActionToSequence(Sprite sprite, SequenceAction sequence);
