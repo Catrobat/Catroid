12b8c9e99766c9a2c911e168694ffbbe3b30cc56
Thomas Hirsch <84n4n4@gmail.com>
2016-06-24 11:15:28 +0200

    CAT HOTFIX - Decrease language version
    decrease language version from 0991 to 099 again.

--- catroid/src/org/catrobat/catroid/io/XStreamToSupportCatrobatLanguageVersion099AndBefore.java
+++ catroid/src/org/catrobat/catroid/io/XStreamToSupportCatrobatLanguageVersion099AndBefore.java
@@ -150,14 +150,14 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
-public class XStreamToSupportCatrobatLanguageVersion0991AndBefore extends XStream {
+public class XStreamToSupportCatrobatLanguageVersion099AndBefore extends XStream {
 
-	private static final String TAG = XStreamToSupportCatrobatLanguageVersion0991AndBefore.class.getSimpleName();
+	private static final String TAG = XStreamToSupportCatrobatLanguageVersion099AndBefore.class.getSimpleName();
 
 	private HashMap<String, BrickInfo> brickInfoMap;
 	private HashMap<String, String> scriptInfoMap;
 
-	public XStreamToSupportCatrobatLanguageVersion0991AndBefore(PureJavaReflectionProvider reflectionProvider) {
+	public XStreamToSupportCatrobatLanguageVersion099AndBefore(PureJavaReflectionProvider reflectionProvider) {
 		super(reflectionProvider);
 	}
 
