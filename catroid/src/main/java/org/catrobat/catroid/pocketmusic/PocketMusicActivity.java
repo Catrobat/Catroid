a3003eaf32a56d9ad041b377469734ed1bac34e0
Pagger Mario (DCCS) <mpagger@dccsintra.net>
2016-09-02 13:05:16 +0200

    MUS-201 Create a Dummy Song
    Create a dummy song containing 2 notes
    as preparation for ticket MUS-197

--- catroid/src/main/java/org/catrobat/catroid/pocketmusic/PocketMusicActivity.java
+++ catroid/src/main/java/org/catrobat/catroid/pocketmusic/PocketMusicActivity.java
@@ -26,6 +26,15 @@ package org.catrobat.catroid.pocketmusic;
 import android.os.Bundle;
 
 import org.catrobat.catroid.R;
+import org.catrobat.catroid.pocketmusic.note.MusicalBeat;
+import org.catrobat.catroid.pocketmusic.note.MusicalInstrument;
+import org.catrobat.catroid.pocketmusic.note.MusicalKey;
+import org.catrobat.catroid.pocketmusic.note.NoteEvent;
+import org.catrobat.catroid.pocketmusic.note.NoteLength;
+import org.catrobat.catroid.pocketmusic.note.NoteName;
+import org.catrobat.catroid.pocketmusic.note.Project;
+import org.catrobat.catroid.pocketmusic.note.Track;
+import org.catrobat.catroid.pocketmusic.note.trackgrid.TrackToTrackGridConverter;
 import org.catrobat.catroid.ui.BaseActivity;
 
 public class PocketMusicActivity extends BaseActivity {
@@ -34,5 +43,27 @@ public class PocketMusicActivity extends BaseActivity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_pocketmusic);
+
+		createDummySong();
+	}
+
+	private void createDummySong() {
+		int bpm = 60;
+
+		Project project = new Project("Dummy Project", MusicalBeat.BEAT_4_4, bpm);
+
+		Track track = new Track(MusicalKey.VIOLIN, MusicalInstrument.VIOLIN);
+		NoteName c1 = NoteName.C1;
+		NoteEvent c1On = new NoteEvent(c1, true);
+		NoteEvent c1Off = new NoteEvent(c1, false);
+
+		track.addNoteEvent(0, c1On);
+		track.addNoteEvent(NoteLength.QUARTER.toTicks(bpm), c1Off);
+		track.addNoteEvent(NoteLength.QUARTER.toTicks(bpm) * 2, c1On);
+		track.addNoteEvent(NoteLength.QUARTER.toTicks(bpm) * 3, c1Off);
+
+		project.addTrack("Track 1", track);
+
+		TrackToTrackGridConverter.convertTrackToTrackGrid(track, MusicalBeat.BEAT_4_4, bpm);
 	}
 }
