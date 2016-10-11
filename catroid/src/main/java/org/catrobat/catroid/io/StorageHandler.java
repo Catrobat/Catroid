e77007126dfb97c71e9b5be1c261166564076ce8
Thomas Schranz <thomas.schranz@student.tugraz.at>
2016-10-11 16:10:45 +0200

    CAT-RELEASE Increased Languageversion and code

--- catroid/src/main/java/org/catrobat/catroid/io/StorageHandler.java
+++ catroid/src/main/java/org/catrobat/catroid/io/StorageHandler.java
@@ -256,7 +256,7 @@ public final class StorageHandler {
 	private static final int JPG_COMPRESSION_SETTING = 95;
 	public static final String BACKPACK_FILENAME = "backpack.json";
 
-	private XStreamToSupportCatrobatLanguageVersion0991AndBefore xstream;
+	private XStreamToSupportCatrobatLanguageVersion0992AndBefore xstream;
 	private Gson backpackGson;
 
 	private FileInputStream fileInputStream;
@@ -289,7 +289,7 @@ public final class StorageHandler {
 	}
 
 	private void prepareProgramXstream(boolean forSupportProject) {
-		xstream = new XStreamToSupportCatrobatLanguageVersion0991AndBefore(new PureJavaReflectionProvider(new
+		xstream = new XStreamToSupportCatrobatLanguageVersion0992AndBefore(new PureJavaReflectionProvider(new
 				FieldDictionary(new CatroidFieldKeySorter())));
 		if (forSupportProject) {
 			xstream.processAnnotations(SupportProject.class);
