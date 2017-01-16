5f87d2c3ad1948184e786c1b500db8cf961e418c
Thomas Mauerhofer <thomas.mauerhofer@student.tugraz.at>
2017-01-16 13:00:33 +0100

    Fixed Bugfix of merging scenes

--- catroid/src/main/java/org/catrobat/catroid/ui/dialogs/MergeSceneDialog.java
+++ catroid/src/main/java/org/catrobat/catroid/ui/dialogs/MergeSceneDialog.java
@@ -79,7 +79,8 @@ public class MergeSceneDialog extends DialogFragment {
 		secondAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinnerFirstScene.setAdapter(firstAdapter);
 		spinnerSecondScene.setAdapter(secondAdapter);
-		spinnerSecondScene.setSelection(2);
+		spinnerFirstScene.setSelection(0);
+		spinnerSecondScene.setSelection(1);
 
 		spinnerFirstScene.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
