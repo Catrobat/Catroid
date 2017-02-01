63c7c72d12de42fa318965cf4ff09498305227da
Thomas Hirsch <84n4n4@gmail.com>
2017-02-01 16:12:56 +0100

    updated copyright header some basic brick tests in espresso

--- catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/util/matchers/StageMatchers.java
+++ catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/util/matchers/StageMatchers.java
@@ -1,6 +1,6 @@
 /*
  * Catroid: An on-device visual programming system for Android devices
- * Copyright (C) 2010-2016 The Catrobat Team
+ * Copyright (C) 2010-2017 The Catrobat Team
  * (<http://developer.catrobat.org/credits>)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -21,7 +21,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.catrobat.catroid.uiespresso.matchers;
+package org.catrobat.catroid.uiespresso.util.matchers;
 
 import android.support.test.espresso.matcher.BoundedMatcher;
 import android.view.View;
