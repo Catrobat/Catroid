806de6e21c9057ab2494f8656c656028a63f4252
Deepak-Avadhya <deepakavadhyaa@gmail.com>
2017-02-26 16:53:05 +0530

    Update SoundController.java

--- catroid/src/main/java/org/catrobat/catroid/ui/controller/SoundController.java
+++ catroid/src/main/java/org/catrobat/catroid/ui/controller/SoundController.java
@@ -119,9 +119,9 @@ public final class SoundController {
 
 				if ("primary".equalsIgnoreCase(type)) {
 					return Environment.getExternalStorageDirectory() + "/" + split[1];
+				}else {
+					return "storage/"+split[0]+"/"+split[1];
 				}
-				else return "storage/"+split[0]+"/"+split[1];
-
 				// TODO handle non-primary volumes
 			} else if (isDownloadsDocument(uri)) {
 				// DownloadsProvider
