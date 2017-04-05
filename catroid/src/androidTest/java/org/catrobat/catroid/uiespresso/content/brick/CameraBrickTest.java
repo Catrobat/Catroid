44fe26409bcda92b22a23a641c9d3d989272044f
Manuel Hess <hess@student.tugraz.at>
2017-04-05 14:45:25 +0200

    Cat-XP6[JK, MH] CameraBrickTest

---
+++ catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/content/brick/CameraBrickTest.java
@@ -0,0 +1,108 @@
+/*
+ * Catroid: An on-device visual programming system for Android devices
+ * Copyright (C) 2010-2017 The Catrobat Team
+ * (<http://developer.catrobat.org/credits>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * An additional term exception under section 7 of the GNU Affero
+ * General Public License, version 3, is available at
+ * http://developer.catrobat.org/license_additional_term
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.catrobat.catroid.uiespresso.content.brick;
+
+import org.catrobat.catroid.R;
+import org.catrobat.catroid.content.Script;
+import org.catrobat.catroid.content.bricks.CameraBrick;
+import org.catrobat.catroid.content.bricks.ChooseCameraBrick;
+import org.catrobat.catroid.ui.ScriptActivity;
+import org.catrobat.catroid.uiespresso.util.BaseActivityInstrumentationRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.catrobat.catroid.uiespresso.content.brick.BrickTestUtils.checkIfBrickAtPositionShowsString;
+import static org.catrobat.catroid.uiespresso.content.brick.BrickTestUtils.checkIfSpinnerOnBrickAtPositionShowsString;
+import static org.catrobat.catroid.uiespresso.content.brick.BrickTestUtils.checkIfValuesAvailableInSpinnerOnBrick;
+import static org.catrobat.catroid.uiespresso.content.brick.BrickTestUtils.clickSelectCheckSpinnerValueOnBrick;
+
+public class CameraBrickTest {
+	private int cameraBrickPosition;
+	private int chooseCameraBrickPosition;
+
+	@Rule
+	public BaseActivityInstrumentationRule<ScriptActivity> baseActivityTestRule = new
+			BaseActivityInstrumentationRule<>(ScriptActivity.class, true, false);
+
+	@Before
+	public void setUp() throws Exception {
+		cameraBrickPosition = 1;
+		chooseCameraBrickPosition = 2;
+		Script script = BrickTestUtils.createProjectAndGetStartScript("cameraBrickTest");
+		script.addBrick(new CameraBrick());
+		script.addBrick(new ChooseCameraBrick());
+		baseActivityTestRule.launchActivity(null);
+	}
+
+	@Test
+	public void testCameraBrick() {
+		checkIfBrickAtPositionShowsString(cameraBrickPosition, R.string.brick_video);
+		checkIfSpinnerOnBrickAtPositionShowsString(R.id.brick_video_spinner, cameraBrickPosition, R.string
+				.video_brick_camera_on);
+
+		List<Integer> spinnerValues = new ArrayList<>();
+		spinnerValues.add(R.string.video_brick_camera_on);
+		spinnerValues.add(R.string.video_brick_camera_off);
+		checkIfValuesAvailableInSpinnerOnBrick(spinnerValues, R.id.brick_video_spinner, cameraBrickPosition);
+	}
+
+	@Test
+	public void testChooseCameraBrick() {
+		checkIfBrickAtPositionShowsString(chooseCameraBrickPosition, R.string.brick_choose_camera);
+		checkIfSpinnerOnBrickAtPositionShowsString(R.id.brick_choose_camera_spinner, chooseCameraBrickPosition, R.string
+				.choose_camera_front);
+
+		List<Integer> spinnerValues = new ArrayList<>();
+		spinnerValues.add(R.string.choose_camera_front);
+		spinnerValues.add(R.string.choose_camera_back);
+		checkIfValuesAvailableInSpinnerOnBrick(spinnerValues, R.id.brick_choose_camera_spinner, chooseCameraBrickPosition);
+	}
+
+	@Test
+	public void testSelectBackCamera() {
+		checkIfBrickAtPositionShowsString(chooseCameraBrickPosition, R.string.brick_choose_camera);
+		checkIfSpinnerOnBrickAtPositionShowsString(R.id.brick_choose_camera_spinner, chooseCameraBrickPosition, R.string
+				.choose_camera_front);
+		clickSelectCheckSpinnerValueOnBrick(R.id.brick_choose_camera_spinner, chooseCameraBrickPosition, R.string
+				.choose_camera_back);
+		checkIfSpinnerOnBrickAtPositionShowsString(R.id.brick_choose_camera_spinner, chooseCameraBrickPosition, R.string
+				.choose_camera_back);
+	}
+
+	@Test
+	public void testSelectCameraOff() {
+		checkIfBrickAtPositionShowsString(cameraBrickPosition, R.string.brick_video);
+		checkIfSpinnerOnBrickAtPositionShowsString(R.id.brick_video_spinner, cameraBrickPosition, R.string
+				.video_brick_camera_on);
+		clickSelectCheckSpinnerValueOnBrick(R.id.brick_video_spinner, cameraBrickPosition, R.string
+				.video_brick_camera_off);
+		checkIfSpinnerOnBrickAtPositionShowsString(R.id.brick_video_spinner, cameraBrickPosition, R.string
+				.video_brick_camera_off);
+	}
+}
