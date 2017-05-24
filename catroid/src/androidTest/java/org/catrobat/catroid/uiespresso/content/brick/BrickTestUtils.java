e37eba938eb44169260f063dcc662f311afd9643
catroid <kirshan.luhano@usindh.edu.pk>
2017-05-24 10:09:11 +0200

    CAT-XP6 [KL,LS] PhiroIfBrickTest

--- catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/content/brick/BrickTestUtils.java
+++ catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/content/brick/BrickTestUtils.java
@@ -45,6 +45,7 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -218,5 +219,24 @@ public final class BrickTestUtils {
 				.perform(click());
 		onScriptList().atPosition(position).onChildView(withId(editTextResourceId))
 				.check(matches(withText("'" + stringToBeEntered + "' ")));
+	}
+
+	public static void clickSelectCheckSpinnerValueOnBrick(int spinnerResourceId, int position, int stringResourceId) {
+		onScriptList().atPosition(position).onChildView(withId(spinnerResourceId))
+				.perform(click());
+		onData(allOf(is(instanceOf(String.class)), is(UiTestUtils.getResourcesString(stringResourceId))))
+				.perform(click());
+		checkIfSpinnerOnBrickAtPositionShowsString(spinnerResourceId, position, stringResourceId);
+	}
+	public static void checkBrickNotExists(int intResourceId) {
+
+		onView(withId(intResourceId)).check(doesNotExist());
+	}
+	public static void deleteBrickAtPosition(int position, int intBrickResouce) {
+		onScriptList().atPosition(position).onChildView(withText(intBrickResouce)).perform(click());
+		onView(withText(R.string.brick_context_dialog_delete_brick))
+				.perform(click());
+		onView(withText(R.string.yes))
+				.perform(click());
 	}
 }
