06d71b95bae8b97969363682e0783ab5cdab0d7d
Christoph Michael Pozvek <pozvek@student.tugraz.at>
2017-07-11 15:07:44 +0200

    refacturing drone, adding rotate left and rotate right bricks for JumpingSumo drone, deleting old uitests

--- catroid/src/main/java/org/catrobat/catroid/common/BrickValues.java
+++ catroid/src/main/java/org/catrobat/catroid/common/BrickValues.java
@@ -121,6 +121,7 @@ public final class BrickValues {
 	public static final int JUMPING_SUMO_MOVE_BRICK_DEFAULT_TIME_MILLISECONDS = 1000;
 	public static final int JUMPING_SUMO_MOVE_BRICK_DEFAULT_MOVE_POWER_PERCENT = 80;
 	public static final int JUMPING_SUMO_SOUND_BRICK_DEFAULT_VOLUME_PERCENT = 50;
+	public static final int JUMPING_SUMO_ROTATE_DEFAULT_DEGREE = 90;
 
 	//Constants Variables
 	public static final double SET_VARIABLE = 1d;
