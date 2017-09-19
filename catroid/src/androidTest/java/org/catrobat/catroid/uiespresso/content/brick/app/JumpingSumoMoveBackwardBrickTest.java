d9f3b8b483649015adcce6092523c70f43b3837c
Christoph Michael Pozvek <pozvek@student.tugraz.at>
2017-09-20 00:48:22 +0200

    deleting unnecessary tests

--- catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/content/brick/app/JumpingSumoMoveBackwardBrickTest.java
+++ catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/content/brick/app/JumpingSumoMoveBackwardBrickTest.java
@@ -21,12 +21,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.catrobat.catroid.uiespresso.content.brick;
+package org.catrobat.catroid.uiespresso.content.brick.app;
 
 import android.support.test.runner.AndroidJUnit4;
 
 import org.catrobat.catroid.R;
-import org.catrobat.catroid.content.bricks.DroneMoveForwardBrick;
+import org.catrobat.catroid.content.bricks.JumpingSumoMoveBackwardBrick;
 import org.catrobat.catroid.ui.ScriptActivity;
 import org.catrobat.catroid.uiespresso.annotations.Flaky;
 import org.catrobat.catroid.uiespresso.content.brick.utils.BrickTestUtils;
@@ -42,7 +42,7 @@ import org.junit.runner.RunWith;
 import static org.catrobat.catroid.uiespresso.content.brick.utils.BrickDataInteractionWrapper.onBrickAtPosition;
 
 @RunWith(AndroidJUnit4.class)
-public class ARDroneMoveForwardBrickTest {
+public class JumpingSumoMoveBackwardBrickTest {
 	private int brickPosition;
 
 	@Rule
@@ -53,9 +53,9 @@ public class ARDroneMoveForwardBrickTest {
 	public void setUp() throws Exception {
 		brickPosition = 1;
 		int initialSeconds = 1000;
-		int initialPower = 20;
-		BrickTestUtils.createProjectAndGetStartScript("ARDroneMoveForwardBrickTest").addBrick(new
-				DroneMoveForwardBrick(initialSeconds, initialPower));
+		int initialPower = 80;
+		BrickTestUtils.createProjectAndGetStartScript("JumpingSumoMoveBackwardBrickTest").addBrick(new
+				JumpingSumoMoveBackwardBrick(initialSeconds, initialPower));
 		baseActivityTestRule.launchActivity(null);
 	}
 
@@ -67,12 +67,12 @@ public class ARDroneMoveForwardBrickTest {
 		int powerValue = 40;
 
 		onBrickAtPosition(0).checkShowsText(R.string.brick_when_started);
-		onBrickAtPosition(brickPosition).checkShowsText("Move AR.Drone 2.0 forward");
+		onBrickAtPosition(brickPosition).checkShowsText("Move Jumping Sumo backward");
 
-		onBrickAtPosition(brickPosition).onFormulaTextField(R.id.brick_drone_move_edit_text_second)
+		onBrickAtPosition(brickPosition).onFormulaTextField(R.id.brick_jumping_sumo_move_edit_text_second)
 				.performEnterNumber(secondsValue)
 				.checkShowsNumber(secondsValue);
-		onBrickAtPosition(brickPosition).onFormulaTextField(R.id.brick_drone_move_edit_text_power)
+		onBrickAtPosition(brickPosition).onFormulaTextField(R.id.brick_jumping_sumo_move_edit_text_power)
 				.performEnterNumber(powerValue)
 				.checkShowsNumber(powerValue);
 	}
--- catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/content/brick/app/JumpingSumoMoveForwardBrickTest.java
+++ catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/content/brick/app/JumpingSumoMoveForwardBrickTest.java
@@ -21,7 +21,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.catrobat.catroid.uiespresso.content.brick;
+package org.catrobat.catroid.uiespresso.content.brick.app;
 
 import android.support.test.runner.AndroidJUnit4;
 
