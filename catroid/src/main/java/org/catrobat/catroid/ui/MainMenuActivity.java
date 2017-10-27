e636418753917642263d1b249f7ff81c94c282f0
Raphael Schluesselbauer <schluesselbauer@student.tugraz.at>
2017-10-27 13:57:50 +0200

    [CAT-2584] Add new splashscreen for google codein that is displayed once

--- catroid/src/main/java/org/catrobat/catroid/ui/MainMenuActivity.java
+++ catroid/src/main/java/org/catrobat/catroid/ui/MainMenuActivity.java
@@ -152,6 +152,14 @@ public class MainMenuActivity extends BaseCastActivity implements OnLoadProjectC
 				CastManager.getInstance().initializeCast(this);
 			}
 		}
+		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+		if (sharedPreferences.getBoolean(Constants.FIRST_APPLICATION_LAUNCH, true)) {
+			SharedPreferences.Editor editor = sharedPreferences.edit();
+			editor.putBoolean(Constants.FIRST_APPLICATION_LAUNCH, false);
+			editor.commit();
+			Intent intent = new Intent(this, GoogleCodeInActivity.class);
+			startActivity(intent);
+		}
 	}
 
 	@Override
