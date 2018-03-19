f15ef6fa7ae7cf087b59251f6edac25fe65d17b3
Thomas Hirsch <84n4n4@gmail.com>
2018-03-19 15:05:20 +0100

    [ACCESSIBILITY] added headlines in profiles fragment

--- catroid/src/main/java/org/catrobat/catroid/ui/settingsfragments/AccessibilityProfilesFragment.java
+++ catroid/src/main/java/org/catrobat/catroid/ui/settingsfragments/AccessibilityProfilesFragment.java
@@ -36,12 +36,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.catrobat.catroid.R;
 import org.catrobat.catroid.ui.MainMenuActivity;
 import org.catrobat.catroid.ui.SettingsActivity;
 import org.catrobat.catroid.ui.recyclerview.SimpleRVItem;
 import org.catrobat.catroid.ui.recyclerview.adapter.SimpleRVAdapter;
+import org.catrobat.catroid.ui.recyclerview.viewholder.SimpleVH;
 import org.catrobat.catroid.utils.ToastUtil;
 
 import java.lang.annotation.Retention;
@@ -66,12 +68,18 @@ public class AccessibilityProfilesFragment extends Fragment implements SimpleRVA
 	@Retention(RetentionPolicy.SOURCE)
 	@IntDef({DEFAULT, ARGUS, FENRIR, ODIN, TIRO, CUSTOM})
 	@interface ProfileId {}
-	private static final int DEFAULT = 0;
-	private static final int ARGUS = 1;
-	private static final int FENRIR = 2;
-	private static final int ODIN = 3;
-	private static final int TIRO = 4;
-	private static final int CUSTOM = 5;
+	private static final int CUSTOM = 0;
+	private static final int DEFAULT = 1;
+	private static final int ARGUS = 2;
+	private static final int FENRIR = 3;
+	private static final int ODIN = 4;
+	private static final int TIRO = 5;
+
+	@Retention(RetentionPolicy.SOURCE)
+	@IntDef({WITH_HEADLINE, NO_HEADLINE})
+	@interface ProfileViewType {}
+	private static final int WITH_HEADLINE = 0;
+	private static final int NO_HEADLINE = 1;
 
 	@Nullable
 	@Override
@@ -86,7 +94,43 @@ public class AccessibilityProfilesFragment extends Fragment implements SimpleRVA
 		super.onActivityCreated(savedInstanceState);
 		SimpleRVAdapter adapter;
 		List<SimpleRVItem> items = getItems();
-		adapter = new SimpleRVAdapter(items);
+		adapter = new SimpleRVAdapter(items) {
+			@Override
+			public SimpleVH onCreateViewHolder(ViewGroup parent, @ProfileViewType int viewType) {
+				LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+				switch (viewType) {
+					case NO_HEADLINE:
+						View view = inflater.inflate(R.layout.vh_simple, parent, false);
+						return new SimpleVH(view);
+					case WITH_HEADLINE:
+						view = inflater.inflate(R.layout.vh_profile_with_headline, parent, false);
+						return new SimpleVH(view);
+					default:
+						throw new IllegalArgumentException(TAG + ": viewType was not defined correctly.");
+				}
+			}
+
+			@Override
+			public void onBindViewHolder(SimpleVH holder, int position) {
+				super.onBindViewHolder(holder, position);
+				if (position == 0) {
+					((TextView) holder.itemView.findViewById(R.id.headline))
+							.setText(R.string.preference_title_accessibility_profile_headline);
+				}
+				if (position == 2) {
+					((TextView) holder.itemView.findViewById(R.id.headline))
+							.setText(R.string.preference_title_accessibility_predefined_profile_headline);
+				}
+			}
+
+			@Override
+			public @ProfileViewType int getItemViewType(@ProfileId int position) {
+				if (position == 0 || position == 2) {
+					return WITH_HEADLINE;
+				}
+				return NO_HEADLINE;
+			}
+		};
 		adapter.setOnItemClickListener(this);
 		recyclerView.setAdapter(adapter);
 	}
@@ -138,14 +182,14 @@ public class AccessibilityProfilesFragment extends Fragment implements SimpleRVA
 
 	private List<SimpleRVItem> getItems() {
 		List<SimpleRVItem> items = new ArrayList<>();
-		items.add(new SimpleRVItem(DEFAULT,
-				ContextCompat.getDrawable(getActivity(), R.drawable.nolb_default_myprofile),
-				getString(R.string.preference_access_title_profile_default),
-				getString(R.string.preference_access_summary_profile_default)));
 		items.add(new SimpleRVItem(CUSTOM,
 				ContextCompat.getDrawable(getActivity(), R.drawable.nolb_default_myprofile),
 				getString(R.string.preference_access_title_profile_custom),
 				getString(R.string.preference_access_summary_profile_custom)));
+		items.add(new SimpleRVItem(DEFAULT,
+				ContextCompat.getDrawable(getActivity(), R.drawable.nolb_default_myprofile),
+				getString(R.string.preference_access_title_profile_default),
+				getString(R.string.preference_access_summary_profile_default)));
 		items.add(new SimpleRVItem(ARGUS,
 				ContextCompat.getDrawable(getActivity(), R.drawable.nolb_argus),
 				getString(R.string.preference_access_title_profile_argus),
