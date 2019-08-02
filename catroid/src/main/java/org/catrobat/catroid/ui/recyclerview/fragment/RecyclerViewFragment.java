1ee332e1520ce589d794ff20e88d626ea60b5a03
Arafat Mohammed <arafatm@stanford.edu>
2019-08-01 23:03:02 -0700

    CATROID-159 Add dialog to enter name of new, merged project

--- catroid/src/main/java/org/catrobat/catroid/ui/recyclerview/fragment/RecyclerViewFragment.java
+++ catroid/src/main/java/org/catrobat/catroid/ui/recyclerview/fragment/RecyclerViewFragment.java
@@ -67,6 +67,7 @@ public abstract class RecyclerViewFragment<T extends Nameable> extends Fragment 
 	@Retention(RetentionPolicy.SOURCE)
 	@IntDef({NONE, BACKPACK, COPY, DELETE, RENAME, MERGE})
 	@interface ActionModeType {}
+
 	protected static final int NONE = 0;
 	protected static final int BACKPACK = 1;
 	protected static final int COPY = 2;
@@ -182,7 +183,7 @@ public abstract class RecyclerViewFragment<T extends Nameable> extends Fragment 
 				showRenameDialog(adapter.getSelectedItems());
 				break;
 			case MERGE:
-				showMergeAlert();
+				showMergeDialog(adapter.getSelectedItems());
 				break;
 			case NONE:
 				throw new IllegalStateException("ActionModeType not set correctly");
@@ -429,12 +430,23 @@ public abstract class RecyclerViewFragment<T extends Nameable> extends Fragment 
 				.show();
 	}
 
-	protected void showMergeAlert() {
+	protected void showMergeDialog(List<T> selectedItems) {
 		if (adapter.getSelectedItems().size() <= 1) {
 			ToastUtil.showError(getContext(), R.string.am_merge_error);
 		} else {
-			ToastUtil.showError(getContext(), "These two projects cannot yet be merged");
+			TextInputDialog.Builder builder = new TextInputDialog.Builder(getContext());
+
+			builder.setHint(getString(R.string.project_name_label))
+					.setPositiveButton(getString(R.string.ok), (TextInputDialog.OnClickListener) (dialog, textInput)
+							-> {
+						mergeProjects(selectedItems, textInput);
+					});
+
+			builder.setTitle(R.string.new_merge_project_dialog_title)
+					.setNegativeButton(R.string.cancel, null)
+					.show();
 		}
+
 		setShowProgressBar(true);
 		finishActionMode();
 	}
@@ -446,6 +458,11 @@ public abstract class RecyclerViewFragment<T extends Nameable> extends Fragment 
 
 	protected void renameItem(T item, String newName) {
 		item.setName(newName);
+		finishActionMode();
+	}
+
+	protected void mergeProjects(List<T> selectedProjects, String mergeProjectName) {
+		ToastUtil.showSuccess(getContext(), R.string.merging_project_text);
 		finishActionMode();
 	}
 }
