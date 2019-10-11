f65d6f153bbc6ae68ede741075607c2cc48af70c
ameyalaad <46456931+ameyalaad@users.noreply.github.com>
2019-10-11 16:16:58 +0530

    Update ShowTextActor.java
    removed unnecessary import

--- catroid/src/main/java/org/catrobat/catroid/stage/ShowTextActor.java
+++ catroid/src/main/java/org/catrobat/catroid/stage/ShowTextActor.java
@@ -40,7 +40,6 @@ import org.catrobat.catroid.ProjectManager;
 import org.catrobat.catroid.R;
 import org.catrobat.catroid.common.Constants;
 import org.catrobat.catroid.content.Sprite;
-import org.catrobat.catroid.content.bricks.Brick;
 import org.catrobat.catroid.content.bricks.ShowTextColorSizeAlignmentBrick;
 import org.catrobat.catroid.formulaeditor.UserVariable;
 
