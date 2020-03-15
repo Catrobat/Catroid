aff64749806efe2fc425c5f98f5ff49b5366bfa2
Suraj Patil <48085134+spielers@users.noreply.github.com>
2020-03-15 10:33:21 +0530

    Added splash screen to the app
    splash screen will help to enhance the app loading time

---
+++ catroid/src/main/java/org/catrobat/catroid/SplashActivity.java
@@ -0,0 +1,60 @@
+/*
+ * Catroid: An on-device visual programming system for Android devices
+ * Copyright (C) 2010-2020 The Catrobat Team
+ * (<http://developer.catrobat.org/credits>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * An additional term exception under section 7 of the GNU Affero
+ * General Public License, version 3, is available at
+ * http://developer.catrobat.org/license_additional_term
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.catrobat.catroid;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ *
+ */
+
+public class SplashActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+
+		new Handler().postDelayed(new Runnable() {
+
+
+			@Override
+			public void run() {
+				// This method will be executed once the timer is over
+				Intent i = new Intent(SplashActivity.this, CatroidApplication.class);
+				startActivity(i);
+				finish();
+			}
+		}, 5000);
+	}
+}
+
