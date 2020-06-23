0bd1d47e071560f5082b5c127259a1751f1a88df
Maximilian Flicker <m.flicker@student.tugraz.at>
2020-06-23 09:35:06 +0200

    CATROID-567 refactor ChangeVariableAction to Kotlin

---
+++ catroid/src/main/java/org/catrobat/catroid/content/actions/ChangeVariableAction.kt
@@ -0,0 +1,49 @@
+/*
+ * Catroid: An on-device visual programming system for Android devices
+ * Copyright (C) 2010-2018 The Catrobat Team
+ * (<http://developer.catrobat.org/credits>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * An additional term exception under section 7 of the GNU Affero
+ * General Public License, version 3, is available at
+ * http://developer.catrobat.org/license_additional_term
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.catrobat.catroid.content.actions
+
+import com.badlogic.gdx.scenes.scene2d.Action
+import org.catrobat.catroid.content.Sprite
+import org.catrobat.catroid.formulaeditor.Formula
+import org.catrobat.catroid.formulaeditor.UserVariable
+
+class ChangeVariableAction : Action() {
+    var sprite: Sprite? = null
+    var changeVariable: Formula? = null
+    var userVariable: UserVariable? = null
+
+    override fun act(delta: Float): Boolean {
+        val originalValue = userVariable?.value as? Double ?: return true
+        val value = changeVariable?.interpretObject(sprite) ?: 0.0
+        (value as? Double ?: (value as? String)?.toDoubleOrNull())?.run {
+            updateUserVariable(originalValue, this)
+        }
+        return true
+    }
+
+    private fun updateUserVariable(originalValue: Double, value: Double) {
+            val original = originalValue.takeUnless { it.isNaN() } ?: 0.0
+            val valueToAdd = value.takeUnless { it.isNaN() } ?: 0.0
+            userVariable?.value = original + valueToAdd
+    }
+}
