4a9dc0ff56ddfafe746988d083d8fe55c48c64ef
PlantinumX <ohalici@live.at>
2020-06-29 16:02:58 +0200

    CATROID-565 refactor AskAction.java  to AskAction.kt. Refactor AskActionTest.java to AskActionTest.kt

---
+++ catroid/src/test/java/org/catrobat/catroid/test/content/actions/AskActionTest.kt
@@ -0,0 +1,72 @@
+/*
+ * Catroid: An on-device visual programming system for Android devices
+ * Copyright (C) 2010-2018 The Catrobat Team
+ * (<http://developer.catrobat.org/credits>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * An additional term exception under section 7 of the GNU Affero
+ * General Public License, version 3, is available at
+ * http://developer.catrobat.org/license_additional_term
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.catrobat.catroid.test.content.actions
+
+import com.badlogic.gdx.utils.GdxNativesLoader
+import junit.framework.Assert
+import org.catrobat.catroid.content.Sprite
+import org.catrobat.catroid.content.actions.AskAction
+import org.catrobat.catroid.formulaeditor.Formula
+import org.catrobat.catroid.formulaeditor.UserVariable
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.powermock.api.mockito.PowerMockito
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+
+@RunWith(PowerMockRunner::class)
+@PrepareForTest(GdxNativesLoader::class)
+class AskActionTest {
+    private lateinit var testSprite: Sprite
+    private lateinit var userVariableForAnswer: UserVariable
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        testSprite = Sprite("testSprite")
+        userVariableForAnswer = UserVariable(TEST_USERVARIABLE)
+        PowerMockito.mockStatic(GdxNativesLoader::class.java)
+    }
+
+    @Test
+    fun testAskAndCheckAnswer() {
+        val action = testSprite.actionFactory.createAskAction(
+            testSprite,
+            Formula(ASK_QUESTION),
+            userVariableForAnswer
+        ) as AskAction
+        action.act(1f)
+        action.setAnswerText(ASK_ANSWER)
+        Assert.assertEquals(
+            ASK_ANSWER,
+            userVariableForAnswer.value.toString()
+        )
+    }
+
+    companion object {
+        private const val TEST_USERVARIABLE = "testUservariable"
+        private const val ASK_QUESTION = "What's your name"
+        private const val ASK_ANSWER = "Catrobat Pocket Cat"
+    }
+}
