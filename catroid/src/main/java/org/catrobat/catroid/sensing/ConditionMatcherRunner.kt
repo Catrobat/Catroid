7da26c1d64dbd2dece932434358addb94c0af233
Patrick Gaisberger <37209425+gPathpp@users.noreply.github.com>
2020-10-19 17:32:28 +0200

    CATROID-786 Refactor ColorCollisionDetection (#3830)
    * CATROID-786 Refactor ColorCollisionDetection
    
    Runtime is now constant. (Can be regulated in tradeoff with accuracy)

--- catroid/src/main/java/org/catrobat/catroid/sensing/ConditionMatcherRunner.kt
+++ catroid/src/main/java/org/catrobat/catroid/sensing/ConditionMatcherRunner.kt
@@ -26,13 +26,10 @@ package org.catrobat.catroid.sensing
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.math.Polygon
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
-import org.catrobat.catroid.BuildConfig
 import kotlin.math.absoluteValue
 
 private const val EPSILON = 8 / 256F
+private val clearColor = Color(1f, 1f, 1f, 0f)
 
 private fun Int.isOdd(): Boolean = this % 2 == 1
 
@@ -40,7 +37,7 @@ private fun Color.equalsColor(color: Color): Boolean {
     return this.r.absDiff(color.r) < EPSILON &&
         this.g.absDiff(color.g) < EPSILON &&
         this.b.absDiff(color.b) < EPSILON &&
-        this.a > 0.0
+        this != clearColor
 }
 
 private fun Float.absDiff(f: Float): Float = (this - f).absoluteValue
@@ -52,16 +49,11 @@ class BoundingRectangle(vertices: List<Float>) {
     val bottom = vertices.getYVertices().max()?.toInt() ?: 0
     val left = vertices.getXVertices().min()?.toInt() ?: 0
     val right = vertices.getXVertices().max()?.toInt() ?: 0
+    val width = right - left
+    val height = bottom - top
 
     private fun List<Float>.getYVertices() = this.filterIndexed { i, _ -> i.isOdd() }
     private fun List<Float>.getXVertices() = this.filterIndexed { i, _ -> !i.isOdd() }
-
-    private fun Pixmap.validXIndex(x: Int) = x >= 0 && x <= this.width
-    private fun Pixmap.validYIndex(y: Int) = y >= 0 && y <= this.height
-
-    fun hasValidBoundaries(pixmap: Pixmap?) = pixmap?.run {
-        validXIndex(left) && validXIndex(right) && validYIndex(top) && validYIndex(bottom)
-    } ?: false
 }
 
 abstract class ConditionMatcher {
@@ -69,16 +61,15 @@ abstract class ConditionMatcher {
     var spritePixmap: Pixmap? = null
 
     abstract fun matches(x: Int, y: Int): Boolean
-    fun validRange(boundingRectangle: BoundingRectangle) = boundingRectangle.hasValidBoundaries(stagePixmap)
 }
 
 class TouchesColorMatcher(color: String) : ConditionMatcher() {
     private val color = Color.valueOf(color)
     override fun matches(x: Int, y: Int): Boolean {
         val stagePixel = stagePixmap?.getPixel(x, y) ?: return false
-        val spritePixel = spritePixmap?.let { it.getPixel(x, it.height - y) } ?: return false
+        val spritePixel = spritePixmap?.getPixel(x, y) ?: return false
 
-        return Color(stagePixel).equalsColor(color) && Color(spritePixel).a > 0
+        return Color(stagePixel).equalsColor(color) && Color(spritePixel) != clearColor
     }
 }
 
@@ -87,26 +78,16 @@ class ColorTouchesColorMatcher(spriteColor: String, stageColor: String) : Condit
     private val stageColor = Color.valueOf(stageColor)
     override fun matches(x: Int, y: Int): Boolean {
         val stagePixel = stagePixmap?.getPixel(x, y) ?: return false
-        val spritePixel = spritePixmap?.let { it.getPixel(x, it.height - y) } ?: return false
+        val spritePixel = spritePixmap?.getPixel(x, y) ?: return false
 
         return Color(stagePixel).equalsColor(stageColor) && Color(spritePixel).equalsColor(spriteColor)
     }
 }
 
 class ConditionMatcherRunner(val matcher: ConditionMatcher) {
-    fun matchAsync(boundingRectangle: BoundingRectangle): Boolean = runBlocking {
-        if (BuildConfig.DEBUG && !matcher.validRange(boundingRectangle)) {
-            error("Wrong projection matrix or rotation")
-        }
-        val matchesColorCoroutine = GlobalScope.async {
-            matchInBoundingRectangle(boundingRectangle)
-        }
-        return@runBlocking matchesColorCoroutine.await()
-    }
-
-    private fun matchInBoundingRectangle(boundingRectangle: BoundingRectangle): Boolean {
-        for (x in boundingRectangle.left until boundingRectangle.right) {
-            for (y in boundingRectangle.top until boundingRectangle.bottom) {
+    fun match(width: Int, height: Int): Boolean {
+        for (x in 0 until width) {
+            for (y in 0 until height) {
                 if (matcher.matches(x, y)) {
                     return true
                 }
