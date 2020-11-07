33ecea42a852d3266360436b13281aa32f008459
Dev Sebastian <devsebastian@gmail.com>
2020-11-07 18:29:50 +0530

    remove unnecessary imports

--- catroid/src/androidTest/java/org/catrobat/catroid/test/web/PrivacyPolicyHttpResponseTest.java
+++ catroid/src/androidTest/java/org/catrobat/catroid/test/web/PrivacyPolicyHttpResponseTest.java
@@ -24,7 +24,6 @@
 package org.catrobat.catroid.test.web;
 
 import android.content.Context;
-import android.util.Log;
 
 import org.catrobat.catroid.R;
 import org.catrobat.catroid.testsuites.annotations.Cat;
