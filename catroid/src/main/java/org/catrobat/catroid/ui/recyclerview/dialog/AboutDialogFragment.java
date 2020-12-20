6af2529deb98d4b23099281d6c1f5409b10a8d34
athar <atharvayawalkar1377@gmail.com>
2020-12-21 01:07:16 +0530

    Resolved Comments 1 and 2 .
    1) The shown text shall be "Catrobat language version:" as a translatable text, i.e., in catroid/src/main/res/values/strings.xml, followed by a space and then the version code.
    
    2) Also, we need an automatic test that checks that the correct information is shown in the about menu.

--- catroid/src/main/java/org/catrobat/catroid/ui/recyclerview/dialog/AboutDialogFragment.java
+++ catroid/src/main/java/org/catrobat/catroid/ui/recyclerview/dialog/AboutDialogFragment.java
@@ -65,8 +65,9 @@ public class AboutDialogFragment extends DialogFragment {
 		aboutVersionNameTextView.setText(versionName);
 
 		TextView aboutCatrobatVersionTextView = view.findViewById(R.id.dialog_about_text_view_catrobat_version_name);
-		double catrobatVersion = Constants.SION;
-		String catrobatVersionName="Catro."+ Double.toString(catrobatVersion);
+		double catrobatVersion = Constants.CURRENT_CATROBAT_LANGUAGE_VERSION;
+		String catrobatVersionName =
+				getString(R.string.dialog_about_catrobat_language_version) + catrobatVersion;
 		aboutCatrobatVersionTextView.setText(catrobatVersionName);
 
 		return new AlertDialog.Builder(getActivity())
