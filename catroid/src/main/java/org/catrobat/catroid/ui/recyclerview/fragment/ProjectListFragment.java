29c071996a7385de959ebf006d44cc861cea33c0
Robert <robert.leiner@student.tugraz.at>
2021-01-05 22:55:40 +0100

    CATROID-866 Copying of several selected projects copies only the first in the list
    Fixed the bug by looping over all elements in the selected item list and adding already used names in the copying to
    the name list to ensure that no two copied projects receive the same name. Also added a test.

--- catroid/src/main/java/org/catrobat/catroid/ui/recyclerview/fragment/ProjectListFragment.java
+++ catroid/src/main/java/org/catrobat/catroid/ui/recyclerview/fragment/ProjectListFragment.java
@@ -32,6 +32,7 @@ import android.view.MenuItem;
 
 import org.catrobat.catroid.ProjectManager;
 import org.catrobat.catroid.R;
+import org.catrobat.catroid.common.Nameable;
 import org.catrobat.catroid.common.ProjectData;
 import org.catrobat.catroid.content.backwardcompatibility.ProjectMetaDataParser;
 import org.catrobat.catroid.io.StorageOperations;
@@ -257,10 +258,15 @@ public class ProjectListFragment extends RecyclerViewFragment<ProjectData> imple
 		finishActionMode();
 		setShowProgressBar(true);
 
-		String name = uniqueNameProvider.getUniqueNameInNameables(selectedItems.get(0).getName(), adapter.getItems());
-		new ProjectCopyTask(selectedItems.get(0).getDirectory(), name)
-				.setListener(this)
-				.execute();
+		ArrayList<Nameable> usedProjectNames = new ArrayList<>(adapter.getItems());
+
+		for (ProjectData projectData : selectedItems) {
+			String name = uniqueNameProvider.getUniqueNameInNameables(projectData.getName(), usedProjectNames);
+			usedProjectNames.add(new ProjectData(name, null, 0, false));
+			new ProjectCopyTask(projectData.getDirectory(), name)
+					.setListener(this)
+					.execute();
+		}
 	}
 
 	@Override
