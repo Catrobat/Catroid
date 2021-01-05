b5cfe463fc86d1955c90af0aa1ed10cbb15ca66c
Robert <robert.leiner@student.tugraz.at>
2021-01-05 14:22:35 +0100

    CATROID-865 Crash on "Delete look" and "Delete background"

--- catroid/src/main/java/org/catrobat/catroid/sensing/CollisionInformation.java
+++ catroid/src/main/java/org/catrobat/catroid/sensing/CollisionInformation.java
@@ -187,7 +187,7 @@ public class CollisionInformation {
 			collisionPolygons = createCollisionPolygonByHitbox(bitmap);
 		}
 
-		if (isCalculationThreadCancelled) {
+		if (isCalculationThreadCancelled || !lookData.isValid()) {
 			return;
 		}
 
