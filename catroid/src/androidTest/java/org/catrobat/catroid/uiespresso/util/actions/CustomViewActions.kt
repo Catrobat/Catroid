2b6fa433a6291e68faa592c997b0e9bb50700a39
Wolfgang Slany <1747888+wslany@users.noreply.github.com>
2021-03-21 11:06:46 +0100

    Espresso custom ViewAction waitForView
    This ViewAction tells espresso to wait till a certain view is found in the view hierarchy.
     * @param viewId The id of the view to wait for.
     * @param timeout The maximum time which espresso will wait for the view to show up (in milliseconds)
    
    Origin: https://www.repeato.app/espresso-wait-for-element/

---
+++ catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/util/actions/CustomViewActions.kt
@@ -0,0 +1,66 @@
+  
+/*
+ * Catroid: An on-device visual programming system for Android devices
+ * Copyright (C) 2010-2021 The Catrobat Team
+ * (<http://developer.catrobat.org/credits>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * An additional term exception under section 7 of the GNU Affero
+ * General Public License, version 3, is available at
+ * http://developer.catrobat.org/license_additional_term
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.catrobat.catroid.uiespresso.util.actions
+
+import android.view.View
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.PerformException
+import androidx.test.espresso.UiController
+import androidx.test.espresso.matcher
+import androidx.test.espresso.util.TreeIterables
+import androidx.test.espresso.util.HumanReadables
+
+fun waitForView(viewId: Int, timeout: Long): ViewAction {
+    return object : ViewAction {
+        override fun getConstraints(): Matcher {
+            return isRoot()
+        }
+
+        override fun getDescription(): String {
+            return "wait for a specific view with id $viewId; during $timeout millis."
+        }
+
+        override fun perform(uiController: UiController, rootView: View) {
+            uiController.loopMainThreadUntilIdle()
+            val startTime = System.currentTimeMillis()
+            val endTime = startTime + timeout
+            val viewMatcher = withId(viewId)
+
+            do {
+                for (child in TreeIterables.breadthFirstViewTraversal(rootView)) {
+                    if (viewMatcher.matches(child)) {
+                        return
+                    }
+                }
+                uiController.loopMainThreadForAtLeast(100)
+            } while (System.currentTimeMillis() &lt; endTime)
+            throw PerformException.Builder()
+                .withCause(TimeoutException())
+                .withActionDescription(this.description)
+                .withViewDescription(HumanReadables.describe(rootView))
+                .build()
+        }
+    }
+}
