44b0e4554817f996a3df85f388475423aa74816b
Souma <souma.hitech@gmail.com>
2021-04-21 15:45:42 +0530

    refactor ScreenModes file

--- catroid/src/main/java/org/catrobat/catroid/common/ScreenModes.kt
+++ catroid/src/main/java/org/catrobat/catroid/common/ScreenModes.kt
@@ -20,8 +20,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.catrobat.catroid.common;
+package org.catrobat.catroid.common
 
-public enum ScreenModes {
-	STRETCH, MAXIMIZE
+enum class ScreenModes {
+    STRETCH, MAXIMIZE
 }
