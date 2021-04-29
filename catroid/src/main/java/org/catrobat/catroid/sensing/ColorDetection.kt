2eb8fe6d044b2c150b6421c33ed8d4f105597efb
Qun Xin Lin <qlin@student.tugraz.at>
2021-04-29 12:52:31 +0200

    CATROID-1080 Fix Crash when calling compute with color sensors

--- catroid/src/main/java/org/catrobat/catroid/sensing/ColorDetection.kt
+++ catroid/src/main/java/org/catrobat/catroid/sensing/ColorDetection.kt
@@ -50,7 +50,7 @@ import org.catrobat.catroid.stage.StageListener
 
 abstract class ColorDetection(
     protected open val scope: Scope,
-    protected open val stageListener: StageListener
+    protected open val stageListener: StageListener?
 ) {
     protected open val look = scope.sprite.look
     protected var bufferWidth = 0
@@ -141,6 +141,6 @@ abstract class ColorDetection(
 
     abstract fun setBufferParameters()
     abstract fun isParameterInvalid(parameter: Any?): Boolean
-    abstract fun getLooksOfRelevantSprites(): MutableList<Look>
+    abstract fun getLooksOfRelevantSprites(): MutableList<Look>?
     abstract fun createProjectionMatrix(project: Project): Matrix4
 }
