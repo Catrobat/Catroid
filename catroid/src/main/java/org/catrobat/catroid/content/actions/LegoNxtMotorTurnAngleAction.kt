0365f320e4b83077d54e94fefde9a795d70a257e
Souma <souma.hitech@gmail.com>
2021-05-09 12:38:24 +0530

    refactor LegoNxtMotorTurnAngleAction class

---
+++ catroid/src/main/java/org/catrobat/catroid/content/actions/LegoNxtMotorTurnAngleAction.kt
@@ -0,0 +1,82 @@
+/*
+ * Catroid: An on-device visual programming system for Android devices
+ * Copyright (C) 2010-2018 The Catrobat Team
+ * (<http://developer.catrobat.org/credits>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * An additional term exception under section 7 of the GNU Affero
+ * General Public License, version 3, is available at
+ * http://developer.catrobat.org/license_additional_term
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.catrobat.catroid.content.actions
+
+import android.util.Log
+import com.badlogic.gdx.scenes.scene2d.actions.TemporalAction
+import org.catrobat.catroid.bluetooth.base.BluetoothDevice
+import org.catrobat.catroid.common.CatroidService
+import org.catrobat.catroid.common.ServiceProvider
+import org.catrobat.catroid.content.Scope
+import org.catrobat.catroid.content.bricks.LegoNxtMotorTurnAngleBrick
+import org.catrobat.catroid.formulaeditor.Formula
+import org.catrobat.catroid.formulaeditor.InterpretationException
+
+class LegoNxtMotorTurnAngleAction : TemporalAction() {
+    private var motorEnum: LegoNxtMotorTurnAngleBrick.Motor? = null
+    private var degrees: Formula? = null
+    private var scope: Scope? = null
+    private val btService = ServiceProvider.getService(CatroidService.BLUETOOTH_DEVICE_SERVICE)
+    override fun update(percent: Float) {
+        var degreesValue: Int
+        try {
+            degreesValue = degrees!!.interpretInteger(scope)
+        } catch (interpretationException: InterpretationException) {
+            degreesValue = 0
+            Log.d(
+                javaClass.simpleName,
+                "Formula interpretation for this specific Brick failed.",
+                interpretationException
+            )
+        }
+        var tmpAngle = degreesValue
+        var direction = 1
+        if (degreesValue < 0) {
+            direction = -1
+            tmpAngle = degreesValue + -2 * degreesValue
+        }
+        val nxt = btService.getDevice(BluetoothDevice.LEGO_NXT)
+            ?: return
+        when (motorEnum) {
+            LegoNxtMotorTurnAngleBrick.Motor.MOTOR_A -> nxt.motorA.move(direction * 30, tmpAngle)
+            LegoNxtMotorTurnAngleBrick.Motor.MOTOR_B -> nxt.motorB.move(direction * 30, tmpAngle)
+            LegoNxtMotorTurnAngleBrick.Motor.MOTOR_C -> nxt.motorC.move(direction * 30, tmpAngle)
+            LegoNxtMotorTurnAngleBrick.Motor.MOTOR_B_C -> {
+                nxt.motorB.move(direction * 30, tmpAngle)
+                nxt.motorC.move(direction * 30, tmpAngle)
+            }
+        }
+    }
+
+    fun setMotorEnum(motorEnum: LegoNxtMotorTurnAngleBrick.Motor?) {
+        this.motorEnum = motorEnum
+    }
+
+    fun setDegrees(degrees: Formula?) {
+        this.degrees = degrees
+    }
+
+    fun setScope(scope: Scope?) {
+        this.scope = scope
+    }
+}
