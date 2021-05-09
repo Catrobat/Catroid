b537022bb54da810b9bdb0c12ebee06ebedfbdbd
Souma <souma.hitech@gmail.com>
2021-05-09 12:14:14 +0530

    refactor GoToRandomPositionAction class

--- catroid/src/main/java/org/catrobat/catroid/content/actions/GoToRandomPositionAction.kt
+++ catroid/src/main/java/org/catrobat/catroid/content/actions/GoToRandomPositionAction.kt
@@ -20,40 +20,29 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package org.catrobat.catroid.content.actions
 
-package org.catrobat.catroid.content.actions;
+import com.badlogic.gdx.scenes.scene2d.actions.TemporalAction
+import org.catrobat.catroid.common.ScreenValues
+import org.catrobat.catroid.content.Sprite
 
-import com.badlogic.gdx.scenes.scene2d.actions.TemporalAction;
+class GoToRandomPositionAction : TemporalAction() {
+    private var sprite: Sprite? = null
+    var randomXPosition = 0f
+        private set
+    var randomYPosition = 0f
+        private set
 
-import org.catrobat.catroid.common.ScreenValues;
-import org.catrobat.catroid.content.Sprite;
+    override fun update(percent: Float) {
+        randomXPosition = Math.random().toFloat() * ScreenValues.SCREEN_WIDTH + 1 - ScreenValues
+            .SCREEN_WIDTH / 2
+        randomYPosition = Math.random().toFloat() * ScreenValues.SCREEN_HEIGHT + 1 - ScreenValues
+            .SCREEN_HEIGHT / 2
+        sprite!!.look.xInUserInterfaceDimensionUnit = randomXPosition
+        sprite!!.look.yInUserInterfaceDimensionUnit = randomYPosition
+    }
 
-public class GoToRandomPositionAction extends TemporalAction {
-
-	private Sprite sprite;
-	private float randomXPosition;
-	private float randomYPosition;
-
-	@Override
-	protected void update(float percent) {
-		randomXPosition = (float) Math.random()
-				* (ScreenValues.SCREEN_WIDTH + 1) - (ScreenValues.SCREEN_WIDTH / 2);
-		randomYPosition = (float) Math.random()
-				* (ScreenValues.SCREEN_HEIGHT + 1) - (ScreenValues.SCREEN_HEIGHT / 2);
-
-		sprite.look.setXInUserInterfaceDimensionUnit(randomXPosition);
-		sprite.look.setYInUserInterfaceDimensionUnit(randomYPosition);
-	}
-
-	public void setSprite(Sprite sprite) {
-		this.sprite = sprite;
-	}
-
-	public float getRandomXPosition() {
-		return this.randomXPosition;
-	}
-
-	public float getRandomYPosition() {
-		return this.randomYPosition;
-	}
+    fun setSprite(sprite: Sprite?) {
+        this.sprite = sprite
+    }
 }
