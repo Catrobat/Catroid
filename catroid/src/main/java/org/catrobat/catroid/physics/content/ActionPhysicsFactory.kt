0f8502d24d35cde89522254b68dba51d18fc2b85
Robert <robert.leiner@student.tugraz.at>
2021-05-11 15:26:30 +0200

    CATROID-1033 Velocity is not updated when Actor/object is gliding
    Fixed the bug by adding a PhysicsObject to the glideToAction and setting the velocity correctly.

--- catroid/src/main/java/org/catrobat/catroid/physics/content/ActionPhysicsFactory.kt
+++ catroid/src/main/java/org/catrobat/catroid/physics/content/ActionPhysicsFactory.kt
@@ -64,6 +64,7 @@ class ActionPhysicsFactory : ActionFactory() {
         val scope = Scope(ProjectManager.getInstance().currentProject, sprite, sequence)
         action.setScope(scope)
         action.setPhysicsLook(sprite.look as PhysicsLook)
+        action.setPhysicsObject(getPhysicsObject(sprite))
         return action
     }
 
