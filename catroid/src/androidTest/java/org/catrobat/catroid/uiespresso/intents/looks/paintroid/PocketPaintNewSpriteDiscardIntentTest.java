8d81d8f0655d0b2cbf92bb844e69340470932215
Piki2000 <patrick.andrawes20@gmail.com>
2021-09-28 14:45:41 +0200

    CATROID-1089 Object cannot be created without a look

--- catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/intents/looks/paintroid/PocketPaintNewSpriteDiscardIntentTest.java
+++ catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/intents/looks/paintroid/PocketPaintNewSpriteDiscardIntentTest.java
@@ -1,6 +1,6 @@
 /*
  * Catroid: An on-device visual programming system for Android devices
- * Copyright (C) 2010-2020 The Catrobat Team
+ * Copyright (C) 2010-2021 The Catrobat Team
  * (<http://developer.catrobat.org/credits>)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -34,8 +34,10 @@ import org.catrobat.catroid.content.Project;
 import org.catrobat.catroid.testsuites.annotations.Cat;
 import org.catrobat.catroid.testsuites.annotations.Level;
 import org.catrobat.catroid.ui.ProjectActivity;
+import org.catrobat.catroid.uiespresso.util.UiTestUtils;
 import org.catrobat.catroid.uiespresso.util.rules.FragmentActivityTestRule;
 import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -52,20 +54,25 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.core.AllOf.allOf;
 
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.Intents.intending;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasCategories;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class PocketPaintNewSpriteDiscardIntentTest {
 
 	private Matcher expectedIntent;
 	private final String projectName = getClass().getSimpleName();
+	private final String newSpriteName = UiTestUtils.getResourcesString(R.string.default_sprite_name);
 
 	@Rule
 	public FragmentActivityTestRule<ProjectActivity> baseActivityTestRule = new
@@ -99,6 +106,35 @@ public class PocketPaintNewSpriteDiscardIntentTest {
 	@Category({Cat.AppUi.class, Level.Smoke.class})
 	@Test
 	public void testCancelImport() {
+		addEmptySprite();
+
+		onView(withText(R.string.cancel))
+				.perform(click());
+
+		onRecyclerView().checkHasNumberOfItems(1);
+	}
+
+	@Category({Cat.AppUi.class, Level.Smoke.class})
+	@Test
+	public void testAddEmptySprite() {
+		addEmptySprite();
+
+		onView(Matchers.allOf(withId(android.R.id.button1), withText(R.string.ok)))
+				.perform(click());
+
+		onRecyclerView().atPosition(1).onChildView(R.id.title_view)
+				.check(matches(withText(newSpriteName)));
+
+		onRecyclerView().checkHasNumberOfItems(2);
+	}
+
+	private void createProject(String projectName) {
+		Project project = new Project(ApplicationProvider.getApplicationContext(), projectName);
+		ProjectManager.getInstance().setCurrentProject(project);
+		ProjectManager.getInstance().setCurrentlyEditedScene(project.getDefaultScene());
+	}
+
+	private void addEmptySprite() {
 		onView(withId(R.id.button_add))
 				.perform(click());
 
@@ -107,12 +143,12 @@ public class PocketPaintNewSpriteDiscardIntentTest {
 
 		intended(expectedIntent);
 
-		onRecyclerView().checkHasNumberOfItems(1);
-	}
+		closeSoftKeyboard();
 
-	private void createProject(String projectName) {
-		Project project = new Project(ApplicationProvider.getApplicationContext(), projectName);
-		ProjectManager.getInstance().setCurrentProject(project);
-		ProjectManager.getInstance().setCurrentlyEditedScene(project.getDefaultScene());
+		onView(withText(newSpriteName))
+				.check(matches(isDisplayed()));
+
+		onView(withText(R.string.new_empty_sprite_note))
+				.check(matches(isDisplayed()));
 	}
 }
