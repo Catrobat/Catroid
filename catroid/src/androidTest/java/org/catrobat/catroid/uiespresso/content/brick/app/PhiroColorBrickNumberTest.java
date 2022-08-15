485092d144aaeba18a8647cf28342785a5c485e3
Robert <robert.leiner@student.tugraz.at>
2022-08-15 21:48:34 +0200

    CATROID-1470 Test: testPhiroLightRGBPickColorCancel fails
    Fixed by removing wrong and unnecessary condition in the test

--- catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/content/brick/app/PhiroColorBrickNumberTest.java
+++ catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/content/brick/app/PhiroColorBrickNumberTest.java
@@ -147,20 +147,11 @@ public class PhiroColorBrickNumberTest {
 		onView(withText(R.string.color_picker_cancel))
 				.perform(click());
 
-		if ((Integer) red < 0 || (Integer) green < 0 || (Integer) blue < 0) {
-			onBrickAtPosition(phiroRGBLightBrickPosition).onFormulaTextField(R.id.brick_phiro_rgb_led_action_red_edit_text)
-					.check(matches(withText(containsString("0"))));
-			onBrickAtPosition(phiroRGBLightBrickPosition).onFormulaTextField(R.id.brick_phiro_rgb_led_action_green_edit_text)
-					.check(matches(withText(containsString("0"))));
-			onBrickAtPosition(phiroRGBLightBrickPosition).onFormulaTextField(R.id.brick_phiro_rgb_led_action_blue_edit_text)
-					.check(matches(withText(containsString("0"))));
-		} else {
-			onBrickAtPosition(phiroRGBLightBrickPosition).onFormulaTextField(R.id.brick_phiro_rgb_led_action_red_edit_text)
-					.checkShowsNumber((Integer) red);
-			onBrickAtPosition(phiroRGBLightBrickPosition).onFormulaTextField(R.id.brick_phiro_rgb_led_action_green_edit_text)
-					.checkShowsNumber((Integer) green);
-			onBrickAtPosition(phiroRGBLightBrickPosition).onFormulaTextField(R.id.brick_phiro_rgb_led_action_blue_edit_text)
-					.checkShowsNumber((Integer) blue);
-		}
+		onBrickAtPosition(phiroRGBLightBrickPosition).onFormulaTextField(R.id.brick_phiro_rgb_led_action_red_edit_text)
+				.checkShowsNumber((Integer) red);
+		onBrickAtPosition(phiroRGBLightBrickPosition).onFormulaTextField(R.id.brick_phiro_rgb_led_action_green_edit_text)
+				.checkShowsNumber((Integer) green);
+		onBrickAtPosition(phiroRGBLightBrickPosition).onFormulaTextField(R.id.brick_phiro_rgb_led_action_blue_edit_text)
+				.checkShowsNumber((Integer) blue);
 	}
 }
