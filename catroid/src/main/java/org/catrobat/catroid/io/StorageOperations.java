27b80a11c7b51a3b6b38f83e381ab409f5314eff
Ilma Lušija <ilma.lusija@student.tugraz.at>
2023-03-30 20:38:26 +0200

    IDE-8 Rewrite look file changed in Paintroid

--- catroid/src/main/java/org/catrobat/catroid/io/StorageOperations.java
+++ catroid/src/main/java/org/catrobat/catroid/io/StorageOperations.java
@@ -366,4 +366,22 @@ public final class StorageOperations {
 			throw new IOException("Cannot delete directory: " + dir.getAbsolutePath());
 		}
 	}
+
+	public static File getLastModified(String directoryFilePath) {
+		File directory = new File(directoryFilePath);
+		File[] files = directory.listFiles(File::isFile);
+		long lastModifiedTime = Long.MIN_VALUE;
+		File chosenFile = null;
+
+		if (files != null) {
+			for (File file : files) {
+				if (file.lastModified() > lastModifiedTime) {
+					chosenFile = file;
+					lastModifiedTime = file.lastModified();
+				}
+			}
+		}
+
+		return chosenFile;
+	}
 }
