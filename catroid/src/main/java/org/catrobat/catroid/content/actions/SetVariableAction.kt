8311131a8f5721146e2e451edf6903e9232e26e1
Smarshal <lcb2021048@iiitl.ac.in>
2023-04-24 23:13:51 +0530

    Refactored SetVariableAction

--- catroid/src/main/java/org/catrobat/catroid/content/actions/SetVariableAction.kt
+++ catroid/src/main/java/org/catrobat/catroid/content/actions/SetVariableAction.kt
@@ -20,83 +20,79 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.catrobat.catroid.content.actions;
+package org.catrobat.catroid.content.actions
 
-import android.util.Log;
+import android.util.Log
+import com.badlogic.gdx.scenes.scene2d.actions.TemporalAction
+import org.catrobat.catroid.ProjectManager
+import org.catrobat.catroid.bluetooth.base.BluetoothDevice
+import org.catrobat.catroid.common.CatroidService
+import org.catrobat.catroid.common.Constants
+import org.catrobat.catroid.common.ServiceProvider
+import org.catrobat.catroid.content.Scope
+import org.catrobat.catroid.devices.multiplayer.MultiplayerInterface
+import org.catrobat.catroid.formulaeditor.Formula
+import org.catrobat.catroid.formulaeditor.FormulaElement
+import org.catrobat.catroid.formulaeditor.UserVariable
+import org.catrobat.catroid.formulaeditor.common.Conversions.convertArgumentToDouble
 
-import com.badlogic.gdx.scenes.scene2d.actions.TemporalAction;
+class SetVariableAction : TemporalAction() {
+    private var scope: Scope? = null
+    private var changeVariable: Formula? = null
+    private var userVariable: UserVariable? = null
+    override fun update(percent: Float) {
+        if (userVariable == null) {
+            return
+        }
+        var value =
+            if (changeVariable == null) java.lang.Double.valueOf(0.0) else changeVariable!!.interpretObject(
+                scope
+            )
+        if (changeVariable != null && changeVariable!!.root.isBoolean(scope)) {
+            value = value as Double != 0.0
+        }
+        var isFirstLevelStringTree = false
+        if (changeVariable != null && changeVariable!!.root.elementType == FormulaElement.ElementType.STRING) {
+            isFirstLevelStringTree = true
+        }
+        try {
+            if (!isFirstLevelStringTree && value is String && userVariable.hashCode() != Constants.TEXT_FROM_CAMERA_SENSOR_HASHCODE && convertArgumentToDouble(
+                    value
+                ) != null
+            ) {
+                value = convertArgumentToDouble(value)!!
+            }
+        } catch (numberFormatException: NumberFormatException) {
+            Log.d(javaClass.simpleName, "Couldn't parse String", numberFormatException)
+        }
+        userVariable!!.value = value
+        val multiplayerVariable =
+            ProjectManager.getInstance().currentProject.getMultiplayerVariable(
+                userVariable!!.name
+            )
+        if (multiplayerVariable != null) {
+            val multiplayerDevice: MultiplayerInterface? = multiplayerDevice
+            multiplayerDevice?.sendChangedMultiplayerVariables(userVariable)
+        }
+    }
 
-import org.catrobat.catroid.ProjectManager;
-import org.catrobat.catroid.common.ServiceProvider;
-import org.catrobat.catroid.content.Scope;
-import org.catrobat.catroid.devices.multiplayer.MultiplayerInterface;
-import org.catrobat.catroid.formulaeditor.Formula;
-import org.catrobat.catroid.formulaeditor.FormulaElement;
-import org.catrobat.catroid.formulaeditor.UserVariable;
+    val multiplayerDevice: MultiplayerInterface
+        get() = ServiceProvider.getService(CatroidService.BLUETOOTH_DEVICE_SERVICE).getDevice(
+            BluetoothDevice.MULTIPLAYER
+        )
 
-import static org.catrobat.catroid.bluetooth.base.BluetoothDevice.MULTIPLAYER;
-import static org.catrobat.catroid.common.CatroidService.BLUETOOTH_DEVICE_SERVICE;
-import static org.catrobat.catroid.common.Constants.TEXT_FROM_CAMERA_SENSOR_HASHCODE;
-import static org.catrobat.catroid.formulaeditor.common.Conversions.convertArgumentToDouble;
+    fun setUserVariable(userVariable: UserVariable?) {
+        if (userVariable == null) {
+            return
+        }
+        this.userVariable = userVariable
+    }
 
-public class SetVariableAction extends TemporalAction {
+    fun setChangeVariable(changeVariable: Formula?) {
+        this.changeVariable = changeVariable
+    }
 
-	private Scope scope;
-	private Formula changeVariable;
-	private UserVariable userVariable;
-
-	@Override
-	protected void update(float percent) {
-		if (userVariable == null) {
-			return;
-		}
-		Object value = changeVariable == null ? Double.valueOf(0d)
-				: changeVariable.interpretObject(scope);
-
-		if (changeVariable != null && changeVariable.getRoot().isBoolean(scope)) {
-			value = (Double) value != 0;
-		}
-
-		boolean isFirstLevelStringTree = false;
-		if (changeVariable != null && changeVariable.getRoot().getElementType() == FormulaElement.ElementType.STRING) {
-			isFirstLevelStringTree = true;
-		}
-
-		try {
-			if (!isFirstLevelStringTree && value instanceof String && userVariable.hashCode() != TEXT_FROM_CAMERA_SENSOR_HASHCODE
-					&& convertArgumentToDouble(value) != null) {
-				value = convertArgumentToDouble(value);
-			}
-		} catch (NumberFormatException numberFormatException) {
-			Log.d(getClass().getSimpleName(), "Couldn't parse String", numberFormatException);
-		}
-		userVariable.setValue(value);
-
-		UserVariable multiplayerVariable = ProjectManager.getInstance().getCurrentProject().getMultiplayerVariable(userVariable.getName());
-		if (multiplayerVariable != null) {
-			MultiplayerInterface multiplayerDevice = getMultiplayerDevice();
-			if (multiplayerDevice != null) {
-				multiplayerDevice.sendChangedMultiplayerVariables(userVariable);
-			}
-		}
-	}
-
-	public MultiplayerInterface getMultiplayerDevice() {
-		return ServiceProvider.getService(BLUETOOTH_DEVICE_SERVICE).getDevice(MULTIPLAYER);
-	}
-
-	public void setUserVariable(UserVariable userVariable) {
-		if (userVariable == null) {
-			return;
-		}
-		this.userVariable = userVariable;
-	}
-
-	public void setChangeVariable(Formula changeVariable) {
-		this.changeVariable = changeVariable;
-	}
-
-	public void setScope(Scope scope) {
-		this.scope = scope;
-	}
+    fun setScope(scope: Scope?) {
+        this.scope = scope
+    }
 }
