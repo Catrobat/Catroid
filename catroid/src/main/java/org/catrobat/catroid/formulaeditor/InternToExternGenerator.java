0d6ab0aeea63c67f416dd2fbd3e9ad6b88a6bf38
JayTropper <48527842+JayTropper@users.noreply.github.com>
2023-07-18 10:35:00 +0200

    CATROID-1312 Added "stage width" and "stage height" reporter constants (#4428)
    * CATROID-1312 Added "stage width" and "stage height" reporter sensor constants

--- catroid/src/main/java/org/catrobat/catroid/formulaeditor/InternToExternGenerator.java
+++ catroid/src/main/java/org/catrobat/catroid/formulaeditor/InternToExternGenerator.java
@@ -259,6 +259,10 @@ public class InternToExternGenerator {
 		INTERN_EXTERN_LANGUAGE_CONVERTER_MAP.put(Sensors.NFC_TAG_ID.name(), R.string.formula_editor_nfc_tag_id);
 		INTERN_EXTERN_LANGUAGE_CONVERTER_MAP.put(Sensors.NFC_TAG_MESSAGE.name(), R.string.formula_editor_nfc_tag_message);
 		INTERN_EXTERN_LANGUAGE_CONVERTER_MAP.put(Sensors.LOUDNESS.name(), R.string.formula_editor_sensor_loudness);
+		INTERN_EXTERN_LANGUAGE_CONVERTER_MAP.put(Sensors.STAGE_WIDTH.name(),
+				R.string.formula_editor_sensor_stage_width);
+		INTERN_EXTERN_LANGUAGE_CONVERTER_MAP.put(Sensors.STAGE_HEIGHT.name(),
+				R.string.formula_editor_sensor_stage_height);
 		INTERN_EXTERN_LANGUAGE_CONVERTER_MAP.put(Sensors.TIMER.name(), R.string.formula_editor_sensor_timer);
 		INTERN_EXTERN_LANGUAGE_CONVERTER_MAP.put(Sensors.DATE_YEAR.name(), R.string.formula_editor_sensor_date_year);
 		INTERN_EXTERN_LANGUAGE_CONVERTER_MAP.put(Sensors.DATE_MONTH.name(), R.string.formula_editor_sensor_date_month);
