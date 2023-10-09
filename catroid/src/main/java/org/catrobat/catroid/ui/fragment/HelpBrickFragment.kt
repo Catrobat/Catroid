56623b404eae5c45a9a9753b1c5a987b47da87cc
ma-zea <ma.zeba17@gmail.com>
2023-10-09 10:59:20 +0200

    IDE-56 Add possibility to access wiki page of a brick

--- catroid/src/main/java/org/catrobat/catroid/ui/fragment/HelpBrickFragment.kt
+++ catroid/src/main/java/org/catrobat/catroid/ui/fragment/HelpBrickFragment.kt
@@ -20,43 +20,48 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.catrobat.catroid.ui.fragment
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.preference.PreferenceManager
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
+import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.ListFragment
 import org.catrobat.catroid.ProjectManager
 import org.catrobat.catroid.R
-import org.catrobat.catroid.cast.CastManager
 import org.catrobat.catroid.content.bricks.Brick
 import org.catrobat.catroid.ui.SpriteActivity
 import org.catrobat.catroid.ui.adapter.PrototypeBrickAdapter
 import org.catrobat.catroid.ui.controller.ActionBarStateManager
 import org.catrobat.catroid.ui.settingsfragments.AccessibilityProfile
-import org.catrobat.catroid.utils.SnackbarUtil
-import org.catrobat.catroid.utils.ToastUtil
 import org.koin.java.KoinJavaComponent.inject
 
-class AddBrickFragment(val title: String) : ListFragment() {
-    private var addBrickListener: OnAddBrickListener? = null
+class HelpBrickFragment(val title: String) : ListFragment() {
     private var adapter: PrototypeBrickAdapter? = null
-    private fun onlyBeginnerBricks(): Boolean = PreferenceManager.getDefaultSharedPreferences(activity).getBoolean(AccessibilityProfile.BEGINNER_BRICKS, false)
+    private fun onlyBeginnerBricks(): Boolean =
+        PreferenceManager.getDefaultSharedPreferences(activity).getBoolean(
+            AccessibilityProfile.BEGINNER_BRICKS, false
+        )
+
     private val projectManager: ProjectManager by inject(ProjectManager::class.java)
     private val actionBarStateManager: ActionBarStateManager = ActionBarStateManager(title)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_brick_add, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_brick_help, container, false)
         setupSelectedBrickCategory()
         return view
     }
@@ -70,11 +75,14 @@ class AddBrickFragment(val title: String) : ListFragment() {
             onlyBeginnerBricks() -> CategoryBeginnerBricksFactory()
             else -> CategoryBricksFactory()
         }
-        val brickList = selectedCategory?.let { context?.let { it1 ->
-            categoryBricksFactory.getBricks(it, backgroundSprite == sprite,
-                                            it1
-            )
-        } }
+        val brickList = selectedCategory?.let {
+            context?.let { it1 ->
+                categoryBricksFactory.getBricks(
+                    it, backgroundSprite == sprite,
+                    it1
+                )
+            }
+        }
         adapter = brickList?.let { PrototypeBrickAdapter(it) }
         listAdapter = adapter
     }
@@ -86,24 +94,25 @@ class AddBrickFragment(val title: String) : ListFragment() {
 
     override fun onPrepareOptionsMenu(menu: Menu) {
         super.onPrepareOptionsMenu(menu)
-        menu.findItem(R.id.help).isVisible = true
+        menu.findItem(R.id.help).isVisible = false
+        menu.findItem(R.id.search).isVisible = false
     }
 
     override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, menuInflater)
         menu.findItem(R.id.comment_in_out).isVisible = false
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        actionBarStateManager.restoreOldState(activity as AppCompatActivity)
+        menu.findItem(R.id.search).isVisible = false
     }
 
     override fun onResume() {
         super.onResume()
         setupSelectedBrickCategory()
-        SnackbarUtil.showHintSnackbar(activity, R.string.hint_bricks)
         actionBarStateManager.setUpActionAndBottomBar(activity as AppCompatActivity)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        actionBarStateManager.restoreOldState(activity as AppCompatActivity)
     }
 
     override fun onStart() {
@@ -112,67 +121,40 @@ class AddBrickFragment(val title: String) : ListFragment() {
             listView.setSelection(listIndexToFocus)
             listIndexToFocus = -1
         }
-        listView.onItemClickListener = AdapterView.OnItemClickListener { parent: AdapterView<*>?, view: View?, position: Int, id: Long -> adapter?.getItem(position)?.let { addBrickToScript(
-            it,
-            activity as SpriteActivity,
-            addBrickListener,
-            parentFragmentManager,
-            ADD_BRICK_FRAGMENT_TAG) }
-        }
-    }
-
-    interface OnAddBrickListener {
-        fun addBrick(brick: Brick?)
+        listView.onItemClickListener =
+            AdapterView.OnItemClickListener { parent: AdapterView<*>?, view: View?, position: Int, id: Long ->
+                adapter?.getItem(position)?.let {
+                    helpBrick(
+                        it,
+                        activity as SpriteActivity
+                    )
+                }
+            }
     }
 
     companion object {
         @JvmField
-        val ADD_BRICK_FRAGMENT_TAG = AddBrickFragment::class.java.simpleName
+        val HELP_BRICK_FRAGMENT_TAG = HelpBrickFragment::class.java.simpleName
         private const val BUNDLE_ARGUMENTS_SELECTED_CATEGORY = "selected_category"
         private var listIndexToFocus = -1
+
         @JvmStatic
-        fun newInstance(selectedCategory: String?, addBrickListener: OnAddBrickListener?): AddBrickFragment {
-            val fragment = AddBrickFragment(selectedCategory.toString())
+        fun newInstance(selectedCategory: String?, title: String): HelpBrickFragment {
+            val fragment = HelpBrickFragment(title)
             val arguments = Bundle()
             arguments.putString(BUNDLE_ARGUMENTS_SELECTED_CATEGORY, selectedCategory)
             fragment.arguments = arguments
-            fragment.addBrickListener = addBrickListener
             return fragment
         }
     }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.search) {
-            (addBrickListener as BrickCategoryFragment.OnCategorySelectedListener).onCategorySelected(arguments?.getString(BUNDLE_ARGUMENTS_SELECTED_CATEGORY))
-        } else if (item.itemId == R.id.help) {
-            (addBrickListener as BrickCategoryFragment.OnCategorySelectedListener).onCategorySelected(arguments?.getString(BUNDLE_ARGUMENTS_SELECTED_CATEGORY) + getString(R.string.help))
-        }
-        return super.onOptionsItemSelected(item)
-    }
 }
-fun addBrickToScript(brick: Brick, activity: SpriteActivity, addBrickListener: AddBrickFragment.OnAddBrickListener?, parentFragmentManager: FragmentManager, tag: String) {
-    if (ProjectManager.getInstance().currentProject.isCastProject && CastManager.unsupportedBricks.contains(brick.javaClass)) {
-        ToastUtil.showError(activity, R.string.error_unsupported_bricks_chromecast)
-        return
-    }
-    try {
-        val brickToAdd = brick.clone()
-        addBrickListener?.addBrick(brickToAdd)
-        SnackbarUtil.showHintSnackbar(activity, R.string.hint_scripts)
-        val fragmentTransaction = parentFragmentManager.beginTransaction()
-        val categoryFragment = parentFragmentManager.findFragmentByTag(BrickCategoryFragment.BRICK_CATEGORY_FRAGMENT_TAG)
-        if (categoryFragment != null) {
-            fragmentTransaction.remove(categoryFragment)
-            parentFragmentManager.popBackStack()
-        }
-        val fragment = parentFragmentManager.findFragmentByTag(tag)
-        if (fragment != null) {
-            fragmentTransaction.remove(fragment)
-            parentFragmentManager.popBackStack()
-        }
-        fragmentTransaction.commit()
-    } catch (e: CloneNotSupportedException) {
-        Log.e(tag, e.localizedMessage)
-        ToastUtil.showError(activity, R.string.error_adding_brick)
-    }
+
+fun helpBrick(brick: Brick, activity: SpriteActivity) {
+    startActivity(
+        activity,
+        Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse(brick.getHelpUrl(""))
+        ), null
+    )
 }
