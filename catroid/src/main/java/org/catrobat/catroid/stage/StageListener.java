4b523dbb2e0dd3513a33a08455cb8809d05952bb
Lukas Jedinger <lukas.jedinger@student.tugraz.at>
2023-10-12 00:01:29 +0200

    Fix some UnitTests

--- catroid/src/main/java/org/catrobat/catroid/stage/StageListener.java
+++ catroid/src/main/java/org/catrobat/catroid/stage/StageListener.java
@@ -820,7 +820,8 @@ public class StageListener implements ApplicationListener {
 		return sprites;
 	}
 
-	private class StageBackup {
+	@VisibleForTesting
+	public static class StageBackup {
 
 		List<Sprite> sprites;
 		Array<Actor> actors;
