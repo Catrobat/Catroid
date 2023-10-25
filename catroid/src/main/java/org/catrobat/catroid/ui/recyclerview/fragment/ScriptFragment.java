2bc35143aa0f9d9a15f5ea673845b546b2ac18e5
Jakob <jakob.raidl@student.tugraz.at>
2023-10-25 10:04:44 +0200

    IDE-60 Add fix for AddBricksWindow and FormulaEditorWindow bug

--- catroid/src/main/java/org/catrobat/catroid/ui/recyclerview/fragment/ScriptFragment.java
+++ catroid/src/main/java/org/catrobat/catroid/ui/recyclerview/fragment/ScriptFragment.java
@@ -516,7 +516,7 @@ public class ScriptFragment extends ListFragment implements
 		BrickCategoryFragment brickCategoryFragment = new BrickCategoryFragment();
 		brickCategoryFragment.setOnCategorySelectedListener(this);
 
-		getFragmentManager().beginTransaction()
+		activity.getSupportFragmentManager().beginTransaction()
 				.add(R.id.fragment_container, brickCategoryFragment, BrickCategoryFragment.BRICK_CATEGORY_FRAGMENT_TAG)
 				.addToBackStack(BrickCategoryFragment.BRICK_CATEGORY_FRAGMENT_TAG)
 				.commit();
@@ -538,7 +538,7 @@ public class ScriptFragment extends ListFragment implements
 			tag = AddBrickFragment.ADD_BRICK_FRAGMENT_TAG;
 		}
 
-		getFragmentManager().beginTransaction()
+		activity.getSupportFragmentManager().beginTransaction()
 				.add(R.id.fragment_container, fragment, tag)
 				.addToBackStack(null)
 				.commit();
