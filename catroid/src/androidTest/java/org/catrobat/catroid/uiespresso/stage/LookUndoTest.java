ec83ef2507ecbafbd8c7040d5f7cb6f76c041752
Simon Schuster <simon.schuster20@gmail.com>
2023-11-29 17:57:39 +0100

    Fix error where directory does not exist in test
    - some tests reference a directory which does not exists. This is now fixed by using a temporary directory.

--- catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/stage/LookUndoTest.java
+++ catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/stage/LookUndoTest.java
@@ -27,7 +27,6 @@ import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Environment;
 
 import org.catrobat.catroid.R;
 import org.catrobat.catroid.common.Constants;
@@ -46,6 +45,7 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.IOException;
@@ -82,8 +82,9 @@ public class LookUndoTest {
 	private final String projectName = getClass().getSimpleName();
 	private final String spriteName = "testSprite";
 	private File imageFile;
-	private final File tmpDir = new File(
-			Environment.getExternalStorageDirectory().getAbsolutePath(), "Pocket Code Test Temp");
+
+	@Rule
+	public TemporaryFolder tmpFolder = new TemporaryFolder();
 
 	@Rule
 	public FragmentActivityTestRule<SpriteActivity> baseActivityTestRule = new
@@ -107,14 +108,10 @@ public class LookUndoTest {
 				hasExtras(bundleHasMatchingString("android.intent.extra.TITLE", chooserTitle)),
 				hasExtras(bundleHasExtraIntent(expectedGetContentIntent)));
 
-		if (!tmpDir.exists()) {
-			tmpDir.mkdirs();
-		}
-
 		imageFile = ResourceImporter.createImageFileFromResourcesInDirectory(
 				InstrumentationRegistry.getInstrumentation().getContext().getResources(),
 				org.catrobat.catroid.test.R.drawable.catroid_banzai,
-				tmpDir,
+				tmpFolder.getRoot(),
 				lookFileName,
 				1);
 
@@ -188,11 +185,5 @@ public class LookUndoTest {
 		Intents.release();
 		baseActivityTestRule.finishActivity();
 		TestUtils.deleteProjects(projectName);
-		if (imageFile != null && imageFile.exists()) {
-			imageFile.delete();
-		}
-		if (tmpDir.exists()) {
-			tmpDir.delete();
-		}
 	}
 }
