5191cc0992ed1bfbae3a016021401e3edcd83fa4
JAKOB RAIDL <jakob.raidl@student.tugraz.at>
2024-02-04 16:22:17 +0100

    IDE-24 rewrite SpriteListUndoTest

--- catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/ui/fragment/SpriteListUndoTest.kt
+++ catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/ui/fragment/SpriteListUndoTest.kt
@@ -23,10 +23,8 @@
 
 package org.catrobat.catroid.uiespresso.ui.fragment
 
-import android.content.Context
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.clearText
 import androidx.test.espresso.action.ViewActions.click
@@ -34,174 +32,195 @@ import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
 import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.catrobat.catroid.ProjectManager
 import org.catrobat.catroid.R
-import org.catrobat.catroid.common.Constants
-import org.catrobat.catroid.common.LookData
-import org.catrobat.catroid.common.SoundInfo
+import org.catrobat.catroid.content.GroupSprite
 import org.catrobat.catroid.content.Project
 import org.catrobat.catroid.content.Sprite
-import org.catrobat.catroid.io.ResourceImporter.createImageFileFromResourcesInDirectory
-import org.catrobat.catroid.io.ResourceImporter.createSoundFileFromResourcesInDirectory
-import org.catrobat.catroid.io.XstreamSerializer
-import org.catrobat.catroid.ui.ProjectListActivity
+import org.catrobat.catroid.test.utils.TestUtils
+import org.catrobat.catroid.testsuites.annotations.Cat
+import org.catrobat.catroid.testsuites.annotations.Level
+import org.catrobat.catroid.ui.ProjectActivity
+import org.catrobat.catroid.ui.settingsfragments.SettingsFragment
 import org.catrobat.catroid.uiespresso.ui.fragment.rvutils.RecyclerViewInteractionWrapper.onRecyclerView
-import org.catrobat.catroid.uiespresso.ui.fragment.rvutils.RecyclerViewItemCountAssertion.Companion.withItemCount
-import org.catrobat.catroid.uiespresso.util.UiTestUtils
-import org.catrobat.catroid.uiespresso.util.rules.BaseActivityTestRule
-import org.junit.Assert
+import org.catrobat.catroid.uiespresso.util.UiTestUtils.Companion.openActionBarMenu
+import org.catrobat.catroid.uiespresso.util.UiTestUtils.Companion.openSpriteActionMenu
+import org.catrobat.catroid.uiespresso.util.rules.FragmentActivityTestRule
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.experimental.categories.Category
 import org.junit.runner.RunWith
 import org.koin.java.KoinJavaComponent.inject
-import java.io.File
 
+@Category(Cat.AppUi::class, Level.Smoke::class)
 @RunWith(AndroidJUnit4::class)
 class SpriteListUndoTest {
-
     @get:Rule
-    var baseActivityTestRule = BaseActivityTestRule(
-        ProjectListActivity::class.java, true, false
+    var baseActivityTestRule = FragmentActivityTestRule(
+        ProjectActivity::class.java, ProjectActivity.EXTRA_FRAGMENT_POSITION,
+        ProjectActivity.FRAGMENT_SPRITES
     )
 
-    private val projectManager: ProjectManager by inject(ProjectManager::class.java)
+    private val firstSpriteName = "firstSprite"
+    private val secondSpriteName = "secondSprite"
+    private val groupSpriteName = "groupSprite"
 
-    private val amountOfSprites = 3
-
-    private val looks = listOf(R.drawable.default_project_clouds_portrait,
-        R.drawable.default_project_bird_wing_up,
-        R.drawable.default_project_background_portrait)
-
-    private val sounds = listOf(R.raw.default_project_tweet_1,
-        R.raw.default_project_tweet_2)
-
-    private lateinit var imageDirectory: File
-    private lateinit var soundDirectory: File
+    private val projectManager by inject(ProjectManager::class.java)
 
     @Before
     fun setUp() {
-        createProject()
-        baseActivityTestRule.launchActivity(null)
+        SettingsFragment.setLanguageSharedPreference(getApplicationContext(), "en")
+        createProject(SpriteListUndoTest::class.java.simpleName)
+        baseActivityTestRule.launchActivity()
+    }
 
-        onView(withText("testProject"))
+    @After
+    fun tearDown() {
+        SettingsFragment.setLanguageSharedPreference(getApplicationContext(), "en")
+        baseActivityTestRule.finishActivity()
+        TestUtils.deleteProjects(RenameSpriteTest::class.java.simpleName)
+    }
+
+    @Test
+    fun undoOfConsecutiveActions() {
+        onView(withText(firstSpriteName)).check(matches(isDisplayed()))
+
+        openSpriteActionMenu(firstSpriteName, isGroupSprite = false)
+
+        onView(withText(R.string.delete))
             .perform(click())
+
+        onView(withText(firstSpriteName)).check(doesNotExist())
+
+        onView(withText(secondSpriteName)).check(matches(isDisplayed()))
+
+        openSpriteActionMenu(secondSpriteName, isGroupSprite = false)
+
+        onView(withText(R.string.delete))
+            .perform(click())
+
+        onView(withText(secondSpriteName)).check(doesNotExist())
+
+        onView(withId(R.id.menu_undo)).check(matches(isCompletelyDisplayed()))
+
+        onView(withId(R.id.menu_undo)).perform(click())
+
+        onView(withText(firstSpriteName)).check(doesNotExist())
+
+        onView(withText(secondSpriteName)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun noUndoOptionAfterOpenScript() {
+        onView(withText(firstSpriteName)).check(matches(isDisplayed()))
+
+        openSpriteActionMenu(firstSpriteName, isGroupSprite = false)
+
+        onView(withText(R.string.delete))
+            .perform(click())
+
+        onView(withText(firstSpriteName)).check(doesNotExist())
+
+        onView(withId(R.id.menu_undo)).check(matches(isCompletelyDisplayed()))
+
+        onView(withText(secondSpriteName)).perform(click())
+
+        pressBack()
+
+        onView(withText(firstSpriteName)).check(doesNotExist())
+
+        onView(withId(R.id.menu_undo)).check(doesNotExist())
     }
 
     @Test
     fun deleteAndUndoSingleSpriteTest() {
-        UiTestUtils.openSpriteActionMenu("testSprite_0", false)
+        onView(withText(firstSpriteName)).check(matches(isDisplayed()))
+
+        openSpriteActionMenu(firstSpriteName, isGroupSprite = false)
+
         onView(withText(R.string.delete))
             .perform(click())
+
+        onRecyclerView().checkHasNumberOfItems(3)
 
         onView(withId(R.id.menu_undo))
             .perform(click())
 
-        onView(withText("testSprite_0"))
-            .check(matches(isDisplayed()))
-
-        val imageFile = imageDirectory.listFiles()?.firstOrNull { file -> file.name == "image_0" }
-        Assert.assertNotNull(imageFile)
-
-        val soundFile = soundDirectory.listFiles()?.firstOrNull { file -> file.name == "sound_0" }
-        Assert.assertNotNull(soundFile)
+        onView(withText(firstSpriteName)).check(matches(isDisplayed()))
     }
 
     @Test
-    fun deleteSingleSpriteAndCheckIfLookAndSoundDataIsCleanedUpTest() {
-        UiTestUtils.openSpriteActionMenu("testSprite_0", false)
+    fun deleteAndUndoMultipleSpritesTest() {
+        openActionBarMenu()
         onView(withText(R.string.delete))
             .perform(click())
 
-        pressBack()
+        onRecyclerView().atPosition(1).performCheckItemClick()
+        onRecyclerView().atPosition(2).performCheckItemClick()
+/*        for (i in 1..2) {
+            onRecyclerView().atPosition(i).performCheckItemClick()
+            onRecyclerView().atPosition(i).performCheckItemCheck() }*/
 
-        onView(withText("testProject"))
+        onView(withText(R.string.confirm))
             .perform(click())
 
-        val imageFile = imageDirectory.listFiles()?.firstOrNull { file -> file.name == "image_0" }
-        Assert.assertNull(imageFile)
-
-        val soundFile = soundDirectory.listFiles()?.firstOrNull { file -> file.name == "sound_0" }
-        Assert.assertNull(soundFile)
-    }
-
-    @Test
-    fun deleteAndUndoMultipleSpritesAtOnceTest() {
-        openActionBarOverflowOrOptionsMenu(baseActivityTestRule.activity)
-        onView(withText(R.string.delete))
-            .perform(click())
-
-        onRecyclerView().atPosition(1)
-            .performCheckItemClick()
-
-        onRecyclerView().atPosition(2)
-            .performCheckItemClick()
-
-        onRecyclerView().atPosition(3)
-            .performCheckItemClick()
-
-        onView(withId(R.id.confirm))
-            .perform(click())
-
-        onView(withId(R.id.recycler_view)).check(withItemCount(1))
+        onRecyclerView().checkHasNumberOfItems(2)
 
         onView(withId(R.id.menu_undo))
             .perform(click())
 
-        onView(withId(R.id.recycler_view)).check(withItemCount(4))
+        onRecyclerView().checkHasNumberOfItems(4)
 
-        // need to assert on 4 elements because there is a ".nomedia" file in the directory
-        Assert.assertEquals(4, imageDirectory.listFiles()?.size)
-        Assert.assertEquals(4, soundDirectory.listFiles()?.size)
+        onView(withText(firstSpriteName)).check(matches(isDisplayed()))
     }
 
+
+
     @Test
-    fun deleteMultipleSpritesAndCheckIfLookAndSoundDataIsCleanedUpTest() {
-        openActionBarOverflowOrOptionsMenu(baseActivityTestRule.activity)
-        onView(withText(R.string.delete))
+    fun copyAndUndoSingleSpriteTest() {
+        onView(withText(firstSpriteName)).check(matches(isDisplayed()))
+
+        onRecyclerView().checkHasNumberOfItems(4)
+
+        openSpriteActionMenu(firstSpriteName, isGroupSprite = false)
+
+        onView(withText(R.string.copy))
             .perform(click())
 
-        onRecyclerView().atPosition(1)
-            .performCheckItemClick()
+        onRecyclerView().checkHasNumberOfItems(5)
 
-        onRecyclerView().atPosition(2)
-            .performCheckItemClick()
-
-        onView(withId(R.id.confirm))
+        onView(withId(R.id.menu_undo))
             .perform(click())
 
-        pressBack()
+        onRecyclerView().checkHasNumberOfItems(4)
 
-        onView(withText("testProject"))
-            .perform(click())
-
-        val imageFile0 = imageDirectory.listFiles()?.firstOrNull { file -> file.name == "image_0" }
-        Assert.assertNull(imageFile0)
-        val imageFile1 = imageDirectory.listFiles()?.firstOrNull { file -> file.name == "image_1" }
-        Assert.assertNull(imageFile1)
-        val imageFile2 = imageDirectory.listFiles()?.firstOrNull { file -> file.name == "image_2" }
-        Assert.assertNotNull(imageFile2)
-
-        val soundFile0 = soundDirectory.listFiles()?.firstOrNull { file -> file.name == "sound_0" }
-        Assert.assertNull(soundFile0)
-        val soundFile1 = soundDirectory.listFiles()?.firstOrNull { file -> file.name == "sound_1" }
-        Assert.assertNull(soundFile1)
-        val soundFile2 = soundDirectory.listFiles()?.firstOrNull { file -> file.name == "sound_2" }
-        Assert.assertNotNull(soundFile2)
+        onView(withText(firstSpriteName)).check(matches(isDisplayed()))
     }
 
     @Test
     fun renameAndUndoSingleSpriteTest() {
-        UiTestUtils.openSpriteActionMenu("testSprite_0", false)
+        onView(withText(firstSpriteName)).check(matches(isDisplayed()))
+
+        openSpriteActionMenu(firstSpriteName, isGroupSprite = false)
         onView(withText(R.string.rename))
             .perform(click())
 
+        onView(withText(R.string.rename_sprite_dialog))
+            .inRoot(RootMatchers.isDialog())
+            .check(matches(isDisplayed()))
+
         onView(withId(R.id.input_edit_text))
-            .perform(clearText(), replaceText("renamedSprite"), closeSoftKeyboard())
+            .perform(clearText())
+            .perform(replaceText("renamedSprite"))
+            .perform(closeSoftKeyboard())
 
         onView(withText(R.string.ok))
             .perform(click())
@@ -212,67 +231,23 @@ class SpriteListUndoTest {
         onView(withId(R.id.menu_undo))
             .perform(click())
 
-        onView(withText("testSprite_0"))
-            .check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun copyAndUndoSingleSpriteTest() {
-        UiTestUtils.openSpriteActionMenu("testSprite_0", false)
-        onView(withText(R.string.rename))
-            .perform(click())
-
-        onView(withId(R.id.input_edit_text))
-            .perform(clearText(), replaceText("renamedSprite"), closeSoftKeyboard())
-
-        onView(withText(R.string.ok))
-            .perform(click())
-
-        onView(withText("renamedSprite"))
-            .check(matches(isDisplayed()))
-
-        onView(withId(R.id.menu_undo))
-            .perform(click())
-
-        onView(withText("testSprite_0"))
-            .check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun copyAndUndoMultipleSpritesAtOnceTest() {
-        openActionBarOverflowOrOptionsMenu(baseActivityTestRule.activity)
-        onView(withText(R.string.copy))
-            .perform(click())
-
-        onRecyclerView().atPosition(1)
-            .performCheckItemClick()
-
-        onRecyclerView().atPosition(2)
-            .performCheckItemClick()
-
-        onRecyclerView().atPosition(3)
-            .performCheckItemClick()
-
-        onView(withId(R.id.confirm))
-            .perform(click())
-
-        onView(withId(R.id.recycler_view)).check(withItemCount(7))
-
-        onView(withId(R.id.menu_undo))
-            .perform(click())
-
-        onView(withId(R.id.recycler_view)).check(withItemCount(4))
+        onView(withText(firstSpriteName)).check(matches(isDisplayed()))
     }
 
     @Test
     fun createGroupAndUndoTest() {
-        openActionBarOverflowOrOptionsMenu(baseActivityTestRule.activity)
+        openActionBarMenu()
 
         onView(withText(R.string.new_group))
             .perform(click())
 
+        onView(withText(R.string.new_group)).inRoot(RootMatchers.isDialog())
+            .check(matches(isDisplayed()))
+
         onView(withId(R.id.input_edit_text))
-            .perform(clearText(), replaceText("newGroup"), closeSoftKeyboard())
+            .perform(clearText())
+            .perform(replaceText("newGroup"))
+            .perform(closeSoftKeyboard())
 
         onView(withText(R.string.ok))
             .perform(click())
@@ -287,44 +262,14 @@ class SpriteListUndoTest {
             .check(doesNotExist())
     }
 
-    private fun createProject(): Project {
-        val project = Project(getApplicationContext(), "testProject")
-        XstreamSerializer.getInstance().saveProject(project)
-
-        val scene = project.defaultScene
-        imageDirectory = File(scene.directory, Constants.IMAGE_DIRECTORY_NAME)
-        soundDirectory = File(scene.directory, Constants.SOUND_DIRECTORY_NAME)
-
+    private fun createProject(projectName: String) {
+        val project = Project(getApplicationContext(), projectName)
+        project.defaultScene.addSprite(Sprite(firstSpriteName))
+        project.defaultScene.addSprite(Sprite(secondSpriteName))
+        project.defaultScene.addSprite(GroupSprite(groupSpriteName))
         projectManager.currentProject = project
-
-        for (i in 0 until amountOfSprites) {
-            projectManager.currentProject.defaultScene.addSprite(createSpriteWithLookAndSound(i))
-        }
-
-        XstreamSerializer.getInstance().saveProject(project)
-        return project
-    }
-
-    private fun createSpriteWithLookAndSound(number: Int): Sprite {
-        val sprite = Sprite("testSprite_$number")
-
-        val lookFile = createImageFileFromResourcesInDirectory(
-            (getApplicationContext() as Context).resources,
-            looks[number % looks.size],
-            imageDirectory,
-            "image_$number",
-            1.0
-        )
-
-        val soundFile = createSoundFileFromResourcesInDirectory(
-            (getApplicationContext() as Context).resources,
-            sounds[number % sounds.size],
-            soundDirectory,
-            "sound_$number"
-        )
-
-        sprite.lookList.add(LookData("Look", lookFile))
-        sprite.soundList.add(SoundInfo("Sound", soundFile))
-        return sprite
+        projectManager.currentlyEditedScene = project.defaultScene
     }
 }
+
+
