f66e30fc8f4efa3ce6a7ace8e8ec7d960451bcb0
Hannes Weilharter <hannes.weilharter@student.tugraz.at>
2026-02-27 16:42:13 +0100

    CATROID-1635 migrate ShowBubbleActorTest

--- catroid/src/test/java/org/catrobat/catroid/test/stage/ShowBubbleActorTest.java
+++ catroid/src/test/java/org/catrobat/catroid/test/stage/ShowBubbleActorTest.java
@@ -20,9 +20,10 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-/*
+
 package org.catrobat.catroid.test.stage;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 
 import com.badlogic.gdx.graphics.Pixmap;
@@ -32,22 +33,27 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import org.catrobat.catroid.common.Constants;
 import org.catrobat.catroid.content.Sprite;
 import org.catrobat.catroid.stage.ShowBubbleActor;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockedConstruction;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static junit.framework.Assert.assertEquals;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({ShowBubbleActor.class, Image.class, Pixmap.class, Bitmap.class, Texture.class})
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ShowBubbleActorTest {
 	public static final float EXPECTED_X_LEFT_FOR_MOVE_X = 5.0f;
 	public static final float EXPECTED_X_RIGHT_FOR_MOVE_X = 847.0f;
-	private Image image;
 	private Sprite sprite;
 	private static final String OBJECT_NAME = "Test";
 	private static final int CALC_X_LEFT = -347;
@@ -66,34 +72,36 @@ public class ShowBubbleActorTest {
 	private static final Float SPRITE_X_FOR_MOVE_X = -40.0f;
 	private static final Float SPRITE_Y_FOR_MOVE_Y = -1397.0f;
 
+	private MockedStatic<Bitmap> bitmapMock;
+	private MockedConstruction<Image> imageMock;
+	private MockedConstruction<Pixmap> pixmapMock;
+	private MockedConstruction<Texture> textureMock;
+
+	@SuppressLint("CheckResult")
 	@Before
 	public void setUp() throws Exception {
-		image = PowerMockito.mock(Image.class);
-		Pixmap pixmap = PowerMockito.mock(Pixmap.class);
-		Bitmap bitmap = PowerMockito.mock(Bitmap.class);
-		Texture texture = PowerMockito.mock(Texture.class);
-
 		sprite = new Sprite(OBJECT_NAME);
 		sprite.look.setX(SPRITE_X_DEFAULT);
 		sprite.look.setY(SPRITE_Y_DEFAULT);
 		sprite.look.setHeight(SPRITE_HEIGHT_DEFAULT);
 		sprite.look.setWidth(SPRITE_WIDTH_DEFAULT);
 
-		Mockito.when(image.getWidth()).thenReturn(IMAGE_WIDTH);
+		bitmapMock = mockStatic(Bitmap.class);
+		bitmapMock.when(() -> Bitmap.createBitmap(anyInt(), anyInt(),
+				Mockito.any(Bitmap.Config.class))).thenReturn(mock(Bitmap.class));
 
-		PowerMockito.mockStatic(Bitmap.class);
+		imageMock = mockConstruction(Image.class,
+				(mock, context) -> when(mock.getWidth()).thenReturn(IMAGE_WIDTH));
+		pixmapMock = mockConstruction(Pixmap.class);
+		textureMock = mockConstruction(Texture.class);
+	}
 
-		Mockito.when(Bitmap.createBitmap(Mockito.any(Integer.class), Mockito.any(Integer.class),
-				Mockito.any(Bitmap.Config.class))).thenReturn(bitmap);
-
-		PowerMockito.whenNew(Image.class).withParameterTypes(Texture.class)
-				.withArguments(Mockito.any()).thenReturn(image);
-
-		PowerMockito.whenNew(Pixmap.class).withArguments(Mockito.anyByte(), Mockito.anyInt(),
-				Mockito.anyInt()).thenReturn(pixmap);
-
-		PowerMockito.whenNew(Texture.class).withParameterTypes(Pixmap.class)
-				.withArguments(Mockito.any()).thenReturn(texture);
+	@After
+	public void tearDown() {
+		bitmapMock.close();
+		imageMock.close();
+		pixmapMock.close();
+		textureMock.close();
 	}
 
 	@Test
@@ -152,4 +160,3 @@ public class ShowBubbleActorTest {
 		assertEquals(EXPECTED_Y_MOVE_Y, bubbleActor.calculateImageY(CALC_Y_RIGHT));
 	}
 }
-*/
