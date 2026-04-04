445ebf149f121fdfcd324a75aef2f3aff05e6dff
harshsomankar123-tech <harshsomankar123@gmail.com>
2026-04-05 00:18:25 +0530

    refactor: convert StageLifeCycleControllerTest to Kotlin

---
+++ catroid/src/test/java/org/catrobat/catroid/stage/StageLifeCycleControllerTest.kt
@@ -0,0 +1,98 @@
+/*
+ * Catroid: An on-device visual programming system for Android devices
+ * Copyright (C) 2010-2026 The Catrobat Team
+ * (<http://developer.catrobat.org/credits>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * An additional term exception under section 7 of the GNU Affero
+ * General Public License, version 3, is available at
+ * http://developer.catrobat.org/license_additional_term
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.catrobat.catroid.stage
+
+import org.catrobat.catroid.ProjectManager
+import org.catrobat.catroid.bluetooth.base.BluetoothDeviceService
+import org.catrobat.catroid.common.CatroidService
+import org.catrobat.catroid.common.ServiceProvider
+import org.catrobat.catroid.content.Project
+import org.catrobat.catroid.ui.runtimepermissions.RequiresPermissionTask
+import org.junit.After
+import org.junit.Assert.assertNull
+import org.junit.Test
+import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.mockStatic
+import org.mockito.Mockito.verify
+
+class StageLifeCycleControllerTest {
+
+	@After
+	fun tearDown() {
+		StageActivity.stageListener = null
+	}
+
+	@Test
+	fun testStageDestroyDoesNotCrashWhenStageListenerIsNull() {
+		val mockStageActivity = mock(StageActivity::class.java)
+		val mockProjectManager = mock(ProjectManager::class.java)
+		val mockProject = mock(Project::class.java)
+
+		mockStatic(ProjectManager::class.java).use { pmMock ->
+			mockStatic(RequiresPermissionTask::class.java).use { rpMock ->
+				mockStatic(StageResourceHolder::class.java).use { srhMock ->
+					mockStatic(ServiceProvider::class.java).use { spMock ->
+						val mockBluetoothDeviceService =
+							mock(BluetoothDeviceService::class.java)
+						spMock.`when`<Any> {
+							ServiceProvider.getService(
+								CatroidService.BLUETOOTH_DEVICE_SERVICE
+							)
+						}.thenReturn(mockBluetoothDeviceService)
+
+						pmMock.`when`<Any> {
+							ProjectManager.getInstance()
+						}.thenReturn(mockProjectManager)
+						org.mockito.Mockito.`when`(
+							mockProjectManager.currentProject
+						).thenReturn(mockProject)
+						org.mockito.Mockito.`when`(
+							mockProjectManager.currentlyEditedScene
+						).thenReturn(null)
+
+						srhMock.`when`<Any> {
+							StageResourceHolder
+								.getProjectsRuntimePermissionList(anyInt())
+						}.thenReturn(emptyList<String>())
+						rpMock.`when`<Any> {
+							RequiresPermissionTask.checkPermission(
+								any(), any()
+							)
+						}.thenReturn(true)
+
+						StageActivity.stageListener = null
+
+						StageLifeCycleController.stageDestroy(mockStageActivity)
+
+						verify(mockStageActivity).manageLoadAndFinish()
+						verify(mockBluetoothDeviceService).destroy()
+						assertNull(StageActivity.stageListener)
+					}
+				}
+			}
+		}
+	}
+}
