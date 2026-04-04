d38ccae7a4cdd977e617c02ff0c40dca0bb3a3f1
harshsomankar123-tech <harshsomankar123@gmail.com>
2026-04-05 00:48:29 +0530

    Fix testCreateNewBackground by adding missing waits and PocketPaint tutorial handling

--- catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/content/brick/app/WhenBackgroundChangesToBrickTest.java
+++ catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/content/brick/app/WhenBackgroundChangesToBrickTest.java
@@ -27,11 +27,13 @@ import org.catrobat.catroid.ProjectManager;
 import org.catrobat.catroid.R;
 import org.catrobat.catroid.common.LookData;
 import org.catrobat.catroid.content.WhenBackgroundChangesScript;
+import org.catrobat.catroid.test.utils.TestUtils;
 import org.catrobat.catroid.testsuites.annotations.Cat;
 import org.catrobat.catroid.testsuites.annotations.Level;
 import org.catrobat.catroid.ui.SpriteActivity;
 import org.catrobat.catroid.uiespresso.util.UiTestUtils;
 import org.catrobat.catroid.uiespresso.util.rules.FragmentActivityTestRule;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -39,21 +41,30 @@ import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import androidx.test.espresso.NoMatchingViewException;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static junit.framework.Assert.assertEquals;
 
+import static org.catrobat.catroid.WaitForConditionAction.waitFor;
 import static org.catrobat.catroid.uiespresso.content.brick.utils.BrickDataInteractionWrapper.onBrickAtPosition;
+import org.catrobat.catroid.uiespresso.util.actions.CustomActions;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import com.google.common.base.Stopwatch;
 
 @RunWith(AndroidJUnit4.class)
 public class WhenBackgroundChangesToBrickTest {
+	private static final String PROJECT_NAME = "WhenBackgroundChangesToBrickTest";
 	private int brickPosition;
 
 	@Rule
@@ -63,9 +74,17 @@ public class WhenBackgroundChangesToBrickTest {
 	@Before
 	public void setUp() throws Exception {
 		brickPosition = 0;
-		UiTestUtils.createProjectWithCustomScript("WhenBackgroundChangesToBrickTest",
+		UiTestUtils.createProjectWithCustomScript(PROJECT_NAME,
 				new WhenBackgroundChangesScript());
+		Intents.init();
 		baseActivityTestRule.launchActivity();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		Intents.release();
+		baseActivityTestRule.finishActivity();
+		TestUtils.deleteProjects(PROJECT_NAME);
 	}
 
 	@Category({Cat.AppUi.class, Level.Smoke.class})
@@ -76,19 +95,42 @@ public class WhenBackgroundChangesToBrickTest {
 		onBrickAtPosition(brickPosition).onSpinner(R.id.brick_when_background_spinner)
 			.performSelectNameable(R.string.new_option);
 
-		Intents.init();
-
 		onView(withId(R.id.dialog_new_look_paintroid))
 			.perform(click());
 
-		onView(withId(R.id.pocketpaint_drawing_surface_view)).perform(click());
+		waitOnViewAndClick(R.id.pocketpaint_btn_skip);
+
+		onView(withId(R.id.pocketpaint_drawing_surface_view))
+			.perform(waitFor(isDisplayed(), 3000))
+			.perform(click());
 		pressBack();
+
+		onView(isRoot()).perform(CustomActions.wait(500));
 
 		List<LookData> lookDataList =
 				ProjectManager.getInstance().getCurrentProject().getDefaultScene().getBackgroundSprite().getLookList();
 
 		assertEquals(1, lookDataList.size());
+	}
 
-		Intents.release();
+	private boolean waitOnViewAndClick(int viewId) {
+		return waitOnViewAndClick(viewId, 1000);
+	}
+
+	private boolean waitOnViewAndClick(int viewId, int timeout) {
+		Stopwatch stopWatch = Stopwatch.createStarted();
+		boolean viewFound;
+		do {
+			viewFound = true;
+			try {
+				onView(withId(viewId)).perform(click());
+			} catch (NoMatchingViewException e) {
+				viewFound = false;
+				if (stopWatch.elapsed(TimeUnit.MILLISECONDS) >= timeout) {
+					break;
+				}
+			}
+		} while (!viewFound);
+		return viewFound;
 	}
 }
