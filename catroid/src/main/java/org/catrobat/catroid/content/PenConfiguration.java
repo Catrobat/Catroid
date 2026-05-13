fb323c4700c72c56c4e72eb7d2a123a96a126f20
harshsomankar123-tech <harshsomankar123@gmail.com>
2026-05-13 20:59:12 +0530

    Refactor SetPenColorAction to Kotlin and add unit tests

--- catroid/src/main/java/org/catrobat/catroid/content/PenConfiguration.java
+++ catroid/src/main/java/org/catrobat/catroid/content/PenConfiguration.java
@@ -117,6 +117,10 @@ public class PenConfiguration {
 		this.penColor = penColor;
 	}
 
+	public PenColor getPenColor() {
+		return penColor;
+	}
+
 	public void setStamp(boolean stamp) {
 		this.stamp = stamp;
 	}
---
+++ catroid/src/main/java/org/catrobat/catroid/content/actions/SetPenColorAction.kt
@@ -0,0 +1,60 @@
+/*
+ * Catroid: An on-device visual programming system for Android devices
+ * Copyright (C) 2010-2026 The Catrobat Team
+ * (<http://developer.catrobat.org/credits>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * An additional term exception under section 7 of the GNU Affero
+ * General Public License, version 3, is available at
+ * http://developer.catrobat.org/license_additional_term
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.catrobat.catroid.content.actions
+
+import android.util.Log
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.scenes.scene2d.actions.TemporalAction
+import org.catrobat.catroid.content.PenColor
+import org.catrobat.catroid.content.Scope
+import org.catrobat.catroid.formulaeditor.Formula
+import org.catrobat.catroid.formulaeditor.InterpretationException
+
+class SetPenColorAction : TemporalAction() {
+    var scope: Scope? = null
+    var red: Formula? = null
+    var green: Formula? = null
+    var blue: Formula? = null
+
+    override fun update(percent: Float) {
+        try {
+            val currentScope = scope ?: return
+            val newRed = red?.interpretInteger(currentScope) ?: 0
+            val newGreen = green?.interpretInteger(currentScope) ?: 0
+            val newBlue = blue?.interpretInteger(currentScope) ?: 0
+
+            val color = Color()
+            // Using manual ARGB to Color conversion to avoid android.graphics.Color dependency in unit tests.
+            // android.graphics.Color.argb(0xFF, newRed, newGreen, newBlue) returns (0xFF shl 24) | (R shl 16) | (G shl 8) | B
+            val argb = (0xFF shl 24) or
+                    ((newRed and 0xFF) shl 16) or
+                    ((newGreen and 0xFF) shl 8) or
+                    (newBlue and 0xFF)
+
+            Color.argb8888ToColor(color, argb)
+            currentScope.sprite.penConfiguration.setPenColor(PenColor(color.r, color.g, color.b, color.a))
+        } catch (interpretationException: InterpretationException) {
+            Log.d(javaClass.simpleName, "Formula interpretation for this specific Brick failed.", interpretationException)
+        }
+    }
+}
