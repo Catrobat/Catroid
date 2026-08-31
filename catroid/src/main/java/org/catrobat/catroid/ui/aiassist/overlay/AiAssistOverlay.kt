e25a0cc539016263470917e2db8ecdf351f9f1ac
Muhammad Haris Sabil Al Karim <97924751+harissabil@users.noreply.github.com>
2026-08-31 20:48:41 +0700

    Reorganize imports in AiAssistOverlay

--- catroid/src/main/java/org/catrobat/catroid/ui/aiassist/overlay/AiAssistOverlay.kt
+++ catroid/src/main/java/org/catrobat/catroid/ui/aiassist/overlay/AiAssistOverlay.kt
@@ -31,14 +31,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import org.catrobat.aitutor.domain.prompt.PromptVersion
 import androidx.compose.ui.res.colorResource
+import org.catrobat.aitutor.domain.prompt.PromptVersion
 import org.catrobat.aitutor.ui.public.AiTutorView
-import org.catrobat.catroid.BuildConfig
-import org.catrobat.catroid.common.Constants
 import org.catrobat.aitutor.ui.theme.AiTutorColors
 import org.catrobat.aitutor.ui.theme.AiTutorTheme
+import org.catrobat.catroid.BuildConfig
 import org.catrobat.catroid.R
+import org.catrobat.catroid.common.Constants
 import org.catrobat.catroid.content.Sprite
 import org.catrobat.catroid.io.XstreamSerializer
 import org.catrobat.catroid.ui.aiassist.diff.AiTutorDiffScreen
